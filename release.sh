7f03cbd3df95bde6a0ac36ea1d96c86cd8663171
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2001-11-02 18:11:19 +0000

    automate ChangeLog generation and release file upload.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@47 fc592f45-0881-4044-91ff-c96495a54512

--- release.sh
+++ release.sh
@@ -1,16 +1,22 @@
 # !/bin/sh
 
-## Run this command to build a new release tarball. This assumes that 
-## the CVSROOT environment variable has been set appropriately, and 
-## that autogen is available on your path.
+## Run this command from the project root directory to build a new 
+## release tarball. This assumes that the CVSROOT environment variable 
+## has been set appropriately, and that autogen is available on your path.
+## You will also need cvs2cl.pl from http://www.red-bean.com/cvs2cl/
 ##
-## Next follow these instructions to perform the upload: 
-##	1. FTP to upload.sourceforge.net 
-## 	2. Login as "anonymous" 
-##	3. Use your e-mail address as the password for this login 
-##	4. Set your client to binary mode ("bin" on command-line clients) 
-##	5. Change your current directory to /incoming ("cd /incoming") 
-##	6. Upload the desired files for the release ("put filename") 
+## Don't forget 'cvs update' to make sure you haven't forgotten anything!
+##
+## Define the following in ~/.netrc:
+##      machine upload.sf.net
+##      login anonymous
+##      password username@users.sf.net
+##      macdef init
+##         bin
+##         cd incoming
+##         mput octave-forge-*
+##         bye
+##      <leave this line empty!!>
 ##
 ## Finally, log in to your source forge account and use the following
 ## page to announce the new release of the package:
@@ -26,6 +32,12 @@ PROJECT=octave-forge
 TAG=R`date +%Y-%m-%d`
 ROOT=$PROJECT-`date +%Y.%m.%d`
 
+# generate the updated ChangeLog
+cvs2cl.pl --fsf --file ChangeLog.tmp
+echo "# Automatically generated file --- DO NOT EDIT" | cat - ChangeLog.tmp > ChangeLog
+rm ChangeLog.tmp
+cvs commit -m '$TAG release' ChangeLog
+
 # tag the CVS tree with the revision number
 cvs rtag $TAG $PROJECT
 
@@ -40,4 +52,10 @@ tar czf $ROOT.tar.gz $ROOT
 
 # remove the tagged directory
 rm -rf $ROOT
+
+# optimistic: upload the file
+ftp upload.sf.net
+
+# display the ChangeLog so that you can generate a release description
+more ChangeLog
 

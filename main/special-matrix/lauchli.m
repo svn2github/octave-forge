4f491c8a705a97f600dc69362168cc261ad8875a
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2001-12-21 20:45:06 +0000

    ill-conditioned rectangular matrix for testing QR
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@98 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/special-matrix/lauchli.m
@@ -0,0 +1,23 @@
+## A = lauchli (n [,mu])
+##    Creates the matrix [ ones(1,n); mu*eye(n) ]
+##    The value mu defaults to sqrt(eps)
+##    This is an ill-conditioned system for testing the
+##    accuracy of the QR routine.
+##    E.g., 
+##       A = lauchli(15);
+##       [Q, R] = qr(A);
+##       norm(Q*R - A)
+##       norm(Q'*Q - eye(rows(Q)))
+
+## This program is in the public domain
+
+function A = lauchli(n,mu)
+  if (nargin < 1 || nargin > 2)
+    usage("A = lauchli(n [, mu])");
+  endif
+
+  if (nargin < 2), mu = sqrt(eps); endif
+
+  A = [ ones(1,n); mu*eye(n) ];
+
+endfunction

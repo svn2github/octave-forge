96ebd046ed37fec45e51044c67860c334fabba83
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-01-29 16:48:16 +0000

    (for Dirk Laurie) increasae range of valid inputs, increased accuracy for existing range, and faster
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@127 fc592f45-0881-4044-91ff-c96495a54512

--- FIXES/invhilb.m
+++ FIXES/invhilb.m
@@ -1,4 +1,4 @@
-## Copyright (C) 1996, 1997 John W. Eaton
+## Copyright (C) 2002 Dirk Laurie
 ##
 ## This file is part of Octave.
 ##
@@ -19,73 +19,92 @@
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {} invhilb (@var{n})
-## Return the inverse of a Hilbert matrix of order @var{n}.  This is exact.
-## Compare with the numerical calculation of @code{inverse (hilb (n))},
+## Return the inverse of a Hilbert matrix of order @var{n}.  This can be 
+## computed computed exactly using
+## @tex
+## $$\eqalign{
+##   A_{ij} &= -1^{i+j} (i+j-1)
+##              \left( \matrix{n+i-1 \cr n-j } \right)
+##              \left( \matrix{n+j-1 \cr n-i } \right)
+##              \left( \matrix{i+j-2 \cr i-2 } \right)^2 \cr
+##          &= { p(i)p(j) \over (i+j-1) }
+## }$$
+## where
+## $$
+##   p(k) = -1^k \left( \matrix{ k+n-1 \cr k-1 } \right)
+##               \left( \matrix{ n \cr k } \right)
+##$$
+## @end tex
+## @ifinfo
+## @example
+##
+##             (i+j)         /n+i-1\  /n+j-1\   /i+j-2\ 2
+##  A(i,j) = -1      (i+j-1)(       )(       ) (       )
+##                           \ n-j /  \ n-i /   \ i-2 /
+##
+##         = p(i) p(j) / (i+j-1)
+##
+## @end example
+## where
+## @example
+##              k  /k+n-1\   /n\
+##     p(k) = -1  (       ) (   )
+##                 \ k-1 /   \k/
+## @end example
+## @end ifinfo
+##
+## The validity of this formula can easily be checked by expanding 
+## the binomial coefficients in both formulas as factorials.  It can 
+## be derived more directly via the theory of Cauchy matrices: 
+## see J W Demmel, Applied Numerical Linear Algebra, page 92)
+##
+## Compare this with the numerical calculation of @code{inverse (hilb (n))},
 ## which suffers from the ill-conditioning of the Hilbert matrix, and the
 ## finite precision of your computer's floating point arithmetic.
+##
 ## @end deftypefn
 ## @seealso{hankel, vander, sylvester_matrix, hilb, and toeplitz}
 
-## Author: jwe
-## Paul Kienzle <pkienzle@kienzle.powernet.co.uk>
-##    vectorize for speed
-
+## Author: Dirk Laurie
 
 function retval = invhilb (n)
 
   if (nargin != 1)
     usage ("invhilb (n)");
   endif
-
-###
-###             1     Prod(k+i-1) Prod(k+j-1)
-### A(i,j) = -------  ----------- -----------
-###          (i+j-1)   Prod(k-i)   Prod(k-j)
-###                    k!=i        k!=i
-###
-### Consider starting at the diagonal and building the matrix along the
-### rows.  That is, find A(i,j+1) in terms of A(i,j).
-### Ignoring the 1/(i+j-1) for the moment, A(i,j+1)/A(i,j) is:
-###
-###      Prod(k+j)     Prod(k-j)       (n+j-1) (n+1-j)        (n)^2
-###     -----------  ------------- =   ------- ------- = 1 - -------
-###     Prod(k+j-1)  Prod(k-(j+1))      (j-1)   (1-j)        (j-i)^2
-###
-### So we can generate a row by taking the cumulative product of
-### (1 - (n/(j-1))^2), multiplying by the initial value Prod(k+i-1)/Prod(k-i)
-### and dividing by 1/(i+j-1).  Since it is symmetric, we only need to
-### generate half a row.
-###
-### The cumulative sum does introduce some error
-###    err_relative < 3*eps for n < 10
-### but rounding gets rid of that.  Error stays under control until at
-### least n=25 (err_relative < 10*eps).  After that I don't know since
-### I get bored of waiting for the old invhilb.  It is a factor of 100
-### slower at n=25, and going up as cube rather than a square.
-###
-### Note that the remaining for loop could probably be eliminated as
-### well, but there is hardly any point. invhilb -> Inf for n>134, and
-### that only takes 0.5 seconds to compute on my machine.  And it's not
-### like invhilb gets called a bunch, either, unlike e.g., an FFT which
-### operates directly on a signal.
-
+  
   nmax = length (n);
   if (nmax == 1)
-    retval = zeros (n);
-    for l = 1:n
-      den = [ 1-l:n-l ]; 
-      den (l) = [];
-      lprod = prod (l:l+n-1) / prod (den);
-      if l==n
-	retval (l,l) = lprod^2 / (l+l-1);
-      else
-	row = [ lprod ;  1-(n./[l:n-1]').^2 ];
-      	retval (l:n, l) = lprod * cumprod (row) ./ ([l:n]' + l-1);
-      endif
-    endfor
-    retval = round (retval + retval.' - diag (diag (retval)));
-  else
-    error ("hilb: expecting scalar argument, found something else");
-  endif
+    
+    ## The point about the second formula above is that when vectorized,
+    ## p(k) is evaluated for k=1:n which involves O(n) calls to bincoeff 
+    ## instead of O(n^2).
+    ##
+    ## We evaluate the expression as (-1)^(i+j)*(p(i)*p(j))/(i+j-1) except
+    ## when p(i)*p(j) would overflow.  In cases where p(i)*p(j) is an exact
+    ## machine number, the result is also exact.  Otherwise we calculate
+    ## (-1)^(i+j)*p(i)*(p(j)/(i+j-1)).
+    ##
+    ## The Octave bincoeff routine uses transcendental functions (lgamma
+    ## and exp) rather than multiplications, for the sake of speed.  
+    ## However, it rounds the answer to the nearest integer, which 
+    ## justifies the claim about exactness made above.
 
+    retval = zeros (n); 
+    k = [ 1:n ]; 
+    p = k .* bincoeff (k+n-1, k-1) .* bincoeff (n, k);
+    p (2:2:n) = -p (2:2:n);
+    if ( n < 203 )
+      for l = 1:n
+        retval (l, :) = ( p (l) * p ) ./ [ l:l+n-1 ]; 
+      endfor
+    else
+      for l = 1:n
+        retval (l, :) = p (l) * ( p ./ [ l:l+n-1 ] );
+      endfor
+    endif
+  else
+    error ("invhilb: expecting scalar argument, found something else");
+  endif
+  
 endfunction

86cbfa6d77cf2a2a4a95c0977371c4a0e999026d
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2002-03-16 10:45:27 +0000

    docu improved
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@181 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/nantest.m
+++ extra/NaN/nantest.m
@@ -1,7 +1,5 @@
-% NANTEST checks whether functions from NaN-toolbox are called
-% or the default functions which come with Octve. 
-% The functions of the NaN-toolbax can handle NaN's. 
-% 
+% NANTEST checks whether the functions from NaN-toolbox have been
+% installed correctly. 
 
 %    This program is free software; you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
@@ -29,6 +27,8 @@ r=zeros(17,2);
 x = [5,NaN,0,1,nan];
 
 % run test, k=1: with NaNs, k=2: all NaN's are removed
+% the result of both should be the same. 
+
 for k=1:2,
         if k==2, x(isnan(x))=[]; end; 
         r(1,k) =sumskipnan(x(1));
@@ -40,7 +40,8 @@ for k=1:2,
         r(7,k) =sem(x);
         r(8,k) =median(x);
         r(9,k) =mad(x);
-        tmp  =  zscore(x); r(10,k)=tmp(1);
+    		tmp = zscore(x); 
+	r(10,k)=tmp(1);
         r(11,k)=coefficient_of_variation(x);
         r(12,k)=geomean(x);
         r(13,k)=harmmean(x);
@@ -57,6 +58,7 @@ tmp = abs(r(:,1)-r(:,2))<eps;
 if all(tmp)
         fprintf(1,'NANTEST successful - your NaN-tools are installed correctely\n');
 else
-        fprintf(1,'NANTEST %i not successful\n', find(~tmp));
+        fprintf(1,'NANTEST %i not successful \n', find(~tmp));
+	fprintf(1,'Some functions must still be replaced\n');
 end;
 

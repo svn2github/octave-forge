a58384ad2276f004451b929bc66f203ad97aff70
aadler <aadler@fc592f45-0881-4044-91ff-c96495a54512>
2002-03-17 05:26:52 +0000

    added support for matrix interpolation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@190 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/interpft.m
+++ main/general/interpft.m
@@ -17,35 +17,85 @@
 ## y = interpft (x, n)
 ## Resample x with n points using Fourier interpolation.
 ## The data x must be equally spaced and n >= length(x).
+## y = interpft (x, n, m)
+## Resample matrix x to NxM useing Fourier interpolation
 
 ## Author: Paul Kienzle
 ## 2001-02-11
 ##    * initial version
+## 2002-03-17 aadler
+##    * added code to work on matrices as well 
+## $Id$
 
-function z = interpft (x, n)
+function z = interpft (x, n, m)
   if (nargin < 2 || nargin > 3)
     usage ("y=interpft (x, n)");
   endif
 
-  transpose = ( rows (x) == 1 ); 
+  transpose = ( rows (x) == 1 );
   if (transpose) x = x (:); endif
+
+if nargin==2
   [nr, nc] = size (x);
   if n > nr
     y = fft (x) / nr;
     k = floor (nr / 2);
     z = n * ifft ( [ y(1:k,:); zeros(n-nr,nc); y(k+1:nr) ] );
-    if isreal (x), z = real (z); endif
   elseif n < nr
-    error ("interpft: n must be bigger than x");
+    warning ("interpft: Poor results possible: n should be bigger than x");
     ## XXX FIXME XXX the following doesn't work so well
     y = fft (x) / nr;
     k = ceil (n / 2);
     z = n * ifft ( [ y(1:k,:); y(nr-k+2:nr) ] );
-    if isreal (x), z = real (z); endif
   else
     z = x;
   endif
+
+else
+   # this code is for matrices as well as vectors,
+   # it could replace the first code except
+   # 1) it depends on sparse being available
+   # 2) it's slightly slower
+   save_empty_list_elements_ok= empty_list_elements_ok;
+   unwind_protect
+   empty_list_elements_ok= 1;
+
+   ny= n;
+   if nargin==2; my=1; else; my= m; endif
+
+   [nx,mx]= size(x);
+
+   if nx < ny
+      nhx= ceil( nx/2 );
+      yconv= sparse( [ 1:nhx, ny-(nx-1-nhx:-1:0) ], 1:nx, 1, ny, nx );
+   elseif  nx > ny
+      nhy= ceil( ny/2 );
+      yconv= sparse( 1:ny, [ 1:nhy, nx-(ny-1-nhy:-1:0) ], 1, ny, nx );
+   elseif  nx == ny
+      yconv= sparse( 1:ny, 1:nx, 1, ny, nx);
+   end
+
+   if mx < my
+      mhx= ceil( mx/2 );
+      xconv= sparse( 1:mx, [ 1:mhx, my-(mx-1-mhx:-1:0) ], 1, mx, my );
+   elseif  mx > my
+      mhy= ceil( my/2 );
+      xconv= sparse( [ 1:mhy, mx-(my-1-mhy:-1:0) ], 1:my, 1, mx, my );
+   elseif  mx == my
+      xconv= sparse( 1:mx, 1:my, 1, mx, my);
+   end
+
+   z= ifft2( yconv*fft2(x)*xconv) * (ny*my)/(nx*mx);
+
+   unwind_protect_cleanup
+      empty_list_elements_ok= save_empty_list_elements_ok;
+   end_unwind_protect
+
+endif
+
+  if isreal (x), z = real (z); endif
   if transpose, z = z.'; endif
+
 endfunction
 
 %!demo
@@ -56,7 +106,7 @@ endfunction
 %! yp = sin (4*ti + 0.3) .* cos (3*ti - 0.1);
 %! plot (ti, yp, 'g;sin(4t+0.3)cos(3t-0.1);', ...
 %!       ti, interp1 (t, y, ti, 'spline'), 'b;spline;', ...
-%!       ti, interpft (y, k), 'c;interpft;', ...
+%!       ti, interpft (y ,k), 'c;interpft;', ...
 %!       t, y, 'r+;data;');
 
 %!#demo
@@ -67,5 +117,5 @@ endfunction
 %! yp = sin (4*ti+0.3).*cos(3*ti-0.1);
 %! plot (ti, yp, 'g;sin(4t+0.3)cos(3t-0.1);', ...
 %!       t, interp1(ti,yp,t,'spline'), 'bx;spline;', ...
-%!       t, interpft(yp,n), 'co;interpft;', ...
-%!       t, y, 'r+;data;');
+%!       t, interpft(yp, n), 'co;interpft;', ...
+%!       t, y, 'r+;data;')

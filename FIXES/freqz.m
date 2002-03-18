a6d11adfe1c9be0c996f9aabc0ebfae41a957080
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-03-18 06:20:12 +0000

    Separate out the plotting portion of freqz so that alternative graphics packages can plot it differently.  Right now freqz_plot uses raw gnuplot commands to align the graph margins, so it won't automatically work in plplot
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@204 fc592f45-0881-4044-91ff-c96495a54512

--- FIXES/freqz.m
+++ FIXES/freqz.m
@@ -154,62 +154,7 @@ function [h_r, w_r] = freqz(b, a, n, region, Fs)
     h_r = h;
     w_r = w;
   else             # plot and don't return values
-    ## ## exclude zero-frequency
-    ## h = h(2:length(h));
-    ## w = w(2:length(w));
-    ## n = n-1;
-    mag = 20*log10(abs(h));
-    phase = unwrap(arg(h));
-    maxmag = max(mag);
-
-    unwind_protect # protect graph state
-      if gnuplot_has_multiplot
-      	subplot(311);
-      	gset lmargin 10;
-	axis("labely");
-	xlabel("");
-      endif
-      grid("on");
-      axis([w(1), w(n), maxmag-3, maxmag]);
-      plot(w, mag, ";Pass band (dB);");
-
-      if gnuplot_has_multiplot
-      	subplot(312);
-	axis("labely");
-	title("");
-	xlabel("");
-      	gset tmargin 0;
-      else
-	input("press any key for the next plot: ");
-      endif
-      grid("on");
-      if (maxmag - min(mag) > 100)
-      	axis([w(1), w(n), maxmag-100, maxmag]);
-      else
-      	axis("autoy");
-      endif
-      plot(w, mag, ";Stop band (dB);");
-      
-      if gnuplot_has_multiplot
-      	subplot(313);
-	axis("label");
-	title("");
-      else
-	input("press any key for the next plot: ");
-      endif
-      grid("on");
-      axis("autoy");
-      xlabel(["Frequency (Fs=", num2str(Fs), ")"]);
-      axis([w(1), w(n)]);
-      plot(w, phase/(2*pi), ";Phase (radians/2pi);");
-      
-    unwind_protect_cleanup # restore graph state
-      grid("off");
-      axis("auto","label");
-      gset lmargin;
-      gset tmargin;
-      oneplot();
-    end_unwind_protect
+    freqz_plot(w,h);
   end
 
 endfunction

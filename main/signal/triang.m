f53e5b4b3779fa6ab5f0f3309fd0c2d88447f968
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-04-22 21:30:47 +0000

    Peak value of 1 at virtual 0 for even n
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@358 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/triang.m
+++ main/signal/triang.m
@@ -20,37 +20,31 @@
 ## Unlike the bartlett window, triang does not go to zero at the edges
 ## of the window.  For odd n, triang(n) is equal to bartlett(n+2) except
 ## for the zeros at the edges of the window.
-##
-## Note that the definition of triang for even values and odd values is
-## different.  For odd values, it samples the continous function
-## y=1-|x| at equally spaced points in the range (-1,1) whereas
-## for even values it samples from y=1+1/n-|x|.
 
 ## 2001-04-07  Paul Kienzle
-##    * return column vector like other window functions.
+## * return column vector like other window functions.
+## 2002-04-22  Paul Kienzle
+## * peak value of 1 at 0 for even n
 
 function w = triang(n)
   if (nargin != 1) 
     usage("w = triang(n)"); 
   endif
-  if (n != fix (n) || n < 1)
-    error("triang(n): n hast to be an integer > 0"); 
+  if (!is_scalar(n) || n != fix (n) || n < 1)
+    error("triang(n): n has to be an integer > 0"); 
   endif
-  k=(n-1)/2;
-  if rem(n,2)
-    w = 1-abs([-k:k]')/(k+1);
-  else
-    w = (k+1-abs([-k:k]'))/(k+1/2);
-  endif
+  w = 1 - abs ([-(n-1):2:(n-1)]' / (n+rem(n,2)));
 endfunction
 
 %!error triang
 %!error triang(1,2)
 %!error triang([1,2]);
 %!assert (triang(1), 1)
-%!assert (triang(2), [1, 1])
+%!assert (triang(2), [1; 1]/2)
+%!assert (triang(3), [1; 2; 1]/2);
+%!assert (triang(4), [1; 3; 3; 1]/4);
 %!test
-%! x = bartlett(5);
+%! x = bartlett(5)';
 %! assert (triang(3), x(2:4));
 
 %!demo

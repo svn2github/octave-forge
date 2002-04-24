72c5003f64e1cd4bcf58afb8a36643cc27112224
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-04-24 10:37:49 +0000

    windows is not unix
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@373 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/Windows/isunix.m
@@ -0,0 +1,6 @@
+## This function always returns false.  If you are on a Unix machine, be
+## sure the alternate version which returns true comes first on your
+## loadpath (e.g., by deleting this one).
+function x = isunix
+  x = !1;
+

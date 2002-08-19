7ac2cf4d7ecf53d7bc9786158f2b1d05a20592df
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2002-08-19 19:34:40 +0000

    implements advanced RANKS algorithm with O(n*log(n)) on average
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@493 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/NaN/ranks.m
@@ -0,0 +1,108 @@
+function r = ranks(X,Mode);
+% RANKS gives the rank of each element in a vector.
+% This program uses an advanced algorithm with averge effort O(m.n.log(n)) 
+% 
+% r = ranks(X)
+%   if X is a vector, return the vector of ranks of X adjusted for ties.
+%   if X is matrix, the rank is calculated for each column. 
+% r = ranks(X,'traditional')
+%   implements the traditional algorithm with O(m*n^2) effort
+% r = ranks(X,'advanced   ')
+%   implements an advanced algorithm with O(m*n*log(n)) effort
+%
+% see also: CORRCOEF, SPEARMAN, RANKCORR
+%
+% REFERENCES:
+% --
+
+
+%    Version 1.26  Date: 19 Aug 2002
+%    Copyright (C) 2000-2002 by  Alois Schloegl <a.schloegl@ieee.org>	
+
+%    This program is free software; you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation; either version 2 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% Features:
+% + interprets NaN's as missing value 
+% + is fast, using an efficient algorithm O(n.log(n)) for the rank correlation
+% - rank correlation works for cell arrays, too (no check for missing values).
+% + compatible with Octave and Matlab
+
+
+if nargin<2, Mode=''; end;
+
+[N,M]=size(X);
+if (N==1) & (M>0)
+        X = X';
+	[N,M] = size(X);
+end; 
+
+if strcmp(Mode(1:min(11,length(Mode))),'traditional'), % traditional, needs O(m.n^2)
+% this method was originally implemented by: KH <Kurt.Hornik@ci.tuwien.ac.at>
+% Comment of KH: This code is rather ugly, but is there an easy way to get the ranks adjusted for ties from sort?
+
+r = zeros(size(X));
+        for i = 1:M;
+                p = X(:, i(ones(1,N)));
+                r(:,i) = [(sum (p < p') + (sum (p == p') + 1) / 2)'];
+        end;
+        % r(r<1)=NaN;
+        
+elseif strcmp(Mode(1:min(11,length(Mode))),'advanced   '), % advanced
+        % + uses sorting, hence needs only O(m.n.log(n)) computations         
+        
+        % [tmp,ix] = sort([X,Y]);     
+        % [tmp,r] = sort(ix);     % r yields rank. 
+        % but because sort does not work accordingly for cell arrays, 
+        % and DIM argument not supported by Octave 
+        % and DIM argument does not work for cell-arrays in Matlab
+        % we sort each column separately:
+        
+        r = zeros(size(X));
+        for k = 1:M,
+                [sX,ix] = sort(X(:,k)); 
+                [tmp,r(:,k)] = sort(ix);	    % iy yields the rank of each element 	
+                
+                % identify multiple occurences (not sure if this important, but implemented to be compatible with traditional version)
+                if isnumeric(X)
+                        N=sum(~isnan(X(:,k)));
+                end;
+                x = [0;find(sX~=[sX(2:end);N])];    % for this reason, cells are not implemented yet.   
+                d = find(diff(x)>1);
+                
+                % correct rank of multiple occurring elements
+                for l = 1:length(d),
+                        t = (x(d(l))+1:x(d(l)+1))';
+                        r(ix(t),k) = mean(t);
+                end;
+        end;
+        r(isnan(X)) = nan;
+        
+else %if strcmp(Mode(1:min(11,length(Mode))),''), 
+% the results of both algorithms are compared for testing.    
+%
+% if the Mode-argument is omitted, both methods are applied and 
+% the results are compared. Once the advanced algorithm is confirmed, 
+% it will become the default Mode. 
+
+        r  = ranks(X,'advanced   ');
+        r(isnan(r)) = 1/2;
+        
+        r1 = ranks(X,'traditional');
+        if ~all(all(r==r1)),
+                fprintf(2,'WARNING RANKS: advanced algorithm does not agree with traditional one\n Please report to <a.schloegl@ieee.org>\n');
+                r = r1;
+        end;
+        r(isnan(X)) = nan;
+end;

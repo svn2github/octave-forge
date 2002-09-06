8224836f99c38c0381b2298b7f10ec4235d20eb2
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2002-09-06 20:58:27 +0000

    add trimean
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@519 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/NaN/trimean.m
@@ -0,0 +1,45 @@
+function O=trimean(Y,DIM)
+% TRIMEAN evaluates basic statistics of a data series (column)
+% TR=TRIMEAN(y)
+% 
+% The trimean is 
+%   TR=(Q1+2*M+Q3)/4
+%  
+% REFERENCES:
+% [1] http://mathworld.wolfram.com/Trimean.html
+
+%	Version 1.27
+%	Copyright (C) 1996-2002 by Alois Schloegl <a.schloegl@ieee.org>	
+
+
+% check dimension
+sz=size(Y);
+if length(sz)>2,
+        fprintf('Error TRIMEAN: data must have no more than 2 dimensions\n');	
+	return;        
+end;
+
+% find the dimension
+if nargin==1,
+        DIM=min(find(size(i)>1));
+        if isempty(DIM), DIM=1; end;
+end;
+
+N = sum(~isnan(Y),DIM);
+Y = sort(Y,DIM);
+
+sz = size(Y);
+sz(DIM) = 1;
+
+Q0500 = repmat(nan,sz);
+Q0250 = Q0500;
+Q0750 = Q0500;
+
+if DIM==2, Y=Y'; end;
+for k=1:size(Y,2),
+        Q0250(k) = flix(Y(:,k),N(k)/4   + 0.75);
+        Q0500(k) = flix(Y(:,k),N(k)/2   + 0.50);
+        Q0750(k) = flix(Y(:,k),N(k)*3/4 + 0.25);
+end;
+O = [Q0250 + 2*Q0500 + Q0750]/4;
+

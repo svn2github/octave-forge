aa2bfe7bc91cfd728979349713ca0e81773bdbcb
etienne <etienne@fc592f45-0881-4044-91ff-c96495a54512>
2002-09-29 17:30:40 +0000

    tar : copy arguments to a struct
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@565 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/struct/tar.m
@@ -0,0 +1,19 @@
+## s = tar (foo,bar, ... ) == struct ("foo",foo,"bar",bar,...)
+##
+## Groups foo, bar, ... into a struct whose fields are "foo", "bar" ...
+## and such that s.foo == foo, s.bar == bar ...  
+##
+## See also : untar
+
+## Author:        Etienne Grossmann  <etienne@isr.ist.utl.pt>
+## Last modified: October 2000
+
+function s = tar(...)
+
+## keyboard
+## form and eval a string like
+## "s.argn(1,:)=va_arg();s.argn(2,:)=va_arg(); ...."
+
+eval(setstr(grep([ones(nargin,1)*toascii("s."),\
+		  toascii(argn),\
+		  ones(nargin,1)*toascii("=va_arg();")]')')) ;

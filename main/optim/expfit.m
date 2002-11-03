90a8db69557a03c4043947ac652080f83111ae91
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-11-03 17:18:58 +0000

    (for Rolf Fabian) correct handling of complex exponentials
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@604 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/expfit.m
+++ main/optim/expfit.m
@@ -1,50 +1,45 @@
-## usage [alpha,c,rms] = expfit(deg,x1,h,y)
+## USAGE  [alpha,c,rms] = expfit( deg, x1, h, y )
 ##
-## Implementation of Prony's method to perform
-## non-linear exponential fitting on equidistant
-## data.
+## Prony's method for non-linear exponential fitting
 ##
-## Fit function: \sum_1^{deg} c(i)*exp(alpha(i)*x)
+## Fit function:   \sum_1^{deg} c(i)*exp(alpha(i)*x)
 ##
-## Data is equidistant, starts at x1 with a stepsize h
+## Elements of data vector y must correspond to
+## equidistant x-values starting at x1 with stepsize h
 ##
-## Function returns linear coefficients c, non-linear
-## coefficients alpha and root mean square error rms
-## This method is known to be more stable than 'brute-
-## force' non-linear least squares fitting.
+## The method is fully compatible with complex linear
+## coefficients c, complex nonlinear coefficients alpha
+## and complex input arguments y, x1, non-zero h .
+## Fit-order deg  must be a real positive integer.
 ##
-## The non-linear coefficients alpha can be imaginary,
-## fitting function becomes a sum of sines and cosines.
+## Returns linear coefficients c, nonlinear coefficients
+## alpha and root mean square error rms. This method is
+## known to be more stable than 'brute-force' non-linear
+## least squares fitting.
 ##
 ## Example
 ##    x0 = 0; step = 0.05; xend = 5; x = x0:step:xend;
 ##    y = 2*exp(1.3*x)-0.5*exp(2*x);
-##
 ##    error = (rand(1,length(y))-0.5)*1e-4;
-##
 ##    [alpha,c,rms] = expfit(2,x0,step,y+error)
 ##
 ##  alpha =
-##
 ##    2.0000
 ##    1.3000
-##
 ##  c =
-##
 ##    -0.50000
 ##     2.00000
+##  rms = 0.00028461
 ##
-##    rms = 0.00028461
-##
-## The fitting is very sensitive to the number of data points.
-## It doesn't perform very well for small data sets (theoretically,
-## you need at least 2*deg data points, but if there are errors on
-## the data, you certainly need more.
+## The fit is very sensitive to the number of data points.
+## It doesn't perform very well for small data sets.
+## Theoretically, you need at least 2*deg data points, but
+## if there are errors on the data, you certainly need more.
 ##
 ## Be aware that this is a very (very,very) ill-posed problem.
-## By the way, this algorithm relies heavily on computing the roots
-## of a polynomial. I used 'roots.m', if there is something better
-## please use that code.
+## By the way, this algorithm relies heavily on computing the
+## roots of a polynomial. I used 'roots.m', if there is
+## something better please use that code.
 ##
 ## Copyright (C) 2000: Gert Van den Eynde
 ## SCK-CEN (Nuclear Energy Research Centre)
@@ -53,58 +48,81 @@
 ## Belgium
 ## na.gvandeneynde@na-net.ornl.gov
 ##
-## This code is under the Gnu Public License (GPL) version 2 or later.
-## I hope that it is useful, but it is WITHOUT ANY WARRANTY;
-## without even the implied warranty of MERCHANTABILITY or FITNESS
-## FOR A PARTICULAR PURPOSE.
+## This code is under the GNU Public License (GPL) version 2 or later.
+## I hope that it is useful, but it is WITHOUT ANY WARRANTY, without
+## even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+## PARTICULAR PURPOSE.
+## __________________________________________________________________
+## Modified for full compatibility with complex fit-functions by
+## Rolf Fabian <fabian@tu-cottbus.de>                2002-Sep-23
+## Brandenburg University of Technology Cottbus
+## Dep. of Air Chemistry and Pollution Control
+##
+## Demo for a complex fit-function:
+## deg= 2; N= 20; x1= -(1+i), x= linspace(x1,1+i/2,N).';
+## h = x(2) - x(1)
+## y= (2+i)*exp( (-1-2i)*x ) + (-1+3i)*exp( (2+3i)*x );
+## A= 5e-2; y+= A*(randn(N,1)+randn(N,1)*i); % add complex noise
+## [alpha,c,rms]= expfit( deg, x1, h, y )
+## __________________________________________________________________
 
 
-function [alpha,c,rms] = expfit(deg,x1,h,y)
-
+function [a,c,rms] = expfit(deg,x1,h,y)
 
 % Check input
+if deg<1, error('expfit: deg must be >= 1');     end
+if  ~h,   error('expfit: vanishing stepsize h'); end
 
-   if (deg < 1) error ('deg must be >= 1');end
+if ( N=length( y=y(:) ) ) < 2*deg
+   error('expfit: less than %d samples',2*deg);
+end
 
-   if (h <= 0) error ('Stepsize h must be > 0');end;
-
-%if (length(y) <= 2*deg)
-% error ('Number of data points must be larger than 2*deg');end;
-
-
-   N = length(y);
-
-% Solve for polynomial coeff
-
-% Compose matrix A(i,j) = y(i+j-1)
-   A1 = hankel(y(1:N-deg),y(N-deg:N-1));
-   b1 = -y(deg+1:N);
-
-% Least squares solution
-   s = A1\b1;
+% Solve for polynomial coefficients
+A = hankel( y(1:N-deg),y(N-deg:N) );
+s = - A(:,1:deg) \ A(:,deg+1);
 
 % Compose polynomial
-   p = [1,fliplr(s')];
+p = flipud([s;1]);
 
 % Compute roots
-   u = roots(p);
+u = roots(p);
 
-% Compose second matrix A2(i,j) = u(j)^(i-1)
-   A2 = ( ones(N,1) * u(:).' )  .^  ( [0:N-1](:) * ones(1,deg) );
-   b2 = y;
+% nonlinear coefficients
+a = log(u)/h;
+
+% Compose second matrix A(i,j) = u(j)^(i-1)
+A = ( ones(N,1) * u(:).' ) .^ ( [0:N-1](:) * ones(1,deg) );
 
 % Solve linear system
-   f = A2\b2;
+f = A\y;
 
-% Decompose all
-   alpha = log(u)/h;
-   c = f./exp(alpha*x1);
+% linear coefficients
+c = f./exp( a*x1 );
 
-% Compute rms of y - approx 
-% where approx(i) = sum_k ( c(k) * exp(x(i)*alpha(k)) )
+% Compute rms of y - approx
+% where approx(i) = sum_k ( c(k) * exp(x(i)*a(k)) )
+if nargout > 2
    x = x1+h*[0:N-1];
-   approx = exp( x(:) * alpha(:).' ) * c(:);
-   rms = sqrt(sumsq(approx - y));
+   approx = exp( x(:) * a(:).' ) * c(:);
+   rms = sqrt( sumsq(approx - y) );
+end
 
 endfunction
+
+% Two demos for users of P. Kienzle's 'demo'-feature :
+
+%!demo	 % same as in help - part
+%! deg= 2; N= 20; x1= -(1+i), x= linspace(x1,1+i/2,N).';
+%! h = x(2) - x(1)
+%! y= (2+i)*exp( (-1-2i)*x ) + (-1+3i)*exp( (2+3i)*x );
+%! A= 5e-2; y+= A*(randn(N,1)+randn(N,1)*i); % add complex noise
+%! [alpha,c,rms]= expfit( deg, x1, h, y )
+
+%!demo	 % demo for stepsize with negative real part
+%! deg= 2; N= 20; x1= +3*(1+i), x= linspace(x1,1+i/3,N).';
+%! h = x(2) - x(1)
+%! y= (2+i)*exp( (-1-2i)*x ) + (-1+3i)*exp( (2+3i)*x );
+%! A= 5e-2; y+= A*(randn(N,1)+randn(N,1)*i); % add complex noise
+%! [alpha,c,rms]= expfit( deg, x1, h, y )
+
 

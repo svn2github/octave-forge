4db9bc8da48b4a4cfd2d9143354778bfe06707fb
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2002-11-27 05:26:11 +0000

    Use assert rather than error messages for tests
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@666 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/lp_test.m
+++ main/optim/lp_test.m
@@ -7,12 +7,7 @@ UB1 = [1;1;1;1;1]*Inf;
 N1 = 3;
 x1 = [-1;0;1;0;2];
 sol1 = lp(F1,A1,B1,LB1,UB1,N1);
-if(x1 != sol1) 
-  disp("Problem with test 1\n");
-else 
-  disp("Test 1 worked\n");
-  i
-endif
+assert(x1,sol1,10*eps);
 
 A2 = [2 1 2 ; 3 3 1];
 B2 = [4 ; 3];
@@ -22,11 +17,7 @@ UB2 = [1;1;1]*Inf;
 N2 = 2;
 x2 = [0;2/5;9/5];
 sol2 = lp(F2,A2,B2,LB2,UB2,N2);
-if(x2 != sol2) 
-  disp("Problem with test 2\n");
-else 
-  disp("Test 2 worked\n");
-endif
+assert(x2,sol2,10*eps);
 
 A3 = [2 1 1; 1 2 3; 2 2 1];
 B3 = [2; 5; 6];
@@ -36,11 +27,7 @@ UB3 = [1;1;1]*Inf;
 N3  = 0;
 x3 = [1/5;0;8/5];
 sol3 = lp(F3,A3,B3,LB3,UB3,N3);
-if(x3 != sol3) 
-  disp("Problem with test 3\n");
-else 
-  disp("Test 3 worked\n");
-endif
+assert(x3,sol3,10*eps);
 
 % This problem is identical to the third problem 
 % accept for the fact that the x3 = x3+10;    
@@ -52,11 +39,7 @@ UB4 = [1;1;1]*Inf;
 N4  = 0;
 x4 = x3+[0;0;10];
 sol4 = lp(F4,A4,B4,LB4,UB4,N4);
-if(x4 != sol4) 
-  disp("Problem with test 4\n");
-else 
-  disp("Test 4 worked\n");
-endif
+assert(x4,sol4,10*eps);
 
 A5 = [1 0 1 -1 2 ; 0 1 2 2 1 ];
 B5 = [5;9];
@@ -66,8 +49,4 @@ UB5 = [7;10;1;5;3];
 N5 = 2;
 sol5 = lp(F5,A5,B5,LB5,UB5,N5);
 x5 = [7;1;1;3;0];
-if(x5 != sol5) 
-  disp("Problem with test 5\n");
-else 
-  disp("Test 5 worked\n");
-endif
+assert(x5,sol5,10*eps);

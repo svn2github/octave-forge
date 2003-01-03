a5d3603789f999c4d8eb29becbe47fcc690a98b9
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2003-01-03 21:46:43 +0000

    separates filename
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@747 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/io/fileparts.m
@@ -0,0 +1,23 @@
+function [p,f,e]=fileparts(fn)
+## FILEPARTS separates the parts of the filename
+##  [PATH,FILE,EXT]=FILEPARTS(FN)
+## 
+## The filename FN can be reconstructed by [PATH,FILE,EXT]
+
+##	Version 1.00  Date: 03 Jan 2003
+##	CopyLeft (C) 2002-2003 by Alois Schloegl
+##	a.schloegl@ieee.org	
+
+
+ix1=max(find(fn=='/'));
+ix2=max(find(fn=='.'));
+
+if isempty(ix1), ix1 = 0; end;
+if isempty(ix1), ix1 = length(fn); end;
+
+p = fn(1:ix1);
+f = fn(ix1+1:ix2-1);
+e = fn(ix2:length(fn));
+
+
+

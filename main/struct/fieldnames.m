7e70c2e359967d7163091df9c09e1ffc54bcbea7
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2003-02-23 03:33:46 +0000

    Have fieldnames return a cell array rather than a list.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@846 fc592f45-0881-4044-91ff-c96495a54512

--- main/struct/fieldnames.m
+++ main/struct/fieldnames.m
@@ -1,29 +1,13 @@
 ## f = fieldnames(s)
 ## 
-## When s is a structure, fieldnames(s) returns a list of the field names 
-## used in s.  When s is not a structure, fieldnames(s) returns the
-## empty list.  Note that this function exists for compatibility with
-## Matlab.  Since Octave lacks the dereferencing operator f{i}, you must
-## replace all dereferences in your script with nth(f,i).  The function 
-## nth is easily implemented in Matlab as:
+## Returns a cell array of field names used in s.
 ##
-## function x = nth(f,i)
-##    x = f{i};
-##
-## See also cmpstruct, getfield, setfield, rmfield, isfield, isstruct,
-## struct. 
+## See also getfield, setfield, rmfield, isfield, isstruct, struct. 
 
-## TODO: return a cell array instead of a list
-
-## Author:        Paul Kienzle <pkienzle@kienzle.powernet.co.uk>
-## Based on fields.m by Etienne Grossmann  <etienne@isr.ist.utl.pt>
+## Author:        Paul Kienzle <pkienzle@users.sf.net>
 
 ## This program is in the public domain.
 
 function f = fieldnames(s)
-  f = list() ;
-  i = 1 ;
-  for [val, key] = s
-    f(i++) = key ;
-  end
+  f = cellstr(struct_elements(s));
 endfunction

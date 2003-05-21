cb81e2c11ffbb1fe0fe25fbd43e794edccd83bdd
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2003-05-21 13:29:50 +0000

    [for Julius Smith] new functions
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@953 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/signal/freqs.m
@@ -0,0 +1,49 @@
+% Copyright (C) 2003 Julius O. Smith III
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+%
+% Octave is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Octave; see the file COPYING.  If not, write to the Free
+% Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
+
+% Usage: H = freqs(B,A,W);
+%
+% Compute the s-plane frequency response of the IIR filter B(s)/A(s) as 
+% H = polyval(B,j*W)./polyval(A,j*W).  If called with no output
+% argument, a plot of magnitude and phase are displayed.
+%
+% Example:
+%	B = [1 2]; A = [1 1];
+%	w = linspace(0,4,128);
+%	freqs(B,A,w);
+
+% 2003-05-16 Julius Smith - created
+
+function [H] = freqs(B,A,W)
+
+if (nargin ~= 3 || nargout>1)
+    usage ("[H] = freqs(B, A, W)");
+end
+
+H = polyval(B,j*W)./polyval(A,j*W);
+
+if nargout<1
+  freqs_plot(W,H);
+end
+
+endfunction
+
+%!demo
+%! B = [1 2];
+%! A = [1 1];
+%! w = linspace(0,4,128);
+%! freqs(B,A,w);
---
+++ main/signal/freqs_plot.m
@@ -0,0 +1,52 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} freqs_plot (@var{w}, @var{h})
+## Plot the amplitude and phase of the vector @var{h}.
+##
+## @end deftypefn
+
+function freqs_plot(w,h)
+    n = length(w);
+    mag = 20*log10(abs(h));
+    phase = unwrap(arg(h));
+    maxmag = max(mag);
+    title('Frequency response plot by freqs');
+    unwind_protect # protect graph state
+      if gnuplot_has_multiplot
+      	subplot(211);
+      	gset lmargin 10;
+	axis("labely");
+	ylabel("dB");
+	xlabel("");
+      endif
+      grid("on");
+      if (maxmag - min(mag) > 100) % make 100 a parameter?
+      	axis([w(1), w(n), maxmag-100, maxmag]);
+      else
+      	axis("autoy");
+      endif
+      plot(w, mag, ";Magnitude (dB);");
+%     semilogx(w, mag, ";Magnitude (dB);");
+
+      if gnuplot_has_multiplot
+      	subplot(212);
+	axis("label");
+	title("");
+      else
+	input("press any key for the next plot: ");
+      endif
+      grid("on");
+      axis("autoy");
+      xlabel("Frequency (rad/sec)");
+      ylabel("Cycles");
+      axis([w(1), w(n)]);
+      plot(w, phase/(2*pi), ";Phase (radians/2pi);");
+      
+    unwind_protect_cleanup # restore graph state
+      grid("off");
+      axis("auto","label");
+      gset lmargin;
+      gset tmargin;
+      oneplot();
+    end_unwind_protect
+
+endfunction

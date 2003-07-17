0b012ddc78aa2831908efb3ec62fc338840a6d77
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2003-07-17 20:09:19 +0000

    Just create libstdc++.dll.a, but don't put it anywhere in particular.  Let the user decide if shared or static linking will be the default, and explain how to override the default.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1028 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mkstdc++dll.sh
+++ admin/Windows/mkstdc++dll.sh
@@ -1,23 +1,24 @@
 #! /bin/sh
 
-# This script builds stdc++.dll and the corresponding libstdc++dll.a.
-# We do not use libstdc++.dll.a since that makes all g++ programs link
-# against stdc++.dll, and we would like to be able to choose shared
-# or static linking. 
+# This script builds stdc++.dll and the corresponding libstdc++.dll.a.
+# Now you have two choices:
 #
-# To statically link against libstdc++.a, use g++ as usual.
-# To dynamically link against stdc++.dll, use gcc with the 
-# following flags:
-#    -Wl,--enable-runtime-pseudo-reloc -lstdc++dll
-# If you use g++, it will link against libstdc++.a even with
-# the extra flags.
+# (1) Put libstdc++.dll.a into /usr/lib.  Now all g++ will link shared 
+#     to stdc++.dll by default, but you can override it by adding
+#     -static to your link line.  You should put stdc++.dll into /usr/bin
+#     if you do this, and be sure to include it with any executables
+#     that you distribute.
 #
-# You may also want to use
-#    -Wl,--export-all-symbols
-#    -Wl,--enable-auto-image-base
-# The flag --enable-auto-image-base may make loading the dll 
-# faster by pre-assigning non-colliding addresses for all the
-# functions loaded from the DLL.
+# (2) Put libstdc++.dll.a into a private lib directory.  All g++ will
+#     continue to link statically by default, but you can override it
+#     by adding -Lpath_to_private_libs to your link line.  You should
+#     install stdc++.dll into the directory containing the executable,
+#     and be sure to include it with any executables that you distribute.
+#
+# Older versions of cygwin (but not too old) require the flag 
+#    -Wl,--enable-runtime-pseudo-reloc
+# to link against stdc++.dll.  As of this writing, this is no
+# longer required.
 #
 # Paul Kienzle
 # 2003-07-17
@@ -25,7 +26,7 @@
 mkdir buildc++
 cd buildc++
 ar x /usr/lib/libstdc++.a
-gcc -shared -o/usr/bin/stdc++.dll \
-	-Wl,--enable-auto-image-base,--out-implib,/usr/lib/libstdc++dll.a *.o
+gcc -shared -o../stdc++.dll \
+	-Wl,--enable-auto-image-base,--out-implib,../libstdc++.dll.a *.o
 cd ..
 rm -rf buildc++

42e6ee595f99b669fe0921e462ff8219733ab371
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2003-10-06 11:14:32 +0000

    function flix replaced
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1088 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/trimean.m
+++ extra/NaN/trimean.m
@@ -10,6 +10,21 @@ function y=trimean(x,DIM)
 % REFERENCES:
 % [1] http://mathworld.wolfram.com/Trimean.html
 
+
+%    This program is free software; you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation; either version 2 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 %	$Revision$
 %	$Id$
 %	Copyright (C) 1996-2003 by Alois Schloegl <a.schloegl@ieee.org>	
@@ -33,14 +48,24 @@ D1 = prod(sz(1:DIM-1));
 D3 = prod(sz(DIM+1:length(sz)));
 D0 = [sz(1:DIM-1),1,sz(DIM+1:length(sz))];
 y  = repmat(nan,D0);
+q  = repmat(nan,3,1);
 for k = 0:D1-1,
 for l = 0:D3-1,
         xi = k + l * D1*sz(DIM) + 1 ;
         xo = k + l * D1 + 1;
-        t = sort(x(xi+(0:sz(DIM)-1)*D1));
+        t = sort(x(xi+(0:sz(DIM)-1)*D1))';
         n = sum(~isnan(t));
         
-        q = flix(t,[n/4 + 0.75; n/2+0.5; n*3/4 + 0.25]);
+        % q = flix(t,x); 	% The following find the quartiles and median.
+        			% INTERP1 is not an alternative since it fails for n<2;
+        x = [n/4 + 0.75; n/2+0.5; n*3/4 + 0.25]; 
+	k  = x - floor(x);	% distance to next sample	 
+
+	ix = ~logical(k);     	% find integer indices
+	q(~k) = t(x(~k)); 	% put integer indices
+	ix = ~ix;	     	% find non-integer indices
+	q(ix) = t(floor(x(ix))).*(1-k(ix)) + t(ceil(x(ix))).*k(ix);  
+        
         y(xo) = (q(1) + 2*q(2) + q(3))/4;
 end;
 end;

d56cfddc3e7e0173056ba60d760e978458a234e1
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2003-12-22 21:30:02 +0000

    Normally distributed random matrices.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1210 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/sparse/sprandn.m
@@ -0,0 +1,39 @@
+## S = sprandn(m,n,d)
+##
+## Generate a random sparse matrix.  m and n give the dimensions, d
+## gives the density (between 0 and 1).  Values will be normally
+## distributed with mean of zero and variance 1.
+##
+## Note: this will fail for large m x n matrices, even if they
+## are very sparse.
+##
+## S = sprandn(Q)
+##
+## Generate a random sparse matrix with non-zero values whereever Q
+## is non-zero.
+
+## This program is public domain
+## Author: Paul Kienzle <pkienzle@users.sf.net>
+
+function S = sprandn(m,n,d)
+  if nargin == 1
+    [i,j,v,nr,nc] = spfind(m);
+    S = sparse(i,j,randn(size(v)),nr,nc);
+  elseif nargin == 3
+    k = round(d*m*n);
+    ## XXX FIXME XXX need a better algorithm for large sparse matrices
+    ## E.g., find p such that p samples out of m*n things with replacement
+    ## yields on average k unique values, and use 
+    ##    idx=unique(fix(rand(1,p)*m*n))+1
+    [v,idx] = sort(rand(m*n,1));
+    j = floor((idx(1:k)-1)/m);
+    i = idx(1:k) - j*m;
+    if isempty(i)
+      S = sparse(m,n);
+    else
+      S = sparse(i,j+1,randn(k,1),m,n);
+    endif
+  else
+    usage("sprandn(m,n,density) OR sprandn(S)");
+  endif
+endfunction

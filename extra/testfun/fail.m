0ee8e1ea810e2ee5414b3b076509ad5e3e4a3127
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-01-29 22:03:42 +0000

    Better argument checking and error reporting.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1273 fc592f45-0881-4044-91ff-c96495a54512

--- extra/testfun/fail.m
+++ extra/testfun/fail.m
@@ -13,19 +13,26 @@
 
 ## PKG_ADD mark_as_command fail
 function ret=fail(code,pattern)
+  if nargin < 1 || nargin > 2
+    usage("fail('code' [, 'pattern'])");
+  end
+
   ## allow assert(fail())
   if nargout, ret=1; end  
 
   ## don't test failure if evalin doesn't exist
-  if !exists('evalin'), return; end
+  if !exist('evalin'), return; end
 
   ## perform the test
   try
     evalin("caller",[code,";"]);
     msg = "expected error but got none";
   catch
-    msg = sprintf("expected %s\nbut got %s",pattern,lasterr);
-    if !isempty(regexp(pattern,lasterr)), return; end
+    err=lasterr;
+    ## allow fail('code')
+    if nargin<2 || isempty(pattern), return; end
+    msg = sprintf("expected message <%s>\nbut got <%s>",pattern,err(1:end-1));
+    if !isempty(regexp(pattern,err(1:end-1))), return; end
   end
 
   ## if we get here, then code didn't fail or error didn't match
@@ -36,7 +43,7 @@ end
 %! a = [1,2];
 %!test fail ('a*[2,3]','nonconformant')
 %!test fail ("fail('a*[2;3]','nonconformant')","expected error but got none")
-%!test fail ("fail('a*[2,3]','usage:')","expected usage:.*but got.*nonconformant")
+%!test fail ("fail('a*[2,3]','usage:')","expected .*usage:.*but got.*nonconformant")
 
 ## Comment out the following tests if you don't want to see what
 ## errors look like

713a2249a10b7152502aade9c2fbae55be9ea84e
tpikonen <tpikonen@fc592f45-0881-4044-91ff-c96495a54512>
2004-02-05 13:03:08 +0000

    Fixed a bug caused by change in glob.m return value type in octave-2.1.53, typo fixes.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1291 fc592f45-0881-4044-91ff-c96495a54512

--- extra/graceplot/toggle_grace_use.m
+++ extra/graceplot/toggle_grace_use.m
@@ -1,3 +1,18 @@
+## Copyright (C) 2003  Rafael Laboissiere
+## Modified: Joao Cardoso
+## Modified to work with Grace: Teemu Ikonen <tpikonen@pcu.helsinki.fi>
+## Created on: Sun Oct 18 22:03:10 CEST 1998
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 2 of the License, or (at your
+## option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+
 ## usage: toggle_grace_use
 ##
 ## Use this script to activate/deactivate the default library for the
@@ -6,31 +21,7 @@
 ## functions. toggle_grace_use post-/pre-pends to LOADPATH the path for
 ## the Grace functions in successive calls.
 
-## File: toggle_grace_use.m
-## Author: Rafael Laboissiere <rafael@debian.org>
-## Modified: Joao Cardoso
-## Modified to work with Grace: Teemu Ikonen <tpikonen@pcu.helsinki.fi>
-## Created on: Sun Oct 18 22:03:10 CEST 1998
-## Last modified on: 25.7.2003
-##
-## Copyright (C) 2003  Rafael Laboissiere
-##
-## This file is free software, distributable under the GPL. No
-## warranties, use it at your own risk.  It  was originally written for
-## inclusion in the Debian octave-grace package.
-
 1;
-
-#if ! exist ("use_grace_state")
-#  global use_grace_state
-#  use_grace_state = "on";
-#else
-#  if strcmp (use_grace_state, "on")
-#    use_grace_state = "off";
-#  else
-#    use_grace_state = "on";
-#  endif
-#endif
 
 global use_grace_state
 
@@ -64,7 +55,7 @@ cd (use_grace_path);
 
 grace_cmds = glob ("*.m");
 for gr_ind = 1:size(grace_cmds,1), 
-  clear (strrep (deblank(grace_cmds(gr_ind,:)), ".m", ""));
+  clear (strrep (deblank(grace_cmds{gr_ind,1}), ".m", ""));
 end
 
 cd (use_grace_lcd);

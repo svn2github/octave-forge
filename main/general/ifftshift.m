093100efc559842707ec4409e1be9415cba00b66
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-04-24 02:15:23 +0000

    [for David Bateman] NDArray support
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1385 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/ifftshift.m
+++ main/general/ifftshift.m
@@ -24,26 +24,46 @@
 ## Created: July 1997
 ## Adapted-By: jwe
 ## Modified-By: Paul Kienzle, converted from fftshift
+## Modified-By: David Bateman, add NDArray capability and option dim arg
 
 function retval = ifftshift (V)
 
   retval = 0;
 
-  if (nargin != 1)
-    usage ("usage: ifftshift (X)");
+  if (nargin != 1 && nargin != 2)
+    usage ("usage: ifftshift (X, dim)");
   endif
 
-  if (is_vector (V))
-    x = length (V);
-    xx = floor (x/2);
-    retval = V([xx+1:x, 1:xx]);
-  elseif (is_matrix (V))
-    [x, y] = size (V);
-    xx = floor (x/2);
-    yy = floor (y/2);
-    retval = V([xx+1:x, 1:xx], [yy+1:y, 1:yy]);
+  if (nargin == 2)
+    if (!isscalar (dim))
+      error ("ifftshift: dimension must be an integer scalar");
+    endif
+    nd = ndims (V);
+    sz = size (V);
+    sz2 = floor (sz(dim) / 2);
+    idx = cell ();
+    for i=1:nd
+      idx {i} = 1:sz(i);
+    endfor
+    idx {dim} = [sz2+1:sz(dim), 1:sz2];
+    retval = V (idx{:});
   else
-    error ("ifftshift: expecting vector or matrix argument");
+    if (isvector (V))
+      x = length (V);
+      xx = floor (x/2);
+      retval = V([xx+1:x, 1:xx]);
+    elseif (ismatrix (V))
+      nd = ndims (V);
+      sz = size (V);
+      sz2 = floor (sz ./ 2);
+      idx = cell ();
+      for i=1:nd
+        idx{i} = [sz2(i)+1:sz(i), 1:sz2(i)];
+      endfor
+      retval = V (idx{:});
+    else
+      error ("ifftshift: expecting vector or matrix argument");
+    endif
   endif
 
 endfunction

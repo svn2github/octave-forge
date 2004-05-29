b896a0eb81b9ac167ab48effcb81161bfab7ede9
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-05-29 21:59:40 +0000

    Fix header so help works
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1441 fc592f45-0881-4044-91ff-c96495a54512

--- main/miscellaneous/map.m
+++ main/miscellaneous/map.m
@@ -1,65 +1,65 @@
-function return_type = map (fun_str,data_struct,varargin)
+## Copyright (C) 2003 Tomer Altman
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public
+## License as published by the Free Software Foundation;
+## either version 2, or (at your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied
+## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+## PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public
+## License along with Octave; see the file COPYING.  If not,
+## write to the Free Software Foundation, 59 Temple Place -
+## Suite 330, Boston, MA 02111-1307, USA.
 
-  ## Copyright (C) 2003 Tomer Altman
-  ##
-  ## Octave is free software; you can redistribute it and/or
-  ## modify it under the terms of the GNU General Public
-  ## License as published by the Free Software Foundation;
-  ## either version 2, or (at your option) any later version.
-  ##
-  ## Octave is distributed in the hope that it will be useful,
-  ## but WITHOUT ANY WARRANTY; without even the implied
-  ## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  ## PURPOSE.  See the GNU General Public License for more
-  ## details.
-  ##
-  ## You should have received a copy of the GNU General Public
-  ## License along with Octave; see the file COPYING.  If not,
-  ## write to the Free Software Foundation, 59 Temple Place -
-  ## Suite 330, Boston, MA 02111-1307, USA.
-
-  ## usage: result = map ( FUN_STR, ARG1, ... )
-  ##
-  ## map, like LISP's ( & numerous other language's ) function for
-  ## iterating the result of a function applied to each of the data
-  ## structure's elements in turn. The results are stored in the
-  ## corresponding input's place. For now, just will work with cells and
-  ## matrices, but support for structs are intended for future versions.
-  ## Also, only "prefix" functions ( like "min(a,b,c,...)" ) are
-  ## supported.
-  ##
-  ## Example:
-  ##
-  ## octave> A
-  ## A 
-  ## {
-  ##   [1,1] = 0.0096243
-  ##   [2,1] = 0.82781
-  ##   [1,2] = 0.052571
-  ##   [2,2] = 0.84645
-  ## }
-  ## octave> B
-  ## B =
-  ## {
-  ##   [1,1] = 0.75563
-  ##   [2,1] = 0.84858
-  ##   [1,2] = 0.16765
-  ##   [2,2] = 0.85477
-  ## }
-  ## octave> map("min",A,B)
-  ## ans =
-  ## {
-  ##   [1,1] = 0.0096243
-  ##   [2,1] = 0.82781
-  ##   [1,2] = 0.052571
-  ##   [2,2] = 0.84645
-  ## }
+## usage: result = map ( FUN_STR, ARG1, ... )
+##
+## map, like LISP's ( & numerous other language's ) function for
+## iterating the result of a function applied to each of the data
+## structure's elements in turn. The results are stored in the
+## corresponding input's place. For now, just will work with cells and
+## matrices, but support for structs are intended for future versions.
+## Also, only "prefix" functions ( like "min(a,b,c,...)" ) are
+## supported.
+##
+## Example:
+##
+## octave> A
+## A 
+## {
+##   [1,1] = 0.0096243
+##   [2,1] = 0.82781
+##   [1,2] = 0.052571
+##   [2,2] = 0.84645
+## }
+## octave> B
+## B =
+## {
+##   [1,1] = 0.75563
+##   [2,1] = 0.84858
+##   [1,2] = 0.16765
+##   [2,2] = 0.85477
+## }
+## octave> map("min",A,B)
+## ans =
+## {
+##   [1,1] = 0.0096243
+##   [2,1] = 0.82781
+##   [1,2] = 0.052571
+##   [2,2] = 0.84645
+## }
  
-  ## Author: Tomer Altman
-  ## Keywords: map matrix cell 
-  ## Maintainer: Tomer Altman
-  ## Created: November 15, 2003
-  ## Version: 0.1
+## Author: Tomer Altman
+## Keywords: map matrix cell 
+## Maintainer: Tomer Altman
+## Created: November 15, 2003
+## Version: 0.1
+
+function return_type = map (fun_str,data_struct,varargin)
   
   if (nargin<2)
 

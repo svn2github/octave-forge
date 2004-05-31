aa0687fab2f9d21964cede15bf5dfc84b228b3e3
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-05-31 18:12:39 +0000

    Update documentation; provide access to the internal plot function
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1447 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/wpolyfit.m
+++ main/optim/wpolyfit.m
@@ -1,5 +1,5 @@
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{p}, @var{dp}] =} wpolyfit (@var{x}, @var{y}, @var{dy}, @var{n}, 'origin')
+## @deftypefn {Function File} {[@var{p}, @var{s}] =} wpolyfit (@var{x}, @var{y}, @var{dy}, @var{n}, 'origin')
 ## Return the coefficients of a polynomial @var{p}(@var{x}) of degree
 ## @var{n} that minimizes
 ## @iftex
@@ -16,8 +16,8 @@
 ## on the observations @var{y} if present are given in @var{dy}.
 ##
 ## The returned value @var{p} contains the polynomial coefficients 
-## suitable for use in the function polyval.  The uncertainty in
-## the coefficients is returned in @var{dp}.
+## suitable for use in the function polyval.  The structure @var{s} returns
+## information necessary to compute uncertainty in the model.
 ##
 ## If no output arguments are requested, then wpolyfit plots the data,
 ## the fitted line and polynomials defining the standard error range.
@@ -27,31 +27,38 @@
 ##
 ## To compute the predicted values of y with uncertainty use
 ## @example
-##     y = polyval(p,x);
-##     dy = sqrt(polyval(dp.^2, x.^2));
+## [y,dy] = polyconf(p,x,s,'ci');
 ## @end example
+## You can see the effects of different confidence intervals and
+## prediction intervals by calling the wpolyfit internal plot
+## function with your fit:
+## @example
+## feval('wpolyfit:plt',x,y,dy,p,s,0.05,'pi')
+## @end example
+## Use @var{dy}=[] if uncertainty is unknown.
+##
+## You can estimate the uncertainty in the polynomial coefficients 
+## themselves using
+## @example
+## dp = sqrt(sumsq(inv(s.R'))'/s.df)*s.normr;
+## @end example
+## but the high degree of covariance amongst them makes this a questionable
+## operation.
 ##
 ## Example
 ## @example
-##     x = linspace(0,4,20);
-##     dy = (1+rand(size(x)))/2;
-##     y = polyval([2,3,1],x) + dy.*randn(size(x));
-##     wpolyfit(x,y,dy,2);
+## x = linspace(0,4,20);
+## dy = (1+rand(size(x)))/2;
+## y = polyval([2,3,1],x) + dy.*randn(size(x));
+## wpolyfit(x,y,dy,2);
 ## @end example
 ##
-## Note that the polynomial coefficients and uncertainties don't 
-## exactly match those computed from a monte carlo simulation of 
-## fits to the data.  You can see this for yourself:
-## @example
-##     n=1000; p=zeros(3,n);
-##     for i=1:n, p(:,i)=polyfit(x,y+randn(size(y)).*dy,2); end
-##     printf("%15g %15g\n", [mean(p'); std(p')]);
-## @end example
-##
-## Farebrother, RW (1988). Linear least squares computations.
-## New York: Marcel Dekker, Inc.
+## Hocking, RR (2003). Methods and Applications of Linear Models.
+## New Jersey: John Wiley and Sons, Inc.
 ##
 ## @end deftypefn
+##
+## @seealso{polyfit,polyconf}
 
 ## This program is in the public domain.
 ## Author: Paul Kienzle <pkienzle@users.sf.net>
@@ -133,66 +140,59 @@ function [p_out, s] = wpolyfit (varargin)
   endif
 
   if nargout == 0
-
-    if iscomplex(p)
-      printf("%32s %15s\n", "Coefficient", "Uncertainty");
-      printf("%15g %+15gi %15g\n",[real(p(:)),imag(p(:)),dp(:)]');
-      # XXX FIXME XXX how to plot complex valued functions?
-      # Maybe using hue for phase and saturation for magnitude
-      # e.g., Frank Farris (Santa Cruz University) has this:
-      # http://www.maa.org/pubs/amm_complements/complex.html
-      # Could also look at the book
-      #   Visual Complex Analysis by Tristan Needham, Oxford Univ. Press
-      # but for now we punt
-      return
-    endif
-
-    ## decorate the graph
-    grid('on');
-    xlabel('abscissa X'); ylabel('data Y');
-    title('Least-squares Polynomial Fit with Error Bounds');
-
-    ## draw fit with estimated error bounds
-    xf = linspace(min(x),max(x),150)';
-    [yf,dyf] = polyconf(p,xf,s,'ci');
-    plot(xf,yf+dyf,"g.;;", xf,yf-dyf,"g.;;", xf,yf,"g-;fit;");
-
-    ## plot the data
-    hold on;
-    if (isempty(dy))
-      plot(x,y,"x;data;");
-    else
-      if isscalar(dy), dy = ones(size(y))*dy; end
-      errorbar (x, y, dy, "~;data;");
-    endif
-    hold off;
-
-    ## display p as a two column vector
-    printf("Polynomial: %s\n", polyout(p,'x'));
-
-    if strcmp(deblank(input('See residuals? [y,n] ','s')),'y')
-      clf;
-      if (isempty(dy))
-	plot(x,y-polyval(p,x),"x;data;");
-      else
-        errorbar(x,y-polyval(p,x),dy, '~;data;');
-      endif
-      hold on;
-      grid on;
-      ylabel('Residuals');
-      xlabel('abscissa X'); 
-      plot(xf,dyf,'g.;;',xf,-dyf,'g.;;');
-      hold off;
-    endif
-
+    printf("Polynomial: %s (chi^2=%g)\n", polyout(p,'x'), s.normr/s.df);
+    plt(x,y,dy,p,s,'ci');
   else
-
-    ## return values as column vector like polyfit
     p_out = p;
-
   endif
 
-endfunction
+function plt(x,y,dy,p,s,varargin)
+
+  if iscomplex(p)
+    # XXX FIXME XXX how to plot complex valued functions?
+    # Maybe using hue for phase and saturation for magnitude
+    # e.g., Frank Farris (Santa Cruz University) has this:
+    # http://www.maa.org/pubs/amm_complements/complex.html
+    # Could also look at the book
+    #   Visual Complex Analysis by Tristan Needham, Oxford Univ. Press
+    # but for now we punt
+    return
+  end
+
+  ## decorate the graph
+  grid('on');
+  xlabel('abscissa X'); ylabel('data Y');
+  title('Least-squares Polynomial Fit with Error Bounds');
+
+  ## draw fit with estimated error bounds
+  xf = linspace(min(x),max(x),150)';
+  [yf,dyf] = polyconf(p,xf,s,varargin{:});
+  plot(xf,yf+dyf,"g.;;", xf,yf-dyf,"g.;;", xf,yf,"g-;fit;");
+
+  ## plot the data
+  hold on;
+  if (isempty(dy))
+    plot(x,y,"x;data;");
+  else
+    if isscalar(dy), dy = ones(size(y))*dy; end
+    errorbar (x, y, dy, "~;data;");
+  endif
+  hold off;
+
+  if strcmp(deblank(input('See residuals? [y,n] ','s')),'y')
+    clf;
+    if (isempty(dy))
+      plot(x,y-polyval(p,x),"x;data;");
+    else
+      errorbar(x,y-polyval(p,x),dy, '~;data;');
+    endif
+    hold on;
+    grid on;
+    ylabel('Residuals');
+    xlabel('abscissa X'); 
+    plot(xf,dyf,'g.;;',xf,-dyf,'g.;;');
+    hold off;
+  endif
 
 %!demo % #1  
 %!     x = linspace(0,4,20);
@@ -205,5 +205,5 @@ endfunction
 %!     noise = ( randn(size(x)) + i*randn(size(x)) )/10;
 %!     P = [2-i,3,1+i];
 %!     y = polyval(P,x) + noise;
-%!     [PEST,DELTA]=wpolyfit(x,y,2)
+%!     wpolyfit(x,y,2)
 

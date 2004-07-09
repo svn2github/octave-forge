42c35d839c6331a4fa1131118abc4c67acb4428a
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-07-09 03:58:52 +0000

    Set style on gplot command so clf is no longer required after quiver.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1562 fc592f45-0881-4044-91ff-c96495a54512

--- main/plot/quiver.m
+++ main/plot/quiver.m
@@ -15,37 +15,35 @@
 ## Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 ## 02111-1307, USA.
 
-## usage: quiver (x, y, u, v)
-## usage: quiver (u, v)
+## quiver (x, y, u, v)
+##   Plot the (u,v) components of a vector field in an (x,y) meshgrid.
+##   If the grid is uniform, you can specify x and y as vectors.
 ##
-## Plot the (u,v) components of a vector field in an (x,y) meshgrid.
-## If the grid is uniform, you can specify x and y as vectors.
-## You can plot vectors and lines simultaneously, but the lines
-## must be plotted with style '-' even though that is the default.
-##
-## Use clf after you are done plotting vector fields.
+## quiver (u, v)
+##   Assume grid is uniform on (1:m,1:n) where [m,n]=size(u).
 ##
 ## Example
 ##
 ##   [x,y] = meshgrid(1:2:20);
 ##   quiver(x,y,sin(2*pi*x/10),sin(2*pi*y/10))
 ##
+## The underlying gnuplot command is much richer than what we provide
+## through quiver.  With four column data M=[x,y,u,v], you can plot
+## a vector field using 'gplot M with vector ...'. 
+##
 ## See also: plot, semilogx, semilogy, loglog, polar, mesh, contour,
 ##           bar, stairs, gplot, gsplot, replot, xlabel, ylabel, title
 
 ## Author: Roberto A. F. Almeida <roberto@calvin.ocfis.furg.br>
-## 2001-02-25 Paul Kienzle
-##     * change parameter order to correspond to matlab
-##     * allow vector x,y,  defaulting to 1:n, 1:m for mxn u
-##     * vectorize
+## Rewrite: Paul Kienzle <pkienzle@users.sf.net>
 
 function quiver (x, y, u, v)
 
-  if (nargin != 2 && nargin != 4)
-    usage ("quiver (x, y, u, v)");
+  if nargin != 2 && nargin != 4
+    usage ("quiver ([x, y,] u, v)");
   endif
 
-  if (nargin == 2)
+  if nargin < 4
     u = x; v = y; x = 1:columns(u); y= 1:rows(u);
   endif
 
@@ -61,9 +59,8 @@ function quiver (x, y, u, v)
     error ("quiver: x, y, u, v must have the same shape");
   endif
 
-  data = [ x(:), y(:), u(:), v(:) ];
-  gset style data vector
-  gplot data title ""
+  M = [ x(:), y(:), u(:), v(:) ];
+  gplot M w v t ""
 endfunction
 
 %!demo
@@ -74,8 +71,7 @@ endfunction
 %! axis('equal');
 %! x=linspace(0,3,80); y=sin(2*pi*x); theta=2*pi*x+pi/2;
 %! quiver(x,y,sin(theta)/10,cos(theta)/10);
-%! hold on; plot(x,y,'-;;'); hold off;
+%! hold on; plot(x,y,';;'); hold off;
 
 %!demo
-%! % Reset the graph after plotting quivers
-%! clf
+%! clf; % reset axis('equal') setting

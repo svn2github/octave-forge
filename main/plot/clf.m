6700f6f66e309e92b7d5423251207abdb920658a
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-07-09 02:10:49 +0000

    Reset data style to lines after resetting gnuplot
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1560 fc592f45-0881-4044-91ff-c96495a54512

--- main/plot/clf.m
+++ main/plot/clf.m
@@ -1,20 +1,11 @@
-## Copyright (C) 2000 Paul Kienzle
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+## clf
+##   Clear the current figure and any associated annotations.
+##   Unlike clg, clf clears text, arrows, etc.  The terminal
+##   type may be wrong after clearing.
 
-## Clear the current figure and any associated annotations.
-
-clg;
-graw "reset;\r"
+## Author: Paul Kienzle
+## This program is public domain
+function clf
+ clg;
+ graw "reset;\r";
+ gset data style lines;

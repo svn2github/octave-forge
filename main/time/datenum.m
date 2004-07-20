5974765e8e681c6f6351aef51dbf658bec3782e6
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-07-20 07:34:08 +0000

    Rewrite for speed and extended range.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1582 fc592f45-0881-4044-91ff-c96495a54512

--- main/time/datenum.m
+++ main/time/datenum.m
@@ -1,37 +1,36 @@
-## Copyright (C) 2000 Paul Kienzle
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {} datenum(Y, M, D [, h , m [, s]])
 ## @deftypefnx {Function File} {} datenum('date' [, P])
-## Returns the specified local time as a number of days since Jan 1, 0000.
-## By this reckoning, Jan 1, 1970 is day number 719529.  The fractional
-## portion, corresponds to the portion of the specified day.
+## Returns the specified local time as a day number, with Jan 1, 0000
+## being day 1. By this reckoning, Jan 1, 1970 is day number 719529.  
+## The fractional portion, corresponds to the portion of the specified day.
 ##
-## Note: 32-bit architectures only handle times between Dec 14, 1901 
-## and Jan 19, 2038, with special handling for 0000-01-01.  datenum
-## returns -1 in case of a range error.
+## Years can be negative and/or fractional.
+## Months below 1 are considered to be January.
+## Days of the month start at 1.
+## Days beyond the end of the month go into subsequent months.
+## Days before the beginning of the month go to the previous month.
+## Days can be fractional.
+##
+## XXX WARNING XXX this function does not attempt to handle Julian
+## calendars so dates before Octave 15, 1582 are wrong by as much
+## as eleven days.  Also be aware that only Roman Catholic countries
+## adopted the calendar in 1582.  It took until 1924 for it to be 
+## adopted everywhere.  See the Wikipedia entry on the Gregorian 
+## calendar for more details.
+##
+## XXX WARNING XXX leap seconds are ignored.  A table of leap seconds
+## is available on the Wikipedia entry for leap seconds.
 ##
 ## @seealso{date,clock,now,datestr,datevec,calendar,weekday}
 ## @end deftypefn
 
-## 2001-08-30 Paul Kienzle <pkienzle@users.sf.net>
-## * make it independent of time zone
+## Paul Kienzle
+## This program is granted to the public domain.
 
 function n = datenum(Y,M,D,h,m,s)
+  persistent monthstart = cumsum([0,31,28,31,30,31,30,31,31,30,31,30]);
+
   if nargin == 0 || (nargin > 2  && isstr(Y)) || nargin > 6
     usage("n=datenum('date' [, P]) or n=datenum(Y, M, D [, h, m [, s]])");
   endif
@@ -39,39 +38,17 @@ function n = datenum(Y,M,D,h,m,s)
     if nargin < 2, M=[]; endif
     [Y,M,D,h,m,s] = datevec(Y,M);
   else
-    if nargin < 6, s = zeros(size(Y)); endif
+    if nargin < 6, s = 0; endif
     if nargin < 5, m = s; endif
     if nargin < 4, h = s; endif
   endif
 
-  n = zeros(size(Y));
-
-  for i=1:prod(size(Y))
-    tm.usec = 1e6*rem(s(i),1);
-    tm.sec = floor(s(i));
-    tm.min = m(i);
-    tm.hour = h(i);
-    tm.mday = D(i);
-    tm.mon = M(i)-1;
-    tm.year = Y(i)-1900;
-    tm.zone = "CUT";
-    tm.wday = 0;
-    tm.yday = 0;
-    tm.isdst = 0;
-    if (Y(i) == 0 && M(i) == 1 && d(i) == 1)
-      n(i) = (h(i)*3600 + m(i)*60 + s(i))/86400;
-    else
-      t = mktime(tm);
-      if (t==-1 && Y(i) != 1969)
-	n(i) = -1;
-      else
-      	n(i) = t / 86400 + 719529;
-      endif
-    endif
-  endfor
-
-  ## Correct for time zone
-  n -= mktime(gmtime(0))/86400;
+  M(M<1) = 1;
+  Y += floor((M-1)/12);
+  M = mod(M-1,12)+1;
+  n = 365*Y + ceil(Y/4) - ceil(Y/100) + ceil(Y/400) + monthstart(M) + ...
+	D + (h+(m+s/60)/60)/24;
+  n += mod(Y,4)==0 & (mod(Y,100)~=0 | mod(Y,400)==0) & M>2;
 endfunction
 
 %!assert(datevec(datenum(2003,11,28)),[2003,11,28,0,0,0])

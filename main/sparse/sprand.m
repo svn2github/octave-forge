6b86c29a2c2fd0b7d698e281ebe46ba9a22babbf
przykry2004 <przykry2004@fc592f45-0881-4044-91ff-c96495a54512>
2004-09-27 15:26:16 +0000

    Used Paul's hint to allow larger random matrices, at the price of sometimes lower density than desired
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1787 fc592f45-0881-4044-91ff-c96495a54512

--- main/sparse/sprand.m
+++ main/sparse/sprand.m
@@ -3,8 +3,8 @@
 ## Generate a random sparse matrix.  m and n give the dimensions, d
 ## gives the density (between 0 and 1).
 ##
-## Note: this will fail for large m x n matrices, even if they
-## are very sparse.
+## Note: sometimes the actual density of S may be a bit smaller than d. This is
+## unlikely to happen for large really sparse matrices.
 ##
 ## S = sprand(Q)
 ##
@@ -13,18 +13,27 @@
 
 ## This program is public domain
 ## Author: Paul Kienzle <pkienzle@users.sf.net>
+##
+## Changelog:
+##
+## Piotr Krzyzanowski <przykry2004@users.sf.net>
+## 	2004-09-27	use Paul's hint to allow larger random matrices
+##			at the price of sometimes lower density than desired
 
 function S = sprand(m,n,d)
   if nargin == 1
     [i,j,v,nr,nc] = spfind(m);
     S = sparse(i,j,rand(size(v)),nr,nc);
   elseif nargin == 3
-    k = round(d*m*n);
-    ## XXX FIXME XXX need a better algorithm for large sparse matrices
-    ## E.g., find p such that p samples out of m*n things with replacement
-    ## yields on average k unique values, and use 
-    ##    idx=unique(fix(rand(1,p)*m*n))+1
-    [v,idx] = sort(rand(m*n,1));
+    mn = n*m;
+    k = round(d*mn); # how many entries in S would be satisfactory?
+    idx=unique(fix(rand(k+10,1)*mn))+1; # idx contains random numbers in [1,mn]
+    		# generate 10 more random values than necessary
+		# in order to reduce the probability that there are less than k
+		# distinct values;
+    		# maybe a better strategy could be used
+     		# but I don't think it's worth the price
+    k = min(length(idx),k);  # actual number of entries in S
     j = floor((idx(1:k)-1)/m);
     i = idx(1:k) - j*m;
     if isempty(i)

7fb306100ec35a80bd355fef9911e6947b418978
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2004-10-20 09:14:36 +0000

    indicate it's a script, with a "1;"
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1810 fc592f45-0881-4044-91ff-c96495a54512

--- main/econometrics/mle_example.m
+++ main/econometrics/mle_example.m
@@ -51,6 +51,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  
+
+1;
 # Example to show how to use MLE functions
 
 # Example likelihood function, no score

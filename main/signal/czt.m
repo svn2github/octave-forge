8675953f7dee513f7b4e04601b233b096382059a
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2004-11-16 05:17:01 +0000

    [with Danial Gunyan] Replace with correct implementation and add tests.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1823 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/czt.m
+++ main/signal/czt.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2000 Paul Kienzle
+## Copyright (C) 2004 Daniel Gunyan
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -23,37 +23,62 @@
 ##
 ## To evaluate the frequency response for the range f1 to f2 in a signal
 ## with sampling frequency Fs, use the following:
-##     m = 32;                          ## number of points desired
-##     w = exp(-2i*pi*(f2-f1)/(m*Fs));  ## freq. step of f2-f1/m
-##     a = exp(2i*pi*f1/Fs);            ## starting at frequency f1
+##     m = 32;                               ## number of points desired
+##     w = exp(-j*2*pi*(f2-f1)/((m-1)*Fs));  ## freq. step of f2-f1/m
+##     a = exp(j*2*pi*f1/Fs);                ## starting at frequency f1
 ##     y = czt(x, m, w, a);
 ##
 ## If you don't specify them, then the parameters default to a fourier 
 ## transform:
-##     m=length(x), w=exp(2i*pi/m), a=1
-## Because it is computed with three FFTs, this will be faster than
-## computing the fourier transform directly for large m (which is
-## otherwise the best you can do with fft(x,n) for n prime).
+##     m=length(x), w=exp(-j*2*pi/m), a=1
+##
+## If x is a matrix, the transform will be performed column-by-column.
 
-## TODO: More testing---particularly when m+N-1 approaches a power of 2
-## TODO: Consider treating w,a as f1,f2 expressed in radians if w is real
+## Algorithm (based on Oppenheim and Schafer, "Discrete-Time Signal
+## Processing", pp. 623-628):
+##   make chirp of length -N+1 to max(N-1,M-1)
+##     chirp => w^([-N+1:max(N-1,M-1)]^2/2)
+##   multiply x by chirped a and by N-elements of chirp, and call it g
+##   convolve g with inverse chirp, and call it gg
+##     pad ffts so that multiplication works
+##     ifft(fft(g)*fft(1/chirp))
+##   multiply gg by M-elements of chirp and call it done
+
 function y = czt(x, m, w, a)
   if nargin < 1 || nargin > 4, usage("y=czt(x, m, w, a)"); endif
-  if nargin < 2 || isempty(m), m = length(x); endif
-  if nargin < 3 || isempty(w), w = exp(2i*pi/m); endif
-  if nargin < 4 || isempty(a), a = 1; endif
 
-  N = length(x);
-  if (columns(x) == 1)
-    k = [0:m-1]';
-    Nk = [-(N-1):m-2]';
-  else
-    k = [0:m-1];
-    Nk = [-(N-1):m-2];
-  endif
-  nfft = 2^nextpow2(min(m,N)+length(Nk)-1); 
-  Wk2 = w.^(-(Nk.^2)/2);
-  AWk2 = (a.^-k) .* (w.^((k.^2)/2));
-  y = ifft(fft(postpad(Wk2,nfft)).*fft(postpad(x,nfft).*postpad(AWk2,nfft)));
-  y = w.^((k.^2)/2).*y(1+N:m+N);
+  [row, col] = size(x);
+  if row == 1, x = x(:); col = 1; endif
+
+  if nargin < 2 || isempty(m), m = length(x(:,1)); endif
+  if length(m) > 1, error("czt: m must be a single element\n"); endif
+  if nargin < 3 || isempty(w), w = exp(-2*j*pi/m); endif
+  if nargin < 4 || isempty(a), a = 1; endif
+  if length(w) > 1, error("czt: w must be a single element\n"); endif
+  if length(a) > 1, error("czt: a must be a single element\n"); endif
+
+  ## indexing to make the statements a little more compact
+  n = length(x(:,1));
+  N = [0:n-1]'+n;
+  NM = [-(n-1):(m-1)]'+n;
+  M = [0:m-1]'+n;
+
+  nfft = 2^nextpow2(n+m-1); # fft pad
+  W2 = w.^(([-(n-1):max(m-1,n-1)]'.^2)/2); # chirp
+
+  for idx = 1:col
+    fg = fft(x(:,idx).*(a.^-(N-n)).*W2(N), nfft);
+    fw = fft(1./W2(NM), nfft);
+    gg = ifft(fg.*fw, nfft);
+
+    y(:,idx) = gg(M).*W2(M);
+  endfor
+
+  if row == 1, y = y.'; endif
 endfunction
+
+%!shared x
+%! x = [1,2,4,1,2,3,5,2,3,5,6,7,8,4,3,6,3,2,5,1];
+%!assert(fft(x),czt(x),10000*eps);
+%!assert(fft(x'),czt(x'),10000*eps);
+%!assert(fft([x',x']),czt([x',x']),10000*eps);

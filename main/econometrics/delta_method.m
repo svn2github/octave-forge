8008d679cbe300443526f6f2a313c71e9da48733
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2005-02-09 16:19:23 +0000

    cleaning and beginnings of help messages
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1879 fc592f45-0881-4044-91ff-c96495a54512

--- main/econometrics/delta_method.m
+++ main/econometrics/delta_method.m
@@ -1,5 +1,4 @@
 # Copyright (C) 2003,2004  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,42 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
-# Copyright (C) 2003  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
-# The GPL license is in the file COPYING
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
-# Copyright (C) 2003  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
-# The GPL license is in the file COPYING
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 # Computes Delta method mean and covariance of a nonlinear
 # transformation defined by "func"
 function [theta_transf, vartheta] = delta_method(func, theta, otherargs, vartheta)
--- main/econometrics/parameterize.m
+++ main/econometrics/parameterize.m
@@ -1,42 +1,5 @@
 # Copyright (C) 2003,2004  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
-# Copyright (C) 2003  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
-# The GPL license is in the file COPYING
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
-# Copyright (C) 2003  Michael Creel michael.creel@uab.es
-# under the terms of the GNU General Public License.
-# The GPL license is in the file COPYING
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -51,12 +14,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+
+# usage: theta = parameterize(theta, otherargs)
+# 
 # This is an empty function, provided so that
 # delta_method will work as is. Replace it with
 # the parameter transformations your models use.
 # Note: you can let "otherargs" contain the model
 # name so that this function can do parameterizations
 # for a variety of models
+
 function theta = parameterize(theta, otherargs)
 
 endfunction	 

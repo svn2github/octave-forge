2ca5aa597ca9cc6263b4b95f52bf132b856e0b54
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-02-28 01:55:26 +0000

    Remove dd/mm forms from tests.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1911 fc592f45-0881-4044-91ff-c96495a54512

--- main/time/datesplit.m
+++ main/time/datesplit.m
@@ -421,25 +421,21 @@ function [y, m, d, h, mi, s] = datesplit(ds, P)
 endfunction
 
 %!shared today
-%! today=datevec(now); % Some tests could fail around midnight!
+%! nowvec=datevec(now); % Some tests could fail around midnight!
 %!assert (datevec("07-Sep-2000 15:38:09"),[2000,9,7,15,38,9]);
 %!assert (datevec("07-Sep-2000"),[2000,9,7,0,0,0]);
-%!assert (datevec("09/07/00"),[2000,9,7,0,0,0]);
-%!assert (datevec("Sep"),[today(1),9,7,0,0,0]);
-%!assert (datevec("09/13"),[today(1),9,13,0,0,0]);
+%!#ambiguous assert (datevec("09/07/00"),[2000,9,7,0,0,0]);
+%!assert (datevec("Sep"),[nowvec(1),9,7,0,0,0]);
+%!#ambiguous assert (datevec("09/13"),[nowvec(1),9,13,0,0,0]);
 %!assert (datevec("2000"),[2000,1,1,0,0,0]);
 %!assert (datevec("Sep00"),[2000,9,1,0,0,0]);
-%!assert (datevec("15:38:09"),[today(1:3),15,38,9]);
-%!assert (datevec("03:38:09 PM"),[today(1:3),15,38,9]);
-%!assert (datevec("15:38"),[today(1:3),15,38,0]);
-%!assert (datevec("3:38 PM"),[today(1:3),15,38,0]);
+%!assert (datevec("15:38:09"),[nowvec(1:3),15,38,9]);
+%!assert (datevec("03:38:09 PM"),[nowvec(1:3),15,38,9]);
+%!assert (datevec("15:38"),[nowvec(1:3),15,38,0]);
+%!assert (datevec("3:38 PM"),[nowvec(1:3),15,38,0]);
 %!assert (datevec("Q3-00"),[2000,7,1,0,0,0]);
-%!assert (datevec("13/03"),[today(1),3,13,0,0,0]);
-%!assert (datevec("13/03/95"),[1995,3,13,0,0,0]);
 %!assert (datevec("Mar.03.1962 13:53:06"),[1962,3,3,13,53,6]);
 %!assert (datavec("03/13/1962"),[1962,3,13,0,0,0]);
-%!assert (datavec("12/03/1962"),[1962,12,3,0,0,0]);
-%!assert (datavec("95/03/13"),[1995,3,13,0,0,0]);
 %!assert (datevec("1995/03/13"),[1995,3,13,0,0,0]);
 %!assert (datevec("Q4-2132"),[2132,10,1,0,0,0]);
 %!assert (datevec("Mar2047"),[2047,3,1,0,0,0]);

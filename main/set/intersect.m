10c0736b56b4e971248b91b2b567bce5f7ad43f1
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-03-01 06:51:03 +0000

    [for Ben Hall] return ia and ib as well
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1923 fc592f45-0881-4044-91ff-c96495a54512

--- main/set/intersect.m
+++ main/set/intersect.m
@@ -21,29 +21,53 @@
 ## order. If @var{a} and @var{b} are both column vectors return a column
 ## vector, otherwise return a row vector.
 ##
+## @deftypefnx {Function File} {[@var{c}, @var{ia}, @var{ib}] = } intersect (@var{a}, @var{b})
+##
+## Return index vectors @var{ia} and @var{ib} such that @code{a(ia)==c} and
+## @code{b(ib)==c}.
+##
 ## @end deftypefn
 ## @seealso{unique, union, setxor, setdiff, ismember}
 
-function c = intersect(a,b)
+function [c,ia,ib] = intersect(a,b)
   if ( nargin != 2 )
     usage ("intersect (a, b)");
   endif
 
   if ( isempty (a) || isempty (b) )
-    c = [];
-  else
-
-    ## form a and b into sets
-    a = unique (a);
-    b = unique (b);
-    
-    ## keep duplicates
-    c = sort ([ a(:) ; b(:) ]);
-    n = length(c);
-    c = c (find (c(1:n-1) == c(2:n)));
-    if ( size (b, 1) == 1 || size (a, 1) == 1 )
-      c = c.';
-    endif
-
+    c = []; ia = []; ib = [];
+    return;
   endif
+
+  ## form a and b into sets
+  [a,ja] = unique (a);
+  [b,jb] = unique (b);
+
+  c = [ a(:); b(:) ];
+  [c, ic] = sort( c );              ## [a(:);b(:)](ic) == c
+  
+  ii = find( c(1:end-1) == c(2:end) );  
+
+  c  = c(ii);                       ## The answer
+  ia = ja(ic(ii));                  ## a(ia) == c
+  ib = jb(ic(ii+1) - length(a));    ## b(ib) == c
+
+    
+  if ( size (b, 1) == 1 || size (a, 1) == 1 )
+    c = c.';
+  endif
+
+
 endfunction
+
+
+%!# Test the routine for index vectors ia and ib
+%!test
+%! a = [3 2 4 5 7 6 5 1 0 13 13];
+%! b = [3 5 12 1 1 7];
+%! [c,ia,ib] = intersect(a,b);
+%! assert( c,[1 3 5 7]);
+%! assert(ia,[8 1 7 5]);
+%! assert(ib,[5 1 2 6]);
+%! assert(a(ia),c);
+%! assert(b(ib),c);

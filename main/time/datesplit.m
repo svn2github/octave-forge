5e86635a3a73978bb43ce3e6d2ebacde9a1b4507
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-03-23 19:22:43 +0000

    Fix some tests
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1946 fc592f45-0881-4044-91ff-c96495a54512

--- main/time/datesplit.m
+++ main/time/datesplit.m
@@ -425,7 +425,7 @@ endfunction
 %!assert (datevec("07-Sep-2000 15:38:09"),[2000,9,7,15,38,9]);
 %!assert (datevec("07-Sep-2000"),[2000,9,7,0,0,0]);
 %!#ambiguous assert (datevec("09/07/00"),[2000,9,7,0,0,0]);
-%!assert (datevec("Sep"),[nowvec(1),9,7,0,0,0]);
+%!assert (datevec("Sep"),[nowvec(1),9,1,0,0,0]);
 %!#ambiguous assert (datevec("09/13"),[nowvec(1),9,13,0,0,0]);
 %!assert (datevec("2000"),[2000,1,1,0,0,0]);
 %!assert (datevec("Sep00"),[2000,9,1,0,0,0]);
@@ -433,11 +433,11 @@ endfunction
 %!assert (datevec("03:38:09 PM"),[nowvec(1:3),15,38,9]);
 %!assert (datevec("15:38"),[nowvec(1:3),15,38,0]);
 %!assert (datevec("3:38 PM"),[nowvec(1:3),15,38,0]);
-%!assert (datevec("Q3-00"),[2000,7,1,0,0,0]);
+%!assert (datevec("Q3-00"),[2000,9,30,23,59,59]);
 %!assert (datevec("Mar.03.1962 13:53:06"),[1962,3,3,13,53,6]);
 %!assert (datevec("03/13/1962"),[1962,3,13,0,0,0]);
 %!assert (datevec("1995/03/13"),[1995,3,13,0,0,0]);
-%!assert (datevec("Q4-2132"),[2132,10,1,0,0,0]);
+%!assert (datevec("Q4-2132"),[2132,12,31,23,59,59]);
 %!assert (datevec("Mar2047"),[2047,3,1,0,0,0]);
 %!assert (datevec("20470313"),[2047,3,13,0,0,0]);
 %!assert (datevec("20470313T132603"),[2047,3,13,13,26,3]);

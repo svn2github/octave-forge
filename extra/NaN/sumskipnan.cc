d667b012599dfdbb06619b424f47b12e61dbcaf6
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2005-04-26 22:42:05 +0000

    improved support for ND-arrays; DIM-related bug fixed
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@1963 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/sumskipnan.cc
+++ extra/NaN/sumskipnan.cc
@@ -17,7 +17,7 @@ DEFUN_DLD (sumskipnan, args, ,"OCT-implementation of SUMSKIPNAN\n")
 //   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 //
-// sumskipnan: sums all non-NaN values
+ 	// sumskipnan: adds all non-NaN values
 //
 // Input:
 // - array to sum
@@ -48,96 +48,92 @@ DEFUN_DLD (sumskipnan, args, ,"OCT-implementation of SUMSKIPNAN\n")
 
 
 { 
-#define ND 2
   int DIM = 0;  
-  int j, k1, k2, k3, D1, D2, D3;
-  int SZ[ND+1], SZ2[ND+1];
-  double aux,s,n,s2,s4;
+  int ND;
+  int j, k1, k2, k3, D1, D2, D3, ix1, ix2, N;
+  dim_vector SZ, SZ2;
+  double aux, s, n, s2, s4;
 
   octave_value_list retval; 
 
   if ( args.length() == 2 ) 
   {
 	  Matrix xin2( args(1).matrix_value() );
-	  DIM = (int)xin2(0);
+	  DIM = (int) xin2(0);
   }
-  else if ( args.length() > 2 ) 
+  else if ( args.length() > 3 ) 
   {
-    error("this version of matrixdemo only takes one argument");
+    error("SUMSKIPNAN.OCT takes at most 2 arguments");
     return retval;
   }
-  
+
   warning("SUMSKIPNAN.OCT supports only real double matrices.\n");
   
+	// only real double matrices are supported; arrays, integer or complex matrices are not supported (yet)
+  	NDArray xin1( args(0).matrix_value() );
+	ND = xin1.ndims();    
+	SZ = xin1.dims();
+	
     	for (j = 0; (DIM < 1) && (j < ND); j++) 
-		if (SZ[j]>1) DIM = j+1;
+		if (SZ(j)>1) DIM = j+1;
 
   	if (DIM < 1) DIM=1;		// in case DIM is still undefined 
 	
 	if ((DIM != 1) & (DIM !=2))
 		error("SUMSKIPNAN.OCT: DIM must be 1 or 2.\n");
 
-	// only real double matrices are supported; arrays, integer or complex matrices are not supported (yet)
-  	Matrix xin1( args(0).matrix_value() );
-  	SZ[0] = xin1.rows();
-  	SZ[1] = xin1.cols();
+	SZ2 = SZ; 
 
-	for (j=0; j<ND; j++)		// copy size of input;  
-		SZ2[j] = SZ[j]; 	
+   	for (j=0, D1=1; j<DIM-1; D1=D1*SZ(j++)); 	// D1 is the number of elements between two elements along dimension  DIM  
+	D2 = SZ(DIM-1);		// D2 contains the size along dimension DIM 	
+    	for (j=DIM, D3=1;  j<ND; D3=D3*SZ(j++)); 	// D3 is the number of blocks containing D1*D2 elements 
 
-  	SZ2[DIM-1] = 1;
+  	SZ2(DIM-1) = 1;
 
-   	for (j=0, D1=1; j<DIM-1; D1=D1*SZ2[j++]); 	// D1 is the number of elements between two elements along dimension  DIM  
-	D2 = SZ[DIM-1];		// D2 contains the size along dimension DIM 	
-    	for (j=DIM, D3=1;  j<ND; D3=D3*SZ2[j++]); 	// D3 is the number of blocks containing D1*D2 elements 
-
-	
 	// Currently, only matrices (2D-arrays) are supported. In future, also N-D arrays should be supported. 
-  	Matrix xout1(SZ2[0],SZ2[1]);
-  	Matrix xout2(SZ2[0],SZ2[1]);
-  	Matrix xout3(SZ2[0],SZ2[1]);
-//  	Matrix xout4(SZ2[0],SZ2[1]);
+  	NDArray xout1(SZ2);
+  	NDArray xout2(SZ2);
+	NDArray xout3(SZ2);
+//  	NDArray xout4(SZ2);
 
 	// OUTER LOOP: along dimensions > DIM
 	for (k3 = 0; k3<D3; k3++) 	
     	{
+		ix2 =  k3*D1;	// index for output 
+		ix1 = ix2*D2;	// index for input 
+
 		// Inner LOOP: along dimensions < DIM
-		for (k1 = 0; k1<D1; k1++) 	
+		for (k1 = 0; k1<D1; k1++, ix1++, ix2++) 	
 		{
-		  	s  = 0;
-		  	n  = 0;
-		  	s2 = 0;
-//		  	s4 = 0;
+		  	s  = 0.0;
+		  	N  = 0;
+		  	s2 = 0.0;
+//		  	s4 = 0.0;
 		
 			// LOOP  along dimension DIM
 	    		for (k2=0; k2<D2; k2++) 	
 			{
-				// this is an ugly solution, 
-				// to avoid this case check, one needs to know the memory layout of Matrix/Array
-				if (DIM==1)
-					aux = xin1(k2,k3);
-				else // if (DIM==2)
-					aux = xin1(k1,k2);
-				
-					
+				aux = xin1(ix1 + k2*D1);
+								
 				if (aux==aux)	// test for NaN
 				{ 
+					N++; 
 					s   += aux; 
-					n++; 
 					aux *= aux;
 					s2  += aux; 
 //					s4  += aux*aux; 
 				}	
  			}
-			xout1(k1,k3) = s;
-			xout2(k1,k3) = n;
-			xout3(k1,k3) = s2;
-//			xout4(k1,k3) = s4;
+			xout1(ix2) = s;
+			xout2(ix2) = (double) N;
+			xout3(ix2) = s2;
+//			xout4(ix2) = s4;
 		}
   	}  
-  	retval.append(octave_value(xout1));
+
+	retval.append(octave_value(xout1));
   	retval.append(octave_value(xout2));
   	retval.append(octave_value(xout3));
 //  	retval.append(octave_value(xout4));
-}
 
+}

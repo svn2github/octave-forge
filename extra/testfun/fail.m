d3bcccfeb4f0b86b059bd917847b993cc72ad137
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-10-25 03:55:27 +0000

    Allow fail(code,'warning',pattern).
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2167 fc592f45-0881-4044-91ff-c96495a54512

--- extra/testfun/fail.m
+++ extra/testfun/fail.m
@@ -1,4 +1,4 @@
-## fail (code,pattern)
+## fail (code, pattern)
 ##    Return true if code fails with an error message matching pattern,
 ##    otherwise produce an error.  If code runs successfully, the error
 ##    produced is:
@@ -7,45 +7,85 @@
 ##           expected <pattern>
 ##           but got <text of actual error>
 ##    The angle brackets are not part of the output.
+##
+## fail (code, 'warning', pattern)
+##    Similar to fail(code,pattern), but produces an error if no warning
+##    is given during code execution or if the code fails.
 
 ## This program is public domain
 ## Author: Paul Kienzle <pkienzle@users.sf.net>
 
 ## PKG_ADD mark_as_command fail
-function ret=fail(code,pattern)
-  if nargin < 1 || nargin > 2
-    usage("fail('code' [, 'pattern'])");
+function ret=fail(code,pattern,warning_pattern)
+  if nargin < 1 || nargin > 3
+    usage("fail(code [, 'warning'] [, pattern])");
   end
+
+  ## sort out arguments
+  test_warning =  (nargin > 1 && strcmp(pattern,'warning'));
+  if nargin == 3
+    pattern = warning_pattern;
+  elseif nargin == 1 || (nargin==2 && test_warning)
+    pattern = "";
+  end
+  if isempty(pattern), pattern = "."; end  # match any nonempty message
 
   ## allow assert(fail())
   if nargout, ret=1; end  
 
   ## don't test failure if evalin doesn't exist
-  if !exist('evalin'), return; end
+  if !exist('evalin') || !exist('lastwarn'), return; end
 
-  ## perform the test
-  try
-    evalin("caller",[code,";"]);
-    msg = "expected error but got none";
-  catch
-    err=lasterr;
-    ## allow fail('code')
-    if nargin<2 || isempty(pattern), return; end
-    msg = sprintf("expected message <%s>\nbut got <%s>",pattern,err(1:end-1));
-    if !isempty(regexp(pattern,err(1:end-1))), return; end
+  if test_warning
+    ## perform the warning test
+  #  lastwarn("");  # clear old warnings
+    state = warning('on'); # make sure warnings are turned on
+    try
+      ## printf("lastwarn before %s: %s\n",code,lastwarn);
+      evalin("caller",[code,";"]);
+      ## printf("lastwarn after %s: %s\n",code,lastwarn);
+      err = lastwarn;  # retrieve new warnings
+      warning(state);
+      if isempty(err), 
+        msg = sprintf("expected warning <%s> but got none", pattern); 
+      else
+        err([1:9,end]) = [];  # transform "warning: ...\n" to "..."
+        if !isempty(regexp(pattern,err)), return; end
+        msg = sprintf("expected warning <%s>\nbut got <%s>", pattern,err);
+      end
+    catch
+      warning(state);
+      err = lasterr;
+      err([1:7,end]) = [];  # transform "error: ...\n", to "..."
+      msg = sprintf("expected warning <%s> but got error <%s>", pattern, err);
+    end
+      
+  else
+    ## perform the error test
+    try
+      evalin("caller",[code,";"]);
+      msg = sprintf("expected error <%s> but got none", pattern);
+    catch
+      err=lasterr;
+      err([1:7,end]) = []; # transform "error: ...\n", to "..."
+      if !isempty(regexp(pattern,err)), return; end
+      msg = sprintf("expected error <%s>\nbut got <%s>",pattern,err);
+    end
   end
 
   ## if we get here, then code didn't fail or error didn't match
   error(msg);
 end
 
-%!shared a
-%! a = [1,2];
-%!test fail ('a*[2,3]','nonconformant')
-%!test fail ("fail('a*[2;3]','nonconformant')","expected error but got none")
-%!test fail ("fail('a*[2,3]','usage:')","expected .*usage:.*but got.*nonconformant")
+%!fail ('[1,2]*[2,3]','nonconformant')
+%!fail ("fail('[1,2]*[2;3]','nonconformant')","expected error <nonconformant> but got none")
+%!fail ("fail('[1,2]*[2,3]','usage:')","expected error <usage:>\nbut got.*nonconformant")
+%!fail ("warning('hello')",'warning','hello');
+
+%!fail ("warning('world')",'warning','world');  ## only allowed one warning test?!?
 
 ## Comment out the following tests if you don't want to see what
 ## errors look like
-% !test fail('a*[2;3]','nonconformant')
-% !test fail('a*[2,3]','usage:')
+% !fail ('a*[2;3]', 'nonconformant')
+% !fail ('a*[2,3]', 'usage:')
+% !fail ("warning('world')", 'warning', 'hello')

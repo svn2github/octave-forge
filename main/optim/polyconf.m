a42dfe5518a65c37c82c9f7ed389a8418b0fdc5f
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-11-01 15:39:44 +0000

    Clarify docs for control parameter 1-alpha
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2189 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/polyconf.m
+++ main/optim/polyconf.m
@@ -19,9 +19,10 @@
 ##
 ## polyconf(...,1-alpha)
 ##
-##   The width of the prediction interval. The default is .05 for
-##   a 95% prediction interval, or erfc(1/sqrt(2)) for a one
-##   standard deviation confidence interval.
+##   Control the width of the interval. If asking for the prediction
+##   interval 'pi', the default is .05 for the 95% prediction interval.
+##   If asking for the confidence interval 'ci', the default is
+##   erfc(1/sqrt(2)) for a one standard deviation confidence interval.
 ##
 ## Example:
 ##  [p,s] = polyfit(x,y,1);
@@ -98,9 +99,10 @@ end
 ##
 ## confidence(...,1-alpha)
 ##
-##   The width of the confidence interval. The default is .05 for
-##   a 95% prediction interval, or erfc(1/sqrt(2)) for a one
-##   standard deviation confidence interval.
+##   Control the width of the interval. If asking for the prediction
+##   interval 'pi', the default is .05 for the 95% prediction interval.
+##   If asking for the confidence interval 'ci', the default is
+##   erfc(1/sqrt(2)) for a one standard deviation confidence interval.
 ##
 ## Confidence intervals for linear system are given by:
 ##    x' p +/- sqrt( Finv(1-a,1,df) var(x' p) )

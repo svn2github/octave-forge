e2023e518ee0659df386c7c32298db5ed6bf05ac
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2005-11-29 06:52:33 +0000

    [with Bill Denney] improved functionality; texinfo docs
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2228 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/dlmwrite.m
+++ main/io/dlmwrite.m
@@ -1,28 +1,62 @@
-## dlmwrite(file, matrix, 'option', value, ...)
-## write matrix to the file using delimiters (defaults to ",")
+## -*- texinfo -*-
+## @deftypefn {Function File} {} dlmwrite(@var{file}, @var{A})
+## @deftypefnx {Function File} {} dmlwrite(@var{file}, @var{A}, @var{delim}, @var{r}, @var{c})
+## @deftypefnx {Function File} {} dmlwrite(@var{file}, @var{A}, @code{"attrib1"}, @var{value1}, @code{"attrib2"}, @var{value2}, ...)
+## @deftypefnx {Function File} {} dmlwrite(@var{file}, @var{A}, @code{"-append"}, ...)
 ##
-## Use one of the following options:
-## '-append'
-##    Add the data to an existing file.
-## 'precision','%#.#f'
-##    Specify the width and number of decimals for the stored field
-##    Default is %.15g
-## 'newline','os'
-##    Specify the line separator
-##    'pc' uses <CR><LF>
-##    'unix' uses <LF>
-##    'mac' uses <CR>
-## 'delimiter','seq'
-##    Use 'seq' to separate elements of the matrix.  Default is ','.  Use
-##    the sequence "\t" for tab-delimited values.
-## 'roffset',r
-##    The number of delimiter-only lines to add to the start of the output.
-## 'coffset',c
-##    The number of delimiters to add to the start of each line.
-## 'seq',r,c
-##    This is equivalent to 'delimiter','seq','roffest',r,'coffset',c.
-## 
+## Write the matrix @var{A} to the text @var{file} using delimiters.
+##
+## @table @var
+## @item delim
+## the delimiter to use to separate values on a row
+##
+## @item r
+## the number of delimiter-only lines to add to the start of the file
+##
+## @item c
+## the number of delimiters to prepend to each line of data.
+##
+## @item '-append'
+## append to the end of the @var{file}.
+##
+## @item 'append', state
+## Either @samp{'on'} or @samp{'off'}.  See @samp{'-append'} above.
+##
+## @item 'delimiter', d
+## See @code{"delim"} above.
+##
+## @item 'newline', os
+## The character(s) to use to separate each row.  Three special cases
+## exist for this option.  @samp{'unix'} is changed into "\n", @samp{'pc'} is
+## changed into "\r\n", and @samp{'mac'} is changed into "\r".  Other
+## values for this option are kept as is.
+##
+## @item 'roffset', r
+## See @var{r} above.
+##
+## @item 'coffset', c
+## See @var{c} above.
+##
+## @item 'precision', p
+## The precision to use when writing the file.  It can either be a
+## format string (as used by fprintf) or a number of significant digits.
+## @end table
+##
+## @example
+## @var{A} = reshape(1:16,4,4);
+## dlmwrite(@code{"file.csv"}, @var{A})
+## @end example
+##
+## Note the extra escaping of the backslashes necessary in using the
+## latex delimiter of "\\" with a unix style newline.
+##
+## @example
+## dlmwrite(@code{"file.tex"}, @var{A}, @code{"delimiter"}, @code{"&"}, @code{"newline"}, @code{"\\\\\n"})
+## @end example
+##
 ## TODO: proper handling of complex data
+## @seealso{dlmread, csvread, csvwrite, csv2cell, cell2csv}
+## @end deftypefn
 
 ## Author: Paul Kienzle <pkienzle@users.sf.net>
 ## 
@@ -30,69 +64,77 @@
 ## 
 ## 2002-03-08 Paul Kienzle <pkienzle@users.sf.net>
 ## * Initial revision
+## 2005-11-27 Bill Denney <bill@givebillmoney.com>
+## * Significant modifications of the input arguements for additional
+## functionality.
 
-function [ ret ] = dlmwrite (file, A, varargin)
-  if nargin < 2, 
-     usage("dlmwrite(file, A, 'option', value, ...)"); 
-  end
+function dlmwrite (file, A, varargin)
+  if (nargin < 2)
+    usage("dlmwrite(file, A, delim, r, c)");
+  endif
 
-  delim = ',';
+  ## set defaults
+  delim = ",";
   r = 0;
   c = 0;
-  mode = 'w';
-  newline = setstr(10);
-  precision = '%.16g';
-  
-  arg = 1;
-  n = length(varargin);
-  while arg <= n
-    code = varargin{arg++};
-    if ~ischar(code), error("expects 'option',value,..."); endif
-    switch code,
-    case {'-append'},
-      ## No value options
-      mode = 'a';
+  newline = "\n";
+  precision = "%.16g";
+  opentype = "wt";
 
-    case {'delimiter','precision','roffset','coffset','newline'},
-      ## One value options
-      if arg > n, error("'%s' expects a value",code); endif
-      value = varargin{arg++};
-      switch code,
-      case 'delimiter',
-        if ~ischar(value), error("expects 'delimiter','seq'"); endif
-        delim = setstr(double(value(:)));
-      case 'precision',
-        if ~ischar(value) && ~any(value(:)=='%'), 
-          error("expects 'precision','%#.#f'"); 
-        endif
-        precision = value;
-      case 'newline',
-        switch value,
-	case 'pc', newline = setstr([13,10]);
-	case 'unix',newline = setstr(10);
-	case 'mac',newline = setstr(13);
-	otherwise,  error("expects 'newline','pc'|'unix'|'mac'");
-	end
-      case 'roffset',
-        if ~isscalar(value), error("expects 'roffset',r"); endif
-        r = value;
-      case 'coffset',
-        if ~isscalar(value), error("expects 'coffset',c"); endif
-        c = value;
-      end
-
-    otherwise,
-      ## Two value options
-      if arg > n-1 || ~ischar(code) || ~isscalar(varargin{arg}) ...
-          || ~isscalar(varargin{arg+1})
-        error("expects 'delimiter',r,c"); 
+  ## process the input arguements
+  i = 0;
+  while (i < length(varargin))
+    i = i + 1;
+    if strcmpi(varargin{i}, "delimiter")
+      i = i + 1;
+      delim = varargin{i};
+    elseif strcmpi(varargin{i}, "newline")
+      i = i + 1;
+      newline = varargin{i};
+      if strcmpi(newline, "unix")
+	newline = "\n";
+      elseif strcmpi(newline, "pc")
+	newline = "\r\n";
+      elseif strcmpi(newline, "mac")
+	newline = "\r";
       endif
-      r = varargin{arg++};
-      c = varargin{arg++};
-    end
-  end
+    elseif strcmpi(varargin{i}, "roffset")
+      i = i + 1;
+      r = varargin{i};
+    elseif strcmpi(varargin{i}, "coffset")
+      i = i + 1;
+      c = varargin{i};
+    elseif strcmpi(varargin{i}, "precision")
+      i = i + 1;
+      precision = varargin{i};
+      if (~ strcmpi(class(precision), "char"))
+	precision = sprintf("%.%gg", precision);
+      endif
+    elseif strcmpi(varargin{i}, "-append")
+      opentype = "at";
+    elseif strcmpi(varargin{i}, "append")
+      i = i + 1;
+      if strcmpi(varargin{i}, "on")
+	opentype = "at";
+      elseif strcmpi(varargin{i}, "off")
+	opentype = "wt";
+      else
+	error("dlmwrite: append must be \"on\" or \"off\".");
+      endif
+    else
+      if (i == 1)
+	delim = varargin{i};
+      elseif (i == 2)
+	r = varargin{i};
+      elseif (i == 3)
+	c = varargin{i};
+      else
+	usage("dlmwrite(file, A, delim, r, c)");
+      endif
+    endif
+  endwhile
 
-  [fid,msg] = fopen(file,mode);
+  [fid,msg] = fopen(file,opentype);
   if (fid < 0)
     error(msg);
   else
@@ -101,7 +143,7 @@ function [ ret ] = dlmwrite (file, A, varargin)
     endif
     template = [ precision, repmat([delim,precision],1,columns(A)-1), newline ];
     if (c > 0)
-        template = [ repmat(delim,1,c), template ];
+      template = [ repmat(delim,1,c), template ];
     endif
     fprintf(fid,template,A.');
     fclose(fid);

f7e11dd3292ff865eda863c9880628cf3b432f45
pkienzle <pkienzle@fc592f45-0881-4044-91ff-c96495a54512>
2006-01-03 03:56:03 +0000

    Purge functions added to Octave
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2278 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/isa.m
+++
@@ -1,8 +0,0 @@
-## isa(f,'class')
-##   Test if f is a value from a particular class.  Common
-##   classes are cell, double, sparse, char and struct.
-
-## Author: Paul Kienzle
-## This program is granted to the public domain
-function b = isa(f,t)
-  b = strcmp(class(f),t);

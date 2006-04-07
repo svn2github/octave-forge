abb3febb38ae812a66bc7c348e51e8338c54489a
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2006-04-07 12:29:14 +0000

    bug related to ancient interface change
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2391 fc592f45-0881-4044-91ff-c96495a54512

--- main/econometrics/gmm_variance_inefficient.m
+++ main/econometrics/gmm_variance_inefficient.m
@@ -1,22 +1,22 @@
 # Copyright (C) 2003,2004  Michael Creel <michael.creel@uab.es>
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 # GMM variance, which assumes weights are not optimal
 function V = gmm_variance_inefficient(theta, data, weight, omega, moments, momentargs)
-	D = NumGradient("average_moments", {theta, data, moments, momentargs});
+	D = numgradient("average_moments", {theta, data, moments, momentargs});
 	D = D';
 
 	m = feval(moments, theta, data, momentargs); # find out how many obsns. we have

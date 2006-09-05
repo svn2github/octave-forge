3789306e7e9a6405a1b5a91cf4c0c2cb5f6cb284
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2006-09-05 16:42:33 +0000

    Bug fix: correct angles in the 3rd and 4th quadrant
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2597 fc592f45-0881-4044-91ff-c96495a54512

--- extra/mapping/inst/azimuth.m
+++ extra/mapping/inst/azimuth.m
@@ -11,44 +11,85 @@
 ## 02111-1307, USA.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {} @var{az} = azimuth(@var{pt1}, @var{pt2})
+## @deftypefn {Function File} {} @var{az} = azimuth(@var{lat1},@var{lon1},@var{lat2},@var{lon2})
+## @deftypefnx {Function File} {} @var{az} = azimuth(@var{lat1},@var{lon1},@var{lat2},@var{lon2},@var{units})
+## @deftypefnx {Function File} {} @var{az} = azimuth(@var{pt1}, @var{pt2})
+## @deftypefnx {Function File} {} @var{az} = azimuth(@var{pt1}, @var{pt2},@var{units})
 ##
-## Calculates the great circle azimuth from @var{pt1} to @var{pt2}.
+## Calculates the great circle azimuth from a point 1 to a point 2.
+## The latitude and longitude of these two points can either be given 
+## independently or as columns of the matrices @var{pt1} and @var{pt2} in 
+## the form [latitude longitude].
 ##
-## @var{pt1} and @var{pt2} are two-column matrices of the form
-## [latitude longitude].
+## The units for the input coordinates and output angles can be 
+## "degrees" (the default) or "radians".
 ##
 ## @example
 ## >> azimuth([10,10], [10,40])
 ## ans = 87.336
 ## >> azimuth([0,10], [0,40])
 ## ans = 90
+## >> azimuth(pi/4,0,pi/4,-pi/2,"radians")
+## ans = 5.3279
 ## @end example
 ##
 ## @seealso{elevation,distance}
 ## @end deftypefn
 
 ## Author: Andrew Collier <abcollier@users.sourceforge.net>
+## Adapted-by: Alexander Barth <abarth93@users.sourceforge.net>
 
 ## Uses "four-parts" formula.
 
-function az = azimuth(pt1, pt2)
-  pt1 = deg2rad(pt1);
-  pt2 = deg2rad(pt2);
+function az = azimuth(varargin)
+  ## default units are degrees
 
-  a = pi / 2 - pt1(1);
-  b = pi / 2 - pt2(1);
-  C = pt2(2) - pt1(2);
+  units = "degrees";
 
-  if C == 0
-	if b <= a
-	  az = 0;
-	else
-	  az = 180;
-	endif
+  [reg,prop] = parseparams(varargin);
+
+  if length(reg) == 2
+    pt1 = reg{1};
+    pt2 = reg{2};
+
+    a = pt1(:,1);
+    b = pt2(:,1);
+    C = pt2(:,2) - pt1(:,2);    
+  elseif length(reg) == 4
+    a = reg{1};
+    b = reg{3};
+    C = reg{4} - reg{2};
   else
-	az = rad2deg(acot((sin(a) * cot(b) - cos(a) * cos(C)) / sin(C)));
-  endif
+     error("Wrong number of type of arguments");
+  end
+  
+  if length(prop) == 1    
+    units = prop{1};
+
+    if (~strcmp(units,"degrees") & ~strcmp(units,"radians"))
+      error("Only degrees and radians are allowed as units");
+    end
+  elseif length(prop) > 1
+    error("Wrong number of type of arguments");
+  end
+
+  if (strcmp(units,"degrees"))
+    a = deg2rad(a);
+    b = deg2rad(b);
+    C = deg2rad(C);
+  end
+
+  az = atan2(sin(C) , cos(a) .* tan(b) - sin(a) .* cos(C) );
+
+  ## bring the angle in the interval [0 2*pi[
+
+  az = mod(az,2*pi);
+
+  ## convert to degrees if desired
+
+   if (strcmp(units,"degrees"))
+     az = rad2deg(az);
+  end
 endfunction
 
 ## http://www.mathworks.com/access/helpdesk/help/toolbox/map/azimuth.shtml

1b815656ab0f9b5066ca1e752bcd2b61783eef2d
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2006-10-12 21:21:00 +0000

    Help and copyright added.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@2836 fc592f45-0881-4044-91ff-c96495a54512

--- main/miscellaneous/inst/zip.m
+++ main/miscellaneous/inst/zip.m
@@ -1,3 +1,25 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} zip (@var{zipfile},@var{files})
+## Compress the list of files specified in 'files' into the archive 'zipfiles' in the same directory.
+## @seealso{unzip,tar}
+## @end deftypefn
+
 function zip(zipfile, files)
 	
   if (nargin == 2)

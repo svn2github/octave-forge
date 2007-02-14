3c580e3ea8a203540dc4e0c04c32df4f8c041a95
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2007-02-14 11:42:32 +0000

    randp -> poisson_rnd (using deprecated for compatibility with 2.1.x)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3295 fc592f45-0881-4044-91ff-c96495a54512

--- main/econometrics/inst/gmm_example.m
+++ main/econometrics/inst/gmm_example.m
@@ -1,22 +1,22 @@
 # Copyright (C) 2003,2004, 2005  Michael Creel <michael.creel@uab.es>
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 # GMM example file, shows initial consistent estimator,
 # estimation of efficient weight, and second round
-# efficient estimator 
+# efficient estimator
 
 
 n = 1000;
@@ -26,7 +26,7 @@ x = [ones(n,1) rand(n,k-1)];
 w = [x, rand(n,1)];
 theta = [-k+1; ones(k-1,1)];
 lambda = exp(x*theta);
-y = randp(lambda);
+y = poisson_rnd(lambda);
 [xs, scalecoef] = scale_data(x);
 
 
--- main/econometrics/inst/mle_example.m
+++ main/econometrics/inst/mle_example.m
@@ -1,15 +1,15 @@
 ## Copyright (C) 2003,2004  Michael Creel <michael.creel@uab.es>
-## 
+##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -27,7 +27,7 @@ theta = 1:3; # true coefficients are 1,2,3
 theta = theta';
 
 lambda = exp(x*theta);
-y = randp(lambda); # generate the dependent variable
+y = poisson_rnd(lambda); # generate the dependent variable
 
 ####################################
 # define arguments for mle_results #
@@ -38,7 +38,7 @@ theta = zeros(3,1);
 # data
 data = [y, x];
 # name of model to estimate
-model = "poisson"; 
+model = "poisson";
 modelargs = {0}; # if this is zero the function gives analytic score, otherwise not
 # parameter names
 names = str2mat("beta1", "beta2", "beta3");

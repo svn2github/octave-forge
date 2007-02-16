ce28c1aa90a4117a00d4108063091f1b33dfedb7
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2007-02-16 10:12:29 +0000

    clean up a bit, add a nice plot of parallel performance
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3298 fc592f45-0881-4044-91ff-c96495a54512

--- main/econometrics/inst/kernel_example.m
+++ main/econometrics/inst/kernel_example.m
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 Michael Creel <michael.creel@uab.es>
+# Copyright (C) 2006 Michael Creel <michael.creel@uab.es>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,13 +18,17 @@
 #
 # usage: kernel_example;
 
-# sample size - should get better fit (on average) as this increases
+# sample size (default n = 500) - should get better fit (on average)
+# as this increases, supposing that you allow the optimal window width
+# to be found, by uncommenting the relevant lines
 n = 500;
 
 # set this to greater than 0 to try parallel computations (requires MPITB)
-compute_nodes = 0;
-
+compute_nodes = 3;
 nodes = compute_nodes + 1; # count master node
+
+close all;
+hold off;
 
 ############################################################
 # kernel regression example
@@ -46,8 +50,7 @@ fit = kernel_regression(x, y, x, bandwidth, false, compute_nodes);
 t1 = toc;
 printf("\n");
 printf("########################################################################\n");
-printf("Kernel regression example\n");
-printf("time using %d data points and %d compute nodes: %f\n", n, nodes, t1);
+printf("time for kernel regression example using %d data points and %d compute nodes: %f\n", n, nodes, t1);
 grid("on");
 title("Example 1: Kernel regression fit");
 plot(x, fit, x, trueline);
@@ -67,8 +70,7 @@ dens = kernel_density(grid_x, data, bandwidth, false, compute_nodes);
 t1 = toc;
 printf("\n");
 printf("########################################################################\n");
-printf("Univariate kernel density example\n");
-printf("time using %d data points and %d compute nodes: %f\n", n, nodes, t1);
+printf("time for univariate kernel density example using %d data points and %d compute nodes: %f\n", n, nodes, t1);
 printf("A rough integration under the fitted univariate density is %f\n", sum(dens)*stepsize);
 figure();
 title("Example 2: Kernel density fit: Univariate Chi^2(3) data");
@@ -98,47 +100,48 @@ dens = kernel_density(eval_points, data, bandwidth, false, compute_nodes);
 t1 = toc;
 printf("\n");
 printf("########################################################################\n");
-printf("Multivatiate kernel density example\n");
-printf("time using %d data points and %d compute nodes: %f\n", n, nodes, t1);
+printf("time for multivariate kernel density example using %d data points and %d compute nodes: %f\n", n, nodes, t1);
 dens = reshape(dens, gridsize, gridsize);
 printf("A rough integration under the fitted bivariate density is %f\n", sum(sum(dens))*stepsize^2);
 figure();
-legend("off");
+#legend("off");
 title("Example 3: Kernel density fit: dependent bivatiate data");
 xlabel("true marginal density is N(0,1)");
 ylabel("true marginal density is Chi^2(3)");
 surf(grid_x, grid_y, dens);
-
-
 # more extensive test of parallel
 if compute_nodes > 0 # only try this if parallel is available
-	############################################################
-	# kernel density example: bivariate
-	# X ~ N(0,1)
-	# Y ~ Chi squared(3)
-	# X, Y are dependent
-	# evaluation points are on a grid for plotting
-	stepsize = 0.2;
-	a = (-5:stepsize:5)'; # for the N(0,1)
-	b = (-1:stepsize:9)';  # for the Chi squared(3)
-	gridsize = rows(a);
-	[grid_x, grid_y] = meshgrid(a, b);
-	eval_points = [vec(grid_x) vec(grid_y)];
-	bandwidth = 0.85;
-	ns = [1000; 2000; 4000; 8000];
+	ns =[4000; 8000; 10000; 12000; 16000; 20000];
 	printf("\n");
 	printf("########################################################################\n");
-	printf("multivariate kernel density example with several sample sizes serial/parallel timings\n");
+	printf("kernel regression example with several sample sizes serial/parallel timings\n");
+	figure();
+	clg;
+	title("Compute time versus nodes, kernel regression with different sample sizes");
+	xlabel("nodes");
+	ylabel("time (sec)");
+	hold on;
+	ts = zeros(rows(ns),4);
 	for i = 1:rows(ns)
-		for compute_nodes = 0:1
+		for compute_nodes = 0:3
 			nodes = compute_nodes + 1;
 			n = ns(i,:);
-			d = randn(n,3);
-			data = [d(:,1) sumsq(d,2)];
+			x = 1:n;
+			x = x';
+			x = 2*x/n;
+			# generate dependent variable
+			trueline =  x + (x.^2)/2 - 3.1*(x.^3)/3 + 1.2*(x.^4)/4;
+			sig = 0.5;
+			y = trueline + sig*randn(n,1);
+			bandwidth = 0.45;
 			tic;
-			dens = kernel_density(eval_points, data, bandwidth, false, compute_nodes);
+			fit = kernel_regression(x, y, x, bandwidth, false, compute_nodes);
 			t1 = toc;
+			ts(i, nodes) = t1;
+			plot(nodes, t1, "*");
 			printf(" %d data points and %d compute nodes: %f\n", n, nodes, t1);
 		endfor
+		plot(ts(i,:)');
 	endfor
+	hold off;
 endif

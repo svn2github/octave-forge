b2f5531339cb8bb4c396f072b2af8200c9dd09cd
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2007-03-21 23:58:58 +0000

    Copyright fix so get_authors works correctly
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3434 fc592f45-0881-4044-91ff-c96495a54512

--- main/comm/inst/fibodeco.m
+++ main/comm/inst/fibodeco.m
@@ -1,5 +1,4 @@
 ## Copyright (C) 2006 Muthiah Annamalai <muthiah.annamalai@uta.edu>
-## Copyright (C) 2006, March 28,Muthiah Annamalai. 
 ## 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by

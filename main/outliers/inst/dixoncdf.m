7255617fb65f210904f17c74d7415bd99542a7ab
hauberg <hauberg@fc592f45-0881-4044-91ff-c96495a54512>
2007-05-17 19:24:24 +0000

    First revision of the 'outliers' package by Lukasz Komsta
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3506 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/outliers/inst/dixoncdf.m
@@ -0,0 +1,26 @@
+## Copyright (C) 2007 Lukasz Komsta, http://www.komsta.net/
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## This function is simple wrapper to dixoninv with 'rev=1' parameter.
+## For more info see 'help dixoninv'
+
+function [q]=dixoncdf(p, n, type)
+
+	if nargin<3
+		type=10;
+	end
+	q = dixoninv(p,n,type,1);
+end

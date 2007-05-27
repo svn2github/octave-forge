80122408922c48c94545e0151fffa4bc52577b3c
goffioul <goffioul@fc592f45-0881-4044-91ff-c96495a54512>
2007-05-27 10:06:15 +0000

    include stripped-down MSYS shell and OplotGL + package manager rebuild script at installation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3570 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/msvc/do_pkg_init.m
@@ -0,0 +1,10 @@
+global_list = pkg ("global_list");
+global_packages = pkg ("rebuild");
+
+for k = 1:length (global_packages)
+  if (strcmp (global_packages{k}.name, "oplot-gl"))
+    global_packages = {global_packages{1:k-1}, global_packages{k+1:end}, global_packages{k}};
+    save (global_list, "global_packages");
+    break;
+  endif
+endfor

361307470f3b1e8e5fb71b77c614718d40633b42
goffioul <goffioul@fc592f45-0881-4044-91ff-c96495a54512>
2007-06-11 12:28:38 +0000

    cleaner (portable) implementation + start using listeners to link areaseries object and the underlying patch object
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3655 fc592f45-0881-4044-91ff-c96495a54512

--- extra/jhandles/inst/__jhandles_go_areaseries.m
+++ extra/jhandles/inst/__jhandles_go_areaseries.m
@@ -17,17 +17,26 @@
 
 function [ h ] = __jhandles_go_areaseries (ax, X, Y, bv, varargin)
 
-  parent = __get_object__ (ax);
   h = [];
 
   YY = cumsum (Y, 2);
 
   for k = 1:size (Y,2)
-    a_obj = java_new ("org.octave.graphics.GroupObject", parent);
-    hh = a_obj.getHandle ();
+    # create group object
+    hh = hggroup (ax);
+	addprop (hh, "BaseValue", "double", bv);
     addprop (hh, "XData", "doublearray", X(:,k));
     addprop (hh, "YData", "doublearray", Y(:,k));
-    addprop (hh, "CDataMapping", "radio", "scaled|direct", "scaled");
+	addprop (hh, "FaceColor", "colorradio", "flat|interp|none", "flat");
+	addprop (hh, "EdgeColor", "colorradio", "flat|interp|none", "k");
+    addprop (hh, "CDataMapping", "radio", "scaled|direct", "scaled", "hidden");
+	addprop (hh, "AreaGroup", "handle", "hidden");
+    set (hh, "CLimInclude", true);
+	set (hh, varargin{:});
+	addlistener (hh, {"FaceColor", "EdgeColor"}, @updatePatchColor);
+    addlistener (hh, {"XData", "YData", "BaseValue"}, @updatePatchData);
+	hh_v = get (hh);
+    # create patch object
     if (k == 1)
       vv = [X(1,k) bv 0; X(:,k) Y(:,k) zeros(size(Y,1),1); X(end,k) bv 0];
       fv = [1:size(vv,1)];
@@ -37,13 +46,43 @@ function [ h ] = __jhandles_go_areaseries (ax, X, Y, bv, varargin)
     endif
     fvc = k * ones (size (vv,1), 1);
     hp = patch (hh, "Faces", fv, "Vertices", vv, "FaceVertexCData", fvc, ...
-      "FaceColor", "flat", "EdgeColor", "k");
-    set (hh, "XLim", get(hp, "XLim"));
-    set (hh, "YLim", get(hp, "YLim"));
-    set (hh, "CLim", get(hp, "CLim"));
-    set (hh, "CLimInclude", true);
-    a_obj.validate ();
+      "FaceColor", hh_v.facecolor, "EdgeColor", hh_v.edgecolor);
     h = [h hh];
+  endfor
+
+  if (! isempty (h))
+    set (h, "AreaGroup", h);
+  endif
+
+endfunction
+
+function updatePatchColor (h)
+
+  hp = get (h, "Children");
+  set (hp(1), "FaceColor", get (h, "FaceColor"), ...
+              "EdgeColor", get (h, "EdgeColor"));
+
+endfunction
+
+function updatePatchData (h)
+
+  hlist = get (h, "AreaGroup");
+  bv = get (h, "BaseValue");
+  YY = [];
+  for hh = hlist(:)'
+    x = get (hh, "XData");
+    y = get (hh, "YData");
+    hp = get (hh, "Children");
+    x = x(:); y = y(:);
+    if (isempty (YY))
+      vv = [x(1) bv 0; x y zeros(length(y), 1); x(end) bv 0];
+      YY = y;
+    else
+      vv = [x(end:-1:1) YY(end:-1:1) zeros(length(y), 1); x y+YY zeros(length (y), 1)];
+      YY += y;
+    endif
+    fv = [1:size(vv,1)];
+    set (hp(1), "Faces", fv, "Vertices", vv);
   endfor
 
 endfunction

c40c5535e420aa40e4dc25dee7550868f67f3bd0
goffioul <goffioul@fc592f45-0881-4044-91ff-c96495a54512>
2007-06-28 20:07:52 +0000

    fix limits computation in group objects
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3724 fc592f45-0881-4044-91ff-c96495a54512

--- extra/jhandles/src/org/octave/graphics/GroupObject.java
+++ extra/jhandles/src/org/octave/graphics/GroupObject.java
@@ -35,23 +35,32 @@ public class GroupObject extends GraphicObject
 		synchronized (Children)
 		{
 			double xmin, xmax, ymin, ymax, zmin, zmax, cmin, cmax, amin, amax;
+			double xmin2, xmax2, ymin2, ymax2, zmin2, zmax2;
 			Iterator it = Children.iterator();
 			double[] lim;
 
 			xmin = ymin = zmin = cmin = amin = Double.POSITIVE_INFINITY;
 			xmax = ymax = zmax = cmax = amax = Double.NEGATIVE_INFINITY;
+			xmin2 = ymin2 = zmin2 = Double.POSITIVE_INFINITY;
+			xmax2 = ymax2 = zmax2 = Double.MIN_VALUE;
 			while (it.hasNext())
 			{
 				GraphicObject obj = (GraphicObject)it.next();
 				lim = obj.XLim.getArray();
 				xmin = Math.min(lim[0], xmin);
 				xmax = Math.max(lim[1], xmax);
+				xmin2 = Math.min(lim[2], xmin2);
+				xmax2 = Math.max(lim[3], xmax2);
 				lim = obj.YLim.getArray();
 				ymin = Math.min(lim[0], ymin);
 				ymax = Math.max(lim[1], ymax);
+				ymin2 = Math.min(lim[2], ymin2);
+				ymax2 = Math.max(lim[3], ymax2);
 				lim = obj.ZLim.getArray();
 				zmin = Math.min(lim[0], zmin);
 				zmax = Math.max(lim[1], zmax);
+				zmin2 = Math.min(lim[2], zmin2);
+				zmax2 = Math.max(lim[3], zmax2);
 				lim = obj.CLim.getArray();
 				cmin = Math.min(lim[0], cmin);
 				cmax = Math.max(lim[1], cmax);
@@ -60,9 +69,9 @@ public class GroupObject extends GraphicObject
 				amax = Math.max(lim[1], amax);
 			}
 
-			XLim.set(new double[] {xmin, xmax}, true);
-			YLim.set(new double[] {ymin, ymax}, true);
-			ZLim.set(new double[] {zmin, zmax}, true);
+			XLim.set(new double[] {xmin, xmax, xmin2, xmax2}, true);
+			YLim.set(new double[] {ymin, ymax, ymin2, ymax2}, true);
+			ZLim.set(new double[] {zmin, zmax, zmin2, zmax2}, true);
 			CLim.set(new double[] {cmin, cmax}, true);
 			ALim.set(new double[] {amin, amax}, true);
 		}

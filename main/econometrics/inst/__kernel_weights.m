778e00c4b18b373b1c209d0b7de0b41ed49fff9c
mcreel <mcreel@fc592f45-0881-4044-91ff-c96495a54512>
2007-06-29 11:22:50 +0000

    alternate version of weights. The good one is an oct file, for performance. This one is for reference.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3733 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/econometrics/inst/__kernel_weights.m
@@ -0,0 +1,33 @@
+# Copyright (C) 2006, 2007  Michael Creel <michael.creel@uab.es>
+# under the terms of the GNU General Public License.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+# __kernel_weights: for internal use by kernel_regression and kernel_density. There
+# is also a faster .oct file version that should be be found automatically.
+
+
+function W = __kernel_weights(data, evalpoints, kernel)
+
+	# calculate distances
+	nn = rows(evalpoints);
+	n = rows(data);
+	W = zeros(nn,n);
+	for i = 1:nn
+		zz = data - repmat(evalpoints(i,:), n, 1);
+		zz = feval(kernel, zz);
+		W(i,:) = zz';
+	endfor
+endfunction

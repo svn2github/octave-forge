8eb4d1942afc40569a5a77d10ad871d163093c73
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2007-07-04 19:24:54 +0000

    Help correction.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3763 fc592f45-0881-4044-91ff-c96495a54512

--- main/comm/inst/qfuncinv.m
+++ main/comm/inst/qfuncinv.m
@@ -15,7 +15,7 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{y}] =} qfunc(@var{x})
+## @deftypefn {Function File} {[@var{y}] =} qfuncinv(@var{x})
 ##	Compute the inverse Q function.
 ## @seealso{erfc,  erf}
 ## @end deftypefn

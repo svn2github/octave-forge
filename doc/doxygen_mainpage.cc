dd1d99f3ae9ac7b3372e097b4233d27151bd587e
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2007-07-12 18:36:27 +0000

    Add __OCTAVE_VERSION__ m4 macro and use in documentation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@3818 fc592f45-0881-4044-91ff-c96495a54512

--- doc/doxygen_mainpage.cc
+++ doc/doxygen_mainpage.cc
@@ -1,8 +1,9 @@
 /*! \mainpage C++ Documentation
  *
- *  This is the documentation for the Octave C++ APIs. It is mainly useful as
- *  a reference to the API and is <b>not</b> intended as an introduction
- *  to working with C++ and Octave. For an introduction to working with
- *  the C++ API see
+ *  This is the documentation for the Octave C++ APIs for version 
+ *  __OCTAVE_VERSION__ of Octave. It is mainly useful as a reference to the 
+ *  API and is <b>not</b> intended as an introduction to working with C++ and 
+ *  Octave. For an introduction to working with the C++ API see the 
+ *  <a href="http://www.octave.org/doc/interpreter/">Octave manual</a> or 
  *  <a href="__BASE_ADDRESS__/coda/index.html">Da Coda Al Fine</a>.
  */

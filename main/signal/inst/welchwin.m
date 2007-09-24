49e9a1d4f21aead91eb2e67936de139a38fd8ca3
gnumuthu <gnumuthu@fc592f45-0881-4044-91ff-c96495a54512>
2007-09-24 14:54:33 +0000

    welchwindow; fix to blackmannuttall window fcn names
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4037 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/inst/welchwin.m
+++ main/signal/inst/welchwin.m
@@ -15,22 +15,20 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{w}] =} blackmannuttall(@var{L})
-##	Compute the Blackman-Nuttall window.
-## @seealso{nuttallwin,  kaiser}
+## @deftypefn {Function File} {[@var{w}] =} welchwin(@var{L})
+##	Compute the Welch window, given by 
+##      w(n) = 1 - ((n-L/2)/(L/2))^2, n=0,1, ... L-1
+## @seealso{blackman,  kaiser}
 ## @end deftypefn
 
-function [w] = blackmannuttall(L)
-	if (nargin < 1); usage('blackmannuttall(L)'); end
+function [w] = welchwin(L)
+	if (nargin < 1); usage('welchwin(L)'); end
 	if(! isscalar(L))
 		error("L must be a number");
 	endif
 	
-	N = L-1;
-	a0 = 0.3635819;
-	a1 = 0.4891775;
-	a2 = 0.1365995;
-	a3 = 0.0106411;
-        n = 0:N;
-	w = a0 - a1.*cos(2.*pi.*n./N) + a2.*cos(4.*pi.*n./N) - a3.*cos(6.*pi.*n./N);
+	N = L/2;
+	n= 0:L-1;
+	w = 1 - ((n-N)./N).^2;
 endfunction;
+

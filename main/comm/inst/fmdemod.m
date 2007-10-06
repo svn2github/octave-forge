e82f248afaf8a99d69e103948cfe48723f85d0f6
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2007-10-06 17:34:56 +0000

    Initial commit into CVS.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4100 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/comm/inst/fmdemod.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2007   Sylvain Pelissier   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## -*- texinfo -*-
+## @deftypefn {Function File} fmdemod (@var{x},@var{fc},@var{fs})
+## Create the FM demodulation of the signal x with carrier frequency fs. Where x is sample at frequency fs.
+## @seealso{ammod,amdemod,fmmod}
+## @end deftypefn
+
+
+function m = my_fmdemod(s,fc,fs)
+	 if (nargin != 3)
+		usage ("ammod(x,fs,fc)");
+	endif
+	
+	ds = diff(s);
+	m = my_amdemod(abs(ds),fc,fs);

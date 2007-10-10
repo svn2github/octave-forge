5a9ca5a6b278333ca2005450d760ce6aecb54a5d
treichl <treichl@fc592f45-0881-4044-91ff-c96495a54512>
2007-10-10 20:45:16 +0000

    Script-update in the form that other users/developers can update Octave.app themselves.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4116 fc592f45-0881-4044-91ff-c96495a54512

--- admin/MacOSX/createapp/selfupdate.sh
+++ admin/MacOSX/createapp/selfupdate.sh
@@ -15,36 +15,88 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-### DON'T USE THIS SCRIPT - IT IS CURRENTLY UNDER DEVELOPMENT. ###
-###                YOU MAY CRASH YOUR OCTAVE.APP.              ###
+########################################################################
+# This file is meant for developers only who know how to look at the   #
+# one or other problem that might appear when using this script. Write #
+# me an email offside any list if you think the script could be        #
+# improved in a better way than this. In any case:                     #
+# DON'T USE ANY OF THE OCTAVE AND OCTAVE-FORGE MAILING-LISTS TO REPORT #
+# ERRORS OR BUGS THAT MAY OCCUR IF THIS SCRIPT FAILS.                  #
+########################################################################
 
-# Check the ./configure script and comment out the 'sed lines' if the
-# local installed sed from /usr/local/bin is used. Change the version
-# number in src/version.h to 2.9.12 instead of 2.9.12+.
+# Put this script into the root directory of your Octave sources.
+# Before you start this script make sure that you don't use Apple's
+# original programs 'sed', 'awk', 'aclocal', 'automake' and 'bison'
+# anymore. Download these latest GNU tools from the GNU website and
+# install them eg. in /usr/local (make sure that you set the $PATH
+# environment variable correctly below). Change the version number in
+# the file src/version.h to the version number of the current
+# Octave.app that should be replaced. Then modify the following
+# APPPATH variable for your needs (the absolut path including
+# Octave.app that should be updated).
+APPPATH=/Applications/Octave.app
 
-APPPATH=~/tmp/octave.app
-
+# Don't change one of the following three lines. These lines are used
+# internally here and are already set up correctly if APPPATH is set
+# up correctly.
 PRFPATH=${APPPATH}/Contents/Resources
 INCPATH=${PRFPATH}/include
 LIBPATH=${PRFPATH}/lib
 
-CFLAGS="-I${INCPATH} -I${INCPATH}/curl -I${INCPATH}/readline" 
-CPPFLAGS="${CFLAGS}"
-CXXFLAGS="${CFLAGS}"
+# Add the option -gdwarf-2 to CFLAGS, CPPFLAGS and CXXFLAGS if you
+# want to create a gdb version of Octave.
+CFLAGS="-O3 -ftree-vectorize -I${INCPATH} -I${INCPATH}/curl -I${INCPATH}/readline" 
+CPPFLAGS="-O3 -ftree-vectorize ${CFLAGS}"
+CXXFLAGS="-O3 -ftree-vectorize ${CFLAGS}"
 LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig"
 
+# I have installed the GNU tools in /usr/local/bin, that's why I need
+# to use this path. We use f2c from the current Octave.app, that's why
+# we add ${PRFPATH}/bin to the current path.
 export PATH=/usr/local/bin:${PRFPATH}/bin:$PATH
 export DYLD_LIBRARY_PATH=${LIBPATH}
 
-mv ${PRFPATH}/bin/octave ${PRFPATH}/bin/_octave
-mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile
+# Function:    evalfailexit
+# Input args:  ${1} is the string that has to be evaluated
+# Description: Evaluates the ${1} string prints a message to ${MSGFILE} 
+#              and exits with an error number 1 on fail
+evalfailexit() {
+  if ( ! eval "${1} 2>&1 >>${MSGFILE}" ); then
+    echo "selfupdate.sh: Building Octave.app has failed !!!"
+    echo "selfupdate.sh: The command that failed was: ${1}"
+    exit 1
+  fi
+}
 
-# ./autogen.sh
-# eval "./configure CFLAGS=\"${CFLAGS}\" CPPFLAGS=\"${CPPFLAGS}\" CXXFLAGS=\"${CXXFLAGS}\" LDFLAGS=\"${LDFLAGS}\" --prefix=${PRFPATH} --enable-shared --with-f2c"
+# Use another file for output while './configure' and 'make' and
+# 'make install', eg. MSGFILE=/tmp/mymessages.log
+MSGFILE=/dev/stdout 
 
-make -j 2
-make uninstall
-make install 
+# Rebuild the './configure' script with './autogen.sh' to make sure
+# that we have the latest changes available.
+evalfailexit "./autogen.sh"
+evalfailexit "./configure CFLAGS=\"${CFLAGS}\" CPPFLAGS=\"${CPPFLAGS}\" \
+  CXXFLAGS=\"${CXXFLAGS}\" LDFLAGS=\"${LDFLAGS}\" --prefix=${PRFPATH} \
+  --enable-shared --with-f2c"
+evalfailexit "make -j 2"
 
-mv ${PRFPATH}/bin/_octave ${PRFPATH}/bin/octave
-mv ${PRFPATH}/bin/_mkoctfile ${PRFPATH}/bin/mkoctfile
+# This is the point of *no return*: If something goes wrong in the
+# following six lines then your new Octave.app may be definitely be
+# broken. We need to store the startup scripts of Octave.app with the
+# following two lines because they will be overwritten.
+evalfailexit "mv ${PRFPATH}/bin/octave ${PRFPATH}/bin/_octave"
+evalfailexit "mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile"
+
+# Cleanup as much as possible of the current Octave.app and install
+# the new binaries.
+evalfailexit "make uninstall"
+evalfailexit "make install"
+
+# Replace the new created startup scripts that have been installed
+# with the old startup scripts of Octave.app.
+evalfailexit "mv ${PRFPATH}/bin/_octave ${PRFPATH}/bin/octave"
+evalfailexit "mv ${PRFPATH}/bin/_mkoctfile ${PRFPATH}/bin/mkoctfile"
+
+# Let's run make check at the end of the installation process to have
+# a look how many of the tests succeed.
+evalexitfail "make check"

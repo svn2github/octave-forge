0a16040f391a05af88493621a74238b9da218fc7
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2007-11-07 13:06:53 +0000

    Initial Commit Into SVN.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4183 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/comm/inst/bsc.m
@@ -0,0 +1,35 @@
+## Copyright (C) 2007   Sylvain Pelissier   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{y}] =} bsc(@var{data},@var{p})
+##	Send DATA into a binary symetric channel with probability P of error one each symbol.
+## @end deftypefn
+
+function [ndata] = bsc(data,p)
+	if (nargin < 1); usage('ndata = bsc(data,p)'); end
+	
+	if(isscalar(p) ~= 1 || p > 1 || p < 0)
+		error('p muste be a positive scalar less than one');
+		exit;
+	end
+	
+	ndata = data;
+	ndata(find(data == 0)) = randsrc(size(ndata(find(data == 0)),1),size(ndata(find(data == 0)),2),[-1 -2;1-p p]);
+	ndata(find(data == 1)) = randsrc(size(ndata(find(data == 1)),1),size(ndata(find(data == 1)),2),[1 0;1-p p]);
+	ndata(find(ndata == -1)) = 0;
+	ndata(find(ndata == -2)) = 1;
+	

27a8ff42f939279e23bfb72aa4a5bbc63c5ba7ae
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2007-11-19 17:23:49 +0000

    Add GLPK build scripts for mingw32 build with gcc-4.2.1
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4255 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/glpk/build-4.17-2.sh
@@ -0,0 +1,77 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=glpk
+# Version of Package
+VER=4.17
+# Release of (this patched) package
+REL=2
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}.tar.gz
+TAR_TYPE=z
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://gd.tuwien.ac.at/gnu/gnusrc/glpk/glpk-4.17.tar.gz"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extracted to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+# Directory the lib is built in
+BUILDDIR=".build_mingw32_${VER}-${REL}_gcc421_dw2"
+
+# Make file to use
+MAKEFILE=""
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS=""
+INCLUDE_DIR=include/glpk
+
+source ../gcc42_common.sh
+
+mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+conf()
+{
+   mkdirs;
+   ( cd ${BUILDDIR} && ${TOPDIR}/${SRCDIR}/configure \
+     --srcdir=../${SRCDIR} \
+     CC=${CC} \
+     CXX=${CXX} \
+     F77=${F77} \
+     CFLAGS="${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     CXXFLAGS="${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     LDFLAGS="${LDFLAGS}" \
+     --prefix="${PREFIX}"
+   )
+}
+
+install()
+{
+   install_pre
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/glpk.dll ${BINARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libglpk.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${SRCDIR}/include/glpk.h ${INCLUDE_PATH}
+   install_post
+}
+
+uninstall()
+{
+   ${RM} ${RM_FLAGS} ${BINARY_PATH}/glpk.dll
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libglpk.dll.a
+   ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/glpk.h
+}
+
+main $*

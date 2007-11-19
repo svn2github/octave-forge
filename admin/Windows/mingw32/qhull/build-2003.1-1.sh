a6870c1690ab90e38f53e6d794ac38431d2f9ce7
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2007-11-19 17:33:50 +0000

    Add QHULL build scripts for mingw32 build with gcc-4.2.1
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4264 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/qhull/build-2003.1-1.sh
@@ -0,0 +1,70 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=qhull
+# Version of Package
+VER=2003.1
+# Release of (this patched) package
+REL=1
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}-src.tgz
+TAR_TYPE=z
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://www.qhull.org/download/qhull-2003.1-src.tgz"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+#TOPDIR=`pwd -W | sed -e 's+\([a-z]\):/+/\1/+'`
+TOPDIR=`pwd`
+# Directory Source code is extracted to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+# Directory the lib is built in
+BUILDDIR=${SRCDIR}
+
+# Make file to use
+MAKEFILE="makefile"
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS="user.h qhull.h qhull_a.h geom.h io.h mem.h merge.h poly.h qset.h stat.h"
+INCLUDE_DIR=include/qhull
+
+source ../gcc42_common.sh
+
+#mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+build() 
+{
+   cd ${BUILDDIR}/src && make lib
+}
+
+install()
+{
+   install_pre
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/qhull.dll ${BINARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libqhull.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libqhull.a ${STATICLIBRARY_PATH}
+   for a in ${INSTALL_HEADERS}; do ${CP} ${CP_FLAGS} ${SRCDIR}/src/$a ${INCLUDE_PATH}; done
+   install_post
+}
+
+uninstall()
+{
+   ${RM} ${RM_FLAGS} ${BINARY_PATH}/qhull.dll
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libqhull.dll.a
+   ${RM} ${RM_FLAGS} ${STATICLIBRARY_PATH}/libqhull.a
+   for a in ${INSTALL_HEADERS}; do ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/$a; done
+}
+
+main $*

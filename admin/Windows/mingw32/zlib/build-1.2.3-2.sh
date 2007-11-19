9dccc7f831d9d5cafe6e7d6aa1d49dbba9fed2c2
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2007-11-19 17:19:58 +0000

    Add ZLIB build scripts for gcc-4.2.1 build
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4250 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/zlib/build-1.2.3-2.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=zlib
+# Version of Package
+VER=1.2.3
+# Release of (this patched) package
+REL=2
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}.tar.bz2
+TAR_TYPE=j
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://www.gzip.org/zlib/zlib-1.2.3.tar.bz2"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extraced to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+# Directory the lib is built in
+BUILDDIR=${SRCDIR}
+
+# Make file to use
+MAKEFILE=win32/Makefile.gcc
+
+source ../gcc42_common.sh
+
+check_pre() { MAKEFILE=""; }
+
+echo ${PREFIX}
+
+main $*

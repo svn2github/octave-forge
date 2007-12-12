39dd5ab2287daaacaafe113bcdde14f997e501d2
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2007-12-12 16:18:10 +0000

    Some nearly final doc updates prior to release
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4387 fc592f45-0881-4044-91ff-c96495a54512

--- main/plot/inst/ellipsoid.m
+++ main/plot/inst/ellipsoid.m
@@ -15,51 +15,54 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{x}, @var{y}, @var{z}] =} ellipsoid (@var{xc},@var{yc},@var{zc},@var{xr},@var{yr},@var{zr},@var{n})
-## @deftypefnx {Function File} {} ellipsoid (@var{xc},@var{yc},@var{zc},@var{xr},@var{yr},@var{zr},@var{n})
+## @deftypefn {Function File} {[@var{x}, @var{y}, @var{z}] =} ellipsoid (@var{xc},@var{yc}, @var{zc}, @var{xr}, @var{yr}, @var{zr}, @var{n})
+## @deftypefnx {Function File} {} sphere (@var{h}, @dots{})
 ## Generates three matrices in @code{meshgrid} format, of an ellipsoid.
-## Called with no return arguments, @code{ellipsoid} call directly 
-## @code{surf (@var{x}, @var{y}, @var{z})}.
+## Called with no return arguments, @code{ellipsoid} calls directly 
+## @code{surf (@var{x}, @var{y}, @var{z})}. If an axes handle is passed
+## as the first argument, the the surface is plotted to this set of axes.
+## @seealso{sphere}
 ## @end deftypefn
 
 function [xx, yy, zz] = ellipsoid(varargin)
-  
-  if (nargin < 1)
+
+  [h, varargin, nargin] = __plt_get_axis_arg__ ((nargout > 0), "ellipsoid", 
+						varargin{:});
+
+  if (nargin != 6 && nargin != 7)
     print_usage ();
-  elseif (nargin == 6)
-    xc = varargin{1};
-	yc = varargin{2};
-	zc = varargin{3};
-	xr = varargin{4};
-	yr = varargin{5};
-	zr = varargin{6};
-    n = 20;
-  elseif(nargin == 7)
-	xc = varargin{1};
-	yc = varargin{2};
-	zc = varargin{3};
-	xr = varargin{4};
-	yr = varargin{5};
-	zr = varargin{6};
-	n = varargin{7};
-  else
-	print_usage ();
   endif
 
-  theta = linspace (0, 2*pi, n+1);
-  phi = linspace (-pi/2, pi/2, n+1);
-  [theta,phi] = meshgrid (theta, phi);
+  xc = varargin{1};
+  yc = varargin{2};
+  zc = varargin{3};
+  xr = varargin{4};
+  yr = varargin{5};
+  zr = varargin{6};
 
-  x = xr.*cos (phi) .* cos (theta)+xc;
-  y = yr.*cos (phi) .* sin (theta)+yc;
-  z = zr.*sin (phi)+zc;
+  if (nargin == 5)
+    n = 20;
+  else
+    n = varargin{7};
+  endif
+
+  theta = linspace (0, 2 * pi, n + 1);
+  phi = linspace (-pi / 2, pi / 2, n + 1);
+  [theta, phi] = meshgrid (theta, phi);
+
+  x = xr .* cos (phi) .* cos (theta) + xc;
+  y = yr .* cos (phi) .* sin (theta) + yc;
+  z = zr .* sin (phi) + zc;
 
   if (nargout > 0)
     xx = x;
     yy = y;
     zz = z;
   else
-    surf ( x, y, z);
+    surf (h, x, y, z);
   endif
 
 endfunction
+
+%!demo
+%! ellipsoid (0, 0, 1, 2, 3, 4, 20);

f30e6d49486f6cee98249597df56bbf65d98e952
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2007-12-15 11:22:26 +0000

    Errors correction.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4404 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/inst/gauspuls.m
+++ main/signal/inst/gauspuls.m
@@ -29,7 +29,7 @@ function [y] = gauspuls(t,fc,bw)
 	if fc < 0 , error("fc must be positive"); end
 	if bw <= 0, error("bw must be stricltly positive"); end
 	
-	fv = -(bw.*bw.*fc.^2)/(8.*log(10.^(-6/20)));
-	tv = 1/(4.*pi.*pi.*f); 
-	y = exp(-t.*t/(2.*tv)).*sin(2.*pi.*fc.*t);
+	fv = -(bw.^2.*fc.^2)/(8.*log(10.^(-6/20)));
+	tv = 1/(4.*pi.^2.*fv); 
+	y = exp(-t.*t/(2.*tv)).*cos(2.*pi.*fc.*t);
 endfunction

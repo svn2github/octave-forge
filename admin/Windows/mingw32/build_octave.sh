97a2fd274e17b60bfef57424821f82f3d7439d32
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-01-11 15:34:40 +0000

    update. Add action parameters which are passed to individual build scripts
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4492 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/build_octave.sh
+++ admin/Windows/mingw32/build_octave.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 
+if [ -z "$1" ]; then ACTION=all; else ACTION="$*"; fi
+echo ACTION = "${ACTION}";
+
 VER_OCTAVE=2.9.17-1
 
 # This script builds OCTAVE
-( cd octave && ./build-${VER_OCTAVE}.sh all );
+( cd octave && ./build-${VER_OCTAVE}.sh ${ACTION} );
 

aabf50257f158c65da26183402ae0e57b402aa15
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-01-11 15:37:10 +0000

    update script. Support multiple versions/releases/builds
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4495 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/create_package.sh
+++ admin/Windows/mingw32/create_package.sh
@@ -3,13 +3,14 @@
 # This packages OCTAVE MINGW32
 
 source pkg_version.sh
+source common.sh
 
 # Name of the package we're building
-PKG=octave_i686-pc-mingw32
+PKG=octave-${PKG_VER}-${PKG_REL}_i686-pc-mingw32_gcc${GCC_VER}${GCC_SYS}
 # Version of the package
-VER=${PKG_VER}
+#VER=${PKG_VER}
 # Release No
-REL=${PKG_REL}
+#REL=${PKG_REL}
 # URL to source code
 URL=
 
@@ -18,9 +19,9 @@ URL=
 # Make sure that we have a MSYS patch starting with a drive letter!
 TOPDIR=`pwd -W | sed -e 's+^\([a-zA-z]\):/+/\1/+'`
 # Name of the source package
-PKGNAME=${PKG}-${VER}
+PKGNAME=${PKG}
 # Full package name including revision
-FULLPKG=${PKGNAME}-${REL}
+FULLPKG=${PKGNAME}
 # Name of the source code package
 #SRCPKG=${PKGNAME}
 # Name of the patch file
@@ -31,19 +32,17 @@ FULLPKG=${PKGNAME}-${REL}
 #SRCDIR=${TOPDIR}/${PKGNAME}
 PKGFILE=${FULLPKG}
 
-source common.sh
-
 SEVENZIP="/c/Program Files/7-Zip/7z.exe"
 
 # Create archive package
-( cd ${PACKAGE_ROOT} && ${TAR} cjvf ${TOPDIR}/${PKGFILE}.tar.bz2 . );
+( cd ${PACKAGE_ROOT} && ${TAR} cjvf ${TOPDIR}/${PKGFILE}.tar.bz2 bin doc include info lib libexec man share mingw32 msys );
 ( cd ${PACKAGE_ROOT} && ${TAR} cjv --exclude=mingw32 --exclude=MSYS -f ${TOPDIR}/${PKGFILE}_wo-compiler.tar.bz2 . );
 ( cd ${PACKAGE_ROOT} && ${TAR} cjvf ${TOPDIR}/${PKGFILE}_mingw32-msys.tar.bz2 mingw32 msys );
 
 CF=7
-"${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}.exe ${PACKAGE_ROOT}/* -mx${CF} -sfx7z.sfx
+"${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}.exe ${PACKAGE_ROOT}{/bin,/doc,/include,/info,/lib,/libexec,/man,/share,/mingw32,/msys} -mx${CF} -sfx7z.sfx
 "${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}_mingw32-msys.exe ${PACKAGE_ROOT}{/mingw32,/msys} -mx${CF} -sfx7z.sfx
-"${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}_wo-compliler.exe ${PACKAGE_ROOT}{/bin,/doc,/include,/info,/lib,/libexec,/man,/share} -mx${CF} -sfx7z.sfx
+"${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}_wo-compiler.exe ${PACKAGE_ROOT}{/bin,/doc,/include,/info,/lib,/libexec,/man,/share} -mx${CF} -sfx7z.sfx
 
 #CF=9
 #"${SEVENZIP}" a -t7z ${TOPDIR}/${PKGFILE}-CF${CF}.exe ${PACKAGE_ROOT} -mx${CF} -sfx7z.sfx

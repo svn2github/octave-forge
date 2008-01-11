f902bb15e7f756d657979b75deebcba24d2bb07b
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-01-11 15:35:58 +0000

    bugfix. create target directories prior to copying
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4494 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/install_deps.sh
+++ admin/Windows/mingw32/install_deps.sh
@@ -6,24 +6,34 @@ source pkg_version.sh
 # INSTALL built deps into package root
 
 install_bin() {
- ( cp -vR ${BINARY_PATH} ${PACKAGE_ROOT};
-   strip ${STRIP_FLAGS} ${PACKAGE_ROOT}/bin/*.dll )
+ ( mkdir -p ${PACKAGE_ROOT}/${BINARY_DIR}
+   cp -vR ${BINARY_PATH} ${PACKAGE_ROOT};
+   strip ${STRIP_FLAGS} ${PACKAGE_ROOT}/bin/*.dll
+ )
 }
 
 install_lib() {
- ( cp -vR ${LIBRARY_PATH} ${PACKAGE_ROOT} )
+ ( mkdir -p ${PACKAGE_ROOT}/${LIBRARY_DIR}
+   cp -vR ${LIBRARY_PATH} ${PACKAGE_ROOT} 
+ )
 }
 
 install_sharedlib() {
- ( cp -vR ${SHAREDLIB_PATH} ${PACKAGE_ROOT} )
+ ( mkdir -p ${PACKAGE_ROOT}/${SHAREDLIB_DIR}
+   cp -vR ${SHAREDLIB_PATH} ${PACKAGE_ROOT} 
+ )
 }
 
 install_staticlib() {
- ( cp -vR ${STATICLIBRARY_PATH} ${PACKAGE_ROOT} )
+ ( mkdir -p ${PACKAGE_ROOT}/${STATICLIB_DIR}
+   cp -vR ${STATICLIBRARY_PATH} ${PACKAGE_ROOT} 
+ )
 }
 
 install_include() {
- ( cp -vR ${INCLUDE_PATH} ${PACKAGE_ROOT} )
+ ( mkdir -p ${PACKAGE_ROOT}/${INCLUDE_DIR}
+   cp -vR ${INCLUDE_PATH} ${PACKAGE_ROOT}
+ )
 }
 
 install_bin;

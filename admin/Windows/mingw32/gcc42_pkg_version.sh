8c72133c5fc0e9561176b2195828cb6b4e5dee1b
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-01-14 21:03:51 +0000

    fix pkg release to -2 for gcc-4.2.1 build
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4504 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/gcc42_pkg_version.sh
+++ admin/Windows/mingw32/gcc42_pkg_version.sh
@@ -2,7 +2,7 @@
 
 # Version of Octave we are building & packaging
 PKG_VER=2.9.17
-PKG_REL=1
+PKG_REL=2
 
 # directory the package is installed to
 PACKAGE_ROOT=/opt/octave_mingw32_gcc42/${PKG_VER}-${PKG_REL}

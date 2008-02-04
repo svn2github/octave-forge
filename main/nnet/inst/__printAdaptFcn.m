c911d9124e1165ab7c9a39c4d017db19141b088e
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-04 09:06:39 +0000

    More copyright fixes
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4579 fc592f45-0881-4044-91ff-c96495a54512

--- main/nnet/inst/__printAdaptFcn.m
+++ main/nnet/inst/__printAdaptFcn.m
@@ -1,8 +1,6 @@
 ## Copyright (C) 2006 Michel D. Schmid <michaelschmid@users.sourceforge.net>
 ##
-## This file is part of Octave.
-##
-## Octave is free software; you can redistribute it and/or modify it
+## This program is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2, or (at your option)
 ## any later version.

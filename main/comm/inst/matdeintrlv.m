1e15ccc7d849d242906f2651936d15a2cc83c8fc
sis-sou <sis-sou@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-08 10:52:28 +0000

    Initial commit into SVN.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4611 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/comm/inst/matdeintrlv.m
@@ -0,0 +1,27 @@
+## Copyright (C) 2008  Sylvain Pelissier   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{intrlvd} =} matintrlv(@var{data},@var{Nrows},@var{Ncols})
+##	Restore elements of @var{data} with a tempory matrix of size Nrows by Ncols.
+## @seealso{matintrlv}
+## @end deftypefn
+
+function intrlvd = matdeintrlv(data,Nrows,Ncols)
+	if(nargin < 3 || nargin >3)
+		error('usage : interlvd = matdeintrlv(data,Nrows,Ncols)');
+	end
+	intrlvd = matintrlv(data,Ncols,Nrows);

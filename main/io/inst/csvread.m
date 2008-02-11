a8f1229615731ed1f5914d9861c09246ad39080c
wsloand <wsloand@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-11 04:08:44 +0000

    csvread.m, csvwrite.m: added docstrings
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4615 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/csvread.m
+++ main/io/inst/csvread.m
@@ -1,3 +1,27 @@
-## csvread(file,...) -> dlmread(file,',',...)
+## Copyright (C) 2001, 2008 Paul Kienzle
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {x} = csvread (@var{filename})
+## Read the matrix @var{x} from a file.
+##
+## This function is equivalent to dlmread (@var{filename}, "," , ...)
+##
+## @seealso{dlmread, dlmwrite, csvwrite, csv2cell}
+## @end deftypefn
+
 function m = csvread(f,varargin)
   m = dlmread(f,',',varargin{:});
--- main/io/inst/csvwrite.m
+++ main/io/inst/csvwrite.m
@@ -1,3 +1,27 @@
-## csvwrite(f,m,...) -> dlmwrite(f,m,',',...)
+## Copyright (C) 2001, 2008 Paul Kienzle
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {x} = csvwrite (@var{filename}, @var{x})
+## Write the matrix @var{x} to a file.
+##
+## This function is equivalent to dlmwrite(@var{filename},@var{x},",",...)
+##
+## @seealso{dlmread, dlmwrite, csvread, csv2cell}
+## @end deftypefn
+
 function m = csvwrite(f,m,varargin)
   dlmwrite(f,m,',',varargin{:});

cc8f6b0a717937be4d13c12556eb3b617943965e
highegg <highegg@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-18 15:15:56 +0000

    correction & code style adjustment to lookup.cc
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4658 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/src/lookup.cc
+++ main/general/src/lookup.cc
@@ -1,28 +1,37 @@
-// Copyright (C) 2008 VZLU Prague a.s., Czech Republic
-//
+/*
+
+Copyright (C) 2008 VZLU Prague a.s., Czech Republic
+
+This file is part of Octave.
+
+Octave is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at
+your option) any later version.
+
+Octave is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Octave; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+
+*/
+
 // Author: Jaroslav Hajek <highegg@gmail.com>
-//
-// This file is part of Octave.
-//
-// Octave is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3 of the License, or (at
-// your option) any later version.
-//
-// Octave is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Octave; see the file COPYING.  If not, see
-// <http://www.gnu.org/licenses/>.
-//
+
 
 #include <oct.h>
 
 // use this to select an appropriate output index type
-#ifdef USE_64_BIT_IDX_T
+
+#define RETURN_REAL_INDICES
+#ifdef RETURN_REAL_INDICES
+typedef double ret_idx_type;
+typedef NDArray ret_idx_array;
+#elif USE_64_BIT_IDX_T
 typedef octave_uint64 ret_idx_type;
 typedef uint64NDArray ret_idx_array;
 #else 
@@ -30,21 +39,36 @@ typedef octave_uint32 ret_idx_type;
 typedef uint32NDArray ret_idx_array;
 #endif
 
-
 // simple function templates & specialization is used
 // to avoid writing two versions of the algorithm
 
 // generic comparison
 template<bool rev>
 inline bool dcomp(const double& a,const double& b);
+template<bool rev>
+inline bool dcompe(const double& a,const double& b);
 // normal comparison
 template<>
 inline bool dcomp<false>(const double& a,const double& b)
-{ return a < b; }
+{ 
+  return a < b; 
+}
+template<>
+inline bool dcompe<false>(const double& a,const double& b)
+{ 
+  return a <= b; 
+}
 // reversed comparison
 template<>
 inline bool dcomp<true>(const double& a,const double& b)
-{ return a > b; }
+{ 
+  return a > b; 
+}
+template<>
+inline bool dcompe<true>(const double& a,const double& b)
+{ 
+  return a >= b; 
+}
 
 template<bool rev>
 static void do_seq_lookup(const double *begin,const double *end,
@@ -54,10 +78,11 @@ static void do_seq_lookup(const double *begin,const double *end,
   const double *first, *last;
   size_t cur;
   // the trivial case of empty array
-  if (begin == end) {
-    for(;nvals;--nvals) *(idx++) = 0;
-    return;
-  }
+  if (begin == end) 
+    {
+      for(;nvals;--nvals) *(idx++) = 0;
+      return;
+    }
   // initialize 
   last = end;
   first = begin;
@@ -66,66 +91,78 @@ bin_search:
   {
     size_t dist = last - first, half;
 
-    while (dist > 0) {
-      half = dist >> 1;
-      last = first + half;
-      if (dcomp<rev>(*last,*vals)) {
-        first = last + 1;
-        dist -= (half + 1);
+    while (dist > 0) 
+      {
+        half = dist >> 1;
+        last = first + half;
+        if (dcomp<rev>(*last,*vals)) 
+          {
+            first = last + 1;
+            dist -= (half + 1);
+          }
+        else
+          dist = half;
       }
-      else
-        dist = half;
-    }
     last = first;
   }
 
 store_cur:
   cur = last - begin;
 
-  if (last == begin) {
-    // leftmost interval 
-    while (nvals) {
-      if (dcomp<rev>(*last,*vals)) goto search_forwards;
-      *(idx++) = cur;
-      nvals--; vals++;
+  if (last == begin) 
+    {
+      // leftmost interval 
+      while (nvals) 
+        {
+          if (dcompe<rev>(*last,*vals)) goto search_forwards;
+          *(idx++) = cur;
+          nvals--; vals++;
+        }
+      return;
+    } 
+  else if (last == end) 
+    {
+      // rightmost interval 
+      first = last - 1;
+      while (nvals) 
+      {
+        if (dcomp<rev>(*vals,*first)) goto search_backwards;
+        *(idx++) = cur;
+        nvals--; vals++;
+      }
+      return;
+    } 
+  else 
+    {
+      // inner interval 
+      first = last - 1;
+      while (nvals) 
+        {
+          if (dcompe<rev>(*last,*vals)) goto search_forwards;
+          if (dcomp<rev>(*vals,*first)) goto search_backwards;
+          *(idx++) = cur;
+          nvals--; vals++;
+        }
+      return;
     }
-    return;
-  } else if (last == end) {
-    // rightmost interval 
-    first = last - 1;
-    while (nvals) {
-      if (dcomp<rev>(*vals,*first)) goto search_backwards;
-      *(idx++) = cur;
-      nvals--; vals++;
-    }
-    return;
-  } else {
-    // inner interval 
-    first = last - 1;
-    while (nvals) {
-      if (dcomp<rev>(*last,*vals)) goto search_forwards;
-      if (dcomp<rev>(*vals,*first)) goto search_backwards;
-      *(idx++) = cur;
-      nvals--; vals++;
-    }
-    return;
-  }
 
 search_forwards:
   {
     size_t dist = 1, max = end - last;
-    while(true) {
-      first = last;
-      if (dist < max) 
-        last += dist;
-      else {
-        last = end;
-        break;
+    while(true) 
+      {
+        first = last;
+        if (dist < max) 
+          last += dist;
+        else 
+          {
+            last = end;
+            break;
+          }
+        if (dcomp<rev>(*vals,*last)) break;
+        max -= dist;
+        dist <<= 1;
       }
-      if (!dcomp<rev>(*last,*vals)) break;
-      max -= dist;
-      dist <<= 1;
-    }
     if (!(--dist))
       goto store_cur; // a shortcut
     else
@@ -135,18 +172,20 @@ search_forwards:
 search_backwards:
   {
     size_t dist = 1, max = first - begin;
-    while(true) {
-      last = first;
-      if (dist < max) 
-        first -= dist;
-      else {
-        first = begin;
-        break;
+    while(true) 
+      {
+        last = first;
+        if (dist < max) 
+          first -= dist;
+        else 
+          {
+            first = begin;
+            break;
+          }
+        if (dcompe<rev>(*first,*vals)) break;
+        max -= dist;
+        dist <<= 1;
       }
-      if (!dcomp<rev>(*vals,*first)) break;
-      max -= dist;
-      dist <<= 1;
-    }
     if (!(--dist))
       goto store_cur; // a shortcut
     else
@@ -196,10 +235,11 @@ If complex values are supplied, their magnitudes are used. \n\
 {
   int nargin = args.length();
 
-  if (nargin != 2 || nargout > 1) {
-    print_usage();
-    return octave_value_list();
-  }
+  if (nargin != 2 || nargout > 1) 
+    {
+      print_usage();
+      return octave_value_list();
+    }
 
   octave_value table = args(0), y = args(1);
   NDArray atable;
@@ -209,22 +249,25 @@ If complex values are supplied, their magnitudes are used. \n\
     atable = table.array_value();
   else if (table.is_complex_type())
     atable = table.complex_array_value().abs();
-  else {
-    error("table is not numeric type");
-  }
+  else 
+    {
+      error("table is not numeric type");
+    }
   if (atable.ndims() > 2 || 
-      (atable.rows() > 1 && atable.columns() > 1)) {
-    error("table should be a vector");
-    return octave_value_list();
-  }
+      (atable.rows() > 1 && atable.columns() > 1)) 
+    {
+      error("table should be a vector");
+      return octave_value_list();
+    }
   NDArray ay;
   if (y.is_real_type()) 
     ay = y.array_value();
   else if (y.is_complex_type())
     ay = y.complex_array_value().abs();
-  else {
-    error("y is not numeric type");
-  }
+  else 
+    {
+      error("y is not numeric type");
+    }
 
   ret_idx_array idx(y.dims());
 

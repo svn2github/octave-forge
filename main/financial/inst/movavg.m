2ee6f973209abfbecb20a7e76024840cc13ae479
wsloand <wsloand@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-24 16:24:19 +0000

    movavg.m: made more compatible with matlab, added tests
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4672 fc592f45-0881-4044-91ff-c96495a54512

--- main/financial/inst/movavg.m
+++ main/financial/inst/movavg.m
@@ -19,13 +19,15 @@
 ## @deftypefnx {Function File} {} movavg (asset, lead, lag, alpha)
 ## @deftypefnx {Function File} {[short, long] =} movavg (asset, lead, lag, alpha)
 ##
-## Plot the @var{lead}ing and @var{lag}ging moving average of an
+## Calculate the @var{lead}ing and @var{lag}ging moving average of an
 ## @var{asset}. If given, @var{alpha} is the weighting power of the
 ## delay; 0 (default) is the simple moving average, 0.5 would be the
 ## square root weighted moving average, 1 would be linear, 2 would be
 ## squared, ..., and 'e' is the exponential moving average.
 ##
-## The plots are drawn in the following order: asset, lag, lead.
+## If no output is requested the data is plotted.  The plots are drawn
+## in the following order: asset, lag, lead.  If output is requested, no
+## plot is generated.
 ##
 ## @seealso{bolling, candle, dateaxis, highlow, pointfig}
 ## @end deftypefn
@@ -52,7 +54,7 @@ function [varargout] = movavg (asset, lead, lag, alpha)
   endif
 
   ## Compute the weights
-  if ischar(alpha)
+  if ischar (alpha)
     lead = exp(1:lead);
     lag = exp(1:lag);
   else
@@ -60,34 +62,50 @@ function [varargout] = movavg (asset, lead, lag, alpha)
     lag = (1:lag).^alpha;
   endif
   ## Adjust the weights to equal 1
-  lead = lead / sum(lead);
-  lag = lag / sum(lag);
+  lead = lead / sum (lead);
+  lag = lag / sum (lag);
 
   short = asset;
   long = asset;
-  for i = 1:length(asset)
-    if i < length(lead)
+  for i = 1:length (asset)
+    if i < length (lead)
       ## Compute the run-in period
-      r = length(lead)-i+1:length(lead);
-      short(i) = dot(asset(1:i), lead(r))./sum(lead(r));
+      r = length (lead) - i + 1:length(lead);
+      short(i) = dot (asset(1:i), lead(r))./sum (lead(r));
     else
-      short(i) = dot(asset(i-length(lead)+1:i), lead);
+      short(i) = dot (asset(i - length(lead) + 1:i), lead);
     endif
-    if i < length(lag)
-      r = length(lag)-i+1:length(lag);
-      long(i) = dot(asset(1:i), lag(r))./sum(lag(r));
+    if i < length (lag)
+      r = length (lag) - i + 1:length(lag);
+      long(i) = dot (asset(1:i), lag(r))./sum (lag(r));
     else
-      long(i) = dot(asset(i-length(lag)+1:i), lag);
+      long(i) = dot (asset(i - length(lag) + 1:i), lag);
     endif
   endfor
 
-  plot((1:length(asset))', [asset(:), long(:), short(:)]);
-
   if nargout > 0
     varargout{1} = short;
+  else
+    plot((1:length(asset))', [asset(:), long(:), short(:)]);
   endif
   if nargout > 1
-    varargout{2} = long
+    varargout{2} = long;
   endif
 
 endfunction
+
+## Tests
+%!shared a
+%! a = [1 2 3 2 4 2 1];
+%!test
+%! [s l] = movavg(a, 2, 4);
+%! assert(s, [1 1.5 2.5 2.5 3 3 1.5])
+%! assert(l, [1 1.5 2 2 2.75 2.75 2.25])
+%!test
+%! [s l] = movavg(a', 2, 4);
+%! assert(s, [1;1.5;2.5;2.5;3;3;1.5])
+%! assert(l, [1;1.5;2;2;2.75;2.75;2.25])
+%!test
+%! [s l] = movavg(a, 3, 4, 1);
+%! assert(s, [3 4.8 7 7 9.5 8 5.5]./3, 10*eps)
+%! assert(l, [1 11/7 20/9 2.2 3 2.7 2], 10*eps)

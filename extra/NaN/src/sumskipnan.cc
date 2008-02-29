91861371b9e282ae2a48df7de3edc106370d6c1b
hauberg <hauberg@fc592f45-0881-4044-91ff-c96495a54512>
2008-02-29 09:55:43 +0000

    Clean ups and input checking
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4681 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/src/sumskipnan.cc
+++ extra/NaN/src/sumskipnan.cc
@@ -15,8 +15,8 @@
 //
 //
 
-//	$Id$
-//    Copyright (C) 2005 by Alois Schloegl <a.schloegl@ieee.org>	
+//    $Id$
+//    Copyright (C) 2005 by Alois Schloegl <a.schloegl@ieee.org>    
 //    This is part of the NaN-toolbox 
 //    http://www.dpmi.tu-graz.ac.at/~schloegl/matlab/NaN/
 
@@ -50,96 +50,106 @@ Sums of squares (optional)\n\
 @end table\n\
 @end deftypefn\n") 
 { 
-  int DIM = 0;  
-  int ND;
-  int j, k1, k2, k3, D1, D2, D3, ix1, ix2, N;
-  dim_vector SZ, SZ2;
-  double aux, s, n, s2, s4;
-
   octave_value_list retval; 
 
-  int nargin = args.length ();
+  const int nargin = args.length ();
+  if (nargin == 0)
+    {
+      error("sumskipnan: not enough input arguments");
+      return retval;
+    }
+  else if (nargin > 2)
+    {
+      error("sumskipnan: too many input arguments");
+      return retval;
+    }
 
-  if ( args.length() == 2 ) 
-  {
-	  Matrix xin2( args(1).matrix_value() );
-	  DIM = (int) xin2(0);
-  }
-  else if ( args.length() > 3 ) 
-  {
-    error("SUMSKIPNAN.OCT takes at most 2 arguments");
-    return retval;
-  }
+  // only real double matrices are supported; arrays, integer or complex matrices are not supported (yet)
+  const NDArray xin1 = args(0).array_value();
+  if (error_state)
+    {
+      error("sumskipnan: first input argument must be a real matrix");
+      return retval;
+    }
+  const int ND = xin1.ndims ();    
+  const dim_vector SZ = xin1.dims ();
+  int DIM = 0;
+  if (nargin > 1)
+    {
+      DIM = (int)args(1).scalar_value();
+      if (error_state)
+        {
+          error("sumskipnan: second input argument must be an integer");
+          return retval;
+        }
+      if (DIM < 1 || DIM > ND)
+        {
+          error("sumskipnan: invalid dimension");
+          return retval;
+        }
+    }
+  for (octave_idx_type j = 0; (DIM < 1) && (j < ND); j++)
+    {
+      if (SZ(j)>1) DIM = j+1;
+    }
+  if (DIM < 1) DIM = 1; // in case DIM is still undefined 
+    
+  dim_vector SZ2 = SZ; 
+  for (octave_idx_type j=ND; j<DIM; SZ2(j++)=1) ;
+  if (DIM > ND)
+    {
+      error("sumskipnan: DIM larger than LENGTH(SIZE(X))");
+      return retval;
+    }    
 
-  warning("SUMSKIPNAN.OCT supports only real double matrices.\n");
-  
-	// only real double matrices are supported; arrays, integer or complex matrices are not supported (yet)
-  	NDArray xin1( args(0).array_value() );
-	ND = xin1.ndims ();    
-	SZ = xin1.dims ();
+  octave_idx_type D1 = 1;
+  for (octave_idx_type j=0; j<DIM-1; D1=D1*SZ(j++));  // D1 is the number of elements between two elements along dimension DIM
+  octave_idx_type D2 = SZ(DIM-1); // D2 contains the size along dimension DIM  
+  octave_idx_type D3 = 1;   
+  for (octave_idx_type j=DIM;  j<ND; D3=D3*SZ(j++));     // D3 is the number of blocks containing D1*D2 elements 
+  SZ2(DIM-1) = 1;
 
-    	for (j = 0; (DIM < 1) && (j < ND); j++) 
-		if (SZ(j)>1) DIM = j+1;
+  // Currently, only matrices (2D-arrays) are supported. In future, also N-D arrays should be supported. 
+  NDArray xout1(SZ2);
+  NDArray xout2(SZ2);
+  NDArray xout3(SZ2);
 
-  	if (DIM < 1) DIM=1;		// in case DIM is still undefined 
-	
-	SZ2 = SZ; 
-	for (j=ND; j<DIM; SZ2(j++)=1);
-	if (DIM>ND)
-	{    	error("SUMSKIPNAN.OCT: DIM larger than LENGTH(SIZE(X))");
-		return retval;
-	}	
-			
-   	for (j=0, D1=1; j<DIM-1; D1=D1*SZ(j++)); 	// D1 is the number of elements between two elements along dimension  DIM  
-	D2 = SZ(DIM-1);		// D2 contains the size along dimension DIM 	
-    	for (j=DIM, D3=1;  j<ND; D3=D3*SZ(j++)); 	// D3 is the number of blocks containing D1*D2 elements 
+  // OUTER LOOP: along dimensions > DIM
+  for (octave_idx_type k3 = 0; k3 < D3; k3++)     
+    {
+      octave_idx_type ix2 =  k3*D1;    // index for output 
+      octave_idx_type ix1 = ix2*D2;    // index for input 
 
-  	SZ2(DIM-1) = 1;
+      // Inner LOOP: along dimensions < DIM
+      for (octave_idx_type k1 = 0; k1<D1; k1++, ix1++, ix2++)     
+        {
+          double s  = 0.0;
+          int N  = 0;
+          double s2 = 0.0;
+        
+          // LOOP  along dimension DIM
+          for (octave_idx_type k2=0; k2<D2; k2++)     
+            {
+              double aux = xin1(ix1 + k2*D1);
+              if (aux==aux)    // test for NaN
+                { 
+                  N++; 
+                  s   += aux; 
+                  aux *= aux;
+                  s2  += aux; 
+                  //  s4  += aux*aux; 
+                }    
+            }
+            xout1(ix2) = s;
+            xout2(ix2) = (double) N;
+            xout3(ix2) = s2;
+            //  xout4(ix2) = s4;
+        }
+    }  
 
-	// Currently, only matrices (2D-arrays) are supported. In future, also N-D arrays should be supported. 
-  	NDArray xout1(SZ2);
-  	NDArray xout2(SZ2);
-	NDArray xout3(SZ2);
-//  	NDArray xout4(SZ2);
-
-	// OUTER LOOP: along dimensions > DIM
-	for (k3 = 0; k3<D3; k3++) 	
-    	{
-		ix2 =  k3*D1;	// index for output 
-		ix1 = ix2*D2;	// index for input 
-
-		// Inner LOOP: along dimensions < DIM
-		for (k1 = 0; k1<D1; k1++, ix1++, ix2++) 	
-		{
-		  	s  = 0.0;
-		  	N  = 0;
-		  	s2 = 0.0;
-//		  	s4 = 0.0;
-		
-			// LOOP  along dimension DIM
-	    		for (k2=0; k2<D2; k2++) 	
-			{
-				aux = xin1(ix1 + k2*D1);
-								
-				if (aux==aux)	// test for NaN
-				{ 
-					N++; 
-					s   += aux; 
-					aux *= aux;
-					s2  += aux; 
-//					s4  += aux*aux; 
-				}	
- 			}
-			xout1(ix2) = s;
-			xout2(ix2) = (double) N;
-			xout3(ix2) = s2;
-//			xout4(ix2) = s4;
-		}
-  	}  
-
-	retval.append(octave_value(xout1));
-  	retval.append(octave_value(xout2));
-  	retval.append(octave_value(xout3));
-//  	retval.append(octave_value(xout4));
-
+  retval.append(octave_value(xout1));
+  retval.append(octave_value(xout2));
+  retval.append(octave_value(xout3));
+  // retval.append(octave_value(xout4));
+  return retval;
 }

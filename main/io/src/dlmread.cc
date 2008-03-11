4f3803582e5bc5121cf84af7242a8097c76085df
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2008-03-11 14:09:02 +0000

    Update dlmread to allow spreadsheet style indexing and to only convert to a complex matrix if needed
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4710 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/src/dlmread.cc
+++ main/io/src/dlmread.cc
@@ -31,12 +31,18 @@ DEFUN_DLD (dlmread, args, ,
 @deftypefnx {Loadable Function} {@var{data} =} dlmread (@var{file},@var{sep})\n\
 @deftypefnx {Loadable Function} {@var{data} =} dlmread (@var{file},@var{sep},@var{R0},@var{C0})\n\
 @deftypefnx {Loadable Function} {@var{data} =} dlmread (@var{file},@var{sep},@var{range})\n\
-Read the matrix @var{data} from a text file\n\
-The @var{range} parameter must be a 4 element vector containing  the upper left and lower right corner\n\
-[@var{R0},@var{C0},@var{R1},@var{C1}]\n\
-The lowest index value is zero.\n\
+Read the matrix @var{data} from a text file. If not defined the separator\n\
+between fields is assumed to be a whitespace character. Otherwise the\n\
+separation character is defined by @var{sep}.\n\
+\n\
+Given two scalar arguments @var{r0} and @var{c0}, these define the starting\n\
+row and column of the data to be read. These values are indexed from zero,\n\
+such that the first row corresponds to an index of zero.\n\
+\n\
+The @var{range} parameter must be a 4 element vector containing  the upper\n\
+left and lower right corner @code{[@var{R0},@var{C0},@var{R1},@var{C1}]} or\n\
+a spreadsheet style range such as 'A2..Q15'. The lowest index value is zero.\n\
 @end deftypefn")
-
 {
   octave_value_list retval;
   int nargin = args.length();
@@ -82,7 +88,9 @@ The lowest index value is zero.\n\
   int i = 0, j = 0, r = 1, c = 1, rmax = 0, cmax = 0;
   std::string line;
   std::string str;
-  ComplexMatrix data;
+  Matrix rdata;
+  ComplexMatrix cdata;
+  bool iscmplx = false;
   size_t pos1, pos2;
 
   // take a subset if a range was given
@@ -91,18 +99,73 @@ The lowest index value is zero.\n\
     {
       if (nargin == 3)
 	{
-	  ColumnVector range(args(2).vector_value());
-	  if (range.length() == 4)
+	  if (args(2).is_string ())
 	    {
-	      // double --> unsigned int     
-	      r0 = static_cast<unsigned long> (range(0));
-	      c0 = static_cast<unsigned long> (range(1));
-	      r1 = static_cast<unsigned long> (range(2));
-	      c1 = static_cast<unsigned long> (range(3));
-	    } 
-	  else 
+	      std::string str = args(2).string_value ();
+	      size_t n = str.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	      size_t m = 0;
+	      if (n == NPOS)
+		error ("dlmread: error parsing range");
+	      else
+		{
+		  c0 = 0;
+		  while (m < n)
+		    {
+		      c0 = c0 * 26;
+		      char ch = str.at (m++);
+		      if (ch >= 'a')
+			ch -= 'a';
+		      else
+			ch -= 'A';
+		      c0 += ch;
+		    }
+
+		  str = str.substr (n);
+		  std::istringstream tmp_stream (str);
+		  r0 = static_cast <unsigned long> 
+		    (octave_read_double (tmp_stream)) - 1;
+
+		  str = str.substr (str.find_first_not_of("0123456789."));
+		  n = str.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		  m = 0;
+		  if (n == NPOS)
+		    error ("dlmread: error parsing range");
+		  else
+		    {
+		      c1 = 0;
+		      while (m < n)
+			{
+			  c1 = c1 * 26;
+			  char ch = str.at (m++);
+			  if (ch >= 'a')
+			    ch -= 'a';
+			  else
+			    ch -= 'A';
+			  c1 += ch;
+			}
+
+		      str = str.substr (n);
+		      std::istringstream tmp_stream2 (str);
+		      r1 = static_cast <unsigned long> 
+			(octave_read_double (tmp_stream2)) - 1;
+		    }
+		}
+	    }
+	  else
 	    {
-	      error("dlmread: range must include [R0 C0 R1 C1]");
+	      ColumnVector range(args(2).vector_value());
+	      if (range.length() == 4)
+		{
+		  // double --> unsigned int     
+		  r0 = static_cast<unsigned long> (range(0));
+		  c0 = static_cast<unsigned long> (range(1));
+		  r1 = static_cast<unsigned long> (range(2));
+		  c1 = static_cast<unsigned long> (range(3));
+		} 
+	      else 
+		{
+		  error("dlmread: range must include [R0 C0 R1 C1]");
+		}
 	    }
 	} 
       else if (nargin == 4) 
@@ -115,79 +178,145 @@ The lowest index value is zero.\n\
 	}
     }
 
-  // Skip tge r0 leading lines as these might be a header
-  for (unsigned long i = 0; i < r0; i++)
-    getline (file, line);
-
-  // read in the data one field at a time, growing the data matrix as needed
-  while (getline (file, line))
+  if (!error_state)
     {
-      // skip blank lines for compatibility
-      if (line.find_first_not_of (" \t") == NPOS)
-	continue;
+      // Skip tge r0 leading lines as these might be a header
+      for (unsigned long m = 0; m < r0; m++)
+	getline (file, line);
 
-      r = (r > i + 1 ? r : i + 1);
-      j = 0;
-      pos1 = 0;
-      do {
-	pos2 = line.find_first_of (sep, pos1);
-	str = line.substr (pos1, pos2 - pos1);
+      // read in the data one field at a time, growing the data matrix as needed
+      while (getline (file, line))
+	{
+	  // skip blank lines for compatibility
+	  if (line.find_first_not_of (" \t") == NPOS)
+	    continue;
 
-	if (sepflag && pos2 != NPOS)
-	  // treat consecutive separators as one
-	  pos2 = line.find_first_not_of (sep, pos2) - 1;
+	  r = (r > i + 1 ? r : i + 1);
+	  j = 0;
+	  pos1 = 0;
+	  do {
+	    pos2 = line.find_first_of (sep, pos1);
+	    str = line.substr (pos1, pos2 - pos1);
 
-	c = (c > j + 1 ? c : j + 1);
-	if (r > rmax || c > cmax)
-	  { 
-	    // use resize_and_fill for the case of not-equal length rows
-	    data.resize_and_fill (r, c, 0);
-	    rmax = r;
-	    cmax = c;
-	  }
+	    if (sepflag && pos2 != NPOS)
+	      // treat consecutive separators as one
+	      pos2 = line.find_first_not_of (sep, pos2) - 1;
 
-	std::istringstream tmp_stream (str);
-	double x = octave_read_double (tmp_stream);
-	if (tmp_stream)
-	  {
-	    if (tmp_stream.eof())
-	      data (i, j++) = x;
+	    c = (c > j + 1 ? c : j + 1);
+	    if (r > rmax || c > cmax)
+	      { 
+		// use resize_and_fill for the case of not-equal length rows
+		if (iscmplx)
+		  cdata.resize_and_fill (r, c, 0);
+		else
+		  rdata.resize_and_fill (r, c, 0);
+		rmax = r;
+		cmax = c;
+	      }
+
+	    std::istringstream tmp_stream (str);
+	    double x = octave_read_double (tmp_stream);
+	    if (tmp_stream)
+	      {
+		if (tmp_stream.eof())
+		  if (iscmplx)
+		    cdata (i, j++) = x;
+		  else
+		    rdata (i, j++) = x;
+		else
+		  {
+		    double y = octave_read_double (tmp_stream);
+
+		    if (!iscmplx && y != 0.)
+		      {
+			iscmplx = true;
+			cdata = ComplexMatrix (rdata);
+		      }
+		    
+		    if (iscmplx)
+		      cdata (i, j++) = Complex (x, y);
+		    else
+		      rdata (i, j++) = x;
+		  }
+	      }
+	    else if (iscmplx)
+	      cdata (i, j++) = 0.;
 	    else
-	      data (i, j++) = Complex (x, octave_read_double (tmp_stream));
-	  }
-	else
-	  data (i, j++) = 0.;
+	      rdata (i, j++) = 0.;
 
-	if (pos2 != NPOS)
-	  pos1 = pos2 + 1;
-	else
-	  pos1 = NPOS;
+	    if (pos2 != NPOS)
+	      pos1 = pos2 + 1;
+	    else
+	      pos1 = NPOS;
 
-      } while ( pos1 != NPOS );
-      i++;
-    }
+	  } while ( pos1 != NPOS );
+	  i++;
+	}
  
-  if (nargin > 2)
-    {
-      if (nargin == 3)
+      if (nargin > 2)
 	{
-	  if (r1 >= r)
-	    r1 = r - 1;
-	  if (c1 >= c)
-	    c1 = c - 1;
-	}
-      else if (nargin == 4) 
-	{
-	  // if r1 and c1 are not given, use what was found to be the maximum
-	  r1 = r - 1;
-	  c1 = c - 1;
-	}
+	  if (nargin == 3)
+	    {
+	      if (r1 >= r)
+		r1 = r - 1;
+	      if (c1 >= c)
+		c1 = c - 1;
+	    }
+	  else if (nargin == 4) 
+	    {
+	      // if r1 and c1 are not given, use what was found to be the maximum
+	      r1 = r - 1;
+	      c1 = c - 1;
+	    }
 
-      // now take the subset of the matrix
-      data = data.extract (0, c0, r1, c1);
-      data.resize (r1 + 1, c1 - c0 + 1);
-    }
+	  // now take the subset of the matrix
+	  if (iscmplx)
+	    {
+	      cdata = cdata.extract (0, c0, r1, c1);
+	      cdata.resize (r1 + 1, c1 - c0 + 1);
+	    }
+	  else
+	    {
+	      rdata = rdata.extract (0, c0, r1, c1);
+	      rdata.resize (r1 + 1, c1 - c0 + 1);
+	    }
+	}
   
-  retval(0) = octave_value(data);
+      if (iscmplx)
+	retval(0) = octave_value(cdata);
+      else
+	retval(0) = octave_value(rdata);
+    }
+
   return retval;
 }
+
+/*
+
+%!shared file
+%! file = tmpnam (); 
+%! fid = fopen (file, "wt");
+%! fwrite (fid, "1, 2, 3\n4, 5, 6\n7, 8, 9");
+%! fclose (fid);
+
+%!assert (dlmread (file), [1, 2, 3; 4, 5, 6; 7, 8, 9]);
+%!assert (dlmread (file, ","), [1, 2, 3; 4, 5, 6; 7, 8, 9]);
+%!assert (dlmread (file, ",", [1, 0, 2, 1]), [4, 5; 7, 8]);
+%!assert (dlmread (file, ",", "B1..C2"), [2, 3; 5, 6]);
+%!test
+%! unlink (file);
+
+%!shared file
+%! file = tmpnam (); 
+%! fid = fopen (file, "wt");
+%! fwrite (fid, "1, 2, 3\n4+4i, 5, 6\n7, 8, 9");
+%! fclose (fid);
+
+%!assert (dlmread (file), [1, 2, 3; 4 + 4i, 5, 6; 7, 8, 9]);
+%!assert (dlmread (file, ","), [1, 2, 3; 4 + 4i, 5, 6; 7, 8, 9]);
+%!assert (dlmread (file, ",", [1, 0, 2, 1]), [4 + 4i, 5; 7, 8]);
+%!assert (dlmread (file, ",", "A2..B3"), [4 + 4i, 5; 7, 8]);
+%!test
+%! unlink (file);
+
+*/

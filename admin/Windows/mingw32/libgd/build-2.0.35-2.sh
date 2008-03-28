1ee7fda733d47deaeb46020af8f67646bc8451c6
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-03-28 20:59:50 +0000

    add libgd build script and patch
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4829 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/libgd/build-2.0.35-2.sh
@@ -0,0 +1,94 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=gd
+# Version of Package
+VER=2.0.35
+# Release of (this patched) package
+REL=2
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}.tar.bz2
+TAR_TYPE=j
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL=""
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extraced to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+
+# Make file to use
+MAKEFILE=""
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS="gd.h gdfx.h gd_io.h gdcache.h gdfontg.h gdfontl.h gdfontmb.h gdfonts.h gdfontt.h"
+#INCLUDE_DIR=
+
+source ../gcc42_common.sh
+
+# Directory the lib is built in
+BUILDDIR=".build_mingw32_${VER}-${REL}_gcc${GCC_VER}${GCC_SYS}"
+
+check_pre() { MAKEFILE=""; }
+
+mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+conf()
+{
+   ( cd ${BUILDDIR} && ${TOPDIR}/${SRCDIR}/configure \
+     CC=${CC} \
+     CXX=${CXX} \
+     F77=${F77} \
+     CFLAGS="${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     CXXFLAGS="${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     CPPFLAGS="-DDEFAULT_FONTPATH=\"C:\\\\WINDOWS\\\\FONTS;C:\\\\WINNT\\\\FONTS\" -DPATHSEPARATOR=\";\"" \
+     LDFLAGS="${LDFLAGS}" \
+     --prefix="${PREFIX}" \
+     --enable-static \
+     --enable-shared
+   )
+}
+
+install()
+{
+   install_pre
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/gd.dll ${BINARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/libgd.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/.libs/libgd.a ${STATICLIBRARY_PATH}
+   for a in ${INSTALL_HEADERS}; do ${CP} ${CP_FLAGS} ${SRCDIR}/$a ${INCLUDE_PATH}; done
+   install_post
+}
+
+uninstall()
+{
+   uninstall_pre;
+   ${RM} ${RM_FLAGS} ${BINARY_PATH}/gd.dll
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libgd.dll.a
+   ${RM} ${RM_FLAGS} ${STATICLIBRARY_PATH}/libgd.a
+   for a in ${INSTALL_HEADERS}; do ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/$a; done
+}
+
+all() {
+  download
+  unpack
+  applypatch
+  mkdirs
+  conf
+  build
+  install
+}
+
+main $*

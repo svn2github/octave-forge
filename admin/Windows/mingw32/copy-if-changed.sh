b6528a432cc8c2dc7aff4be19765d5b7a6cc2119
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-04-05 18:27:20 +0000

    update to allow multiple source files in copying
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4859 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/copy-if-changed.sh
+++ admin/Windows/mingw32/copy-if-changed.sh
@@ -3,25 +3,42 @@
 # Like cp $1 $2, but if the files are the same, do nothing.
 # Status is 0 if $2 is changed, 1 otherwise.
 
+#cp -ufvp $*
+#exit
 
 case $1 in
   -*)
      shift;;
 esac
 
-if [ -d $2 ]; then
-   bn=`basename $1`
-   tn=$2/${bn}
-else
-   tn=$2;
-fi
 
-if test -e ${tn}; then
-  if cmp $1 ${tn} > /dev/null; then
-    echo $1 is unchanged
-  else
-    cp -vf $1 $2
-  fi
-else
-  cp -vf $1 $2
-fi
+src="";
+tgt="";
+until [ "0" == "1" ];
+do
+   src="${src} $1"
+   tgt=$2;
+   if [ "$3" == "" ]; then 
+      break;
+   fi
+   shift;
+done;
+
+for a in $src; do 
+   if [ -d $tgt ]; then
+      bn=`basename $a`
+      tn=$tgt/${bn}
+   else
+      tn=$tgt;
+   fi
+   
+   if test -e ${tn}; then
+      if cmp $a ${tn} > /dev/null; then
+         echo $a is unchanged
+      else
+         cp -vfp $a $tgt
+      fi
+   else
+      cp -vfp $a $tgt
+   fi
+done

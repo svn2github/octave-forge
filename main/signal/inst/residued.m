4bd5c38aa6093244ee6838293ef5653670356706
adb014 <adb014@fc592f45-0881-4044-91ff-c96495a54512>
2008-04-09 13:46:45 +0000

    Allow for swapped return values in residue function, and relax a tolerance in sgolay test
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4895 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/inst/residued.m
+++ main/signal/inst/residued.m
@@ -157,5 +157,5 @@ if f2, error('f2 not empty as expected'); end
 %!test 
 %! B=[1 0 0 0 1]; A=[1 0 0 0 -1];
 %! [r,p,f,m] = residued(B,A);
-%! assert({r,p,f,m},{[-1/2;j/2;1/2;-j/2],[-1;j;1,-j],1,[1;1;1;1]},100*eps);
+%! assert({r,p,f,m},{[-1/2;1/2;-j/2;j/2],[-1;1;-j;j],1,[1;1;1;1]},100*eps);
 %  Verified in maxima: ratsimp(%I/2/(1-%I * d) - %I/2/(1+%I * d)); etc.

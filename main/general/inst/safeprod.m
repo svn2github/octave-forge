c74768a3c27e44c69da5e0650bb73965b1a74b55
highegg <highegg@fc592f45-0881-4044-91ff-c96495a54512>
2008-04-23 14:12:05 +0000

    fixes to safeprod
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@4981 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/inst/safeprod.m
+++ main/general/inst/safeprod.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2008 Jaroslav Hajek
+## Copyright (C) 2008 VZLU Prague, a.s., Czech Republic
 ## 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -11,35 +11,44 @@
 ## GNU General Public License for more details.
 ## 
 ## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
+## along with this program; see the file COPYING.  If not, see
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn{Function File} @var{p} = safeprod (@var{x}, @var{dim})
-## @deftypefnx{Function File} [@var{p}, @var{e}] = safeprod (@var{x}, @var{dim})
-## This functions sums the array @var{x} along the dimension specified
-## by @var{dim}, analogically to @code{sum}, but avoid overflows and underflows 
-## if possible. If called with 2 output arguments, @var{p} and @var{e} are
-## computed so that the product is @code{@var{p} * 2^@var{e}}.
+## @deftypefn{Function File} {@var{p} =} safeprod (@var{x}, @var{dim})
+## @deftypefnx{Function File} {[@var{p}, @var{e}] =} safeprod (@var{x}, @var{dim})
+## This function forms product(s) of elements of the array @var{x} along the dimension 
+## specified by @var{dim}, analogically to @code{prod}, but avoids overflows and underflows 
+## if possible. If called with 2 output arguments, @var{p} and @var{e} are computed 
+## so that the product is @code{@var{p} * 2^@var{e}}.
 ## @seealso{prod,log2}
 ## @end deftypefn
 
-## Author: Jaroslav Hajek highegg@gmail.com
+## Author: Jaroslav Hajek <highegg@gmail.com>
 ## Created: 2008-04-22
 
-function [p, e] = safeprod (x, dim = 1)
+function [p, e] = safeprod (x, dim)
   if (nargin < 1 || nargin > 2)
     print_usage ();
   endif
 
+  if (nargin < 2)
+    if (rows(x) == 1)
+      dim = 2;
+    else
+      dim = 1;
+    endif
+  endif
+
+  % try the normal algorithm first
   if (nargout < 2) 
     p = prod (x, dim);
   else
     p = 0;
   endif
 
-  # 0, Inf and NaN are possibly problematic results. If detected, use the safe
-  # formula.
+  % 0, Inf and NaN are possibly problematic results. If detected, use the safe
+  % formula.
 
   flag = (p == 0 | ! isfinite (p));
 

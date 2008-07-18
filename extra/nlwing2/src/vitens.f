2102929669f04c1aca31e817bca81fdb4e0c4c91
highegg <highegg@fc592f45-0881-4044-91ff-c96495a54512>
2008-07-18 06:44:57 +0000

    update GPL2 files to GPL3
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5179 fc592f45-0881-4044-91ff-c96495a54512

--- extra/nlwing2/src/vitens.f
+++ extra/nlwing2/src/vitens.f
@@ -6,7 +6,7 @@ c This file is part of NLWing2.
 c 
 c NLWing2 is free software; you can redistribute it and/or modify
 c it under the terms of the GNU General Public License as published by
-c the Free Software Foundation; either version 2 of the License, or
+c the Free Software Foundation; either version 3 of the License, or
 c (at your option) any later version.
 c 
 c This program is distributed in the hope that it will be useful,

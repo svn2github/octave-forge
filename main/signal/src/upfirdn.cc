12856c157b7a091b4756ae2de9a0308a0b599786
hauberg <hauberg@fc592f45-0881-4044-91ff-c96495a54512>
2008-08-18 18:20:17 +0000

    Also handle complex input
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5247 fc592f45-0881-4044-91ff-c96495a54512

--- main/signal/src/upfirdn.cc
+++ main/signal/src/upfirdn.cc
@@ -23,6 +23,69 @@
 #include <octave/quit.h>
 #include <octave/variables.h>
 
+template<class MT, class ET>
+MT upfirdn (MT &x, ColumnVector &h, octave_idx_type p, octave_idx_type q)
+{
+  octave_idx_type rx = x.rows ();
+  octave_idx_type cx = x.columns ();
+  bool isrowvector = false;
+
+  if ((rx == 1) && (cx > 1)) // if row vector, transpose to column vector
+    {
+      x = x.transpose ();
+      rx = x.rows ();
+      cx = x.columns ();
+      isrowvector = true;      
+    }
+
+  octave_idx_type Lh = h.length ();
+  const double r = p/(static_cast<double> (q));
+
+  const octave_idx_type Ly = ceil (static_cast<double> ((rx-1)*p + Lh) / 
+                                   static_cast<double> (q));
+
+  MT y (Ly, cx, 0.0);
+  
+  for (octave_idx_type c = 0; c < cx; c++)
+    {
+      octave_idx_type m = 0;
+      while (m < Ly)
+        {
+          const octave_idx_type n = floor (m/r);
+          const octave_idx_type lm = (m * q) % p;
+          octave_idx_type k = 0;
+          ET accum;
+          accum = 0.0;
+          do
+            {
+              octave_idx_type ix = n - k;
+              if (ix >= rx)
+                {
+                  k ++;
+                  continue;
+                }
+              
+              const octave_idx_type ih = k * p + lm;
+              if ((ih >= Lh) | (ix < 0))
+                break;
+              
+              accum += h (ih) * x (ix, c);
+              k++;
+            }
+          while (1);
+          
+          y (m, c) = accum;
+          m ++;
+        }
+
+    }
+    
+  if (isrowvector)
+    y = y.transpose ();
+
+  return y;
+}
+
 DEFUN_DLD (upfirdn, args,,
   "-*- texinfo -*-\n\
 @deftypefn {Loadable Function} {@var{y} =} upfirdn (@var{x},@var{h},@var{p},@var{q})\n\
@@ -31,109 +94,64 @@ Upsample, FIR filtering and downsample.@*\n\
 {
   octave_value_list retval;
   
-  int nargin = args.length ();
+  const int nargin = args.length ();
   
   if (nargin < 4)
     {
-      print_usage();
+      print_usage ();
       return retval;
     }
 
-  Matrix x ( args(0).matrix_value () );
+  ColumnVector h (args (1).vector_value ());
 
   if (error_state) 
     { 
-      gripe_wrong_type_arg("upfirdn",args(0));
+      gripe_wrong_type_arg ("upfirdn", args (1));
       return retval; 
     }
 
-  octave_idx_type rx=x.rows();
-  octave_idx_type cx=x.columns();
+  octave_idx_type p = args (2).idx_type_value ();
 
-  bool isrowvector=false;
+  if (error_state) 
+    { 
+      gripe_wrong_type_arg ("upfirdn", args (2));
+      return retval; 
+    }
 
-  if ((rx==1)&&(cx>1)) // if row vector, transpose to column vector
+  octave_idx_type q = args (3).idx_type_value ();
+
+  if (error_state) 
+    { 
+      gripe_wrong_type_arg ("upfirdn", args (3));
+      return retval;
+    }
+
+  // Do the dispatching
+  if (args (0).is_real_matrix ())
     {
-      x=x.transpose();
-      rx=x.rows();
-      cx=x.columns();
-      isrowvector=true;      
+      Matrix x = args (0).matrix_value ();
+      if (error_state) 
+        { 
+          gripe_wrong_type_arg ("upfirdn", args (0));
+          return retval; 
+        }
+
+      Matrix y = upfirdn<Matrix, double> (x, h, p, q);
+      retval (0) = y;
     }
-
-  octave_idx_type Lx=rx;
-
-  ColumnVector h( args(1).vector_value() );
-
-  if (error_state) 
-    { 
-      gripe_wrong_type_arg("upfirdn",args(1));
-      return retval; 
-    }
-
-  octave_idx_type Lh=h.length();
-
-  octave_idx_type p=args(2).idx_type_value();
-
-  if (error_state) 
-    { 
-      gripe_wrong_type_arg("upfirdn",args(2));
-      return retval; 
-    }
-
-  octave_idx_type q=args(3).idx_type_value();
-
-  if (error_state) 
-    { 
-      gripe_wrong_type_arg("upfirdn",args(3));
-      return retval; 
-    }
-
-  double r=p/(static_cast<double>(q));
-
-  octave_idx_type Ly= ceil( static_cast<double>((Lx-1)*p + Lh) / 
-			    static_cast<double>(q));
-
-  Matrix y(Ly,cx,0.0);
-  
-  for (octave_idx_type c=0; c<cx; c++)
+  else if (args (0).is_complex_matrix ())
     {
+      ComplexMatrix x = args (0).complex_matrix_value ();
+      if (error_state) 
+        { 
+          gripe_wrong_type_arg ("upfirdn", args (0));
+          return retval; 
+        }
 
-      octave_idx_type m=0;
-      while (m<Ly)
-	{
-	  octave_idx_type n=floor(m/r);
-	  octave_idx_type lm=(m*q)%p;
-	  octave_idx_type k=0;
-	  double accum=0.0;
-	  do
-	    {
-	      octave_idx_type ix=n-k;
-	      if (ix>=Lx)
-		{
-		  k++;
-		  continue;
-		}
-	      
-	      octave_idx_type ih=k*p+lm;
-	      if ((ih>=Lh)|(ix<0))
-		break;
-	      
-	      accum += h(ih)*x(ix,c);
-	      k++;
-	    }
-	  while(1);
-	  
-	  y(m,c)=accum;
-	  m++;
-	}
-
+      ComplexMatrix y = upfirdn<ComplexMatrix, std::complex<double> > (x, h, p, q);
+      retval (0) = y;
     }
-
-  if (isrowvector)
-    y=y.transpose();
-
-  retval(0)=y;
  
   return retval;
 }  
-  
+

b89ae28e9902fecb030161d83307edea28b00fe7
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2008-09-17 07:11:39 +0000

    evaluate with default method if flag is empty
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5300 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/inst/nanstd.m
+++ extra/NaN/inst/nanstd.m
@@ -17,9 +17,14 @@ function [y] = nanstd(i,FLAG,DIM)
 % 
 % see also: SUM, SUMSKIPNAN, NANSUM, STD
 
+%    Copyright (C) 2000-2003,2006,2008 by Alois Schloegl <a.schloegl@ieee.org>	
+%    $Id$
+%    This is part of the NaN-toolbox. For more details see
+%    	   http://www.dpmi.tu-graz.ac.at/~schloegl/matlab/NaN/
+
 %    This program is free software; you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation; either version 2 of the License, or
+%    the Free Software Foundation; either version 3 of the License, or
 %    (at your option) any later version.
 %
 %    This program is distributed in the hope that it will be useful,
@@ -29,10 +34,6 @@ function [y] = nanstd(i,FLAG,DIM)
 %
 %    You should have received a copy of the GNU General Public License
 %    along with this program; If not, see <http://www.gnu.org/licenses/>.
-
-%    Copyright (C) 2000-2003,2006 by Alois Schloegl <a.schloegl@ieee.org>	
-%	$Id$
-
 
 if nargin<2,
 	FLAG = 0; 
@@ -48,9 +49,10 @@ end;
 
 [y,n] = sumskipnan(center(i,DIM).^2,DIM);
 
-if (FLAG~=1)
-        y = sqrt(y./max(n-1,0));	% normalize with N-1
-else
+if (FLAG==1)
         y = sqrt(y)./n;	% normalize with N
+else
+	% default method
+        y = sqrt(y./max(n-1,0));	% normalize with N-1
 end;
 

d1257448c6de0456633ddedcf79df9f25ecd7b9d
michaelschmid <michaelschmid@fc592f45-0881-4044-91ff-c96495a54512>
2008-10-15 13:42:27 +0000

    modified   doc/latex/perl/createFunctionIndexDocu.pl
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5380 fc592f45-0881-4044-91ff-c96495a54512

--- main/nnet/doc/latex/perl/createFunctionIndexDocu.pl
+++ main/nnet/doc/latex/perl/createFunctionIndexDocu.pl
@@ -44,13 +44,13 @@ my @FuncNames = @FuncReferences;
 # now I have to remove the path and file extension
 foreach (@FuncNames)
 {
- 	s/\.m//g;
- 	s/$Dir\///g;
-	print "$_\n";
-#	sleep(1);
+ 	s/\.m//g;   # removes file ending
+ 	s/$Dir\///g; # removes any parts of the file & directory path
 }
 my @input = ();
 my @calledFunction = ();
+my $deleteFile = 1;
+my $anzahl_elemente = 0;
 # now analyze functions to see which other functions are called
 foreach my $FuncRef (@FuncReferences)
 {
@@ -58,7 +58,8 @@ foreach my $FuncRef (@FuncReferences)
   open(FILE,$FuncRef); # opens e.g. 'subset.m'
   @input = <FILE>; # read the complete content of the file to @input
   # now remove all comment and test lines ..
-  @input = grep s/^\s+//, @input; # removes white-space characters at the beginning of a line
+  @input = grep s/^\s+//, @input; # removes white-space characters at the
+  								  # beginning of a line
   @input = grep /^[^#|%]/ , @input; # removes lines starting with # or %
 
 #   foreach (@input)
@@ -75,9 +76,12 @@ foreach my $FuncRef (@FuncReferences)
       # now search for each $FuncName
       # inside of the @input array
       # if one is found, put them to a list
+      # \todo ERRORs are still available
+      # if the $FuncName occures in another context!!
+      # such lines should be deleted!
 	  if (grep /$FuncName/,@input)
 	  {
-        push (@calledFunction, "$FuncName");
+          push (@calledFunction, "$FuncName");
       }
     }else{
       $actFuncName = $FuncName;
@@ -87,153 +91,82 @@ foreach my $FuncRef (@FuncReferences)
   undef my %saw;
   @saw{@calledFunction} = ();
   @calledFunction = sort keys %saw;  # remove sort if undesired
-  open(DAT,">>$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
-  print DAT "\\begin{verbatim}\n";
-  print DAT "Function-File: $actFuncName\n";
-  print DAT "=============================\n";
-  foreach (@calledFunction){
-	print DAT "$_\n";
+  if (-e "$funcIndDir" . "/" . "$mainLatexFuncIndexFile")
+  {
+    # if the file exist, delete it
+    if ($deleteFile){
+      unlink("$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
+      $deleteFile = 0;
+      open(DAT,">$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
+      print DAT "\\begin{longtable}{ll}\n";
+      print DAT "\\textbf{main function}	&	\\textbf{called function} \\\\ \n";
+      print DAT "\\hline\n";
+	  $_ = $actFuncName;
+	  s/_/\\_/g;     # put a backslash for each underscore
+      print DAT "$_	";
+      $anzahl_elemente = @calledFunction;
+      if ($anzahl_elemente > 0)
+      {
+      	foreach (@calledFunction){          
+          	  s/_/\\_/g;  # put a backslash for each underscore
+	    	print DAT " &	$_\\\\ \n";
+  	  	}
+	  }else{
+        print DAT " & \\\\ \n";
+      }
+
+  	  close(DAT);
+
+    }else{
+      # file doesn't have to be deleted
+      open(DAT,">>$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
+      print DAT "\\hline\n";
+      	  $_ = $actFuncName;
+	  s/_/\\_/g;     # put a backslash for each underscore
+      print DAT "$_	";
+      $anzahl_elemente = @calledFunction;
+      if ($anzahl_elemente > 0)
+      {
+      	foreach (@calledFunction){
+          s/_/\\_/g;  # put a backslash for each underscore
+	    	print DAT " &	$_\\\\ \n";
+  	  	}
+	  }else{
+        print DAT " & \\\\ \n";
+      }
+  	    close(DAT);
+    }
+
+  }else{
+    # File doesn't exist yet
+    open(DAT,">$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
+    print DAT "\\begin{longtable}{l l}\n";
+    print DAT "\\textbf{main function}	&	\\textbf{called function} \\\\ \n";
+    print DAT "\\hline\n";
+          	  $_ = $actFuncName;
+	  s/_/\\_/g;    # put a backslash for each underscore
+      print DAT "$_	";
+      $anzahl_elemente = @calledFunction;
+      if ($anzahl_elemente > 0)
+      {
+      	foreach (@calledFunction){
+            s/_/\\_/g;  # put a backslash for each underscore
+	    	print DAT " &	$_\\\\ \n";
+  	  	}
+	  }else{
+        print DAT " & \\\\ \n";
+      }
+  	  close(DAT);
   }
-  print DAT "\\end{verbatim}\n";
-  close(DAT);
+#  print DAT "Function-File: $actFuncName\n";
+#  print DAT "=============================\n";
+#   foreach (@calledFunction){
+# 	print DAT "$_\n";
+#   }
+
   @calledFunction = ();
 
 }
-#
-# # analyze file structure
-# my $nFiles = @DirArray;
-# if ($nFiles>=1){ # if $nFiles==0 readDirTree will die
-# 
-#   # if we are in this brand we have to parse all files
-#   # ending with *.m for function reference! As reference
-#   # functions, we will take all the functions inside of
-#   # nnet/inst
-#   
-#   #\todo, an dieser stelle muss weiter gearbeitet werden !!!
-#   foreach (@DirArray){
-# 	  # open file and search for lines beginning with
-# 	  # !%
-# 	  if (-d $_){
-#         # if directory, do nothing
-#       }else{
-#       	print "$_\n";
-#       	sleep(0.1);
-#       	
-# 	    open(FILE,$_) or die "File $_ not found!\n";
-# 	    my @fileContent = <FILE>;
-# 	    chomp(@fileContent);
-# 	    $nTestLines = @fileContent;
-#         my @temp = grep /^%!/, @fileContent;
-#         my $nTemp = @temp;
-#         if ($nTemp>0){ # this means, 
-#                        # test lines are available
-#           # now create latex files without header
-#           # take the same name like the *.m file
-#           # and save in specified directory $testDir
-#           # with file extens in $testFileExt
-#           # use verbatim environment
-#           @filesName = split(/\//, $_);
-#           $fileName = $filesName[$#filesName];
-#           # now remove file extension .m
-#           @filesName = split(/\./,$fileName);
-#           $savePath = ("$testDir" . "\\\\" . "$filesName[0]." . "$testFileExt");
-#           open(OUTFILE, ">$savePath");
-#           my $i = 0;
-#           print OUTFILE "\\begin{verbatim}\n";
-#           while ($i < $nTestLines){
-#             if ($fileContent[$i]=~/^%!/){
-#               print OUTFILE "$fileContent[$i]\n";            
-#             }
-#             $i++;
-# 		  } # END while ($i <= $#fileContent)
-# 		  print OUTFILE "\\end{verbatim}\n";
-# 		  close OUTFILE;
-# 		  
-# 		  ## now set entries in the main test latex file ..
-#           my $mainTestFile = ("$testDir" . "\\\\" . "$mainLatexTestFile");
-#           if ($m==0){
-#             open(TESTFILE,">$mainTestFile") or die "Could not found file $mainTestFile!\n";
-#             print TESTFILE "\\chapter{$chapter}\n\n";
-#             
-# 			# test if back slash needed
-#             # a back slash is needed if the sign "underscore"
-#             # is used in the file name. This happens at each
-#             # "sub function". There are two underscores!
-#             my $tempString = "";
-#             my $oldString = $filesName[0];
-# 			$_ = $filesName[0];
-# 			s/_/\\_/g; # s/ : search & replace pattern (everything between / /)
-# 					   # here: search underscore
-# 					   # if found, replace with \_ (two back slashes are needed
-# 					   # to get \_ as sign)
-# 					   # /g means: each occurence of pattern, otherwise, only one _
-# 					   # will be replaced
-# 
-# 
-#            #  if ($filesName[0]=~/_/){
-# #             	my $pos = 0; #temp Position
-# #             	my $posBefore = 0;
-# #             	while ($pos < (length $filesName[0]) and $pos != -1){
-# #             		$pos = index($filesName[0], "_", $pos);
-# #             		if ($pos != -1 ){
-# #             			$tempString = substr($filesName[0],$posBefore,$pos) . "\\" . substr($filesName[0],$pos,length $filesName[0]);
-# #             			$posBefore = $pos;
-# #             			$pos++;
-# #             	    }
-# #             	}# END while ($pos < ...)
-# #             	$filesName[0] = $tempString;
-# #             }
-# 			print "test file name: $_\n";
-#             print TESTFILE "\\section{$_}\n";
-#             $tempString = $relTestDir . $oldString;
-#             print TESTFILE "\\input{$tempString}\n";
-#           }else{
-#             open(TESTFILE,">>$mainTestFile") or die "Could not found file $mainTestFile!\n";
-#             # test if back slash needed
-#             my $tempString = "";
-#             my $oldString = $filesName[0];
-#             # test if back slash needed
-#             # a back slash is needed if the sign "underscore"
-#             # is used in the file name. This happens at each
-#             # "sub function". There are two underscores!
-#             my $tempString = "";
-#             my $oldString = $filesName[0];
-# 			$_ = $filesName[0];
-# 			s/_/\\_/g; # s/ : search & replace pattern (everything between / /)
-# 					   # here: search underscore
-# 					   # if found, replace with \_ (two back slashes are needed
-# 					   # to get \_ as sign)
-# 					   # /g means: each occurence of pattern, otherwise, only one _
-# 					   # will be replaced
-#             # if ($filesName[0]=~/_/){
-# #             	my $pos = 0; #temp Position
-# #             	my $posBefore = 0;
-# #             	while ($pos < (length $filesName[0]) and $pos != -1){
-# #             		$pos = index($filesName[0], "_", $pos);
-# #             		if ($pos != -1 ){
-# #             			$tempString = substr($filesName[0],$posBefore,$pos) . "\\" . substr($filesName[0],$pos,length $filesName[0]);
-# #             			$pos++;
-# #             	    }
-# #             	}# END while ($pos < ...)
-# #             	$filesName[0] = $tempString;
-# #             }
-#             
-#             print TESTFILE "\\section{$_}\n";
-#             $tempString = $relTestDir . $oldString;
-#             print TESTFILE "\\input{$tempString}\n";
-#           }
-#           $m++;
-#           close TESTFILE;
-#   
-# 		  
-#         }# END if($nTemp>0)
-#         close FILE;
-#                
-# 	  }# END if(-d $_)
-# 	  
-# 	}# END foreach (@DirArray)
-# 
-# }else{ # if $nFiles==0
-#   print "No file found with valid file extension: .$fileExt.\n";
-#   die;
-# }
+  open(DAT,">>$funcIndDir" . "/" . "$mainLatexFuncIndexFile");
+  print DAT "\\end{longtable}\n";
+  close(DAT);

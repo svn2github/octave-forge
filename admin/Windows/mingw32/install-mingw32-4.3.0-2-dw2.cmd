5f6facb14016ff4b7d44eb4deb538e540f1c1bfd
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-10-30 15:25:08 +0000

    add cmd scripts to setup msys and mingw32 environment for building octave
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5393 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/install-mingw32-4.3.0-2-dw2.cmd
@@ -0,0 +1,79 @@
+@setlocal
+
+@rem This script installs GCC Mingw32-TDM 4.3.0-2 DW2 suitable for building
+@rem Octave using mingw32-GCC on win32
+@rem
+@rem Usage: INSTALL-MINGW32-4.3.0-2-dw2.CMD <target-path>
+@rem
+@rem The target (=installation) directory must exist!
+@rem
+@rem This script requires LIBARCHIVE available from gnuwin32.sourceforge.net
+@rem 
+@rem Download the following files PRIOR to executing this script into the directory MINGW32-ENV:
+@rem    http://downloads.sourceforge.net/tdm-gcc/gcc-4.3.0-tdm-2-dw2-core.tar.gz
+@rem    http://downloads.sourceforge.net/tdm-gcc/gcc-4.3.0-tdm-2-dw2-fortran.tar.gz
+@rem    http://downloads.sourceforge.net/tdm-gcc/gcc-4.3.0-tdm-2-dw2-g++.tar.gz
+@rem    http://downloads.sourceforge.net/mingw/binutils-2.18.50-20080109-2.tar.gz
+@rem    http://downloads.sourceforge.net/mingw/mingw32-make-3.81-20080326-3.tar.gz
+@rem    http://downloads.sourceforge.net/mingw/gdb-6.8-mingw-3.tar.bz2
+@rem    http://downloads.sourceforge.net/mingw/w32api-3.12-mingw32-dev.tar.gz
+@rem    http://downloads.sourceforge.net/mingw/mingwrt-3.15.1-mingw32-dev.tar.gz
+@rem    http://downloads.sourceforge.net/mingw/mingw-utils-0.3.tar.gz
+
+
+if %1/==/ goto usage
+
+SET DST=%~1
+
+IF NOT EXIST "%DST%" goto notexist
+
+SET BSDTAR=bsdtar
+SET TAROPT=-x -C "%DST%" -f
+
+SET SRC=mingw32-env
+
+SET GCCVER=-4.3.0
+SET GCCREL=-2
+SET GCCVEND=-tdm
+SET GCCSYS=-dw2
+
+%BSDTAR% %TAROPT% %SRC%\gcc%GCCVER%%GCCVEND%%GCCREL%%GCCSYS%-core.tar.gz
+%BSDTAR% %TAROPT% %SRC%\gcc%GCCVER%%GCCVEND%%GCCREL%%GCCSYS%-fortran.tar.gz
+%BSDTAR% %TAROPT% %SRC%\gcc%GCCVER%%GCCVEND%%GCCREL%%GCCSYS%-g++.tar.gz
+
+%BSDTAR% %TAROPT% %SRC%\binutils-2.18.50-20080109-2.tar.gz
+%BSDTAR% %TAROPT% %SRC%\mingw32-make-3.81-20080326-3.tar.gz
+%BSDTAR% %TAROPT% %SRC%\mingwrt-3.15.1-mingw32-dev.tar.gz
+%BSDTAR% %TAROPT% %SRC%\mingw-utils-0.3.tar.gz
+%BSDTAR% %TAROPT% %SRC%\w32api-3.12-mingw32-dev.tar.gz
+
+copy "%DST%\bin\mingw32-g++%GCCSYS%.exe" "%DST%\bin\mingw32-g++%GCCVER%%GCCSYS%.exe"
+copy "%DST%\bin\mingw32-gfortran%GCCSYS%.exe" "%DST%\bin\mingw32-gfortran%GCCVER%%GCCSYS%.exe"
+copy "%DST%\bin\mingw32-c++%GCCSYS%.exe" "%DST%\bin\mingw32-c++%GCCVER%%GCCSYS%.exe"
+copy "%DST%\bin\cpp%GCCSYS%.exe" "%DST%\bin\mingw32-cpp%GCCVER%%GCCSYS%.exe"
+
+del /q "%DST%\bin\cpp%GCCSYS%.exe"
+del /q "%DST%\bin\gcc%GCCSYS%.exe"
+del /q "%DST%\bin\g++%GCCSYS%.exe"
+del /q "%DST%\bin\c++%GCCSYS%.exe"
+del /q "%DST%\bin\gfortran%GCCSYS%.exe"
+
+del /q "%DST%\bin\mingw32-g++%GCCSYS%.exe"
+del /q "%DST%\bin\mingw32-gfortran%GCCSYS%.exe"
+del /q "%DST%\bin\mingw32-c++%GCCSYS%.exe"
+del /q "%DST%\bin\mingw32-gcc%GCCSYS%.exe"
+
+goto :end
+
+:usage
+@echo USAGE %0 ^<TARGET-DIRECTORY^>
+@echo.
+goto :end
+
+:notexist
+@echo ERROR Target Directory "%DST%" dows not exist!
+@echo.
+goto :END
+
+:end
+
---
+++ admin/Windows/mingw32/install-msys-1.0.11.cmd
@@ -0,0 +1,74 @@
+@setlocal
+
+
+@rem This script installs a MSYS-1.0.11 environment suitable for building
+@rem Octave using mingw32-GCC on win32
+@rem
+@rem Usage: INSTALL.CMD <target-path>
+@rem
+@rem The target (=installation) directory must exist!
+@rem
+@rem This script requires LIBARCHIVE available from gnuwin32.sourceforge.net
+@rem 
+@rem Download the following files PRIOR to executing this script into the directory MSYS-ENV:
+@rem   http://downloads.sourceforge.net/mingw/msysCORE-1.0.11-20080826.tar.gz
+@rem   http://downloads.sourceforge.net/mingw/perl-5.6.1-MSYS-1.0.11-1.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/crypt-1.1-1-MSYS-1.0.11-1.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/autoconf2.1-2.13-3-bin.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/autoconf2.5-2.61-1-bin.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/autoconf-4-1-bin.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/automake1.10-1.10-1-bin.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/automake1.9-1.9.6-2-bin.tar.bz2
+@rem   http://downloads.sourceforge.net/mingw/automake-3-1-bin.tar.bz2
+
+
+IF %1/==/ goto usage
+
+SET DST=%~1
+
+IF NOT EXIST "%DST%" goto notexist
+
+SET TAR=bsdtar
+SET TAROPT=-x -C "%DST%" 
+
+SET SRC=msys-env
+
+%TAR% %TAROPT% -f %SRC%\msysCORE-1.0.11-20080826.tar.gz
+
+%TAR% %TAROPT% -f %SRC%\perl-5.6.1-MSYS-1.0.11-1.tar.bz2
+%TAR% %TAROPT% -f %SRC%\crypt-1.1-1-MSYS-1.0.11-1.tar.bz2 bin/*.*
+
+move "%DST%\m.ico" "%DST%\the-m.ico"
+
+mkdir tmp
+mkdir "%DST%\local"
+%TAR% -x -C tmp -f %SRC%\autoconf2.1-2.13-3-bin.tar.bz2
+%TAR% -x -C tmp -f %SRC%\autoconf2.5-2.61-1-bin.tar.bz2
+%TAR% -x -C tmp -f %SRC%\autoconf-4-1-bin.tar.bz2
+%TAR% -x -C tmp -f %SRC%\automake1.10-1.10-1-bin.tar.bz2
+%TAR% -x -C tmp -f %SRC%\automake1.9-1.9.6-2-bin.tar.bz2
+%TAR% -x -C tmp -f %SRC%\automake-3-1-bin.tar.bz2
+xcopy /E /Q tmp\usr\local "%DST%\local"
+rmdir /s /q tmp
+
+sed -i -e "s/Courier-12/\"Lucida Console-10\"/" "%DST%\msys.bat"
+
+goto :EOF
+
+
+:usage
+@echo.
+@echo  USAGE:  %0 TARGET-PATH
+@echo.
+@echo The target (=installation) directory must exist!
+@echo.
+
+goto :EOF
+
+
+:notexist
+@echo.
+@echo ERROR: The target directory ^<%DST%^> does not exist!
+@echo.
+
+goto :EOF

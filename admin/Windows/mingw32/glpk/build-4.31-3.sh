087b08a59f9ff7cf284792130921957c480a1800
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2008-11-10 20:19:33 +0000

    update GLPK to version 4.31
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5420 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/glpk/build-4.31-3.sh
@@ -0,0 +1,97 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=glpk
+# Version of Package
+VER=4.31
+# Release of (this patched) package
+REL=3
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}.tar.gz
+TAR_TYPE=z
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://gd.tuwien.ac.at/gnu/gnusrc/glpk/glpk-4.31.tar.gz"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extracted to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+
+# Make file to use
+MAKEFILE=""
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS=""
+INCLUDE_DIR=include/glpk
+
+source ../gcc43_common.sh
+
+# Directory the lib is built in
+BUILDDIR=".build_mingw32_${VER}-${REL}_gcc${GCC_VER}${GCC_SYS}"
+
+mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+conf()
+{
+   ( cd ${BUILDDIR} && ${TOPDIR}/${SRCDIR}/configure \
+     --srcdir=../${SRCDIR} \
+     CC=${CC} \
+     CXX=${CXX} \
+     F77=${F77} \
+     CFLAGS="$CFLAGS ${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     CXXFLAGS="$CXXFLAGS ${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall" \
+     LDFLAGS="${LDFLAGS}" \
+     --prefix="${PREFIX}" \
+     --with-gmp \
+     --enable-static \
+     --enable-shared
+   )
+}
+
+install()
+{
+   install_pre;
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/glpk.dll ${SHAREDLIB_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/.libs/libglpk.a ${STATICLIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libglpk.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${SRCDIR}/include/glpk.h ${INCLUDE_PATH}
+   
+   mkdir -vp ${LICENSE_PATH}/${PKG}
+   ${CP} ${CP_FLAGS} ${SRCDIR}/COPYING ${LICENSE_PATH}/${PKG}
+   
+   install_post
+}
+
+uninstall()
+{
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/glpk.dll
+   ${RM} ${RM_FLAGS} ${STATICLIBRARY_PATH}/libglpk.a
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libglpk.dll.a
+   ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/glpk.h
+}
+
+all()
+{
+   download
+   unpack
+   applypatch
+   mkdirs
+   conf
+   build
+   install
+}
+
+main $*

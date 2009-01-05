3ce988f6d6b7953c60f7b79cb2d41d243b12985a
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2009-01-05 16:33:04 +0000

    add build scripts and patches for: FLKT-1.1.9, FTGL-2.1.3-RC5, GRAPHICSMAGICK-1.3.3
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5510 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/fltk/build-1.1.9-3.sh
@@ -0,0 +1,149 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=fltk
+# Version of Package
+VER=1.1.9
+# Release of (this patched) package
+REL=3
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}-source.tar.bz2
+TAR_TYPE=j
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://www.fltk.org/software.php?VERSION=1.3.x-r6598&FILE=fltk/1.1.9/fltk-1.1.9-source.tar.bz2"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extracted to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+
+# Make file to use
+MAKEFILE=""
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS=""
+INCLUDE_DIR=include/FL
+
+source ../gcc43_common.sh
+
+# Directory the lib is built in
+BUILDDIR=".build_mingw32_${VER}-${REL}_gcc${GCC_VER}${GCC_SYS}"
+
+mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+mkdirs_post()
+{
+   mkdir -vp ${BUILDDIR}/src
+   mkdir -vp ${BUILDDIR}/lib
+}
+
+conf()
+{
+   ( cd ${BUILDDIR} && ${TOPDIR}/${SRCDIR}/configure \
+     --srcdir=../${SRCDIR} \
+     CC=${CC} \
+     CXX=${CXX} \
+     F77=${F77} \
+     CFLAGS="$CFLAGS ${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall -DFL_DLL" \
+     CXXFLAGS="$CXXFLAGS ${GCC_ARCH_FLAGS} ${GCC_OPT_FLAGS} -Wall -DFL_DLL" \
+     LDFLAGS="${LDFLAGS}" \
+     CXXLIBS="$CXXLIBS" \
+     DSOFLAGS="${LDFLAGS}" \
+     --prefix="${PREFIX}" \
+     --enable-static \
+     --enable-shared \
+     --enable-gl
+   )
+
+   substvars ${SRCDIR}/src/makefile ${BUILDDIR}/src/makefile
+   substvars ${SRCDIR}/makefile ${BUILDDIR}/makefile
+   
+   sed -e \
+	"s@../FL@\$(SRCDIR)/FL@g" \
+	${SRCDIR}/src/makedepend > ${BUILDDIR}/src/makedepend
+}
+
+install()
+{
+   install_pre;
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/mgwfltknox_forms-1.1.dll ${SHAREDLIB_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/mgwfltknox_gl-1.1.dll ${SHAREDLIB_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/mgwfltknox_images-1.1.dll ${SHAREDLIB_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/mgwfltknox-1.1.dll ${SHAREDLIB_PATH}
+   
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libfltk.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libfltk_forms.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libfltk_gl.dll.a ${LIBRARY_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/src/libfltk_images.dll.a ${LIBRARY_PATH}
+   
+   #for a in $INSTALL_HEADERS; do
+   #   ${CP} ${CP_FLAGS} ${SRCDIR}/FL/$a ${INCLUDE_PATH}/$a
+   #done
+   for a in ${SRCDIR}/FL/*.[hH]; do
+      ${CP} ${CP_FLAGS} $a ${INCLUDE_PATH}
+   done
+   
+   
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/fltk-config ${BINARY_PATH}
+   
+   mkdir -vp ${LICENSE_PATH}/${PKG}
+   ${CP} ${CP_FLAGS} ${SRCDIR}/COPYING ${LICENSE_PATH}/${PKG}
+   
+   install_post
+}
+
+uninstall()
+{
+   uninstall_pre;
+   
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/mgwfltknox_forms-1.1.dll
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/mgwfltknox_gl-1.1.dll
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/mgwfltknox_images-1.1.dll
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/mgwfltknox-1.1.dll
+   
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libfltk.dll.a
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libfltk_forms.dll.a
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libfltk_gl.dll.a
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libfltk_images.dll.a
+   
+   #for a in $INSTALL_HEADERS; do
+   #   ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/$a
+   #done
+   for a in ${SRCDIR}/FL/*.[hH]; do
+      b=`basename $a`
+      ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/$b
+   done
+   
+   ${RM} ${RM_FLAGS} ${BINARY_PATH}/fltk-config
+   
+   ${RM} ${RM_FLAGS} ${LICENSE_PATH}/${PKG}/COPYING
+   
+   uninstall_post;
+   
+}
+
+all()
+{
+   download
+   unpack
+   applypatch
+   mkdirs
+   conf
+   build
+   install
+}
+
+main $*
---
+++ admin/Windows/mingw32/ftgl/build-2.1.3-rc5-3.sh
@@ -0,0 +1,129 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=ftgl
+# Version of Package
+VER=2.1.3-rc5
+# Release of (this patched) package
+REL=3
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file
+SRCFILE=${PKGVER}.tar.bz2
+TAR_TYPE=j
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL of source code file
+URL="http://downloads.sourceforge.net/ftgl/ftgl-2.1.3-rc5.tar.bz2"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory Source code is extracted to (relative to TOPDIR)
+SRCDIR=ftgl-2.1.3~rc5
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+
+# Make file to use
+MAKEFILE=makefile.mingw32
+
+# Additional DIFF Flags for generating diff file
+#DIFF_FLAGS="-x *.def"
+
+# header files to be installed
+INSTALL_HEADERS="
+FTBBox.h
+FTBitmapGlyph.h
+FTBuffer.h
+FTBufferFont.h
+FTBufferGlyph.h
+FTExtrdGlyph.h
+FTFont.h
+ftgl.h
+FTGLBitmapFont.h
+FTGLExtrdFont.h
+FTGLOutlineFont.h
+FTGLPixmapFont.h
+FTGLPolygonFont.h
+FTGLTextureFont.h
+FTGlyph.h
+FTLayout.h
+FTOutlineGlyph.h
+FTPixmapGlyph.h
+FTPoint.h
+FTPolyGlyph.h
+FTSimpleLayout.h
+FTTextureGlyph.h"
+INCLUDE_DIR=include/FTGL
+
+source ../gcc43_common.sh
+
+# Directory the lib is built in
+BUILDDIR=".build_mingw32_${VER}-${REL}_gcc${GCC_VER}${GCC_SYS}"
+
+#unpack()
+#{
+#   ${TAR} -${TAR_TYPE} -${TAR_FLAGS} ${TOPDIR}/${SRCFILE} && mv FTGL FTGL-${VER}
+#}
+
+mkdirs_pre() { if [ -e ${BUILDDIR} ]; then rm -rf ${BUILDDIR}; fi; }
+
+#mkdirs_post()
+#{
+#   mkdir -vp ${BUILDDIR}/src
+#   mkdir -vp ${BUILDDIR}/lib
+#}
+
+conf()
+{
+   substvars ${SRCDIR}/makefile.mingw32.in ${BUILDDIR}/makefile.mingw32
+   ${CP} ${CP_FLAGS} ${SRCDIR}/msvc/config.h ${BUILDDIR}/config.h
+}
+
+install()
+{
+   install_pre;
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/ftgl.dll ${SHAREDLIB_PATH}
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/libftgl.dll.a ${LIBRARY_PATH}
+   
+   for a in $INSTALL_HEADERS; do
+      ${CP} ${CP_FLAGS} $SRCDIR/src/FTGL/$a ${INCLUDE_PATH}/$a
+   done
+   
+   mkdir -vp ${LICENSE_PATH}/${PKG}
+   ${CP} ${CP_FLAGS} ${SRCDIR}/COPYING ${LICENSE_PATH}/${PKG}
+   
+   install_post;
+}
+
+uninstall()
+{
+   uninstall_pre;
+   
+   ${RM} ${RM_FLAGS} ${SHAREDLIB_PATH}/ftgl.dll
+   ${RM} ${RM_FLAGS} ${LIBRARY_PATH}/libftgl.dll.a
+
+   for a in $INSTALL_HEADERS; do
+      ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/$a
+   done
+   
+   ${RM} ${RM_FLAGS} ${LICENSE_PATH}/${PKG}/COPYING
+   
+   uninstall_post;
+}
+
+all()
+{
+   download
+   unpack
+   applypatch
+   mkdirs
+   conf
+   build
+   install
+}
+
+main $*

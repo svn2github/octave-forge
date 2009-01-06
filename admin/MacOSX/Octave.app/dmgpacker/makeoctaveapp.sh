c562bb502553bc93da67dfd4c29d04a7188c418f
treichl <treichl@fc592f45-0881-4044-91ff-c96495a54512>
2009-01-06 15:01:30 +0000

    Copyright changes from GPL2 to GPL3.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5514 fc592f45-0881-4044-91ff-c96495a54512

--- admin/MacOSX/Octave.app/dmgpacker/makeoctaveapp.sh
+++ admin/MacOSX/Octave.app/dmgpacker/makeoctaveapp.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# Copyright (C) 2007, Thomas Treichl and Paul Kienzle
+# Copyright (C) 2007-2008, Thomas Treichl and Paul Kienzle
+# Copyright (C) 2009, Thomas Treichl
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful, but

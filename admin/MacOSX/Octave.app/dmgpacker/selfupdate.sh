a9b1328b56709ff116d190d3376239d647d70737
treichl <treichl@fc592f45-0881-4044-91ff-c96495a54512>
2009-01-06 14:52:07 +0000

    Removed function "evalfailexit" and added "set -euv" instead.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5513 fc592f45-0881-4044-91ff-c96495a54512

--- admin/MacOSX/Octave.app/dmgpacker/selfupdate.sh
+++ admin/MacOSX/Octave.app/dmgpacker/selfupdate.sh
@@ -16,8 +16,15 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
+# Cf. the manpage of bash: "set -e" exits the script immediately if a
+# simple command exits with a non-zero status. "set -u" treats unset
+# variables as an error. "set -v" print shell input lines as they are
+# read.
+set -euv
+
 # USAGE:
 #   Put this script into the root directory of your Octave sources.
+#
 #   Before you start this script by a call of './selfupdate.sh' make
 #   sure that you don't use Apple's original programs 'sed', 'awk',
 #   'aclocal', 'automake', 'bison' and 'flex' anymore. Download these
@@ -25,6 +32,7 @@
 #   your system where they can be found (eg. in /usr/local/bin, make
 #   sure that you set the $PATH environment variable correctly
 #   below). On my system this looks like
+#
 #       bash ~$ which {sed,awk,aclocal,automake,bison,flex}
 #       /usr/local/bin/sed
 #       /usr/local/bin/awk
@@ -33,6 +41,7 @@
 #       /usr/local/bin/bison
 #       /usr/local/bin/flex
 #       /usr/local/bin/sed
+#
 #   Change the version number in the file src/version.h to that
 #   version number of the current Octave.app that should be
 #   updated. Then modify the following APPPATH variable for your
@@ -51,37 +60,41 @@ BINPATH=${PRFPATH}/bin
 # Add the option -ggdb to CFLAGS, CPPFLAGS and CXXFLAGS if you want to
 # create a debug version of Octave by adding -ggdb to OPTFLAGS.
 ARCH="-arch i386"
-OPTFLAGS="-O3 -ftree-vectorize -march=i686 -mfpmath=sse,387 -fforce-addr -mieee-fp -msse3 -msse2 -msse -mmmx -ggdb"
+OPTFLAGS="-O3 -fforce-addr -march=i686 -mfpmath=sse,387 -mieee-fp \
+-msse3 -msse2 -msse -mmmx -ggdb"
 
 # Here are the optimization flags that can be used on a PPC platform
 # ARCH="-arch ppc"
-# OPTFLAGS="-O3 -ftree-vectorize -mpowerpc -faltivec -maltivec -mabi=altivec"
+# OPTFLAGS="-O3 -mpowerpc -faltivec -maltivec -mabi=altivec"
 
-# If you are running a Mac OS X 10.3 on the PPC platform then change to 
-# MACOSX_DEPLOYMENT_TARGET=10.3 
+# The MacOSX deployment target must be set to 10.4 for PPC and i386
 export MACOSX_DEPLOYMENT_TARGET=10.4
 
+# The compiler directives
 GCC="gcc ${ARCH} ${OPTFLAGS}"
 CPP="gcc ${ARCH} -E"
 CXX="g++ ${ARCH} ${OPTFLAGS}"
 
-# If you are running a Mac OS X 10.3 on the PPC platform then change to 
-# -isysroot /Developer/SDKs/MacOSX10.3.9.sdk and in the LDFLAGS use that
-# line -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.3.9.sdk
-CFLAGS="-isysroot /Developer/SDKs/MacOSX10.4u.sdk -I/usr/X11R6/include -I${INCPATH} -I${INCPATH}/curl -I${INCPATH}/readline -I${INCPATH}/freetype -I${INCPATH}/GraphicsMagick" 
+# The compiler flags
+CFLAGS="-isysroot /Developer/SDKs/MacOSX10.4u.sdk -I/usr/X11R6/include \
+-I${INCPATH} -I${INCPATH}/curl -I${INCPATH}/readline -I${INCPATH}/freetype \
+-I${INCPATH}/GraphicsMagick" 
 CPPFLAGS="${CFLAGS}"
 CXXFLAGS="${CFLAGS}"
-#LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk -Xlinker -m"
-LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk"
+LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names \
+-Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk"
+# LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names \
+# -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk -Xlinker -m"
 
-# Note: another architecture flag and isysroot information here, change
-# this if you are running a Mac OS X on the PPC platform
+# Note: another architecture flag here, change this if you are running
+# a Mac OS X on the PPC platform
 F77="fort77 ${OPTFLAGS}"
 FLIBS="-L${LIBPATH} -lf2c"
-FFLAGS="-Wc,-arch -Wc,i386 -Wc,-isysroot -Wc,/Developer/SDKs/MacOSX10.4u.sdk ${OPTFLAGS} -I${INCPATH}"
+FFLAGS="${OPTFLAGS} -I${INCPATH} -Wc,-arch -Wc,i386 \
+-Wc,-isysroot -Wc,/Developer/SDKs/MacOSX10.4u.sdk"
 
-# If you are running a Mac OS X 10.3 on the PPC platform then change to 
-# EXTRACONF="--host=powerpc-apple-darwin7.9.1"
+# This is just used to display the right message at startup of
+# Octave.app, for PPC use EXTRACONF="--host=i386-apple-darwin7.9.1"
 EXTRACONF="--host=i386-apple-darwin8.9.1"
 
 # I have installed the GNU tools in /usr/local/bin, that's why I need
@@ -90,48 +103,36 @@ EXTRACONF="--host=i386-apple-darwin8.9.1"
 export PATH=${BINPATH}:/usr/local/bin:${PATH}  
 export DYLD_LIBRARY_PATH=${LIBPATH}
 
-# Function:    evalfailexit
-# Input args:  ${1} is the string that has to be evaluated
-# Description: Evaluates the ${1} string prints a message to ${MSGFILE} 
-#              and exits with an error number 1 on fail
-evalfailexit() {
-  if ( ! eval "${1} 2>&1 >>${MSGFILE}" ); then
-    echo "selfupdate.sh: Building Octave.app has failed !!!"
-    echo "selfupdate.sh: The command that failed was: ${1}"
-    exit 1
-  fi
-}
-
 # Use another file for output while './configure' and 'make' and
 # 'make install', eg. MSGFILE=/tmp/mymessages.log
-MSGFILE=/dev/stdout # /tmp/messages.log # /dev/stdout 
+MSGFILE=/tmp/messages.log # /dev/stdout 
 
 # Rebuild the './configure' script with './autogen.sh' to make sure
 # that we have the latest changes available.
-evalfailexit "./autogen.sh"
-evalfailexit "./configure CC=\"${GCC}\" CPP=\"${CPP}\" CXX=\"${CXX}\" \
+./autogen.sh >>${MSGFILE} 2>&1
+eval "./configure CC=\"${GCC}\" CPP=\"${CPP}\" CXX=\"${CXX}\" \
   F77=\"${F77}\" FLIBS=\"${FLIBS}\" FFLAGS=\"${FFLAGS}\" \
   CFLAGS=\"${CFLAGS}\" CPPFLAGS=\"${CPPFLAGS}\" CXXFLAGS=\"${CXXFLAGS}\" \
-  LDFLAGS=\"${LDFLAGS}\" --prefix=${PRFPATH} --enable-shared"
-evalfailexit "make"
+  LDFLAGS=\"${LDFLAGS}\" --prefix=${PRFPATH} --enable-shared >>${MSGFILE} 2>&1"
+make >>${MSGFILE} 2>&1
 
 # This is the point of *no return*: If something goes wrong in the
 # following six lines then your new Octave.app may be definitely be
 # broken. We need to store the startup scripts of Octave.app with the
 # following two lines because they will be overwritten.
-evalfailexit "mv ${PRFPATH}/bin/octave ${PRFPATH}/bin/_octave"
-evalfailexit "mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile"
+mv ${PRFPATH}/bin/octave ${PRFPATH}/bin/_octave
+mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile
 
 # Cleanup as much as possible of the current Octave.app and install
 # the new binaries.
-evalfailexit "make uninstall"
-evalfailexit "make install"
+make uninstall >>${MSGFILE} 2>&1
+make install >>${MSGFILE} 2>&1
 
 # Replace the new created startup scripts that have been installed
 # with the old startup scripts of Octave.app - keep the _* files.
-evalfailexit "mv ${PRFPATH}/bin/_octave ${PRFPATH}/bin/octave"
-evalfailexit "mv ${PRFPATH}/bin/_mkoctfile ${PRFPATH}/bin/mkoctfile"
+mv ${PRFPATH}/bin/_octave ${PRFPATH}/bin/octave
+mv ${PRFPATH}/bin/_mkoctfile ${PRFPATH}/bin/mkoctfile
 
 # Let's run make check at the end of the installation process to have
 # a look how many of the tests succeed.
-evalfailexit "make check"
+make check >>${MSGFILE} 2>&1

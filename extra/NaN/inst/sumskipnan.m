46fc5ab7083856d9627276a7ce7511189cd21b63
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2009-03-05 16:09:52 +0000

    prepare for use of sumskipnan_mex and sumskipnan_oct
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5582 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/inst/sumskipnan.m
+++ extra/NaN/inst/sumskipnan.m
@@ -1,4 +1,4 @@
-function [o,count,SSQ,S4M] = sumskipnan(i,DIM)
+function [o,count,SSQ,S4M] = sumskipnan(x,DIM)
 % SUMSKIPNAN adds all non-NaN values. 
 %
 % All NaN's are skipped; NaN's are considered as missing values. 
@@ -49,7 +49,7 @@ function [o,count,SSQ,S4M] = sumskipnan(i,DIM)
 %    along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 %	$Id$
-%    	Copyright (C) 2000-2005 by Alois Schloegl <a.schloegl@ieee.org>	
+%    	Copyright (C) 2000-2005,2009 by Alois Schloegl <a.schloegl@ieee.org>	
 %       This function is part of the NaN-toolbox
 %       http://www.dpmi.tu-graz.ac.at/~schloegl/matlab/NaN/
 
@@ -65,9 +65,9 @@ end;
 % Outline of the algorithm: 
 % for { k=1,o=0,count=0; k++; k<N} 
 % 	if ~isnan(i(k)) 
-% 	{ 	o     += i(k);
+% 	{ 	o     += x(k);
 %               count += 1;
-%		tmp    = i(k)*i(k)
+%		tmp    = x(k)*x(k)
 %		o2    += tmp;
 %		o3    += tmp.*tmp;
 %       }; 
@@ -78,36 +78,137 @@ if isempty(DIM),
 end
 if (DIM<1) DIM = 1; end; %% Hack, because min([])=0 for FreeMat v3.5
 
-FLAG = (length(size(i))<3); 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% non-float data 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if ~isa(x,'float') %%%% || flag_implicit_skip_nan, %%% skip always NaN's
+	x = double(x); 
+	o = sum(x,DIM);
+	if nargin>1
+		sz = size(x);	
+		count = repmat(sz(DIM),sz([1:DIM-1,DIM+1:end]));
+		if nargin>2
+			x = x.*x; 
+			SSQ = sum(x,DIM);
+			if nargin>3
+				S4M = sum(x.*x,DIM);
+			end; 
+		end; 
+	end; 	
+	return; 
+end; 	
+
+%% mex and oct files expect double
+x = double(x); 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% use Matlab-MEX function when available  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% it seems there is no advantage in using the mex-file, therefore this option is disabled by default.   
+if 0,	
+%%try
+	
+	%% using sumskipnan_mex.mex
+	if (nargout<3),
+		[o,count] = sumskipnan_mex(x,DIM);
+		return; 
+	elseif (nargout==3),
+		[o,count,SSQ] = sumskipnan_mex(x,DIM);
+		return; 
+	elseif (nargout==4) && isreal(i),
+		[o,count,SSQ,S4M] = sumskipnan_mex(x,DIM);
+		return; 
+	end; 	
+end; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% use OCT function when available  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% no performance analysis showing a distinct advantage has been performed.  
+if 0 %%if exist('OCTAVE_VERSION','builtin'),
+
+	%% for Octave, there might be an sumskipnan_oct.oct
+	try
+	if isreal(x)
+		%% using sumskipnan_oct.mex
+		if (nargout<3),
+			[o,count] = sumskipnan_oct(x,DIM);
+			return; 
+		elseif (nargout==3),
+			[o,count,SSQ] = sumskipnan_oct(x,DIM);
+			return; 
+		elseif (nargout==4) && isreal(i),
+			[o,count,SSQ,S4M] = sumskipnan_oct(x,DIM);
+			return; 
+		end; 	
+	else	%% if ~isreal(x)
+		if (nargout<3),
+			[or,count] = sumskipnan_oct(real(x),DIM);
+			[oi,counti] = sumskipnan_oct(imag(x),DIM);
+			if any(count(:)-counti(:))
+				error('number of NaN differ between real and imag part\n');
+			end;
+			o = or + i*oi;
+			return; 
+		elseif (nargout<4),
+			[or,count,SSQ] = sumskipnan_oct(real(x),DIM);
+			[oi,counti,SSQi] = sumskipnan_oct(imag(x),DIM);
+			if any(count(:)-counti(:))
+				error('number of NaN differ between real and imag part\n');
+			end;
+			o = or + oi;
+			SSQ = SSQ+SSQi; 
+			return; 
+		end; 	
+	end;	%% if 
+	end; 	%% try
+end; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% fall-back in case no OCT/MEX support is avialable   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% performance tweak: some tests have shown that x*ones(:,1) is faster than sum(x,2)
+FLAG = (length(size(x))<3); 
 if FLAG, FLAG = DIM; end; 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% count non-NaN's
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargout>1,
         if FLAG~=2,
-                count = sum(i==i,DIM); 
+                count = sum(x==x,DIM); 
         else
-                count = real(i==i)*ones(size(i,2),1);
+                count = real(x==x)*ones(size(x,2),1);
         end;
 end;
 
-%if flag_implicit_skip_nan, %%% skip always NaN's
-i(i~=i) = 0;
-%end;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% replace NaN's with zero 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+x(x~=x) = 0;	
 
-if FLAG~=2,
-        o = sum(i,DIM);
+if ~exist('OCTAVE_VERSION','builtin'),
+        o = sum(x,DIM);
+elseif FLAG == 1	
+        o = ones(1,size(x,2))*x;
+elseif FLAG == 2	
+        o = x*ones(size(x,2),1);
 else
-        o = i*ones(size(i,2),1);
+        o = sum(x,DIM);
 end;
 
 if nargout>2,
-        i = real(i).^2 + imag(i).^2;
+        x = real(x).^2 + imag(x).^2;
         if FLAG~=2,
-                SSQ = sum(i,DIM);
+                SSQ = sum(x,DIM);
         else
-                SSQ = i*ones(size(i,2),1);
+                SSQ = x*ones(size(x,2),1);
         end;
         if nargout>3,
-                S4M = sumskipnan(i.^2,DIM);
+                S4M = sumskipnan(x.^2,DIM);
         end;
 end;
 

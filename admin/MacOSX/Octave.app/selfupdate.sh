b7e79ea447d36b27e7bf8377e1d543732b47d1c4
treichl <treichl@fc592f45-0881-4044-91ff-c96495a54512>
2009-03-09 18:44:32 +0000

    Replace old 3.0.x version file with new file for 3.1.x.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5591 fc592f45-0881-4044-91ff-c96495a54512

--- admin/MacOSX/Octave.app/selfupdate.sh
+++ admin/MacOSX/Octave.app/selfupdate.sh
@@ -41,10 +41,12 @@ set -euv
 #       /usr/local/bin/sed
 #
 #   Change the version number in the file src/version.h to that
-#   version number of the current Octave.app that should be
-#   updated. Then modify the following APPPATH variable for your
-#   needs (the absolut path including Octave.app that should be
-#   updated).
+#   version number of the current Octave.app that should be updated,
+#   eg. you want to update Octave.app 3.1.51 with the sources of GNU
+#   Octave 3.1.54+ then change VERSION in src/version.h to 3.1.51.
+#
+#   Finally, modify the following APPPATH variable for your needs (the
+#   absolut path including Octave.app that should be updated).
 APPPATH=/Applications/Octave.app
 
 # Don't change one of the following four lines. These lines are used
@@ -55,11 +57,11 @@ INCPATH=${PRFPATH}/include
 LIBPATH=${PRFPATH}/lib
 BINPATH=${PRFPATH}/bin
 
-# Add the option -ggdb to CFLAGS, CPPFLAGS and CXXFLAGS if you want to
-# create a debug version of Octave by adding -ggdb to OPTFLAGS.
+# Add the flag -ggdb to OPTFLAGS if you want to create a debug version
+# of Octave.app.
 ARCH="-arch i386"
 OPTFLAGS="-O3 -fforce-addr -march=i686 -mfpmath=sse,387 -mieee-fp \
--msse3 -msse2 -msse -mmmx -ggdb"
+  -msse3 -msse2 -msse -mmmx -ggdb"
 
 # Here are the optimization flags that can be used on a PPC platform
 # ARCH="-arch ppc"
@@ -68,42 +70,44 @@ OPTFLAGS="-O3 -fforce-addr -march=i686 -mfpmath=sse,387 -mieee-fp \
 # The MacOSX deployment target must be set to 10.4 for PPC and i386
 export MACOSX_DEPLOYMENT_TARGET=10.4
 
-# The compiler directives
-GCC="gcc ${ARCH} ${OPTFLAGS}"
-CPP="gcc ${ARCH} -E"
-CXX="g++ ${ARCH} ${OPTFLAGS}"
+# The compiler directives, once again, make sure that /usr/bin/gcc and
+# /usr/bin/g++ are found and not any other compiler on your system
+GCC="gcc"
+CPP="gcc -E"
+CXX="g++"
 
-# The compiler flags
-CFLAGS="-isysroot /Developer/SDKs/MacOSX10.4u.sdk -I/usr/X11R6/include \
--I${INCPATH} -I${INCPATH}/curl -I${INCPATH}/readline -I${INCPATH}/freetype \
--I${INCPATH}/GraphicsMagick" 
+# The compiler suite flags and the linker flags
+CFLAGS="${ARCH} ${OPTFLAGS} -isysroot /Developer/SDKs/MacOSX10.4u.sdk \
+  -I${INCPATH} -I${INCPATH}/GraphicsMagick"
 CPPFLAGS="${CFLAGS}"
 CXXFLAGS="${CFLAGS}"
-LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names \
--Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk"
-# LDFLAGS="-L${LIBPATH} -L${LIBPATH}/pkgconfig -Wl,-headerpad_max_install_names \
-# -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk -Xlinker -m"
+LDFLAGS="-L${LIBPATH} -Wl,-headerpad_max_install_names \
+  -Wl,-syslibroot -Wl,/Developer/SDKs/MacOSX10.4u.sdk"
 
-# Note: another architecture flag here, change this if you are running
-# a Mac OS X on the PPC platform
-F77="fort77 ${OPTFLAGS}"
-FLIBS="-L${LIBPATH} -lf2c"
-FFLAGS="${OPTFLAGS} -I${INCPATH} -Wc,-arch -Wc,i386 \
--Wc,-isysroot -Wc,/Developer/SDKs/MacOSX10.4u.sdk"
+# Set up the Fortran compiler and flags that come with Octave.app
+F77="i386-apple-darwin8.11.1-g95"
+FLIBS="-L${LIBPATH}/gcc-lib/i386-apple-darwin8.11.1/4.0.3 -lf95"
+FFLAGS="${OPTFLAGS} -I${INCPATH}"
 
-# This is just used to display the right message at startup of
-# Octave.app, for PPC use EXTRACONF="--host=i386-apple-darwin7.9.1"
-EXTRACONF="--host=i386-apple-darwin8.9.1"
+# Set up the Fortran compiler and flags that come with Octave.app
+# on a PPC platform
+# F77="powerpc-apple-darwin6.8-g95"
+# FLIBS="-L${LIBPATH}/gcc-lib/powerpc-apple-darwin6.8/4.0.3 -lf95"
+# FFLAGS="${OPTFLAGS} -I${INCPATH}"
+
+# Here are some options that are passed to the "./configure" script
+EXTRACONF="--host=i386-apple-darwin8.11.1 --prefix=${PRFPATH} \
+  --enable-shared --without-x"
 
 # I have installed the GNU tools in /usr/local/bin, that's why I need
-# to use this path. I use f2c from the current Octave.app, that's why
+# to use this path. I use g95 from the current Octave.app, that's why
 # I add ${PRFPATH}/bin to the current path.
 export PATH=${BINPATH}:/usr/local/bin:${PATH}  
 export DYLD_LIBRARY_PATH=${LIBPATH}
 
 # Use another file for output while './configure' and 'make' and
-# 'make install', eg. MSGFILE=/tmp/mymessages.log
-MSGFILE=/tmp/messages.log # /dev/stdout 
+# 'make install', eg. MSGFILE=/tmp/messages.log
+MSGFILE=/dev/stdout # /dev/stdout /tmp/messages.log
 
 # Rebuild the './configure' script with './autogen.sh' to make sure
 # that we have the latest changes available.
@@ -111,13 +115,14 @@ MSGFILE=/tmp/messages.log # /dev/stdout
 eval "./configure CC=\"${GCC}\" CPP=\"${CPP}\" CXX=\"${CXX}\" \
   F77=\"${F77}\" FLIBS=\"${FLIBS}\" FFLAGS=\"${FFLAGS}\" \
   CFLAGS=\"${CFLAGS}\" CPPFLAGS=\"${CPPFLAGS}\" CXXFLAGS=\"${CXXFLAGS}\" \
-  LDFLAGS=\"${LDFLAGS}\" --prefix=${PRFPATH} --enable-shared >>${MSGFILE} 2>&1"
+  LDFLAGS=\"${LDFLAGS}\" ${EXTRACONF} >>${MSGFILE} 2>&1"
 make >>${MSGFILE} 2>&1
 
 # This is the point of *no return*: If something goes wrong in the
 # following six lines then your new Octave.app may be definitely be
-# broken. We need to store the startup scripts of Octave.app with the
-# following two lines because they will be overwritten.
+# broken.
+# We need to store the startup scripts of Octave.app with the
+# following two lines because they will be overwritten by 'install'.
 mv ${PRFPATH}/bin/octave ${PRFPATH}/bin/_octave
 mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile
 
@@ -126,11 +131,14 @@ mv ${PRFPATH}/bin/mkoctfile ${PRFPATH}/bin/_mkoctfile
 make uninstall >>${MSGFILE} 2>&1
 make install >>${MSGFILE} 2>&1
 
-# Replace the new created startup scripts that have been installed
-# with the old startup scripts of Octave.app - keep the _* files.
+# Replace the startup scripts that have been installed with the old
+# startup scripts of Octave.app.
 mv ${PRFPATH}/bin/_octave ${PRFPATH}/bin/octave
 mv ${PRFPATH}/bin/_mkoctfile ${PRFPATH}/bin/mkoctfile
 
 # Let's run make check at the end of the installation process to have
-# a look how many of the tests succeed.
+# a look how many of the tests succeed. If you want to run the local
+# 'run-octave' script from the GNU Octave sources then don't forget to
+# set DYLD_LIBRARY_PATH. Eg. start 'run-octave' in debug mode:
+#   DYLD_LIBRARY_PATH=/Applications/Octave.app/Contents/Resources/lib run-octave -d
 make check >>${MSGFILE} 2>&1

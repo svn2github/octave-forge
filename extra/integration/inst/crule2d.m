2379635514cd9c49c9330a42015db3b66dc8ae66
cdf <cdf@fc592f45-0881-4044-91ff-c96495a54512>
2009-03-16 23:28:13 +0000

    change meshdom to meshgrid
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5613 fc592f45-0881-4044-91ff-c96495a54512

--- extra/integration/inst/crule2d.m
+++ extra/integration/inst/crule2d.m
@@ -4,9 +4,9 @@ function [bpx,bpy,wfxy] = crule2d (nquadx,nquady)
 %
 	[bpxv,wfxv]=crule(nquadx);
 	[bpyv,wfyv]=crule(nquady);
-%	[bpx,bpy]=meshgrid(bpxv,bpyv);
-%	[wfx,wfy]=meshgrid(wfxv,wfyv);
-	[bpx,bpy]=meshdom(bpxv,bpyv);
-	[wfx,wfy]=meshdom(wfxv,wfyv);
+	[bpx,bpy]=meshgrid(bpxv,bpyv);
+	[wfx,wfy]=meshgrid(wfxv,wfyv);
+%	[bpx,bpy]=meshdom(bpxv,bpyv);
+%	[wfx,wfy]=meshdom(wfxv,wfyv);
 	wfxy=wfx.*wfy;
 endfunction
--- extra/integration/inst/grule2d.m
+++ extra/integration/inst/grule2d.m
@@ -4,9 +4,9 @@ function [bpx,bpy,wfxy] = grule2d (nquadx,nquady)
 %
 	[bpxv,wfxv]=grule(nquadx);
 	[bpyv,wfyv]=grule(nquady);
-%	[bpx,bpy]=meshgrid(bpxv,bpyv);
-%	[wfx,wfy]=meshgrid(wfxv,wfyv);
-	[bpx,bpy]=meshdom(bpxv,bpyv);
-	[wfx,wfy]=meshdom(wfxv,wfyv);
+	[bpx,bpy]=meshgrid(bpxv,bpyv);
+	[wfx,wfy]=meshgrid(wfxv,wfyv);
+%	[bpx,bpy]=meshdom(bpxv,bpyv);
+%	[wfx,wfy]=meshdom(wfxv,wfyv);
 	wfxy=wfx.*wfy;
 endfunction

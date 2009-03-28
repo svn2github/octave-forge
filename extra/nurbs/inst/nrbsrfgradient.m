bad36edf37aa4bbbf42e91a56942fd1ebaf3b2b8
cdf <cdf@fc592f45-0881-4044-91ff-c96495a54512>
2009-03-28 16:52:41 +0000

    corrected gradient calculation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5660 fc592f45-0881-4044-91ff-c96495a54512

--- extra/nurbs/inst/nrbsrfgradient.m
+++ extra/nurbs/inst/nrbsrfgradient.m
@@ -15,7 +15,7 @@
 ## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{dzdx}, @var{dzdy}, @var{z}, @var{x}, @var{y}]=} nrbsrfgradient (@var{nrb}, @var{nrbder}, @var{u}, @var{v})
+## @deftypefn {Function File} {[@var{dzdx}, @var{dzdy}]=} nrbsrfgradient (@var{nrb}, @var{nrbder}, @var{u}, @var{v})
 ## Compute the gradient of a NURBS surface.
 ## @seealso{nrbderiv}
 ## @end deftypefn
@@ -38,12 +38,50 @@ function [dzdx, dzdy]  = nrbsrfgradient (nrb, nrbder, u, v)
   dydv = squeeze (dp{2}(2,:,:));
   dzdv = squeeze (dp{2}(3,:,:));
 
-  dzdx = dzdy = zeros(length(u), length(v));
-
-  dzdx(dxdu~=0) += (dzdu./dxdu)(dxdu~=0);
-  dzdx(dxdv~=0) += (dzdv./dxdv)(dxdv~=0);
-
-  dzdy(dydu~=0) += (dzdu./dydu)(dydu~=0);
-  dzdy(dydv~=0) += (dzdv./dydv)(dydv~=0);
-
+  detjac = dxdu.*dydv - dydu.*dxdv;
+  
+  dzdx = ( dydv .* dzdu - dydu .*dzdv)./detjac;
+  dzdy = (-dxdv .* dzdu + dxdu .*dzdv)./detjac;
+  
 endfunction
+
+
+%!shared nrb, cntl, k, rec
+%!
+%!test
+%! p = 5; n = 5; x = y = linspace(0,3,n+1); [cntl(1,:,:), cntl(2,:,:)] = meshgrid(x,y);
+%! cntl(4,:,:) = 1;
+%! k{1} = k{2} = [zeros(1,p) linspace(0,1,n-p+2) ones(1,p)];
+%! rec = nrbmak(cntl,k);
+%! rec = nrbtform(rec, vecrotz(pi/3));
+%! rec.coefs(3,:,:) = rec.coefs(1,:,:);
+%! recd = nrbderiv(rec);
+%! [P, w] = nrbeval(rec, {linspace(0,1,20),
+%!      linspace(0,1,15)});
+%! [dzdx, dzdy]  = nrbsrfgradient (rec, recd, linspace(0,1,20),
+%!      linspace(0,1,15));
+%! quiver(squeeze(P(1,:,:)), squeeze(P(2,:,:)), dzdx, dzdy)
+%! assert(dzdx, ones(20,15),  10*eps);
+%! assert(dzdy, zeros(20,15), 10*eps);
+%!
+%!test 
+%! rec = nrbtform(rec, vecrotz(pi/2));
+%! recd = nrbderiv(rec);
+%! [P, w] = nrbeval(rec, {linspace(0,1,20),
+%!      linspace(0,1,15)});
+%! [dzdx, dzdy]  = nrbsrfgradient (rec, recd, linspace(0,1,20),
+%!      linspace(0,1,15));
+%! quiver(squeeze(P(1,:,:)), squeeze(P(2,:,:)), dzdx, dzdy)
+%! assert(dzdy, ones(20,15),  10*eps);
+%! assert(dzdx, zeros(20,15), 10*eps);
+%!
+%!test 
+%! rec = nrbtform(rec, vecrotz(-pi/4));
+%! recd = nrbderiv(rec);
+%! [P, w] = nrbeval(rec, {linspace(0,1,20),
+%!      linspace(0,1,15)});
+%! [dzdx, dzdy]  = nrbsrfgradient (rec, recd, linspace(0,1,20),
+%!      linspace(0,1,15));
+%! quiver(squeeze(P(1,:,:)), squeeze(P(2,:,:)), dzdx, dzdy)
+%! assert(dzdy, dzdx,  10*eps);
+

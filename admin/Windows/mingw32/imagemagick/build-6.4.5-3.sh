effff266d82b83d2e80f413fa4b6ac876968ee7b
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2009-05-18 18:54:13 +0000

    add magick and wand headers to installed include/ directory, remove "-shared-libgcc" and all -I and -L flags from the *-config scripts, as compile-time paths have no meaning on the installed system, and -shared-libgcc is not recognized my mkoctfile.exe (but added implicitly)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5844 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/imagemagick/build-6.4.5-3.sh
+++ admin/Windows/mingw32/imagemagick/build-6.4.5-3.sh
@@ -72,27 +72,151 @@ build()
 }
 
 TOOLS="convert.exe"
-MAGICKPP_HEADERS="Blob.h CoderInfo.h Color.h Drawable.h Exception.h Geometry.h Image.h Include.h Montage.h Pixels.h STL.h TypeMetric.h" 
+MAGICKPP_HEADERS="
+Blob.h
+CoderInfo.h 
+Color.h 
+Drawable.h 
+Exception.h 
+Geometry.h 
+Image.h 
+Include.h 
+Montage.h 
+Pixels.h 
+STL.h 
+TypeMetric.h" 
 
+MAGICK_HEADERS="
+methods.h
+magick-type.h
+animate.h
+annotate.h
+artifact.h
+blob.h
+cache.h
+cache-view.h
+cipher.h
+client.h
+coder.h
+color.h
+colorspace.h
+compare.h
+composite.h
+compress.h
+configure.h
+constitute.h
+decorate.h
+delegate.h
+deprecate.h
+display.h
+distort.h
+draw.h
+effect.h
+enhance.h
+exception.h
+fx.h
+gem.h
+geometry.h
+hashmap.h
+identify.h
+image.h
+layer.h
+list.h
+locale_.h
+log.h
+magic.h
+magick.h
+matrix.h
+memory_.h
+module.h
+mime.h
+monitor.h
+montage.h
+option.h
+paint.h
+pixel.h
+prepress.h
+profile.h
+property.h
+quantize.h
+quantum.h
+registry.h
+random_.h
+resample.h
+resize.h
+resource_.h
+segment.h
+semaphore.h
+shear.h
+signature.h
+splay-tree.h
+stream.h
+statistic.h
+string_.h
+timer.h
+token.h
+transform.h
+threshold.h
+type.h
+utility.h
+version.h
+xml-tree.h
+xwindow.h
+magickcore.h"
+
+WAND_HEADERS="
+animate.h
+compare.h
+composite.h
+conjure.h
+convert.h
+deprecate.h
+display.h
+drawing-wand.h
+identify.h
+import.h
+magick-property.h
+magick-image.h
+MagickWand.h
+magick-wand.h
+mogrify.h
+montage.h
+pixel-iterator.h
+pixel-view.h
+pixel-wand.h
+stream.h"
+
+modify_config()
+{
+   $SED \
+   -e "s@-shared-libgcc@@g" \
+   -e "s@-L[^ ]*@@g" \
+   -e "s@-I[^ ]*@@g" \
+   < $1 > $2
+}
+   
 install()
 {
    install_pre;
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick/.libs/libmagickcore.dll      ${SHAREDLIB_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick/.libs/libmagickcore.dll.a    ${LIBRARY_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick/.libs/libmagickcore.a        ${STATICLIBRARY_PATH}
-   ${CP} ${CP_FLAGS} ${BUILDDIR}/magick/Magick-config ${BINARY_PATH}
-   ${CP} ${CP_FLAGS} ${BUILDDIR}/magick/MagickCore-config ${BINARY_PATH}
-
+   
+   modify_config ${BUILDDIR}/magick/Magick-config ${BINARY_PATH}/Magick-config
+   modify_config ${BUILDDIR}/magick/MagickCore-config ${BINARY_PATH}/MagickCore-config
+   
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick++/lib/libmagick++.dll      ${SHAREDLIB_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick++/lib/libmagick++.dll.a    ${LIBRARY_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/magick++/lib/.libs/libmagick++.a        ${STATICLIBRARY_PATH}
-   ${CP} ${CP_FLAGS} ${BUILDDIR}/magick++/bin/Magick++-config ${BINARY_PATH}
+   
+   modify_config ${BUILDDIR}/magick++/bin/Magick++-config ${BINARY_PATH}/Magick++-config
 
    ${CP} ${CP_FLAGS} ${BUILDDIR}/wand/.libs/libmagickwand.dll      ${SHAREDLIB_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/wand/.libs/libmagickwand.dll.a    ${LIBRARY_PATH}
    ${CP} ${CP_FLAGS} ${BUILDDIR}/wand/.libs/libmagickwand.a        ${STATICLIBRARY_PATH}
-   ${CP} ${CP_FLAGS} ${BUILDDIR}/wand/Wand-config ${BINARY_PATH}
-   ${CP} ${CP_FLAGS} ${BUILDDIR}/wand/MagickWand-config ${BINARY_PATH}
+   
+   modify_config ${BUILDDIR}/wand/Wand-config ${BINARY_PATH}/Wand-config
+   modify_config ${BUILDDIR}/wand/MagickWand-config ${BINARY_PATH}/MagickWand-config
    
    for a in ${TOOLS}; do
       ${CP} ${CP_FLAGS} ${BUILDDIR}/utilities/.libs/$a ${BINARY_PATH}
@@ -102,6 +226,17 @@ install()
    ${CP} ${CP_TOOLS} ${SRCDIR}/magick++/lib/Magick++.h ${INCLUDE_PATH}
    for a in ${MAGICKPP_HEADERS}; do
       ${CP} ${CP_TOOLS} ${SRCDIR}/magick++/lib/Magick++/$a ${INCLUDE_PATH}/Magick++
+   done
+   
+   mkdir -vp ${INCLUDE_PATH}/Magick
+   ${CP} ${CP_TOOLS} ${BUILDDIR}/magick/magick-config.h ${INCLUDE_PATH}/Magick
+   for a in ${MAGICK_HEADERS}; do
+      ${CP} ${CP_TOOLS} ${SRCDIR}/magick/$a ${INCLUDE_PATH}/Magick
+   done
+   
+   mkdir -vp ${INCLUDE_PATH}/Wand
+   for a in ${WAND_HEADERS}; do
+      ${CP} ${CP_TOOLS} ${SRCDIR}/Wand/$a ${INCLUDE_PATH}/Wand
    done
    
    mkdir -vp ${LICENSE_PATH}/${PKG}
@@ -147,6 +282,17 @@ uninstall()
       ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/magick++/$a
    done
    rmdir -v ${INCLUDE_PATH}/Magick++
+   
+   for a in ${MAGICK_HEADERS}; do
+      ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/magick/$a
+   done
+   ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/magick/magick-config.h
+   rmdir -v ${INCLUDE_PATH}/Magick
+   
+   for a in ${WAND_HEADERS}; do
+      ${RM} ${RM_FLAGS} ${INCLUDE_PATH}/Wand/$a
+   done
+   rmdir -v ${INCLUDE_PATH}/Wand
    
    
    ${RM} ${RM_FLAGS} ${LICENSE_PATH}/${PKG}/LICENSE

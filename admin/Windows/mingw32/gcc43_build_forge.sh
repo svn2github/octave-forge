bde6fbfea9b8828297f99138a30860506279d404
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2009-06-10 08:55:27 +0000

    update octave forge toolbox installation for octave 3.2.0
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@5926 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/gcc43_build_forge.sh
+++ admin/Windows/mingw32/gcc43_build_forge.sh
@@ -3,70 +3,77 @@
 source gcc43_common.sh
 source gcc43_pkg_version.sh
 
+# == current BUGS ==
+#
+#  FIXED 0.7.10 fails in coatve 3.2.0, not yet tracked down
+#  VRML 1.0.11 fails with syntax error
 
 # the packages installed
 # in the form <package-name>-<version>
 FORGEPACK="\
-general-1.0.7 \
-miscellaneous-1.0.7 \
-time-1.0.8 \
-optim-1.0.4 \
-signal-1.0.8 \
-audio-1.1.2 \
-benchmark-1.0.0 \
-bioinfo-0.1.1 \
-combinatorics-1.0.7 \
-communications-1.0.8 \
-control-1.0.7 \
-data-smoothing-1.1.1 \
-econometrics-1.0.7 \
-financial-0.3.0 \
-ga-0.9.4 \
-gsl-1.0.7 \
-ident-1.0.6 \
-image-1.0.8 \
-informationtheory-0.1.6 \
-io-1.0.7 \
-irsa-1.0.6 \
-linear-algebra-1.0.6 \
-missing-functions-1.0.1 \
-nnet-0.1.8 \
-odebvp-1.0.5 \
-odepkg-0.6.4 \
-optiminterp-0.3.1 \
-outliers-0.13.8 \
-physicalconstants-0.1.6 \
-plot-1.0.6 \
+general-1.1.3 \
+miscellaneous-1.0.9 \
+time-1.0.9 \
+specfun-1.0.8 \
+optim-1.0.6 \
+signal-1.0.10 \
+audio-1.1.4 \
+benchmark-1.1.1 \
+bioinfo-0.1.2 \
+combinatorics-1.0.9 \
+communications-1.0.10 \
+control-1.0.11 \
+data-smoothing-1.2.0 \
+econometrics-1.0.8 \
+financial-0.3.2 \
+ga-0.9.7 \
+gsl-1.0.8 \
+ident-1.0.7 \
+image-1.0.10 \
+informationtheory-0.1.8 \
+io-1.0.9 \
+irsa-1.0.7 \
+linear-algebra-1.0.8 \
+missing-functions-1.0.2 \
+nnet-0.1.10 \
+octcdf-1.0.13 \
+odebvp-1.0.6 \
+odepkg-0.6.7 \
+optiminterp-0.3.2 \
+outliers-0.13.9 \
+physicalconstants-0.1.7 \
+plot-1.0.7 \
+quaternion-1.0.0 \
 sockets-1.0.5 \
-specfun-1.0.7 \
-special-matrix-1.0.6 \
-splines-1.0.6 \
-statistics-1.0.7 \
-strings-1.0.6 \
-struct-1.0.6 \
-vrml-1.0.7 \
-zenity-0.5.6 \
-fixed-0.7.8 \
-windows-1.0.6 \
-mapping-1.0.6 \
-integration-1.0.6
+special-matrix-1.0.7 \
+splines-1.0.7 \
+statistics-1.0.9 \
+strings-1.0.7 \
+struct-1.0.7 \
+symbolic-1.0.9 \
+zenity-0.5.7 \
+integration-1.0.7 \
+mapping-1.0.7 \
+windows-1.0.8 \
 "
 
+
 # OPTIM depends on MISCELLANEOUS
-# SIGNAL depends on OPTIM
+# SIGNAL depends on OPTIM and SPECFUN
 # COMMUNICATIONS depends on SIGNAL
 # FINANCIAL depends on TIME
+# DATA-SMOOTHING depends on OPTIM
+# GA depends on COMMUNICATIONS
 
-# ann fixed parallel symbolic video ftp database octcdf
+# ann parallel video ftp database 
 
 # FTP requires ftplib
-# IMAGE should find imagemagick
 # DATABASE requires sql
 # OCTCDF requires netcdf
 # VIDEO requires pkg-config, libavcodec
 # SYMBOLIC requires GINAC
 # OPTIMINTERP requires the fortran libaries in FLIB  <= **DONE!**
-# DATA-SMOOTHING requires OPTIM
+
 
 AUTO="-auto"
 

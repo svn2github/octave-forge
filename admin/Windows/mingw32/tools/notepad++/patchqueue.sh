169a0284220839babcad3cf6f9b382e6f6dc6015
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2009-10-24 17:26:40 +0000

    mingw32: update notepad++ patchqueue to npp-5.5.1 with gcc-4.4.0 and mingwrt-3.16
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6374 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/tools/notepad++/patchqueue.sh
+++ admin/Windows/mingw32/tools/notepad++/patchqueue.sh
@@ -3,6 +3,28 @@
 # CHANGELOG
 # ---------
 #
+# 24-oct-2009 benjamin lindner <lindnerb@users.sourceforge.net>
+#
+#    *) update for npp-5.5.1 and gcc-4.4.0 with mingwrt-3.16
+#       - remove np_xml.localization.xml
+#       - add countof-mingw.patch, mingwrt-3.16-fixes.patch
+#       - updated np_npp-disable-minidump.patch
+#                 np_npp-localizationString.patch
+#                 np_npp.patch
+#                 np_nppmakefile.patch
+#                 np_optifon.patch
+#                 np_scimakefile.patch
+#                 np_scintilla.patch
+#                 np_wtof_unicode.patch
+#                 npp_catch-refrerence-to-const.patch
+#                 npp_sci-bugfixes.patch
+#                 npp_xml-config-model.patch
+#                 npp_xml-langs-model.patch
+#                 npp_xml-shortcut.patch
+#                 npp_xml-userdefinelang.patch
+#       
+#    *) add --binary flag patch.exe
+#
 # 12-may-2009 benjamin lindner <lindnerb@users.sourceforge.net>
 #
 #    *) CREATION
@@ -12,7 +34,7 @@
 # subdirecory where patches reside
 PATCHDIR=patches
 # directory of gnuplot sources to patch
-NPP_DIR=npp-5.3.1
+NPP_DIR=npp-5.5.1
 
 # the patches to apply
 PATCHES="
@@ -27,16 +49,15 @@ npp_xml-config-model.patch
 npp_xml-langs-model.patch
 npp_xml-shortcut.patch
 npp_xml-userdefinelang.patch
-npp_sci-sloppy-code.patch
 np_npp-disable-minidump.patch
 np_npp-localizationString.patch
-np_xml-localization.patch
-npp_sci-lexprogress.patch
 npp_catch-refrerence-to-const.patch
+countof-mingw.patch
+mingwrt-3.16-fixes.patch
 "
 
 for a in $PATCHES; do
    echo applying $a ...
-   ( cd $NPP_DIR && patch -p 1 -u -i ../$PATCHDIR/$a )
+   ( cd $NPP_DIR && patch --binary -p 1 -u -i ../$PATCHDIR/$a )
 done
 

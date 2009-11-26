d16bf65fb43ff227ff94571fce94740110586181
rikcorradini <rikcorradini@fc592f45-0881-4044-91ff-c96495a54512>
2009-11-26 13:30:32 +0000

    Updated with the new comunicator class
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6521 fc592f45-0881-4044-91ff-c96495a54512

--- extra/openmpi_ext/inst/hello2dimmat.m
+++ extra/openmpi_ext/inst/hello2dimmat.m
@@ -1,9 +1,14 @@
-if not(MPI_Initialized)
-   MPI_Init();
-end
+# Please add the oct files openmpi_ext folder 
+# For instance 
+h = genpath("/home/corradin/working_directory/octave-forge/extra/openmpi_ext/");
+addpath(h);
+clear h;
+  MPI_SUCCESS =0;
+  MPI_Init();
 
-  my_rank = MPI_Comm_rank();
-  p = MPI_Comm_size();
+  CW = octave_comm_make("MPI_COMM_WORLD");
+  my_rank = MPI_Comm_rank(CW);
+  p = MPI_Comm_size(CW);
   mytag = 48;
 
 
@@ -14,20 +19,19 @@ end
 #        load message
 #       rankvect is the vector containing the list of rank  destination process
      rankvect = 0;
-     [info] = MPI_Send(message,rankvect,mytag);
+     [info] = MPI_Send(message,rankvect,mytag,CW);
   else
         for source = 1:p-1
           disp("We are at rank 0 that is master etc..");
-          [messager, info] = MPI_Recv(source,mytag);
+          [messager, info] = MPI_Recv(source,mytag,CW);
+          
 #	You could also save each result and make comparisons if you don't trust MPI
           disp("Rank 0 is the master receiving ... :");
-            if (info == true)
+            if (info == MPI_SUCCESS)
               disp('OK!');
           endif
           endfor
   end   
 
 
-  if not(MPI_Finalized)
    MPI_Finalize();
-  end

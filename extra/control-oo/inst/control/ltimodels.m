94c94a5ad1c53bf283b49b72740828d9c93abdbb
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2009-11-27 18:18:29 +0000

    control-oo: move test for ss zeros to ltimodels.m
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6546 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-oo/inst/control/ltimodels.m
+++ extra/control-oo/inst/control/ltimodels.m
@@ -129,3 +129,41 @@ endfunction
 %! Hinf = norm (sys, inf);
 %!assert (H2, 1.2527, 1.5e-5);
 %!assert (Hinf, 2.7, 0.1);
+
+## transmission zeros of state-space models
+
+## Results from the "Dark Side" 7.5 and 7.8
+##
+##  -13.2759
+##   12.5774
+##  -0.0155
+
+## Results from Scilab 5.2.0b1 (trzeros)
+##
+##  - 13.275931  
+##    12.577369  
+##  - 0.0155265
+
+%!shared z, z_exp
+%! A = [   -0.7   -0.0458     -12.2        0
+%!            0    -0.014   -0.2904   -0.562
+%!            1   -0.0057      -1.4        0
+%!            1         0         0        0 ];
+%!
+%! B = [  -19.1      -3.1
+%!      -0.0119   -0.0096
+%!        -0.14     -0.72
+%!            0         0 ];
+%!
+%! C = [      0         0        -1        1
+%!            0         0     0.733        0 ];
+%!
+%! D = [      0         0
+%!       0.0768    0.1134 ];
+%!
+%! sys = ss (A, B, C, D);
+%! z = sort (zero (sys));
+%!
+%! z_exp = sort ([-13.2759; 12.5774; -0.0155]);
+%!
+%!assert (z, z_exp, 1e-4);

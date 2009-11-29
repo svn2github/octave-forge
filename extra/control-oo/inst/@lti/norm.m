da2f71fcaeb64bb6a1058db7990961fb9d7ea7c0
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2009-11-29 04:17:08 +0000

    control-oo: add L-inf norm (SLICOT AB13DD)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6551 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-oo/inst/@lti/norm.m
+++ extra/control-oo/inst/@lti/norm.m
@@ -20,7 +20,7 @@
 ## @deftypefn {Function File} {@var{gain} =} norm (@var{sys}, @var{2})
 ## @deftypefnx {Function File} {@var{gain} =} norm (@var{sys}, @var{inf})
 ## @deftypefnx {Function File} {@var{gain} =} norm (@var{sys}, @var{inf}, @var{tol})
-## Return norm of LTI model.
+## Return norm of LTI model. L-infinity norm uses SLICOT AB13DD.
 ## @end deftypefn
 
 ## Author: A. S. Hodel <a.s.hodel@eng.auburn.edu>
@@ -33,11 +33,9 @@
 ## Date: November 2009
 ## Version: 0.1
 
-## TODO: Use Fortran code from Slicot
+function [gain, varargout] = norm (sys, ntype = "2", tol = 0.01)
 
-function gain = norm (sys, ntype = "2", tol = 0.001)
-
-  if (nargin > 3)  # norm () is catched by built-in function
+  if (nargin > 3)  # norm () is caught by built-in function
     print_usage ();
   endif
 
@@ -60,7 +58,7 @@ function gain = norm (sys, ntype = "2", tol = 0.001)
       gain = h2norm (sys);
 
     case "inf"
-      gain = hinfnorm (sys, tol);
+      [gain, varargout{1}] = linfnorm (sys, tol);
 
     otherwise
       error ("lti: norm: invalid norm type");
@@ -93,80 +91,27 @@ function gain = h2norm (sys)
 endfunction
 
 
-function g = hinfnorm (sys, tol = 0.001, gmin = 1e-9, gmax = 1e9, ptol = 1e-9)
+function [gain, wpeak] = linfnorm (sys, tol = 0.01)
 
-  if (isstable (sys))
-
-    [A, B, C, D, tsam] = ssdata (sys);
-    n = rows (A);     # states
-    m = columns (B);  # inputs
-    p = rows (C);     # outputs
-    dflg = (tsam > 0);
-
-    Dnrm = norm (D);
-    if (nargin < 3)
-      gmin = max (gmin, Dnrm);  # min gain value
-    elseif (gmin < Dnrm)
-      warning ("hinfnorm: setting Gmin=||D||=%g", Dnrm);
+  [a, b, c, d, tsam] = ssdata (sys);
+  tol = max (tol, 100*eps);
+  
+  [fpeak, gpeak] = slab13dd (a, b, c, d, tsam, tol);
+  
+  if (fpeak(2) > 0)
+    if (tsam > 0)
+      wpeak = fpeak(1) / tsam;
+    else
+      wpeak = fpeak(1);
     endif
-
-    In = eye (n);
-    Im = eye (m);
-    Ip = eye (p);
-
-    ## find the Hinf norm via binary search
-    while (gmax/gmin - 1 > tol)
-      g = (gmax+gmin)/2;
-
-      if (dflg)
-        ## multiply g's through in formulas to avoid extreme magnitudes...
-        Rg = g^2*Im - D'*D;
-        Ak = A + (B/Rg)*D'*C;
-        Ck = g^2*C'*((g^2*Ip-D*D')\C);
-
-        ## set up symplectic generalized eigenvalue problem per Iglesias & Glover
-        s1 = [Ak , zeros(n); -Ck, In];
-        s2 = [In, -(B/Rg)*B'; zeros(n), Ak'];
-
-        ## guard against roundoff again: zero out extremely small values
-        ## prior to balancing
-        s1 = s1 .* (abs(s1) > ptol*norm(s1,"inf"));
-        s2 = s2 .* (abs(s2) > ptol*norm(s2,"inf"));
-        [cc, dd, s1, s2] = balance (s1, s2);
-        [qza, qzb, zz, pls] = qz (s1, s2, "S"); # ordered qz decomposition
-        eigerr = abs (abs(pls)-1);
-        normH = norm ([s1, s2]);
-        Hb = [s1, s2];
-
-        ## check R - B' X B condition (Iglesias and Glover's paper)
-        X = zz((n+1):(2*n),1:n)/zz(1:n,1:n);
-        dcondfailed = min (real (eig (Rg - B'*X*B)) < ptol);
-      else
-        Rinv = inv(g*g*Im - (D' * D));
-        H = [A + B*Rinv*D'*C,        B*Rinv*B';
-             -C'*(Ip + D*Rinv*D')*C, -(A + B*Rinv*D'*C)'];
-
-        ## guard against roundoff: zero out extremely small values prior
-        ## to balancing
-        H = H .* (abs (H) > ptol * norm (H, "inf"));
-        [DD, Hb] = balance (H);
-        pls = eig (Hb);
-        eigerr = abs (real (pls));
-        normH = norm (H);
-        dcondfailed = 0;          # digital condition; doesn't apply here
-      endif
-
-      if ((min (eigerr) <= ptol * normH) | dcondfailed)
-        gmin = g;
-      else
-        gmax = g;
-      endif
-    endwhile
-
   else
-    warning ("norm: Hinf: unstable system (ptol=%g), returning Inf", ptol);
-    g = Inf;
+    wpeak = inf;
+  endif
+  
+  if (gpeak(2) > 0)
+    gain = gpeak(1);
+  else
+    gain = inf;
   endif
 
 endfunction
-

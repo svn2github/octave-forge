082c185ce34a18918a6d80caa2f1a87ca4075afb
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2010-01-13 22:44:58 +0000

    add make file for ml* for win
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6768 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/NaN/src/make.m
@@ -0,0 +1,26 @@
+% This make.m is used for Matlab under Windows
+
+%	$Id$
+%	Copyright (C) 2010 by Alois Schloegl <a.schloegl@ieee.org>
+%       This function is part of the NaN-toolbox
+%       http://www.dpmi.tu-graz.ac.at/~schloegl/matlab/NaN/
+
+% add -largeArrayDims on 64-bit machines
+
+mex covm_mex.cpp
+mex histo_mex.cpp
+mex sumskipnan_mex.cpp
+mex -c svm.cpp
+mex -c svm_model_matlab.c
+mex -c tron.cpp
+mex -c linear.cpp
+mex -c linear_model_matlab.c
+if strcmp(computer,'PCWIN')
+	mex svmtrain.cpp svm.obj svm_model_matlab.obj
+	mex svmpredict_mex.cpp svm.obj svm_model_matlab.obj
+	mex train.c tron.obj linear.obj linear_model_matlab.obj 
+else
+	mex svmtrain.cpp svm.o svm_model_matlab.o
+	mex svmpredict_mex.cpp svm.o svm_model_matlab.o
+	mex train.c tron.o linear.o linear_model_matlab.o 
+end; 

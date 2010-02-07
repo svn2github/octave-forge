25679872150d5ccaf4afd14a0b9052350c6a66ca
hauberg <hauberg@fc592f45-0881-4044-91ff-c96495a54512>
2010-02-07 21:20:47 +0000

    Remove an 'end' that didn't partse
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6837 fc592f45-0881-4044-91ff-c96495a54512

--- main/vrml/inst/vrml_interp.m
+++ main/vrml/inst/vrml_interp.m
@@ -39,13 +39,14 @@ else
   error ("vrml_interp : typ should be a string in:\n%s",typ,e2);
 end
 
-if isempty (val), vs = "";
+if isempty (val)
+  vs = "";
 else 
   vs = sprintf ("%8.5f, ", val);
   vs = sprintf ("    keyValue [ %s ]\n", vs(1:length(vs)-2));
 end
-end
-if isempty (key), ks = "";
+if isempty (key)
+  ks = "";
 else              
   ks = sprintf ("%8.5f, ", key);
   ks = sprintf ("    key [ %s ]\n", ks(1:length(ks)-2));

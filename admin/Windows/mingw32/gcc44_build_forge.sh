5fbafd6417a6530d6137f6e44d83a5c0d6d1b036
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2010-02-22 19:56:54 +0000

    mingw32: add & update several octave forge packages
    *) CHG: add the following packages from octave forge to the installer:
                jhandles-0.3.5+
                java-1.2.6+
                video-1.0.2
                msh-1.0.0
                fpl-1.0.0
                actuarial-1.1.0
                bim-1.0.10
                fenv-0.1.0
                generate_html-0.1.2
                gnuplot-1.0.1
                gpc-0.1.7
                nlwing2-1.1.1
                nurbs-1.0.3
                ocs-0.0.4
                oct2mat-1.0.7
                octgpr-1.1.5
                simp-1.1.0
                spline-gcvspl-1.0.8
                symband-1.0.10
    
    *) CHG: updated the following octave forge packages
                windows  1.0.8  -> 1.0.8+
                octcdf   1.0.13 -> 1.0.17+
                optim    1.0.6  -> 1.0.10+
                general  1.1.3  -> 1.2.0
                io       1.0.9  -> 1.0.10
                odepkg   0.6.7  -> 0.6.10
    
    
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6948 fc592f45-0881-4044-91ff-c96495a54512

--- admin/Windows/mingw32/gcc44_build_forge.sh
+++ admin/Windows/mingw32/gcc44_build_forge.sh
@@ -10,54 +10,70 @@ source gcc44_pkg_version.sh
 # the packages installed
 # in the form <package-name>-<version>
 FORGEPACK="\
-general-1.1.3 \
+windows-1.0.8 \
+java-1.2.6 \
+splines-1.0.7 \
+msh-1.0.0 \
+fpl-1.0.0 \
+actuarial-1.1.0 \
+general-1.2.0 \
 miscellaneous-1.0.9 \
 time-1.0.9 \
 specfun-1.0.8 \
-optim-1.0.6 \
+optim-1.0.10 \
 signal-1.0.10 \
 audio-1.1.4 \
 benchmark-1.1.1 \
+bim-1.0.0 \
 bioinfo-0.1.2 \
 combinatorics-1.0.9 \
 communications-1.0.10 \
 control-1.0.11 \
 data-smoothing-1.2.0 \
 econometrics-1.0.8 \
+fenv-0.1.0 \
 financial-0.3.2 \
 fixed-0.7.10 \
 ga-0.9.7 \
+generate_html-0.1.2 \
+gnuplot-1.0.1 \
+gpc-0.1.7 \
 gsl-1.0.8 \
 ident-1.0.7 \
 image-1.0.10 \
 informationtheory-0.1.8 \
-io-1.0.9 \
+integration-1.0.7 \
+io-1.0.10 \
 irsa-1.0.7 \
+jhandles-0.3.5 \
 linear-algebra-1.0.8 \
+mapping-1.0.7 \
 missing-functions-1.0.2 \
+nlwing2-1.1.1 \
 nnet-0.1.10 \
-octcdf-1.0.13 \
+nurbs-1.0.3 \
+ocs-0.0.4 \
+oct2mat-1.0.7 \
+octcdf-1.0.17 \
+octgpr-1.1.5 \
 odebvp-1.0.6 \
-odepkg-0.6.7 \
+odepkg-0.6.10 \
 optiminterp-0.3.2 \
 outliers-0.13.9 \
 physicalconstants-0.1.7 \
 plot-1.0.7 \
 quaternion-1.0.0 \
+simp-1.1.0 \
 sockets-1.0.5 \
 special-matrix-1.0.7 \
-splines-1.0.7 \
+spline-gcvspl-1.0.8 \
 statistics-1.0.9 \
 strings-1.0.7 \
 struct-1.0.7 \
+symband-1.0.10 \
 symbolic-1.0.9 \
-zenity-0.5.7 \
-integration-1.0.7 \
-mapping-1.0.7 \
 video-1.0.2 \
-windows-1.0.8 \
-java-1.2.6 \
-jhandles-0.3.5 \
+zenity-0.5.7 \
 "
 
 
@@ -67,7 +83,10 @@ jhandles-0.3.5 \
 # FINANCIAL depends on TIME
 # DATA-SMOOTHING depends on OPTIM
 # GA depends on COMMUNICATIONS
-
+# IO requires JAVA and WINDOWS
+# JHANDLES requires JAVA
+# BIM requires FPL and MSH
+# MSH depends on SPLINES
 # ann parallel ftp database 
 
 # FTP requires ftplib
@@ -94,8 +113,14 @@ FORGE_OCT_ROOT=${PACKAGE_ROOT}/libexec/octave/packages/
 
 FORGELOG=forge.log
 FORGELISTLOG=forgelist.txt
+FORGENAMELISTLOG=forgenamelist.txt
 
 rm -f ${FORGELISTLOG}
+rm -f ${FORGENAMELISTLOG}
+
+if [ ! -z "$1" ]; then
+   FORGEPACK=$1
+fi
 
 echo Installing forge packages for octave ${VER_OCTAVE} > ${FORGELOG}
 echo The following packages are installed: >> ${FORGELOG}
@@ -106,11 +131,14 @@ done
 for a in $FORGEPACK; do
    # the package under process
    PACK=$a
+   PACKNAME=`echo $a | sed -ne "s@^\(.\+\)-\([0-9]\+\.[0-9]\+\.[0-9]\+\)\\$@\1@p"`
+   PACKVER=`echo $a | sed -ne "s@^\(.\+\)-\([0-9]\+\.[0-9]\+\.[0-9]\+\)\\$@\2@p"`
    
    if [ -d ${FORGE_M_ROOT}/$PACK -o -d ${FORGE_OCT_ROOT}/$PACK ]; then
       echo Forge Package "${PACK}" is already installed...
       echo Forge Package "${PACK}" is already installed...>>${FORGELOG}
       echo $PACK >> ${FORGELISTLOG}
+      echo $PACKNAME >> ${FORGENAMELISTLOG}
    else
       echo installing package "${PACK}" ...
       
@@ -135,22 +163,34 @@ for a in $FORGEPACK; do
       if [ -e ${FORGESRCDIR}/${PACK}.patch ]; then
          # extract to temporary directory
          if [ -e /tmp/${PACK} ]; then rm -rf /tmp/${PACK}; fi
+         if [ -e /tmp/$PACKNAME ]; then rm -rf /tmp/$PACKNAME; fi
          bsdtar x -C /tmp -f ${FORGESRCDIR}/${PACK}.tar.gz
          
          # the patch file to apply 
          PF=`pwd`/${FORGESRCDIR}/${PACK}.patch
          # patch
-         ( cd /tmp/${PACK} && patch -u -p 1 -i $PF )
+         if [ -e /tmp/$PACK ]; then
+            PACKDIR=$PACK
+         else
+            if [ -e /tmp/$PACKNAME ]; then
+               PACKDIR=$PACKNAME
+            else
+               echo ERROR The package $PACK unpacks neither into $PACK/ nor $PACKNAME/
+               exit 1
+            fi
+         fi
+         
+         ( cd /tmp/$PACKDIR && patch -u -p 1 -i $PF )
          
          # call autogen if present (patch might have changed configure.base)
-         if [ -e /tmp/$PACK/src/autogen.sh ]; then
-            ( cd /tmp/$PACK/src && ./autogen.sh )
+         if [ -e /tmp/$PACKDIR/src/autogen.sh ]; then
+            ( cd /tmp/$PACKDIR/src && ./autogen.sh )
          fi
          
          # ... and install from unpacked directory
          ( cd /tmp
            "${PACKAGE_ROOT}/bin/octave.exe" -q -f -H \
-           --eval "page_screen_output(0); pkg install -verbose ${AUTO} ${PACK}"
+           --eval "page_screen_output(0); pkg install -verbose ${AUTO} ${PACKDIR}"
          )
       else
          "${PACKAGE_ROOT}/bin/octave.exe" -q -f -H \
@@ -160,6 +200,7 @@ for a in $FORGEPACK; do
       if [ -d ${FORGE_M_ROOT}/$PACK ]; then
          echo Successfully installed $PACK >> ${FORGELOG}
          echo $PACK >> ${FORGELISTLOG}
+         echo $PACKNAME >> ${FORGENAMELISTLOG}
       else
          echo ERROR INSTALLING PACKAGE $PACK >> ${FORGELOG}
       fi

43bb3534a7991ce3a82ead8826a7d1109a836987
highegg <highegg@fc592f45-0881-4044-91ff-c96495a54512>
2010-02-22 06:52:56 +0000

    remove inz, grep, improve nze
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6942 fc592f45-0881-4044-91ff-c96495a54512

--- main/miscellaneous/inst/nze.m
+++ main/miscellaneous/inst/nze.m
@@ -1,19 +1,31 @@
+## Copyright (C) 2010 VZLU Prague
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 3 of the License, or (at your
+## option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+## Public License for more details.
+## 
+## You should have received a copy of the General Public License along with
+## this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ## -*- texinfo -*-
-## @deftypefn{Function File} {@var{[y,f]} = } nze ( x ) 
-## @cindex  
-##
-## This function is just to save typing and improve readability, as in
-## foo(find(foo(:,p)),p)  vs.  nze(foo(:,p)) 
-##
-## @example
-##       y = nze(x) ==  x(find(x)) - Non-Zero Elements
-##   [y,f] =        == [x(find(x)),find(x)]
-## @end example
-## @seealso{grep}
+## @deftypefn{Function File} {[@var{y}, @var{f}] = } nze (@var{x}) 
+## Extract nonzero elements of @var{x}. Equivalent to @code{@var{x}(@var{x} != 0)}.
+## Optionally, returns also linear indices.
 ## @end deftypefn
 
 ## Author:        Etienne Grossmann <etienne@cs.uky.edu>
-## Last modified: November 2002
+## Author:        Jaroslav Hajek <highegg@gmail.com>
 
-function [y,f] = nze(x)
-[f,j,y] = find(x(:));
+function [y, f] = nze (x)
+  nz = x != 0;
+  y = x(nz);
+  if (nargout > 1)
+    f = find (nz);
+  endif
+endfunction

1c780e2111bb7d6a6748d7120bb429c6f52ee8ad
hauberg <hauberg@fc592f45-0881-4044-91ff-c96495a54512>
2010-03-05 23:28:11 +0000

    Update Doxygen stuff to work with my current setup (this is not very reliable)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@6997 fc592f45-0881-4044-91ff-c96495a54512

--- doc/doxygen_mainpage.cc
+++ doc/doxygen_mainpage.cc
@@ -1,9 +1,13 @@
-/*! \mainpage C++ Documentation
+/*! \mainpage C++ API
  *
- *  This is the documentation for the Octave C++ APIs for version 
- *  __OCTAVE_VERSION__ of Octave. It is mainly useful as a reference to the 
+ *  This is the documentation for the C++ APIs for version 
+ *  3.2 of GNU Octave. It is mainly useful as a reference to the 
  *  API and is <b>not</b> intended as an introduction to working with C++ and 
  *  Octave. For an introduction to working with the C++ API see the 
  *  <a href="http://www.octave.org/doc/interpreter/">Octave manual</a> or 
- *  <a href="__BASE_ADDRESS__/coda/index.html">Da Coda Al Fine</a>.
+ *  <a href="http://octave.sf.net/coda/index.html">Da Coda Al Fine</a>.
+ *
+ *  It should be noted that the C++ API is only stable within minor revisions
+ *  of GNU Octave. Hence the API for e.g. version 3.2.0 is that same as in 3.2.1,
+ *  but not necessaryly the same as in 3.4.0.
  */

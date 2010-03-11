72dcd0ff403b5674b3bc3a64b847874071341cd3
rikcorradini <rikcorradini@fc592f45-0881-4044-91ff-c96495a54512>
2010-03-11 16:07:11 +0000

    added texinfo comment
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7032 fc592f45-0881-4044-91ff-c96495a54512

--- extra/openmpi_ext/src/MPI_Comm_Test.cc
+++ extra/openmpi_ext/src/MPI_Comm_Test.cc
@@ -1,6 +1,21 @@
 #include "simple.h"
-
-DEFUN_DLD(MPI_Comm_Test, args, ,"")
+DEFUN_DLD(MPI_Comm_Test, args, ,"-*- texinfo -*-\n\
+@deftypefn {Built-in Function} {} @var{exprout} = MPI_Comm_Test (@var{exprin})\n\
+Return @var{exprout} string description of the MPI_Communicator  @var{exprin}\n\
+For\n\
+example,\n\
+\n\
+@example\n\
+@group\n\
+MPI_Init();\n\
+X = MPI_Comm_Load(\"description\"); \n\
+whos X\n\
+MPI_Comm_Test(X) \n\
+@result{} \"description\"\n\
+MPI_Finalize();\n\
+@end group\n\
+@end example\n\
+@end deftypefn")
 {
   if (!simple_type_loaded)
     {

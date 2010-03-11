1b88330aa51a37a5649661f41a46bb7bc1d7b586
rikcorradini <rikcorradini@fc592f45-0881-4044-91ff-c96495a54512>
2010-03-11 11:22:45 +0000

    Added standard texinfo explanations
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7018 fc592f45-0881-4044-91ff-c96495a54512

--- extra/openmpi_ext/src/MPI_Comm_Load.cc
+++ extra/openmpi_ext/src/MPI_Comm_Load.cc
@@ -1,6 +1,23 @@
 #include "simple.h"
 
-DEFUN_DLD(MPI_Comm_Load, args, ,"")
+DEFUN_DLD(MPI_Comm_Load, args, ,"-*- texinfo -*-\n\
+@deftypefn {Built-in Function} {} @var{exprout} = MPI_Comm_Load (@var{exprin})\n\
+Return @var{exprout} the MPI_Communicator object whose description is  @var{exprin}, as a string.\n\
+The default value will be MPI_COMM_WORLD. \n\
+If @var{exprin} is omitted, return anyway an MPI_COMM_WORLD comunicator object \n\
+with no decription.\n\
+For\n\
+example,\n\
+\n\
+@example\n\
+@group\n\
+MPI_Init();\n\
+X = MPI_Comm_Load(\"description\"); \n\
+whos X\n\
+MPI_Finalize();\n\
+@end group\n\
+@end example\n\
+@end deftypefn")
 {
   if (!simple_type_loaded)
     {

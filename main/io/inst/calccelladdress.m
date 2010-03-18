c161ed4be0c2ebf95ba3a09da6cd609bb868dca4
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2010-03-18 23:20:31 +0000

    Removed first two input arguments; usage is much more logical now (just row & column index needed).
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7072 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/calccelladdress.m
+++ main/io/inst/calccelladdress.m
@@ -14,33 +14,35 @@
 ## along with Octave; see the file COPYING.  If not, see
 ## <http://www.gnu.org/licenses/>.
 
-## calccelladdress - compute spreadsheet style cell address from
-## row & column index.
+## calccelladdress (R, C) - compute spreadsheet style cell address from
+## row & column index (both 1-based).
 ## 
 ## Max column index currently set to 18278 (max ODS: 1024, OOXML: 16384).
 ## Row limits for ODF and OOXML are 65536 and 1048576, resp.
 
 ## Author: Philip Nienhuis <prnienhuis at users.sf.net>
 ## Created: 2009-12-12
-## Last updated: 2009-12-27
+## Updates:
+## 2009-12-27 Fixed OOXML limits
+## 2010-03-17 Simplified argument list, only row + column needed.
 
-function [ celladdress ] = calccelladdress (trow, lcol, row, column)
+function [ celladdress ] = calccelladdress (row, column)
 
-	if (nargin < 4) error ("calccelladdress: not enough arguments.") endif
-	colnr = lcol + column - 1;
-	if (colnr > 18278) error ("Column nr > 18278"); endif
-	rem1 = rem ((colnr-1), 26);
+	if (nargin < 2) error ("calccelladdress: not enough arguments.") endif
+
+	if (column > 18278) error ("Column nr > 18278"); endif
+	rem1 = rem ((column-1), 26);
 	str = char (rem1 + 'A');								# A-Z; rightmost digit
-	if (colnr > 26 && colnr < 703)							# AA-ZZ
-		tmp = char (floor(colnr - rem1) / 26 - 1 + 'A');	# Leftmost digit
+	if (column > 26 && column < 703)							# AA-ZZ
+		tmp = char (floor(column - rem1) / 26 - 1 + 'A');	# Leftmost digit
 		str = [tmp str];
-	elseif (colnr > 702 && colnr < 18279)					# AAA-ZZZ
-		rem2 = rem ((colnr - 26 - rem1) - 1, 676);
+	elseif (column > 702 && column < 18279)					# AAA-ZZZ
+		rem2 = rem ((column - 26 - rem1) - 1, 676);
 		str2 = char (rem2 / 26 + 'A');						# Middle digit
-		colnr = colnr -  rem2 - rem1;
-		str3 = char (colnr / 676 - 1 + 'A');				# Leftmost digit
+		column = column -  rem2 - rem1;
+		str3 = char (column / 676 - 1 + 'A');				# Leftmost digit
 		str = [str3 str2 str];
 	endif	
-	celladdress = sprintf ("%s%d", str, trow + row - 1);
+	celladdress = sprintf ("%s%d", str, row);
 
 endfunction

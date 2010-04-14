5198fde3fcbe3f55a0ae68d6c17a358fd309bc0b
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2010-04-14 21:25:13 +0000

    API changed to the standard style.
    Changed help text to reflect new API.
    
    Uses the function _zenity_options_ for the processing of the options.
    
    Updated license for GPL v3 or later (at your option).
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7195 fc592f45-0881-4044-91ff-c96495a54512

--- main/zenity/inst/zenity_file_selection.m
+++ main/zenity/inst/zenity_file_selection.m
@@ -3,7 +3,7 @@
 ## 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
+## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
 ## 
 ## This program is distributed in the hope that it will be useful,
@@ -15,69 +15,62 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {Function File} @var{files} = zenity_file_selection(@var{title}, @var{option1}, ...)
-## Opens a file selection dialog using Zenity. The variable @var{title} sets the
-## title of the file selection window. The optional string arguments can be:
+## @deftypefn  {Function File} @var{files} = zenity_file_selection(@var{param1}, @var{value1}, ...)
+## Opens a file selection dialog using Zenity. All @var{parameters} are 
+## optional, but if given, may require a corresponding @var{value}. All possible
+## parameters are:
+## 
 ## @table @samp
-## @item save
-## The file selection dialog is a dialog for saving files.
-## @item multiple
-## It is possible to select multiple files. @var{files} will hold a cell array,
-## even if user selects only one or no file.
 ## @item directory
-## Activates directory-only selection.
-## @item timeout=XX
-## Specifies timeout @var{XX} in seconds after which the dialog is closed. If
-## no file is selected in the specified time, returns an empty string or cell
-## array, depending whether `multiple' was selected or not.
-## @item Anything else
-## The argument will be the default selected file.
+## Activates directory-only selection. No value is required.
+## @item filename
+## Sets the default selected file. Requires a string as value.
+## @item height
+## Sets the height of the dialog window. Requires a scalar as value.
+## @item multiple
+## Allows selection of multiple files. No value is required. @var{files} will
+## hold a cell array, even if user selects only one or no file.
+## @item save
+## The file selection dialog is a dialog for saving files. No value is required.
+## @item timeout
+## Sets the time in seconds after which the dialog is closed. Requires a scalar
+## as value.
+## @item title
+## Sets the title of the window. Requires a string as value.
+## @item width
+## Sets the width of the dialog window. Requires a scalar as value.
 ## @end table
 ##
-## @seealso{zenity_calendar, zenity_list, zenity_progress, zenity_entry, zenity_message,
-## zenity_text_info, zenity_notification}
+## @seealso{zenity_list, zenity_entry, zenity_message, zenity_text_info}
 ## @end deftypefn
 
-function files = zenity_file_selection(title, varargin)
+function files = zenity_file_selection(varargin)
 
-  save = multiple = directory = filename = timeout = "";
-  if (nargin == 0 || isempty(title))
-    title = "Select a file";
-  endif
-  for i = 1:length(varargin)
-    option  = varargin{i};
-    isc     = ischar(option);
-    if (isc && strcmpi(option, "save"))
-      save = "--save";
-    elseif (isc && strcmpi(option, "multiple"))
-      multiple = "--multiple";
-    elseif (isc && strcmpi(option, "directory"))
-      directory = "--directory";
-    elseif (isc)
-      if (strfind(option, "timeout="))
-        timeout = ["--timeout=", option(strfind(option, "=")+1 : end)];
-      else
-        filename = sprintf('--filename="%s"', varargin{i});
-      endif
-    else
-      error("zenity_file_selection: unsupported option");
-    endif
-  endfor
+  options = _zenity_options_ ("file selection", varargin);
 
-# The separator is set to "/" because it's an invalid character for filenames
-# in most filesystems and so, unlikely to exist in the middle of filenames.
-# It's also the fileseparator so filenames will always already start with a
-# '/' which is good since we can look for double '//' as separator for filepaths
-  cmd = sprintf('zenity --file-selection --title="%s" --separator="/" %s %s %s %s %s', ...
-                 title, save, multiple, directory, filename, timeout);
+  # The separator is set to "/" because it's an invalid character for filenames
+  # in most filesystems and so, unlikely to exist in the middle of filenames.
+  # It's also the fileseparator so filenames will always already start with a
+  # '/' which is good since we can look for double '//' as separator for filepaths
+  cmd = sprintf("zenity --file-selection --separator=\"/\" %s %s %s %s %s %s", ...
+                 options.directory, options.filename, options.height, ...
+                 options.multiple, options.save, options.timeout, ...
+                 options.title, options.width);
+
   [status, output] = system(cmd);
+
+  # Exit code -1 = An unexpected error has occurred
+  # Exit code  0 = The user has pressed either OK or Close. 
+  # Exit code  1 = The user has either pressed Cancel, or used the window
+  # functions to close the dialog
+  # Exit code  5 = The dialog has been closed because the timeout has been reached
   if (status == 0)
     if (output(end) == "\n")
         output = output(1:end-1);
     endif
-# With 'multiple', always place the output in a cell array, even if only one
-# file is selected.
-    if (multiple)
+  # With 'multiple', always place the output in a cell array, even if only one
+  # file is selected.
+    if (options.multiple)
       idx = strfind(output, "//");
       if (idx)
         files = cell(length(idx)+1, 1);
@@ -92,17 +85,13 @@ function files = zenity_file_selection(title, varargin)
     else
       files = output;
     endif
-# Exit code -1 = An unexpected error has occurred
-# Exit code  1 = The user has either pressed Cancel, or used the window
-# functions to close the dialog
-# Exit code  5 = The dialog has been closed because the timeout has been reached
-  elseif (status == 1 && multiple)
+  elseif (status == 1 && options.multiple)
     warning("No file selected. Returning empty cell array.");
     files = cell(1);
   elseif (status == 1)
     warning("No file selected. Returning empty string.");
     files = "";
-  elseif (status == 5 && multiple)
+  elseif (status == 5 && options.multiple)
     warning("Timeout reached. No file selected selected. Returning empty cell array.");
     files = cell(1);
   elseif (status == 5)

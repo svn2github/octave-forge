fd14cb393402403c9f0d155de77eee22bfe6a17b
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2010-04-26 08:36:36 +0000

    fix typos
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7246 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/src/make.m
+++ extra/NaN/src/make.m
@@ -8,13 +8,13 @@ function make(arg1)
 
 % add -largeArrayDims on 64-bit machines
 
-if (arg1>0 && strcmp(arg1,'clean')),
+if (nargin>0 && strcmp(arg1,'clean')),
 	if strcmp(computer,'PCWIN')
-		!del *.obj
-		!del *.mex*
+		dos('del *.obj');
+		dos('del *.mex*');
 	else
-		!rm *.o 
-		!rm *.mex*
+		unix('rm *.o'); 
+		unix('rm *.mex*');
 	end;
 	return; 
 end;
@@ -28,14 +28,14 @@ mex -c svm_model_matlab.c
 mex -c tron.cpp
 mex -c linear.cpp
 mex -c linear_model_matlab.c
-if strcmp(computer,'PCWIN') && ~exist('OCTAVE_VERSION,'builtin'),
+if strcmp(computer,'PCWIN') && ~exist('OCTAVE_VERSION','builtin'),
 	mex svmtrain_mex.cpp svm.obj svm_model_matlab.obj
 	mex svmpredict_mex.cpp svm.obj svm_model_matlab.obj
 
         if ~exist('LAPACK/daxpy.f','file') || ~exist('LAPACK/ddot.f','file') || ~exist('LAPACK/dscal.f','file') || ~exist('LAPACK/dnrm2.f','file'),
-                fprintf(1,'The lapack functions daxpy, ddot, dscal, and dnrm2 are required.\n);
-                fprintf(1,'If some functions are missing, get them from here:\n);
-                if ~exist('LAPACK','dir), mkdir('LAPACK'); end; 
+                fprintf(1,'The lapack functions daxpy, ddot, dscal, and dnrm2 are required.\n');
+                fprintf(1,'If some functions are missing, get them from here:\n');
+                if ~exist('LAPACK','dir') mkdir('LAPACK'); end; 
 	        fprintf(1,'Get http://www.netlib.org/blas/daxpy.f and save to %s',fullfile(pwd,'LAPACK')); 
 	        fprintf(1,'Get http://www.netlib.org/blas/ddot.f and save to %s',fullfile(pwd,'LAPACK')); 
 	        fprintf(1,'Get http://www.netlib.org/blas/dscal.f and save to %s',fullfile(pwd,'LAPACK')); 
@@ -48,7 +48,7 @@ if strcmp(computer,'PCWIN') && ~exist('OCTAVE_VERSION,'builtin'),
         mex -c LAPACK/dscal.f
         mex -c LAPACK/dnrm2.f
 	mex('train.cpp','tron.obj','linear.obj','linear_model_matlab.obj','daxpy.obj','ddot.obj','dscal.obj','dnrm2.obj')
-	!del *.obj
+	dos('del *.obj');
 
 else
 	mex svmtrain_mex.cpp svm.o svm_model_matlab.o 

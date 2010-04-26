4f6dc92376cec7d116b578fa25ef47b6d7a9e657
lindnerb <lindnerb@fc592f45-0881-4044-91ff-c96495a54512>
2010-04-26 14:58:26 +0000

    mingw32: (gcc-4.5.0) add zlib
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7256 fc592f45-0881-4044-91ff-c96495a54512

---
+++ admin/Windows/mingw32/zlib/build-1.2.5-5.sh
@@ -0,0 +1,147 @@
+#! /usr/bin/sh
+
+# Name of package
+PKG=zlib
+# Version of Package
+VER=1.2.5
+# Release of (this patched) package
+REL=5
+# Name&Version of Package
+PKGVER=${PKG}-${VER}
+# Full name of this patched Package
+FULLPKG=${PKGVER}-${REL}
+
+# Name of source file(s)
+SRCFILE=${PKGVER}.tar.gz
+# Name of Patch file
+PATCHFILE=${FULLPKG}.patch
+
+# URL(s) of source code file(s)
+URL="http://zlib.net/zlib-1.2.5.tar.gz"
+
+# Top dir of this building process (i.e. where the patch file and source file(s) reside)
+TOPDIR=`pwd`
+# Directory source code is extraced to (relative to TOPDIR)
+SRCDIR=${PKGVER}
+# Directory original source code is extracted to (for generating diffs) (relative to TOPDIR)
+SRCDIR_ORIG=${SRCDIR}-orig
+
+# Make file to use (optional)
+MAKEFILE=win32/Makefile.gcc
+# Any extra flags to pass make to
+MAKE_XTRA=
+
+# Header files to install
+HEADERS_INSTALL="zlib.h zconf.h"
+
+# License file(s) to install
+LICENSE_INSTALL="README"
+
+# pkg-config .pc files to install
+PKG_CONFIG_INSTALL=""
+
+# Additional DIFF Flags for generating diff file
+DIFF_FLAGS=
+
+# load common functions
+source ../gcc45_common.sh
+
+# Directory the lib is built in (set this *after* loading gcc44_common.sh)
+BUILDDIR=".build_${BUILD_TARGET}_${FULLPKG}_gcc${GCC_VERSION}${GCC_SYSTEM}"
+
+# == override resp. specify build actions ==
+
+mkdirs_post() 
+{ 
+   # directory makefile is located
+   mkdir -p ${BUILDDIR}/win32
+}
+
+conf()
+{
+   conf_pre;
+   substvars ${SRCDIR}/${MAKEFILE} ${BUILDDIR}/${MAKEFILE}
+   conf_post;
+}
+
+install()
+{
+   install_pre;
+   
+   # Install library, import library and static library
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/zlib1.dll  $PREFIX/$BIN_DIR
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/libz.dll.a $PREFIX/$LIB_DIR
+   ${CP} ${CP_FLAGS} ${BUILDDIR}/libz.a     $PREFIX/$STATICLIB_DIR
+   
+   # Install pkg-config .pc files
+   for a in $PKG_CONFIG_INSTALL; do
+      ${CP} ${CP_FLAGS} ${BUILDDIR}/$a $PREFIX/$PKGCONFIG_DIR
+   done
+   
+   # Install headers
+   for a in $HEADERS_INSTALL; do
+      ${CP} ${CP_FLAGS} ${SRCDIR}/$a $PREFIX/$INC_DIR
+   done
+   
+   # Install license file
+   for a in $LICENSE_INSTALL; do
+      ${CP} ${CP_FLAGS} ${SRCDIR}/$a $PREFIX/$LIC_DIR/$PKG
+   done
+   
+   install_post;
+}
+
+install_strip()
+{
+   install;
+   $STRIP $STRIP_FLAGS $PREFIX/$BIN_DIR/zlib1.dll
+}
+
+uninstall()
+{
+   uninstall_pre;
+   
+   # Install library, import library and static library
+   ${RM} ${RM_FLAGS} $PREFIX/$BIN_DIR/zlib1.dll
+   ${RM} ${RM_FLAGS} $PREFIX/$LIB_DIR/libz.dll.a
+   ${RM} ${RM_FLAGS} $PREFIX/$STATICLIB_DIR/libz.a
+   
+   # Uninstall headers
+   for a in $HEADERS_INSTALL; do
+      ${RM} ${RM_FLAGS} $PREFIX/$INC_DIR/$a
+   done
+   
+   # Uninstall pkg-config .pc files
+   for a in $PKG_CONFIG_INSTALL; do
+      ${RM} ${RM_FLAGS} $PREFIX/$PKGCONFIG_DIR/$a
+   done
+   
+   # Uninstall license file
+   for a in $LICENSE_INSTALL; do
+      ${RM} ${RM_FLAGS} $PREFIX/$LIC_DIR/$PKG/$a
+   done
+   
+   uninstall_post;
+}
+
+check()
+{
+   check_pre;
+   
+   ( cd ${BUILDDIR} && make_common test )
+   ( cd ${BUILDDIR} && make_common testdll )
+   
+   check_post;
+}
+
+all() {
+  download
+  unpack
+  applypatch
+  mkdirs
+  conf
+  build
+  install
+}
+
+main $*

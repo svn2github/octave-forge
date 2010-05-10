7903bfb306c3776feb9157b13a847b11bbae9e85
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2010-05-10 20:17:29 +0000

    conversion of DO...UNTIL and multiple assignments added
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7294 fc592f45-0881-4044-91ff-c96495a54512

--- extra/oct2mat/inst/test_oct2mat.m
+++ extra/oct2mat/inst/test_oct2mat.m
@@ -6,11 +6,32 @@ b=zeros(5);
 c=ones(5);
 mat = randn(5);
 BW2=1; BW=[-2:2];
-x = 1:5; y=1:2;
+x = 1:5; y=1:2; z = 2**a;
+## multiple assignments
+a=b=c;  ## coment1
+a=b=c=0; ## comment2
+a=b=c=d=e; 
+a=b=c~=d=e; 
+a = b = c = d = e; 
+aa = bb = cc = dd = ee; 
+## exclude comparison 
+aa=bb~=cc;
+aa=bb<=cc;
+aa=bb~=cc=dd;
+aa=bb>=cc=dd;
+##***********************
+ undo; 
+ do2
+ 1do
+ do
+ until (5) ## comment       
+ until (5)
+ a++; % until 5 o'clock
 
 b(1,:) += c(2,:);
 [cols{1:3}] = num2cell (mat){:};
-if(all((BW2==BW & BW != 0)(:)))
+if (all((BW2==BW & BW != 0)(:)))
+
   k_1 += 2 + a;
   k_2 += 2 | a;        ## make sure RHS is in parenthesis
   k_3 |= 2 || a;       ## make sure RHS is in parenthesis
@@ -56,14 +77,17 @@ do         ## not resolved
     x_5_-- ;
     _x_5_-- ;
     x(2)++ ;
+    x(2) -= 1;
     a=b+c; x++ ;
     x -- ;
 until (x(3)>10);
 
 function x = __underscore_function__(x)         ## todo 
+function x = __underscore_function__ (x)         ## todo 
         x += 2;
 endfunction      
 
 x = __underscore_function__(5);              ## todo
 __underscore_variable__ = 5;                 ## todo 
+ __underscore_variable__ = 5;                 ## todo 
 

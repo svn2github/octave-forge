b560f865a9c6e36c75657619a88502200a8718ce
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2010-05-26 18:04:35 +0000

    Added support for jOpenDocument. Actually this part works outside java as the entire sheet is processed as character array using octave's character functions (which turns out to be faster than jOpenDocument's methods).
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7349 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/getusedrange.m
+++ main/io/inst/getusedrange.m
@@ -20,14 +20,14 @@
 ## Created: 2010-03-18 (First usable version) for ODS (java/OTK)
 ## Updates:
 ## 2010-03-20 Added Excel support (Java/POI)
+## 2010-05-23 Added in support for jOpenDocument ODS
 
 function [ trow, lrow, lcol, rcol ] = getusedrange (spptr, ii)
 
 	if (strcmp (spptr.xtype, 'OTK'))
 		[ trow, lrow, lcol, rcol ] = getusedrange_otk (spptr, ii);
 	elseif (strcmp (spptr.xtype, 'JOD'))
-		error ('jOpenDocument not supported yet by getusedrange'); 
-#		[ trow, lrow, lcol, rcol ] = getusedrange_jod (spptr, ii);
+		[ trow, lrow, lcol, rcol ] = getusedrange_jod (spptr, ii);
 	elseif (strcmp (spptr.xtype, 'POI'))
 		[ trow, lrow, lcol, rcol ] = getusedrange_poi (spptr, ii);
 	elseif (strcmp (spptr.xtype, 'JXL'))
@@ -39,7 +39,7 @@ function [ trow, lrow, lcol, rcol ] = getusedrange (spptr, ii)
 endfunction
 
 
-## Copyright (C) 2010 Philip Nienhuis, pr.nienhuis@users.sf.net
+## Copyright (C) 2010 Philip Nienhuis, pr.nienhuis -at- users.sf.net
 ## 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -133,6 +133,129 @@ function [ trow, lrow, lcol, rcol ] = getusedrange_otk (ods, ii)
 	endif
 	
 endfunction
+
+## Copyright (C) 2010 Philip Nienhuis <prnienhuis -aT- users.sf.net>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## getusedrange_jod
+
+## Author: Philip <Philip@DESKPRN>
+## Created: 2010-05-25
+
+function [ trow, brow, lcol, rcol ] = getusedrange_jod (ods, wsh)
+
+	# This function works by virtue of sheets in JOD actually being a Java string.
+	# It works outside of the Java memory/heap space which is an added benefit.
+
+	sh = char (ods.workbook.getSheet (wsh - 1));
+
+	# Get table-row pointers
+	id_trow = strfind (sh, '<table:table-row');
+	id = strfind (sh, '</table:table>') - 1;
+	id_trow = [id_trow id];
+
+#	brow = length (id_trow) - 1;
+	trow = rcol = 0;
+	lcol = 1024;
+	if (~isempty (id))
+		# Loop over all table-rows
+		rowrepcnt = 0;
+		for irow = 1:length (id_trow)-1
+			# Isolate single table-row
+			tablerow = sh(id_trow(irow):id_trow(irow+1)-1);
+			# Search table-cells. table-c covers both table-cell & table-covered-cell
+			id_tcell = strfind (tablerow, '<table:table-c'); 
+			id_tcell = [id_tcell id];
+			rowl = length (tablerow);
+			if (isempty (id_tcell(1:end-1)))
+				rowend = rowl;
+			else
+				rowend = id_tcell(1);
+			endif
+			# Add in table-number-rows-repeated attribute values
+			rowrept = strfind (tablerow(1:rowend), 'number-rows-repeated');
+			if (~isempty (rowrept))
+				[st, en] = regexp (tablerow(rowrept:min (rowend, rowrept+30)), '\d+');
+				rowrepcnt += str2num (rowrept+st-1:min (rowend, rowrept+en-1)) - 1;
+			endif
+
+			# Search table-cells. table-c covers both table-cell & table-covered-cell
+			id_tcell = strfind (tablerow, '<table:table-c'); 
+			id_tcell = [id_tcell rowl];
+			if (~isempty (id_tcell(1:end-1)))
+				# Search for non-empty cells (i.e., with an office:value-type)
+				id_valtcell = strfind (tablerow, 'office:value-type=');
+				id_valtcell = [id_valtcell rowl];
+				if (~isempty (id_valtcell(1:end-1)))
+					brow = irow + rowrepcnt;
+					# First set trow if it hadn't already been found
+					if (~trow) trow = irow; endif
+					# Search for repeated table-cells
+					id_reptcell = strfind (tablerow, 'number-columns-repeated');
+					id_reptcell = [id_reptcell rowl];
+					# Search for leftmost non-empty table-cell. llcol = counter for this table-row
+					llcol = 1;
+					while (id_tcell (llcol) < id_valtcell(1) && llcol <= length (id_tcell) - 1)
+						++llcol;
+					endwhile
+					--llcol;
+					# Compensate for repeated cells. First count all repeats left of llcol
+					ii = 1;
+					repcnt = 0;
+					if (~isempty (id_reptcell(1:end-1)))
+						# First try lcol
+						while (ii <= length (id_reptcell) - 1 && id_reptcell(ii) < id_valtcell(1))
+							# Add all repeat counts left of leftmost data tcell minus 1 for each
+							[st, en] = regexp (tablerow(id_reptcell(ii):id_reptcell(ii)+30), '\d+');
+							repcnt += str2num (tablerow(id_reptcell(ii)+st-1:id_reptcell(ii)+en-1)) - 1;
+							++ii;
+						endwhile
+						# Next, add current repcnt value to llcol and update lcol
+						lcol = min (lcol, llcol + repcnt);
+						# Get last value table-cell in table-cell idx
+						jj = 1;
+						while (id_tcell (jj) < id_valtcell(length (id_valtcell)-1))
+							++jj;
+						endwhile
+
+						# Get rest of column repeat counts in value table-cell range
+						while (ii < length (id_reptcell) && id_reptcell(ii) < id_tcell(jj))
+							# Add all repeat counts minus 1 for each tcell in value tcell range
+							[st, en] = regexp (tablerow(id_reptcell(ii):id_reptcell(ii)+30), '\d+');
+							repcnt += str2num (tablerow(id_reptcell(ii)+st-1:id_reptcell(ii)+en-1)) - 1;
+							++ii;
+						endwhile
+					endif
+					# Count all table-cells in value table-cell-range
+					ii = 1; 			# Indexes cannot be negative
+					while (ii < length (id_tcell) && id_tcell(ii) < id_valtcell(length (id_valtcell) - 1))
+						++ii;
+					endwhile
+					--ii;
+					rcol = max (rcol, ii + repcnt);
+				endif
+			endif
+		endfor
+	else
+		# No data found, empty sheet
+		lcol = 0;
+	endif
+
+endfunction
+
 
 ## Copyright (C) 2010 Philip Nienhuis, prnienhuis at users.sf.net
 ## 

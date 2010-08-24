e3885aeffa5fb1abd3db83cd776eb5d56e09baba
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2010-08-24 12:19:37 +0000

    add paired t-test for data with missing values
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7566 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/inst/ttest.m
+++ extra/NaN/inst/ttest.m
@@ -1,45 +1,44 @@
-function [h, pval, ci, stats, df] = ttest2 (x, y, alpha, tail, vartype, DIM)
-% TTEST2 (unpaired) t-test
-%     For two samples x and y from normal distributions with unknown
-%     means and unknown equal variances, perform a two-sample t-test of
-%     the null hypothesis of equal means.  Under the null, the test
-%     statistic T follows a Student distribution with DF degrees of
-%     freedom.
+function [h, pval, ci, stats] = ttest (x, m, alpha, tail, vartype, DIM)
+% TTEST (paired) t-test
+%     For a sample X from a normal distribution with unknown mean and
+%     variance, perform a t-test of the null hypothesis `mean (X) == M'.
+%     Under the null, the test statistic T follows a Student
+%     distribution with `DF = length (X) - 1' degrees of freedom.
 %
-%     TTEST2 treads NaNs as "Missing values" and ignores these. 
+%     TTEST treads NaNs as "Missing values" and ignores these. 
 %
-% H = ttest2(x,y)
-% H = ttest2(x,y,alpha)
-% H = ttest2(x,y,alpha,tail)
-% H = ttest2(x,y,alpha,tail,vartype)
-% H = ttest2(x,y,alpha,tail,vartype,DIM)
-% [H,PVAL] = ttest2(...)
-% [h,p,ci,stats] = ttest2(...)
+% H = ttest(x,m)
+%	tests Null-hypothesis that mean of x is m. 		
+% H = ttest(x,y)
+% 	size of x and size of y must match, it is tested whether the 
+%	difference x-y is significantly different to m=0; 
+% H = ttest(x,y,alpha)
+% H = ttest(x,y,alpha,tail)
+% H = ttest(x,y,alpha,tail,DIM)
+% [H,PVAL] = ttest(...)
 %
 %     H=1 indicates a rejection of the Null-hypothesis at a significance 
 %     level of alpha (default alpha = 0.05).	 
 % 
-%     With the optional argument string TAIL, the Alternative of interest
+%     With the optional argument string TAIL, the alternative of interest
 %     can be selected.  If TAIL is '!=' or '<>' or 'both', the null is tested
 %     against the two-sided Alternative `mean (X) ~= mean (Y)'.  If TAIL
 %     is '>' or 'right', the one-sided Alternative `mean (X) > mean (Y)' is used.
 %     Similarly for '<' or 'left', the one-sided Alternative `mean (X) < mean
 %     (Y)' is used.  The default is the two-sided case.
 % 
-%     vartype support only 'equal' (default value); the value 'unequal' is not supported. 
-%
 %     H returns whether the Null-Hypotheses must be rejected. 
 %     The p-value of the test is returned in PVAL. 
 % 
-%     TTEST2 works on the first non-singleton dimension or on DIM. 
+%     TTEST works on the first non-singleton dimension or on DIM. 
 %
 %     If no output argument is given, the p-value of the test is
 %     displayed.
 %
 
 %%% not supported yet 
-% [h,p,ci] = ttest2(...)
-% [h,p,ci,stats] = ttest2(...)
+% [h,p,ci] = ttest(...)
+% [h,p,ci,stats] = ttest(...)
 
 %       $Id$
 %       Copyright (C) 1995, 1996, 1997, 1998, 2000, 2002, 2005, 2006, 2007
@@ -61,10 +60,13 @@ function [h, pval, ci, stats, df] = ttest2 (x, y, alpha, tail, vartype, DIM)
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  if ((nargin < 2) || (nargin > 6) || nargout > 4)
+  if ((nargin < 2) || (nargin > 5) || nargout > 4)
         print_usage ;
   end
 
+  if (nargin == 2)
+    alt  = '~=';
+  end
   if (nargin < 3) || isempty(alpha)
     alpha = .05;
   end
@@ -73,7 +75,7 @@ function [h, pval, ci, stats, df] = ttest2 (x, y, alpha, tail, vartype, DIM)
     tail = '~=';
   end
   if (~ ischar (tail))
-    error ('ttest2: tail must be a string');
+    error ('ttest: tail must be a string');
   end
   if (nargin < 5) || isempty(vartype)
     vartype = 'equal';
@@ -86,29 +88,25 @@ function [h, pval, ci, stats, df] = ttest2 (x, y, alpha, tail, vartype, DIM)
   end;
   if isempty(DIM), DIM=1; end;
 
+
   szx = size(x); 
-  szy = size(y);	
-  szy(DIM) = 1;	  
-  szx(DIM) = 1;
+  szm = size(m);	
+  szx(DIM) = 1;	  
+  szm(DIM) = 1;
+  if size(m,DIM)==1
+  	;
+  elseif size(x,DIM) == size(m,DIM)
+  	x = x-m;
+  	m = zeros(szm);
+  else
+    error ('ttest: dimension of X and Y do not fit');
+  end 	  
   
-  if (any(szx-szy))
-    error ('ttest2: dimension of X and Y do not fit');
-  end
-
-  [SX, NX] = sumskipnan(x, DIM);
-  [SY, NY] = sumskipnan(y, DIM);
-  stats.df = NX + NY - 2;
-  MX = SX ./ NX;
-  MY = SY ./ NY;
-
-  if any(size(x)==0) || any(size(y)==0)
-  	v = NaN;
-  else	
-  	v = sumsq(x-repmat(MX,size(x)./size(MX))) + sumsq(y-repmat(MY,size(y)./size(MY)));
-  end; 	
-  stats.sd    = sqrt(v/stats.df);
-  stats.tstat = (MX - MY) .* sqrt ((NX .* NY .* stats.df) ./ (v .* (NX + NY)));
-  cdf         = tcdf (stats.tstat, stats.df);
+  [S, N] = sumskipnan(x, DIM);
+  stats.df = N - 1;
+  stats.sd = std (x);
+  stats.tstat = sqrt (N) .* (S./N - m) ./ stats.sd;
+  cdf = tcdf (stats.tstat, stats.df);
 
   if (strcmp (tail, '~=') || strcmp (tail, '!=') || strcmp (tail, '<>')) || strcmp(tail,'both'),
     pval = 2 * min (cdf, 1 - cdf);
@@ -117,11 +115,10 @@ function [h, pval, ci, stats, df] = ttest2 (x, y, alpha, tail, vartype, DIM)
   elseif strcmp (tail, '<') || strcmp(tail,'left'),
     pval = cdf;
   else
-    error ('ttest2: option %s not recognized', tail);
+    error ('ttest: option %s not recognized', tail);
   end
 
   h = pval < alpha;	
-
   if (nargout == 0)
     fprintf(1,'  pval: %g\n', pval);
   end

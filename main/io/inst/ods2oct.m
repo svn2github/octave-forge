bca1d40d6e444b83469058639df4785a83c1a9f5
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2010-08-27 20:35:51 +0000

    Improved hep text; added support for odfdom-0.8.6; more checking on input args
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@7592 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/ods2oct.m
+++ main/io/inst/ods2oct.m
@@ -23,9 +23,6 @@
 ## Read data contained within range @var{range} from worksheet @var{wsh}
 ## in an OpenOffice.org spreadsheet file pointed to in struct @var{ods}.
 ##
-## ods2oct is a mere wrapper for interface-dependent scripts (e.g.,
-## ods2jotk2oct and ods2jod2oct) that do the actual reading.
-##
 ## @var{wsh} is either numerical or text, in the latter case it is 
 ## case-sensitive and it may be max. 31 characters long.
 ## Note that in case of a numerical @var{wsh} this number refers to the
@@ -78,6 +75,9 @@
 ## to the leftmost or upper OpenOffice.org cell will be treated as if the
 ## "corresponding" cells are empty.
 ##
+## ods2oct is a mere wrapper for interface-dependent scripts (e.g.,
+## ods2jotk2oct and ods2jod2oct) that do the actual reading.
+##
 ## Examples:
 ##
 ## @example
@@ -91,7 +91,7 @@
 ##   [An, ods2, status] = ods2oct (ods2, 'Third_sheet');
 ## @end example
 ##
-## @seealso odsopen, odsclose, parsecell, odsread, odsfinfo
+## @seealso odsopen, odsclose, parsecell, odsread, odsfinfo, oct2ods, odswrite
 ##
 ## @end deftypefn
 
@@ -107,22 +107,40 @@
 ## 2010-05-31 Updated help text (delay i.c.o. empty range due to getusedrange call)
 ## 2010-08-03 Added support for reading back formulas (works only in OTK)
 ## 2010-08-12 Added explicit support for jOpenDocument v 1.2b3+
+## 2010-08-25 Improved helptext (moved some text around)
+## 2010-08-27 Added ods3jotk2oct-internal function for odfdom-0.8.6.jar
+##      "     Extended check on spsh_opts (must be a struct) 
 ##
-## (Latest update of subfunctions below: 2010-08-12)
+## (Latest update of subfunctions below: 2010-08-27)
 
 function [ rawarr, ods, rstatus ] = ods2oct (ods, wsh=1, datrange=[], spsh_opts=[])
 
-	persistent odf08;
+	# Check ods file ptr
+	odschk = 1;
+	odschk = odschk && isstruct(ods);
+	odschk = odschk && (~isempty (ods.filename));
+	odschk = odschk	&& (ods.xtype == 'OTK' || ods.xtype == 'JOD');
+	if (~odschk) error ("Arg # 2 is an invalid ods file ptr\n"); endif
 
 	if isempty (spsh_opts)
 		spsh_opts.formulas_as_text = 0;
 		# Other options here
+		#
+	elseif (~isstruct (spsh_opts))
+		error ("struct expected for OPTIONS argument (# 4)");
 	endif
 	
 	if (strcmp (ods.xtype, 'OTK'))
 		# Read ods file tru Java & ODF toolkit
-		[rawarr, ods, rstatus] = ods2jotk2oct (ods, wsh, datrange, spsh_opts);
-		
+		switch ods.odfvsn
+			case '0.7.5'
+				[rawarr, ods, rstatus] = ods2jotk2oct (ods, wsh, datrange, spsh_opts);
+			case '0.8.6'
+				[rawarr, ods, rstatus] = ods3jotk2oct (ods, wsh, datrange, spsh_opts);
+			otherwise
+				error ("Unsupported odfdom version or invalid ods file pointer.");
+		endswitch
+
 	elseif (strcmp (ods.xtype, 'JOD'))
 		[rawarr, ods, rstatus] = ods2jod2oct  (ods, wsh, datrange);
 		
@@ -371,6 +389,183 @@ endfunction
 
 #===========================================================================
 
+## Copyright (C) 2010 Philip Nienhuis <prnienhuis@users.sf.net>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## ods3jotk2oct: internal function for reading odf files using odfdom-0.8.6
+
+## Author: Philip Nienhuis <Philip@DESKPRN>
+## Created: 2010-08-24. First workable version Aug 27, 2010
+## Updates:
+##
+
+function [ rawarr, ods, rstatus ] = ods3jotk2oct (ods, wsh, crange, spsh_opts)
+
+	rstatus = 0;
+
+	# Get contents and table stuff from the workbook
+	odfcont = ods.workbook;		# Use a local copy just to be sure. octave 
+								# makes physical copies only when needed (?)
+	
+	# Parse sheets ("tables") from ODS file
+	sheets = ods.app.getTableList();
+	nr_of_sheets = sheets.size ();
+
+	# Check user input & find sheet pointer (1-based)
+	if (~isnumeric (wsh))
+		try
+			sh = ods.app.getTableByName (wsh);
+			sh_err = isempty (sh);
+		catch
+			sh_err = 1;
+		end_try_catch
+		if (sh_err)
+			error (sprintf ("Sheet %s not found in file %s\n", wsh, ods.filename)); 
+		endif
+	elseif (wsh > nr_of_sheets || wsh < 1)
+		# We already have a numeric sheet pointer. If it's not in range:
+		error (sprintf ("Worksheet no. %d out of range (1 - %d)", wsh, nr_of_sheets));
+	else
+		sh = sheets.get (wsh - 1);
+	endif
+
+	# Either parse (given cell range) or prepare (unknown range) help variables 
+	if (isempty (crange))
+		if ~isnumeric (wsh)
+			# Get sheet index
+			jj = nr_of_sheets;
+			while jj-- >= 0
+				if (strcmp (wsh, sheets.get(jj).getTableName()) == 1)
+					wsh = jj +1;
+					jj = -1;
+				endif
+			endwhile
+		endif
+		[ trow, brow, lcol, rcol ] = getusedrange (ods, wsh);
+		nrows = brow - trow + 1;	# Number of rows to be read
+		ncols = rcol - lcol + 1;	# Number of columns to be read
+	else
+		[dummy, nrows, ncols, trow, lcol] = parse_sp_range (crange);
+		# Check ODS row/column limits
+		if (lcol > 1024 || trow > 65536) 
+			error ("ods2oct: invalid range; max 1024 columns & 65536 rows."); 
+		endif
+		# Truncate range silently if needed
+		rcol = min (lcol + ncols - 1, 1024);
+		ncols = min (ncols, 1024 - lcol + 1);
+		nrows = min (nrows, 65536 - trow + 1);
+	endif
+
+	# Create storage for data content
+	rawarr = cell (nrows, ncols);
+
+	# Read from worksheet row by row. Row numbers are 0-based
+	for ii=trow:nrows+trow-1
+		row = sh.getRowByIndex (ii-1);
+		for jj=lcol:ncols+lcol-1;
+			ocell = row.getCellByIndex (jj-1);
+			if ~isempty (ocell)
+				otype = ocell.getValueType ();
+				if (spsh_opts.formulas_as_text)
+					if ~isempty (ocell.getFormula ())
+						otype = 'formula';
+					endif
+				endif
+				switch deblank (tolower (otype))
+					case  {'float', 'currency', 'percentage'}
+						rawarr(ii-trow+1, jj-lcol+1) = ocell.getDoubleValue ();
+					case 'date'
+						# Dive into TableTable API
+						tvalue = ocell.getOdfElement ().getOfficeDateValueAttribute ();
+						# Dates are returned as octave datenums, i.e. 0-0-0000 based
+						yr = str2num (tvalue(1:4));
+						mo = str2num (tvalue(6:7));
+						dy = str2num (tvalue(9:10));
+						if (index (tvalue, 'T'))
+							hh = str2num (tvalue(12:13));
+							mm = str2num (tvalue(15:16));
+							ss = str2num (tvalue(18:19));
+							rawarr(ii-trow+1, jj-lcol+1) = datenum (yr, mo, dy, hh, mm, ss);
+						else
+							rawarr(ii-trow+1, jj-lcol+1) = datenum (yr, mo, dy);
+						endif
+					case 'time'
+						# Dive into TableTable API
+						tvalue = ocell.getOdfElement ().getOfficeTimeValueAttribute ();
+						if (index (tvalue, 'PT'))
+							hh = str2num (tvalue(3:4));
+							mm = str2num (tvalue(6:7));
+							ss = str2num (tvalue(9:10));
+							rawarr(ii-trow+1, jj-lcol+1) = datenum (0, 0, 0, hh, mm, ss);
+						endif
+					case 'boolean'
+						rawarr(ii-trow+1, jj-lcol+1) = ocell.getBooleanValue ();
+					case 'string'
+						rawarr(ii-trow+1, jj-lcol+1) = ocell.getStringValue ();
+#						# Code left in for in case odfdom 0.8.6 has similar bug
+#						# as 0.7.5
+#						cvalue = tcell.getOfficeStringValueAttribute ();
+#						if (isempty (cvalue))     # Happens with e.g., hyperlinks
+#							tmp = char (tcell);
+#							# Hack string value from between <text:p|r> </text:p|r> tags
+#							ist = findstr (tmp, '<text:');
+#							if (ist)
+#								ist = ist (length (ist));
+#								ist = ist + 8;
+#								ien = index (tmp(ist:end), '</text') + ist - 2;
+#								tmp (ist:ien);
+#								cvalue = tmp(ist:ien);
+#							endif
+#						endif
+#						rawarr(ii-trow+1, jj-lcol+1)= cvalue;
+					case 'formula'
+						rawarr(ii-trow+1, jj-lcol+1) = ocell.getFormula ();
+					otherwise
+						# Nothing
+				endswitch
+			endif
+		endfor
+	endfor
+
+	# Crop rawarr from all empty outer rows & columns just like Excel does
+	# & keep track of limits
+	emptr = cellfun ('isempty', rawarr);
+	if (all (all (emptr)))
+		rawarr = {};
+		ods.limits= [];
+	else
+		irowt = 1;
+		while (all (emptr(irowt, :))), irowt++; endwhile
+		irowb = nrows;
+		while (all (emptr(irowb, :))), irowb--; endwhile
+		icoll = 1;
+		while (all (emptr(:, icoll))), icoll++; endwhile
+		icolr = ncols;
+		while (all (emptr(:, icolr))), icolr--; endwhile
+		# Crop textarray
+		rawarr = rawarr(irowt:irowb, icoll:icolr);
+		rstatus = 1;
+		ods.limits = [lcol+icoll-1, lcol+icolr-1; trow+irowt-1, trow+irowb-1];
+	endif
+
+endfunction
+
+
+#===========================================================================
+
 ## Copyright (C) 2009 Philip Nienhuis <pr.nienhuis at users.sf.net>
 ## 
 ## This program is free software; you can redistribute it and/or modify
@@ -500,6 +695,7 @@ function [ rawarr, ods, rstatus] = ods2jod2oct (ods, wsh, crange)
 				endif
 			endfor
 		endfor
+
 	else
 		# Version 1.2b3+
 		for ii=1:nrows

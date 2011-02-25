1ab14176c66e4f393c437ee69e424b5d313d31ba
i7tiol <i7tiol@fc592f45-0881-4044-91ff-c96495a54512>
2011-02-25 08:44:30 +0000

    Update copyright notices of functions for 'subpackage' nonlin_residmin.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8137 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/inst/__collect_constraints__.m
+++ main/optim/inst/__collect_constraints__.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2010 Olaf Till <olaf.till@uni-jena.de>
+## Copyright (C) 2010, 2011 Olaf Till <olaf.till@uni-jena.de>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
--- main/optim/inst/cpiv_bard.m
+++ main/optim/inst/cpiv_bard.m
@@ -1,4 +1,4 @@
-%% Copyright (C) 2010 Olaf Till
+%% Copyright (C) 2010, 2011 Olaf Till
 %%
 %% This program is free software; you can redistribute it and/or modify
 %% it under the terms of the GNU General Public License as published by

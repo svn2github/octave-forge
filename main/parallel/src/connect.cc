b0ecd746b933883f178937b9e3ede9ff5f5153b3
i7tiol <i7tiol@fc592f45-0881-4044-91ff-c96495a54512>
2011-02-25 08:58:49 +0000

    Update copyright notices.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8138 fc592f45-0881-4044-91ff-c96495a54512

--- main/parallel/src/connect.cc
+++ main/parallel/src/connect.cc
@@ -1,6 +1,7 @@
 /*
 
 Copyright (C) 2002 Hayato Fujiwara
+Copyright (C) 2010, 2011 Olaf Till <olaf.till@uni-jena.de>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by

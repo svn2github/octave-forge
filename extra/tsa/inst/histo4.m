ea1149e3f96859dd53bcf9b0eea57460f74da8f3
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2011-07-06 18:00:32 +0000

    make code cleaner and leaner
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8379 fc592f45-0881-4044-91ff-c96495a54512

--- extra/tsa/inst/histo4.m
+++ extra/tsa/inst/histo4.m
@@ -1,4 +1,3 @@
-
 function [R,tix]=histo4(Y)
 % HISTO4 calculates histogram for rows and supports data compression
 %
@@ -6,7 +5,7 @@ function [R,tix]=histo4(Y)
 % 	R is a struct with th fields 
 %       R.X  are the bin-values 
 %       R.H  is the frequency of occurence of value X 
-%  	R.N  are the number of samples 
+%  	R.N  are the total number of samples 
 %
 % HISTO4 might be useful for data compression, because
 % [R,tix] = histo4(Y) 
@@ -42,33 +41,16 @@ function [R,tix]=histo4(Y)
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-if 0, exist('histo_mex','file')
-	% TODO: Performance tests showing an advantage and correctness  
-	[R,tix] = histo_mex(Y,'rows');
-	return; 
-end; 	
-
 [yr, yc] = size(Y);
-if yr==1,
-        % Makes sure there is a second row
-        % Sort does not support the DIM-argument, therefore,
-        % this function would not work correctly with this software
-        % Once this is fixed, this part can be removed. 
-        Y = [Y; NaN+ones(size(Y))];  
-end;
 
 % identify all possible X's and overall Histogram
-[Y,   idx] = sortrows(Y);
-[tmp, idx] = sort(idx);        % inverse index
+[Y, idx] = sortrows(Y);
 
-%[ix, iy] = (diff(Y,1)>0);
-ix = logical(zeros(yr-1,1));
-
-d = diff(Y,[],1);
-ix = any((~isnan(d) & (d~=0)) | diff(isnan(Y),[],1),2);
+d  = diff(Y,[],1);
+ix = any( (~isnan(d) & (d~=0) ) | diff(isnan(Y),[],1), 2);
 
 tmp = [find(ix); yr];
-R.H = diff([0; tmp]);
+R.H = [tmp(1); diff(tmp)];
 R.X = Y(tmp,:);
 R.N = yr;
 R.datatype = 'HISTOGRAM';
@@ -77,19 +59,18 @@ R.datatype = 'HISTOGRAM';
 if nargout>1,
         tix = cumsum([1;ix]);	% rank 
         cc  = 1;
-        tmp = sum(ix)+1;
-	if 0, exist('OCTAVE_VERSION','builtin'),
-		; % NOP; no support for integer datatyp 
-        elseif tmp <= 2^8;
+        tmp = sum(ix);
+	if tmp < 2^8;
                 tix = uint8(tix);
                 cc = 8/1;
-        elseif tmp <= 2^16;
+        elseif tmp < 2^16;
                 tix = uint16(tix);
                 cc = 8/2;
-        elseif tmp <= 2^32;
+        elseif tmp < 2^32;
                 tix = uint32(tix);
                 cc = 8/4;
         end;
+	[tmp, idx] = sort(idx);        % inverse index
         tix = tix(idx);		% inverse sort rank
         
         R.compressionratio = (prod(size(R.X)) + yr/cc) / (yr*yc);

cb62a50ec6db45ec11f019ee2d1f80170ee35f98
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-09-14 10:58:31 +0000

    corr2cov: added new function to financial package by Hong Yu
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8531 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/financial/inst/corr2cov.m
@@ -0,0 +1,48 @@
+## Copyright (C) 2011 Hong Yu <hyu0401@hotmail.com>
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} @var{cov} = corr2cov (@var{sigma}, @var{corr})
+## Convert standard deviation @var{sigma} and correlation coefficients @var{corr}
+## to covariance @var{cov}.
+##
+## Note that the rate @var{r} is specified as a fraction (i.e., 0.05,
+## not 5 percent).
+## @seealso{corrcoef, cov, cov2corr, std}
+## @end deftypefn
+
+function ret = corr2cov (sigma, corr)
+
+  if ( nargin != 2 )
+    print_usage ();
+  endif
+
+  ssize = size(sigma);
+  csize = size(corr);
+
+  if ( csize(1) != csize(2) )
+    error("correlation coefficients must be a NxN matrix");
+  elseif ( ssize(1) != 1 )
+    error("sigma must be a 1xN vector (single row) with the standard deviation values");
+  elseif ( ssize(2) < csize(1) )
+    error("sigma: must be 1xN \ncorr: must be nxn"); 
+  endif
+
+  sigma = sigma(:);
+  ret   = corr .* (sigma * sigma');
+
+endfunction
+

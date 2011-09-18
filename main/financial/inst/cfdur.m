84cf9f292bee56f1044ba6eeb436c03430653e82
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-09-18 19:21:21 +0000

    cfdur: new function to financial package submitted by Hong Yu <hyu0401@hotmail.com>. Closes artifact #3410737
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8558 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/financial/inst/cfdur.m
@@ -0,0 +1,50 @@
+## Copyright (C) 2011 Hong Yu <hyu0401@hotmail.com>
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} [@var{dur}, @var{mod_dur}] = cfdur (@var{cf}, @var{yield})
+## Calculate duration @var{dur} and modified duration @var{mod_dur}, from given
+## fixed-paid cash flow @var{cf} and period yield @var{yield}.
+##
+## Reference:
+## http://en.wikipedia.org/wiki/Bond_duration
+## Using periodic compounding instead of continuous compounding.
+##
+## @seealso{cfconv}
+## @end deftypefn
+
+function [dur, modDur] = cfdur (cf, yield)
+
+  if ( nargin != 2 )
+    print_usage ();
+  endif
+
+  if ( ! isscalar(yield) )
+    error("input yield must be a scalar");
+  endif
+
+  if ( rows(1) != 1 )
+    error("input cash flow must be a 1xN matrix");
+  endif
+
+  v_idx   = 1:columns(cf);
+  t1      = (1+yield) .^ (-v_idx);
+  t2      = v_idx .* t1;
+
+  dur     = (cf*t2') / (cf*t1');
+  modDur  = dur / (1+yield);
+
+endfunction

5d4eb4ff92c32cf51a22c84078d70d6c39016fb2
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-09-29 10:06:30 +0000

    mechanics. Adding subfolder structure and pre_install.m function
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8628 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/mechanics/pre_install.m
@@ -0,0 +1,21 @@
+function pre_install (desc)
+%% Prepares for installation a package that is organized in subfolders 
+
+  %% List of subfolders
+  subfld = {"molecularDynamics"};
+
+  %% Create correct strings
+  subfld_ready = strcat({[pwd() filesep() "inst" filesep()]},
+                         subfld,[filesep() "*"]);
+
+  %% Destination folder
+  to_fld = strcat(pwd, filesep ());
+
+
+  %% Copy files to package/ folder
+  for from_fld = subfld_ready
+    system (["cp -Rf " from_fld{1} " " to_fld]);
+    system (["rm -R " from_fld{1}(1:end-2)]);
+  end
+
+end

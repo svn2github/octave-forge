9ac20c55bb976d3d0edb67d14901fc11c3f89959
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-02 21:23:49 +0000

    data-smoothing: e-mail address to copyright notices
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8656 fc592f45-0881-4044-91ff-c96495a54512

--- main/data-smoothing/inst/regdatasmooth.m
+++ main/data-smoothing/inst/regdatasmooth.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2008 Jonathan Stickel
+## Copyright (C) 2008 Jonathan Stickel <jonathan.stickel@nrel.gov>
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or

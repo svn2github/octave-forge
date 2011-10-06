e70cd61730cfaeba4ccac1940dafda431e4147ce
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-06 23:56:35 +0000

    geometry. modifying mathod loadpath
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8702 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/devel/@svg/loadpaths.m
+++ main/geometry/devel/@svg/loadpaths.m
@@ -13,7 +13,7 @@
 %%    You should have received a copy of the GNU General Public License
 %%    along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-function Paths = loadpaths (svg, varargin)
+function Paths = loadpaths (obj, svg, varargin)
 
   %% Call python script
   if exist (svg,'file')

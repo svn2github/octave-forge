d9c59014e1681c35255d204cbdb480de36b8e842
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-06 23:54:11 +0000

    geometry. Bug in loadpaths
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8701 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/devel/@svg/loadpaths.m
+++ main/geometry/devel/@svg/loadpaths.m
@@ -16,7 +16,7 @@
 function Paths = loadpaths (svg, varargin)
 
   %% Call python script
-  if exist (name,'file')
+  if exist (svg,'file')
   % read from file  
     [st str]=system (sprintf ('python parsePath.py %s', svg));
     

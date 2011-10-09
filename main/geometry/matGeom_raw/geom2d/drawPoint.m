742298f69f4dbb1668ffa3db4acec74f5713b987
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-09 18:09:08 +0000

    geometry. Adding points2d
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8719 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/matGeom_raw/geom2d/drawPoint.m
+++ main/geometry/matGeom_raw/geom2d/drawPoint.m
@@ -31,7 +31,6 @@
 %% those of the authors and should not be interpreted as representing official
 %% policies, either expressed or implied, of copyright holder.
 
-
 function varargout = drawPoint(varargin)
 %DRAWPOINT Draw the point on the axis.
 %

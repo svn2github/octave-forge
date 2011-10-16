e7d680291b5aba3683a2402991cabd5a0822ddf8
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-16 20:55:49 +0000

    geometry. Adding rest of create* functions 2 to go
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8757 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/geom2d/inst/createDirectedCircle.m
+++ main/geometry/geom2d/inst/createDirectedCircle.m
@@ -1,5 +1,5 @@
 %% Copyright (c) 2011, INRA
-%% 2007-2011, David Legland <david.legland@grignon.inra.fr>
+%% 2005-2011, David Legland <david.legland@grignon.inra.fr>
 %% 2011 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 %%
 %% All rights reserved.
@@ -31,62 +31,62 @@
 %% those of the authors and should not be interpreted as representing official
 %% policies, either expressed or implied, of copyright holder.
 
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{circle} = } createDirectedCircle (@var{p1}, @var{p2}, @var{p3})
+%% @deftypefnx {Function File} {@var{circle} = } createDirectedCircle (@var{p1}, @var{p2})
+%% Create a circle from 2 or 3 points.
+%%
+%%   Creates the circle passing through the 3 given points. 
+%%   C is a 1x4 array of the form: [XC YX R INV].
+%%
+%% When two points are given, creates the circle whith center @var{p1} and passing
+%% throuh the point @var{p2}.
+%%
+%%   Works also when input are point arrays the same size, in this case the
+%%   result has as many lines as the point arrays.
+%%
+%%   Example
+%% 
+%%
+%%   @seealso{circles2d, createCircle}
+%% @end deftypefn
 
 function circle = createDirectedCircle(varargin)
-%CREATEDIRECTEDCIRCLE Create a directed circle
-%
-%   C = createDirectedCircle(P1, P2, P3);
-%   Creates a circle going through the given points.
-%   C is a 1*4 array of the form: [XC YC R INV].
-%   The last parameter is set to 1 if the points are located in clockwise
-%   order on the circle.
-%
-%   C = createDirectedCircle(P1, P2);
-%   Creates the circle whith center P1 and passing throuh the point P2.
-%
-%   Works also when input are point arrays the same size, in this case the
-%   result has as many lines as the point arrays.
-%
-%   See also:
-%   circles2d, createCircle
-%
-%   ---------
-%   author : David Legland 
-%   INRA - TPV URPOI - BIA IMASTE
-%   created the 12/01/2005.
-%
 
-if nargin == 2
-    % inputs are the center and a point on the circle
-    p1 = varargin{1};
-    p2 = varargin{2};
-    x0 = (p1(:,1) + p2(:,1))/2;
-    y0 = (p1(:,2) + p2(:,2))/2;
-    r = hypot((p2(:,1)-p1(:,1)), (p2(:,2)-p1(:,2)))/2;
-    
-    % circle is direct by default
-    d = 0;
-    
-elseif nargin == 3
-    % inputs are three points on the circle
-    p1 = varargin{1};
-    p2 = varargin{2};
-    p3 = varargin{3};
+  if nargin == 2
+      % inputs are the center and a point on the circle
+      p1 = varargin{1};
+      p2 = varargin{2};
+      x0 = (p1(:,1) + p2(:,1))/2;
+      y0 = (p1(:,2) + p2(:,2))/2;
+      r = hypot((p2(:,1)-p1(:,1)), (p2(:,2)-p1(:,2)))/2;
+      
+      % circle is direct by default
+      d = 0;
+      
+  elseif nargin == 3
+      % inputs are three points on the circle
+      p1 = varargin{1};
+      p2 = varargin{2};
+      p3 = varargin{3};
 
-    % compute circle center
-    line1 = medianLine(p1, p2);
-    line2 = medianLine(p1, p3);
-    center = intersectLines(line1, line2);
-    x0 = center(:, 1); 
-    y0 = center(:, 2);
-    
-    % circle radius
-    r = hypot((p1(:,1)-x0), (p1(:,2)-y0));
-    
-    % compute circle orientation
-    angle = angle3Points(p1, center, p2) + angle3Points(p2, center, p3);
-    d = angle>2*pi;
-end
-    
-        
-circle = [x0 y0 r d];
+      % compute circle center
+      line1 = medianLine(p1, p2);
+      line2 = medianLine(p1, p3);
+      center = intersectLines(line1, line2);
+      x0 = center(:, 1); 
+      y0 = center(:, 2);
+      
+      % circle radius
+      r = hypot((p1(:,1)-x0), (p1(:,2)-y0));
+      
+      % compute circle orientation
+      angle = angle3Points(p1, center, p2) + angle3Points(p2, center, p3);
+      d = angle>2*pi;
+  end
+      
+          
+  circle = [x0 y0 r d];
+
+endfunction
+

09f5ba2d032bc1b0dc133204e114f4876e677ec4
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-18 15:39:48 +0000

    geometry. Cleaning up
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8782 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/geom2d/inst/drawLine.m
+++ main/geometry/geom2d/inst/drawLine.m
@@ -85,3 +85,110 @@ endfunction
 %!   drawLine([30 40 10 20]);
 %!   drawLine([30 40 20 -10], 'color', 'm', 'linewidth', 2);
 
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [30 40 10 0];
+%%!  edge = [0 40 100 40];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [30 40 -10 0];
+%%!  edge = [100 40 0 40];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [30 140 10 0];
+%%!  hl = drawLine(line);
+%%!  assertEqual(-1, hl);
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [30 40 0 10];
+%%!  edge = [30 0 30 100];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [30 40 0 -10];
+%%!  edge = [30 100 30 0];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [140 30 0 10];
+%%!  hl = drawLine(line);
+%%!  assertEqual(-1, hl);
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [80 30 10 10];
+%%!  edge = [50 0 100 50];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [20 70 10 10];
+%%!  edge = [0 50 50 100];
+%%!  hl = drawLine(line);
+%%!  assertElementsAlmostEqual(edge([1 3]), get(hl, 'xdata'));
+%%!  assertElementsAlmostEqual(edge([2 4]), get(hl, 'ydata'));
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [140 -30 10 10];
+%%!  hl = drawLine(line);
+%%!  assertEqual(-1, hl);
+%%!  line = [-40 130 10 10];
+%%!  hl = drawLine(line);
+%%!  assertEqual(-1, hl);
+
+%%!test
+%%!  box = [0 100 0 100];
+%%!  hf = figure();
+%%!  axis(box);
+%%!  line = [...
+%%!      80 30 10 10; ...
+%%!      20 70 10 10; ...
+%%!      140 -30 10 10; ...
+%%!      -40 130 10 10];
+%%!  edge = [...
+%%!      50 0 100 50; ...
+%%!      0 50 50 100];
+%%!  hl = drawLine(line);
+%%!  assertEqual(4, length(hl));
+%%!  assertElementsAlmostEqual(edge(1, [1 3]), get(hl(1), 'xdata'));
+%%!  assertElementsAlmostEqual(edge(1, [2 4]), get(hl(1), 'ydata'));
+%%!  assertElementsAlmostEqual(edge(2, [1 3]), get(hl(2), 'xdata'));
+%%!  assertElementsAlmostEqual(edge(2, [2 4]), get(hl(2), 'ydata'));
+%%!  assertEqual(-1, hl(3));
+%%!  assertEqual(-1, hl(4));

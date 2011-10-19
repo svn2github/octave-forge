458d90e8b44dcf4762e6be58debb78254667fac8
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-19 17:01:28 +0000

    jacobs: upgrade to function submitted by Olaf Till on the mailing list
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8791 fc592f45-0881-4044-91ff-c96495a54512

--- main/optim/inst/jacobs.m
+++ main/optim/inst/jacobs.m
@@ -14,28 +14,39 @@
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {Df =} jacobs (@var{f}, @var{x})
-## @deftypefnx {Function File} {Df =} jacobs (@var{f}, @var{x}, @var{h})
+## @deftypefn {Function File} {Df =} jacobs (@var{x}, @var{f})
+## @deftypefnx {Function File} {Df =} jacobs (@var{x}, @var{f}, @var{hook})
 ## Calculate the jacobian of a function.
 ##
-## Let @var{f} be a user-supplied function. Given a point @var{x} at which
-## we seek for the Jacobian, the function @command{jacobs} returns the Jacobian
-## matrix @code{d(f(1), @dots{}, df(n))/d(x(1), @dots{}, x(n))}. The
-## function uses the complex step method and thus can be applied to real
-## analytic functions. The optional argument @var{h} can be used to define
-## the magnitude of the complex step and defaults to 1e-20.
+## Let @var{f} be a user-supplied function. Given a point @var{x} at
+## which we seek for the Jacobian, the function @command{jacobs} returns
+## the Jacobian matrix @code{d(f(1), @dots{}, df(end))/d(x(1), @dots{},
+## x(n))}. The function uses the complex step method and thus can be
+## applied to real analytic functions.
+##
+## The optional argument @var{hook} is a structure with additional options. @var{hook}
+## can have the following fields:
+## @itemize @bullet
+## @item
+## @code{h} - can be used to define the magnitude of the complex step and defaults
+## to 1e-20; steps larger than 1e-3 are not allowed.
+## @item
+## @code{fixed} - is a logical vector internally usable by some optimization
+## functions; it indicates for which elements of @var{x} no gradient should be
+## computed, but zero should be returned.
+## @end itemize
 ##
 ## For example:
 ## 
 ## @example
 ## @group
 ## f = @@(x) [x(1)^2 + x(2); x(2)*exp(x(1))];
-## Df = jacobs (f, [1,2])
+## Df = jacobs ([1, 2], f)
 ## @end group
 ## @end example
 ## @end deftypefn
 
-function Df = jacobs (f, x, h = 1e-20)
+function Df = jacobs (x, f, hook)
 
   if ( (nargin < 2) || (nargin > 3) )
     print_usage ();
@@ -45,20 +56,61 @@ function Df = jacobs (f, x, h = 1e-20)
     f = str2func (f, "global");
   endif
 
-  if (! (isscalar (h)))
-    error ("jacobs: H must be a scalar");
-  endif
-
-  if (abs (h) >= 1e-2)
-    warning ("jacobs: H is too big and the result should not be trusted");
-  endif
-
   n  = numel (x);
-  Df = zeros (n, n);
+
+  default_h = 1e-20;
+  max_h = 1e-3; # must be positive
+
+  if (nargin > 2)
+
+    if (isfield (hook, "h"))
+      if (! (isscalar (hook.h)))
+        error ("complex step magnitude must be a scalar");
+      endif
+      if (abs (hook.h) > max_h)
+        warning ("complex step magnitude larger than allowed, set to %e", ...
+                 max_h)
+        h = max_h;
+      else
+        h = hook.h;
+      endif
+    else
+      h = default_h;
+    endif
+
+    if (isfield (hook, "fixed"))
+      if (numel (hook.fixed) != n)
+        error ("index of fixed parameters has wrong dimensions");
+      endif
+      fixed = hook.fixed;
+    else
+      fixed = false (n, 1);
+    endif
+
+  else
+
+    h = default_h;
+    fixed = false (n, 1);
+
+  endif
+
+  if (all (fixed))
+    error ("all elements of 'x' are fixed");
+  endif
 
   x = repmat (x(:), 1, n) + h * 1i * eye (n);
 
-  for count = 1:n
+  idx = find (! fixed);
+
+  ## after first evaluation, dimensionness of 'f' is known
+  t_Df = imag (f (x(:, idx(1)))(:));
+  dim = numel (t_Df);
+
+  Df = zeros (dim, n);
+
+  Df(:, idx(1)) = t_Df;
+
+  for count = idx(2:end)
     Df(:, count) = imag (f (x(:, count))(:));
   endfor
 
@@ -66,10 +118,10 @@ function Df = jacobs (f, x, h = 1e-20)
 
 endfunction
 
-%!assert (jacobs (@(x) x, 1), 1)
-%!assert (jacobs (@(x) x^2, 6), 12)
-%!assert (jacobs (@(x) [x(1)^2; x(1)*x(2)], [1; 1]), [2, 0; 1, 1])
-%!assert (jacobs (@(x) [x(1)^2 + x(2); x(2)*exp(x(1))], [1; 2]), [2, 1; 2*exp(1), exp(1)])
+%!assert (jacobs (1, @(x) x), 1)
+%!assert (jacobs (6, @(x) x^2), 12)
+%!assert (jacobs ([1; 1], @(x) [x(1)^2; x(1)*x(2)]), [2, 0; 1, 1])
+%!assert (jacobs ([1; 2], @(x) [x(1)^2 + x(2); x(2)*exp(x(1))]), [2, 1; 2*exp(1), exp(1)])
 
 %% Test input validation
 %!error jacobs ()
@@ -80,10 +132,10 @@ endfunction
 
 %!demo
 %! # Relative error against several h-values
-%! k = 1:20; h = 10 .^ (-k); x = 0.3*pi;
+%! k = 3:20; h = 10 .^ (-k); x = 0.3*pi;
 %! err = zeros (1, numel (k));
-%! for count = k
-%!   err(count) = abs (jacobs (@sin, x, h(count)) - cos (x)) / abs (cos (x)) + eps;
+%! for count = 1 : numel (k)
+%!   err(count) = abs (jacobs (x, @sin,	struct ("h", h(count))) - cos (x)) / abs (cos (x)) + eps;
 %! endfor
 %! loglog (h, err); grid minor;
 %! xlabel ("h"); ylabel ("|Df(x) - cos(x)| / |cos(x)|")

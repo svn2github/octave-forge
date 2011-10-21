841c8f593187216d6f34665e9e77185c0eea0f83
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-21 19:10:17 +0000

    geometry. All intersect* functions
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8825 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/geom2d/inst/squareGrid.m
+++ main/geometry/geom2d/inst/squareGrid.m
@@ -1,5 +1,5 @@
 %% Copyright (c) 2011, INRA
-%% 2007-2011, David Legland <david.legland@grignon.inra.fr>
+%% 2005-2011, David Legland <david.legland@grignon.inra.fr>
 %% 2011 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 %%
 %% All rights reserved.
@@ -31,39 +31,52 @@
 %% those of the authors and should not be interpreted as representing official
 %% policies, either expressed or implied, of copyright holder.
 
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{pts} = } squaregrid (@var{bounds}, @var{origin}, @var{size})
+%% Generate equally spaces points in plane.
+%%
+%%   usage
+%%   PTS = squareGrid(BOUNDS, ORIGIN, SIZE)
+%%   generate points, lying in the window defined by BOUNDS (=[xmin ymin
+%%   xmax ymax]), starting from origin with a constant step equal to size.
+%%   
+%%   Example
+%%   PTS = squareGrid([0 0 10 10], [3 3], [4 2])
+%%   will return points : 
+%%   [3 1;7 1;3 3;7 3;3 5;7 5;3 7;7 7;3 9;7 9];
+%%
+%%   TODO: add possibility to use rotated grid
+%%
+%% @end deftypefn
 
-function varargout = triangleGrid(bounds, origin, size, varargin)
-%TRIANGLEGRID Generate triangular grid of points in the plane.
-%
-%   usage
-%   PTS = triangleGrid(BOUNDS, ORIGIN, SIZE)
-%   generate points, lying in the window defined by BOUNDS, given in form
-%   [xmin ymin xmax ymax], starting from origin with a constant step equal
-%   to size. 
-%   SIZE is constant and is equals to the length of the sides of each
-%   triangles. 
-%
-%   TODO: add possibility to use rotated grid
-%
-%   ---------
-%
-%   author : David Legland 
-%   INRA - TPV URPOI - BIA IMASTE
-%   created the 06/08/2005.
-%
+function varargout = squareGrid(bounds, origin, size)
 
-dx = size(1);
-dy = size(1)*sqrt(3);
+  % find all x coordinate
+  x1 = bounds(1) + mod(origin(1)-bounds(1), size(1));
+  x2 = bounds(3) - mod(bounds(3)-origin(1), size(1));
+  lx = (x1:size(1):x2)';
 
-% consider two square grids with different centers
-pts1 = squareGrid(bounds, origin, [dx dy], varargin{:});
-pts2 = squareGrid(bounds, origin + [dx dy]/2, [dx dy], varargin{:});
+  % find all y coordinate
+  y1 = bounds(2) + mod(origin(2)-bounds(2), size(2));
+  y2 = bounds(4) - mod(bounds(4)-origin(2), size(2));
+  ly = (y1:size(2):y2)';
 
-% gather points
-pts = [pts1;pts2];
+  % number of points in each coord, and total number of points
+  ny = length(ly);
+  nx = length(lx);
+  np = nx*ny;
 
+  % create points
+  pts = zeros(np, 2);
+  for i=1:ny
+      pts( (1:nx)'+(i-1)*nx, 1) = lx;
+      pts( (1:nx)'+(i-1)*nx, 2) = ly(i);
+  end    
 
-% process output
-if nargout>0
-    varargout{1} = pts;
-end
+  % process output
+  if nargout>0
+      varargout{1} = pts;
+  end
+
+endfunction
+

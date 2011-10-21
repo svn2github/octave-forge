73a781c31f9fb58a92b776365c81dfff10920aa8
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-21 18:51:49 +0000

    geometry. All create* functions
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8823 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/geom2d/inst/createVector.m
+++ main/geometry/geom2d/inst/createVector.m
@@ -1,5 +1,5 @@
 %% Copyright (c) 2011, INRA
-%% 2007-2011, David Legland <david.legland@grignon.inra.fr>
+%% 2010-2011, David Legland <david.legland@grignon.inra.fr>
 %% 2011 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 %%
 %% All rights reserved.
@@ -31,62 +31,24 @@
 %% those of the authors and should not be interpreted as representing official
 %% policies, either expressed or implied, of copyright holder.
 
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{vect} = } createVector (@var{p1}, @var{p2})
+%% Create a vector from two points.
+%%
+%%   V12 = createVector(P1, P2)
+%%   Creates the vector V12, defined as the difference between coordinates
+%%   of points P1 and P2.
+%%   P1 and P2 are row vectors with ND elements, ND being the space
+%%   dimension.
+%%
+%%   If one of the inputs is a N-by-Nd array, the other input is
+%%   automatically repeated, and the result is N-by-Nd.
+%%
+%%   If both inputs have the same size, the result also have the same size.
+%%
+%%   @seealso{vectors2d, vectors3d, points2d}
+%% @end deftypefn
+function vect = createVector(p1, p2)
+  vect = bsxfun(@minus, p2, p1);
+endfunction
 
-function line = createMedian(varargin)
-%CREATEMEDIAN create a median line
-%
-%   DEPRECATED: use medianLine instead
-%
-%   l = CREATEMEDIAN(P1, P2) create the median line of points P1 and P2, 
-%   that is the line containing all points located at equal distance of 
-%   P1 and P2.
-%
-%   l = CREATEMEDIAN(points) create the median line of 2 points, given as
-%   a 2*2 array. array has the form :
-%   [ [ x1 y1 ] ; [ x2 y2 ] ]
-%
-%   l = CREATEMEDIAN(edge) create the median of the edge. Edge is a 1*4
-%   array, containing [x0 y0] coordiantes of first point, and [dx dy],
-%   the vector from first point to second point.
-% 
-%   
-%
-%   ---------
-%
-%   author : David Legland 
-%   INRA - TPV URPOI - BIA IMASTE
-%   created the 31/10/2003.
-%
-
-% deprecation warning
-warning('geom2d:deprecated', ...
-    '''createMedian'' is deprecated, use ''medianLine'' instead');
-
-
-nargs = length(varargin);
-x0=0;
-y0=0;
-dx=0;
-dy=0;
-
-if nargs == 1
-    tab = varargin{1};
-    if size(tab, 2)==2
-        % array of two points
-        x0 = tab(1,1); y0 = tab(1,2);
-        dx = tab(2,1)-x0; dy = tab(2,2)-y0;
-    else
-        % edge equation
-        x0 = tab(1); y0 = tab(2);
-        dx = tab(3); dy = tab(4);
-    end
-elseif nargs==2
-    % two points
-    p1 = varargin{1};
-    p2 = varargin{2};
-    x0 = p1(1); y0 = p1(2);
-    dx = p2(1)-x0; dy = p2(2)-y0;
-end
-    
-        
-line = [x0+dx/2, y0+dy/2, -dy, dx];

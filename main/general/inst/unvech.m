73c72b74650a9c85afe83ef18f4035902af9fc0d
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-10-21 10:15:18 +0000

    pgmres.m: removing duplicated function implemented in C++ and Octave
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8802 fc592f45-0881-4044-91ff-c96495a54512

--- main/general/inst/unvech.m
+++ main/general/inst/unvech.m
@@ -1,9 +1,11 @@
 ## Copyright (C) 2006  Michael Creel <michael.creel@uab.es>
 ## Copyright (C) 2009  Jaroslav Hajek <highegg@gmail.com>
+## Copyright (c) 2011 Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
+## Copyright (c) 2011 Carnë Draug <carandraug+dev@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
+## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
 ##
 ## This program is distributed in the hope that it will be useful,
@@ -15,39 +17,53 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {} unvech (@var{v})
-## Performs the reverse of "vech". Generates a symmetric matrix from the lower
-## triangular elements, received as a vector @var{v}.
+## @deftypefn {Function File} {@var{m} =} unvech (@var{v}, @var{scale})
+## Performs the reverse of @code{vech} on the vector @var{v}.
+##
+## Given a Nx1 array @var{v} describing the lower triangular part of a
+## matrix (as obtained from @code{vech}), it returns the full matrix.
+##
+## The upper triangular part of the matrix will be multiplied by @var{scale} such
+## that 1 and -1 can be used for symmetric and antisymmetric matrix respectively.
+## @var{scale} must be a scalar and defaults to 1.
+##
+## @seealso{vech, ind2sub, sub2ind_tril}
 ## @end deftypefn
 
-function x = unvech (v)
+function M = unvech (v, scale = 1)
 
-  if (nargin != 1)
-    usage ("unvech (v)");
+  if ( nargin < 1 || nargin > 2 )
+    print_usage;
+  elseif ( !ismatrix (v) && any (size (v) != 1) )
+    error ("V must be a row orcolumn matrix")
+  elseif ( !isnumeric (scale) || !isscalar (scale) )
+    error ("SCALE must be a scalar")
   endif
-  
-  if (! isvector(v))
-    usage ("unvech (v)");
-  endif
-  
-  # find out dimension of symmetric matrix
-  p = length (v);
-  n = -(1 - sqrt (1 + 8*p))/2;
-  
-  if (mod (n, 1) != 0)
-    error("unvech: the input vector does not generate a square matrix");
-  endif
-  
-  x = zeros (n, n);
 
-  # do the reverse of vech
-  count = 0;
-  for j = 1 : n
-    i = j : n;
-    x(j,i) = x(i,j) = v(count + i);
-    count += n - j;
-  endfor
+  N      = length (v);
+  dim    = (sqrt ( 1 + 8*N ) - 1)/2;
+  [r, c] = ind2sub_tril (dim, 1:N);
+  M      = accumarray ([r; c].', v);
+  M     += scale * tril (M, -1).';
+
 endfunction
 
 %!assert(unvech([1;0;0;1;0;1]), full(eye(3,3)) );
-%!error <does not generate a square matrix> unvech([1;0;0;1;0;1;1]);
+
+#%!error <does not generate a square matrix> unvech([1;0;0;1;0;1;1]);
+
+%!test %symmetric
+%! dim = 10;
+%! A = tril( floor ( 5*(2*rand(dim)-1) ) );
+%! A += A.';
+%! M = vech(A);
+%! M = unvech(M, 1);
+%! assert (A, M);
+
+%!test %antisymmetric
+%! dim = 10;
+%! A = tril( floor ( 5*(2*rand(dim)-1) ) );
+%! A -= A.';
+%! M = vech(A);
+%! M = unvech(M, -1);
+%! assert (A, M);

4ae76ff99a4d58ad59926a8883ab213f24774225
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2011-11-02 15:47:06 +0000

    geometry. Adding some functions to manipulate piecewise smooth polygons
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@8931 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/shapecentroid.m
+++ main/geometry/inst/shapecentroid.m
@@ -14,44 +14,48 @@
 %% along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 %% -*- texinfo -*-
-%% @deftypefn {Function File} { @var{a} =} shapearea (@var{pp})
-%%  Shape is defined with piecewise smooth polynomials. @var{pp} is a
-%% cell where each elements is a 2-by-(poly_degree+1) array containing 
+%% @deftypefn {Function File} { @var{cm} =} shapecentroid (@var{pp})
+%%  Centroid of a plane shape. 
+%%
+%% The shape is defined with piecewise smooth polynomials. @var{pp} is a
+%% cell where each elements is a 2-by-(poly_degree+1) matrix containing 
 %% @code{px(i,:) = pp@{i@}(1,:)} and @code{py(i,:) = pp@{i@}(2,:)}.
 %%
+%% @seealso{shapearea}
 %% @end deftypefn
 
-function A = shapearea (shape)
+function cm = shapecentroid (shape)
 
-  A = sum(cellfun (@Aint, shape));
-  if A < 0
-    A = -A;
-  end
+  cm = sum( cell2mat ( cellfun (@CMint, shape, 'UniformOutput', false)));
+  A = shapearea(shape);
+  cm = cm / A;
   
 endfunction
 
-function dA = Aint (x)
+function dcm = CMint (x)
 
     px = x(1,:);
     py = x(2,:);
-
-    P = polyint (conv (px, polyderiv(py)));
+    Px = polyint (conv(conv (px , px)/2 , polyderiv (py)));
+    Py = polyint (conv(-conv (py , py)/2 , polyderiv (px)));
     
-    dA = diff(polyval(P,[0 1]));
+    dcm = zeros (1,2);
+    dcm(1) = diff(polyval(Px,[0 1]));
+    dcm(2) = diff(polyval(Py,[0 1]));
 
-end
+endfunction
 
 %!demo % non-convex bezier shape
 %! weirdhearth ={[-17.6816  -34.3989    7.8580    3.7971; ...
 %!                15.4585  -28.3820  -18.7645    9.8519]; ...
 %!                 [-27.7359   18.1039  -34.5718    3.7878; ...
 %!                  -40.7440   49.7999  -25.5011    2.2304]};
-%! A = shapearea (weirdhearth)
+%! CoM = shapecentroid (weirdhearth)
 
 %!test
-%! triangle = {[1 0; 0 0]; [-0.5 1; 1 0]; [-0.5 0.5; -1 1]};
-%! A = shapearea (triangle);
-%! assert (0.5, A);
+%! square = {[1 -0.5; 0 -0.5]; [0 0.5; 1 -0.5]; [-1 0.5; 0 0.5]; [0 -0.5; -1 0.5]};
+%! CoM = shapecentroid (square);
+%! assert (CoM, [0 0], sqrt(eps));
 
 %!test
 %! circle = {[1.715729  -6.715729    0   5; ...
@@ -62,6 +66,6 @@ end
 %!             1.715729   1.568542  -8.284271    0]; ...
 %!            [-1.715729  -1.568542   8.284271    0; ...
 %!            -1.715729   6.715729    0  -5]};
-%! A = shapearea (circle);
-%! assert (pi*5^2, A, 5e-2);
+%! CoM = shapecentroid (circle);
+%! assert (CoM , [0 0], 5e-3);
 

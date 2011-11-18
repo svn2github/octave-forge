43fe353aa6ff33f69ed727d1505b61dd05cccd39
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2011-11-18 18:47:00 +0000

    control-devel: update draft code
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9135 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/devel/options.m
+++ extra/control-devel/devel/options.m
@@ -1,5 +1,13 @@
 function opt = options (varargin)
 
+  if (nargin == 0)
+    print_usage ();
+  endif
+
+  if (rem (nargin, 2))
+    error ("options: properties and values must come in pairs");
+  endif
+
   key = reshape (varargin(1:2:end-1), [], 1);
   val = reshape (varargin(2:2:end), [], 1);
 

5ac13a5306f037d048ccc6bf788f9f5771904eb3
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2011-12-01 11:49:59 +0000

    quaternion_oo: reorganize creation of pdf manual
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9231 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/quaternion_oo/devel/generate_quaternion_pdf.m
@@ -0,0 +1,12 @@
+homedir = pwd ();
+develdir = fileparts (which ("generate_quaternion_pdf"));
+pdfdir = [develdir, "/pdfdoc"];
+cd (pdfdir);
+
+collect_texinfo_strings
+
+for i = 1:5
+  system ("pdftex -interaction batchmode quaternion.tex");
+endfor
+
+cd (homedir);

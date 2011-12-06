15f4f173e7fd0978fe3fa4b248f8d591fe157929
blondandy <blondandy@fc592f45-0881-4044-91ff-c96495a54512>
2011-12-06 22:18:06 +0000

    packaging script now works
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9294 fc592f45-0881-4044-91ff-c96495a54512

--- extra/dicom/mkpkg.sh
+++ extra/dicom/mkpkg.sh
@@ -4,14 +4,16 @@ WDIR=$(pwd)
 PKG=$(basename $WDIR)
 echo "Package=$PKG" 
 cd ..
-VLINE=$(cat $PKG/DESCRIPTION | grep Version:)
-PKG_VERSION=${VLINE#* }
-ARCHNAME=$PKG-$PKG_VERSION.tar.gz
 rm -r -f ~/$PKG
 svn export $PKG ~/$PKG
+VLINE=$(cat ~/$PKG/DESCRIPTION | grep Version:)
+PKG_VERSION=${VLINE#* }
+ARCHNAME=$PKG-$PKG_VERSION.tar.gz
+rm ~/dicom/mkpkg.sh
 echo "Version=$PKG_VERSION"
+cd ~
 tar czf $ARCHNAME $PKG/
 ls $ARCHNAME
 md5sum $ARCHNAME
 uuencode $ARCHNAME < $ARCHNAME > $ARCHNAME.uue
-cd $PKG
+cd $WDIR

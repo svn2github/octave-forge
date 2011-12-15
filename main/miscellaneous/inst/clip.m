328d77e09cc0c7af466340b985039384eb9c2485
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2011-12-15 20:20:04 +0000

    clip: improved input check, removed calll to find, and help text to texinfo
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9395 fc592f45-0881-4044-91ff-c96495a54512

--- main/miscellaneous/inst/clip.m
+++ main/miscellaneous/inst/clip.m
@@ -2,7 +2,7 @@
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
+## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
 ##
 ## This program is distributed in the hope that it will be useful,
@@ -13,40 +13,50 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
-## Clip values outside the range to the value at the boundary of the
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{x} =} clip (@var{x})
+## @deftypefnx {Function File} {@var{x} =} clip (@var{x}, @var{hi})
+## @deftypefnx {Function File} {@var{x} =} clip (@var{x}, [@var{lo}, @var{hi}])
+## Clip @var{x} values outside the range.to the value at the boundary of the
 ## range.
 ##
-## X = clip(X)
+## Range boundaries, @var{lo} and @var{hi}, default to 0 and 1 respectively.
+##
+## @var{x} = clip (@var{x})
 ##   Clip to range [0, 1]
 ##
-## X = clip(X, hi)
-##   Clip to range [0, hi]
+## @var{x} = clip (@var{x}, @var{hi})
+##   Clip to range [0, @var{hi}]
 ##
-## X = clip(X, [lo, hi])
-##   Clip to range [lo, hi]
+## @var{x} = clip (@var{x}, [@var{lo}, @var{hi}])
+##   Clip to range [@var{lo}, @var{hi}]
+## @end deftypefn
 
 ## TODO: more clip modes, such as three level clip(X, [lo, mid, hi]), which
 ## TODO: sends everything above hi to hi, below lo to lo and between to
 ## TODO: mid; or infinite peak clipping, which sends everything above mid
 ## TODO: to hi and below mid to lo.
 
-function x = clip (x, range)
+function x = clip (x, range = [0, 1])
 
-  if (nargin == 2)
-    if (length(range) == 1)
-      range = [0, range];
-    end
-  elseif (nargin == 1)
-    range = [0, 1];
+  if (nargin < 1 || nargin > 2)
+    print_usage;
   else
-    usage("X = clip(X [, range])");
-  end
-  try wfi = warning("query", "Octave:fortran-indexing").state;
+    if (numel (range) == 2)
+      ## do nothing, it's good
+    elseif (numel (range) == 1)
+      range = [0, range];
+    else
+      print_usage;
+    endif
+  endif
+
+  try   wfi = warning ("query", "Octave:fortran-indexing").state;
   catch wfi = "off";
-  end
+  end_try_catch
   unwind_protect
-    x (find (x > range (2))) = range (2);
-    x (find (x < range (1))) = range (1);
+    x (x > range (2)) = range (2);
+    x (x < range (1)) = range (1);
   unwind_protect_cleanup
     warning(wfi, "Octave:fortran-indexing");
   end_unwind_protect

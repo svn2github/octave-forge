a3507ff3a271e1cc9d0840964ec5545517728475
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-01-13 16:25:42 +0000

    control-devel: fix messed up example
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9526 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/devel/anderson.m
+++ extra/control-devel/devel/anderson.m
@@ -1,5 +1,17 @@
-## Anderson, B.D.O (1989) Controller Reduction: Concepts and Approaches, IEEE Transactions of Automatic Control
+%% -*- texinfo -*-
+%% Frequency-weighted coprime factorization controller reduction.
 
+% ===============================================================================
+% Coprime Factorization Controller Reduction     Lukas Reichlin     December 2011
+% ===============================================================================
+% Reference: Anderson, B.D.O.: Controller Reduction: Concepts and Approaches
+%            IEEE Transactions of Automatic Control, Vol. 34, No. 8, August 1989
+% ===============================================================================
+
+% Tabula Rasa
+clear all, close all, clc
+
+% Plant
 A = [ -0.161      -6.004      -0.58215    -9.9835     -0.40727    -3.982       0.0         0.0
        1.0         0.0         0.0         0.0         0.0         0.0         0.0         0.0
        0.0         1.0         0.0         0.0         0.0         0.0         0.0         0.0
@@ -22,14 +34,13 @@ C = [  0.0         0.0         6.4432e-3   2.3196e-3   7.1252e-2   1.0002      0
 
 G = ss (A, B, C);
 
-
+% LQG Design
 H = [  0.0         0.0         0.0         0.0         0.55       11.0         1.32       18.0     ];
 
 q1 = 1e-6;
-q2 = 100;   # [100, 1000, 2000]
+q2 = 100;   % [100, 1000, 2000]
 
 Q = q1 * H.' * H;
-%Q = q1 * (H.' * H);
 R = 1;
 
 W = q2 * B * B.';
@@ -37,52 +48,33 @@ V = 1;
 
 F = lqr (G, Q, R)
 L = lqr (G.', W, V).'
-%[~, L] = kalman (G, W, V)
 
-%{
-[Kr, info] = fwcfconred (G, F, L, "cf", "right")
-
+% Coprime Factorization using Balanced Truncation Approximation
 figure (1)
-T = feedback (G*Kr)
-step (T, 150)
-%}
-
-figure (1)
-clf
-
 for k = 8:-1:2
-  Kr = cfconred (G, F, L, k);
+  Kr = cfconred (G, F, L, k);   % 'method', 'bfsr-bta'
   T = feedback (G*Kr);
   step (T, 200)
   hold on
 endfor
-
 hold off
-print -depsc2 anderson-figure1.eps
 
+% Coprime Factorization using Singular Perturbation Approximation
 figure (2)
-clf
-
 for k = 8:-1:2
   Kr = cfconred (G, F, L, k, 'method', 'bfsr-spa');
   T = feedback (G*Kr);
   step (T, 200)
   hold on
 endfor
-
 hold off
-print -depsc2 anderson-figure2.eps
 
-
+% Frequency-Weighted Coprime Factorization using BTA
 figure (3)
-clf
-
 for k = 8:-1:2
   Kr = fwcfconred (G, F, L, k);
   T = feedback (G*Kr);
   step (T, 300)
   hold on
 endfor
-
 hold off
-print -depsc2 anderson-figure3.eps

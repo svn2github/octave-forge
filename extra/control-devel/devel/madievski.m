f13b74f148183375cadca29737779ca4c40c3d40
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-01-20 15:42:58 +0000

    control-devel: touch up example
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9547 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/devel/madievski.m
+++ extra/control-devel/devel/madievski.m
@@ -1,5 +1,19 @@
-%clear all, close all, clc;
+%% -*- texinfo -*-
+%% Frequency-weighted controller reduction.
 
+% ===============================================================================
+% Frequency Weighted Controller Reduction       Lukas Reichlin      December 2011
+% ===============================================================================
+% Reference: Madievski, A.G. and Anderson, B.D.O.
+%            Sampled-Data Controller Reduction Procedure
+%            IEEE Transactions of Automatic Control
+%            Vol. 40, No. 11, November 1995
+% ===============================================================================
+
+% Tabula Rasa
+clear all, close all, clc
+
+% Plant
 Ap1 = [  0.0         1.0
          0.0         0.0     ];
 
@@ -29,7 +43,7 @@ Dp = [   0.0     ];
 
 P = ss (Ap, Bp, Cp, Dp);
 
-
+% Controller
 Ac = [  -0.4077      0.9741      0.1073      0.0131      0.0023     -0.0186     -0.0003     -0.0098
         -0.0977     -0.1750      0.0215     -0.0896     -0.0260      0.0057      0.0109     -0.0105
          0.0011      0.0218     -0.0148      0.7769      0.0034     -0.0013     -0.0014      0.0011
@@ -54,89 +68,59 @@ Dc = [   0.0     ];
 
 K = ss (Ac, Bc, Cc, Dc);
 
-Kr = btaconred (P, K, 4, 'feedback', '-')
+% Controller Reduction
+Kr4 = spaconred (P, K, 4, 'feedback', '-')
+Kr2 = spaconred (P, K, 2, 'feedback', '-')
 
-
-tau = 0.1;
-Kd = c2d (K, tau);
-Pd = c2d (P, tau);
-
-s = tf ('s');
-F = 5 / (s + 5);
-
+% Open Loop
 L = P * K;
-%Ld = Pd * Kd;
-Lr = P * Kr;
+Lr4 = P * Kr4;
+Lr2 = P * Kr2;
 
+% Closed Loop
 T = feedback (L);
-%Td = feedback (Ld);
-Tr = feedback (Lr);
+Tr4 = feedback (Lr4);
+Tr2 = feedback (Lr2);
 
+% Frequency Range
 w = {1e-2, 1e1};
 
-figure (1)
-step (T, 100)
-
-figure (2)
-%bode (L)
-bode (K, w)
-
-figure (3)
-%step (Td, 100)
-step (Tr, 100)
-
-figure (4)
-%bode (Ld)
-bode (Kr, w)
-
-figure (5)
-%step (feedback (P*Kr), 100)
-
-
-
+% Bode Plot of Controller
 [mag, pha, w] = bode (K, w);
-[magr, phar, wr] = bode (Kr, w);
+[magr4, phar4, wr4] = bode (Kr4, w);
+[magr2, phar2, wr2] = bode (Kr2, w);
 
 mag = 20 * log10 (mag);
-magr = 20 * log10 (magr);
+magr4 = 20 * log10 (magr4);
+magr2 = 20 * log10 (magr2);
 
+figure (1)
+subplot (2, 1, 1)
+semilogx (w, mag, wr4, magr4, wr2, magr2)
+axis ('tight')
+ylim (__axis_margin__ (ylim))
+grid ('on')
+title ('Bode Diagrams of K and Kr')
+ylabel ('Magnitude [dB]')
 
-%figure (6)
-
-
-      xl_str = "Frequency [rad/s]";
-
-
-    subplot (2, 1, 1)
-    semilogx (w, mag, "b", wr, magr, "r")
-    axis ("tight")
-    ylim (__axis_margin__ (ylim))
-    grid ("on")
-    title ("Bode Diagrams of K and Kr")
-    ylabel ("Magnitude [dB]")
-
-    subplot (2, 1, 2)
-    semilogx (w, pha, "b", wr, phar, "r")
-    axis ("tight")
-    ylim (__axis_margin__ (ylim))
-    grid ("on")
-    xlabel (xl_str)
-    ylabel ("Phase [deg]")
-    legend ("K (8 states)", "Kr (4 states)", "location", "southwest")
+subplot (2, 1, 2)
+semilogx (w, pha, wr4, phar4, wr2, phar2)
+axis ('tight')
+ylim (__axis_margin__ (ylim))
+grid ('on')
+xlabel ('Frequency [rad/s]')
+ylabel ('Phase [deg]')
+legend ('K (8 states)', 'Kr (4 states)', 'Kr (2 states)', 'location', 'southwest')
     
-    print -depsc2 madievski-bode.eps
-
-
+% Step Response of Closed Loop
 [y, t] = step (T, 100);
-[yr, tr] = step (Tr, 100);
+[yr4, tr4] = step (Tr4, 100);
+[yr2, tr2] = step (Tr2, 100);
 
-figure (6)
-plot (t, y, 'b', tr, yr, 'r')
+figure (2)
+plot (t, y, tr4, yr4, tr2, yr2)
 grid ('on')
 title ('Step Response of Closed Loop')
 xlabel ('Time [s]')
 ylabel ('Output [-]')
-legend ('K (8 states)', 'Kr (4 states)', 'Location', 'SouthEast')
-
-print -depsc2 madievski-step.eps
-
+legend ('K (8 states)', 'Kr (4 states)', 'Kr (2 states)', 'Location', 'SouthEast')

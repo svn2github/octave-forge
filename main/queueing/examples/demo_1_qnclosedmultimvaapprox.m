696470b035841e6f84f7a942ca62fb649479721c
mmarzolla <mmarzolla@fc592f45-0881-4044-91ff-c96495a54512>
2012-02-03 22:01:31 +0000

    Package restructuring
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9578 fc592f45-0881-4044-91ff-c96495a54512

--- main/queueing/examples/demo_1_qnclosedmultimvaapprox.m
+++
@@ -1,5 +0,0 @@
- S = [ 1, 1, 1, 1; 2, 1, 3, 1; 4, 2, 3, 3 ];
- V = ones(3,4);
- N = [10 5 1];
- m = [1 0 1 1];
- [U R Q X] = qnclosedmultimvaapprox(N,S,V,m);
--- main/queueing/examples/demo_1_qnopensingle.m
+++
@@ -1,6 +0,0 @@
- lambda = 3;
- V = [16 7 8];
- S = [0.01 0.02 0.03];
- [U R Q X] = qnopensingle( lambda, S, V );
- R_s = dot(R,V) # System response time
- N = sum(Q) # Average number in system

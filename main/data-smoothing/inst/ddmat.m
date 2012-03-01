59b1e0562ea1bbc0f180212efbf571fd6652caf5
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-01 16:34:16 +0000

    ddmat: added e-mail address to copyright holder (deduced by checking the function reference)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9730 fc592f45-0881-4044-91ff-c96495a54512

--- main/data-smoothing/inst/ddmat.m
+++ main/data-smoothing/inst/ddmat.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2003 Paul Eilers
+## Copyright (C) 2003 Paul H. C. Eilers <p.eilers@erasmusmc.nl>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software

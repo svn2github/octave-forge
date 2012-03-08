fdd43e841645a3694acc320c6e138fe50f276d1a
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-08 17:54:19 +0000

    normc: use print_usage
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9792 fc592f45-0881-4044-91ff-c96495a54512

--- main/miscellaneous/inst/normc.m
+++ main/miscellaneous/inst/normc.m
@@ -1,20 +1,20 @@
-## Copyright (C) 2011   Thomas Weber
+## Copyright (C) 2011 Thomas Weber <tweber@debian.org>
 ##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
 ##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this program; If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{x} = } normc (@var{M})
+## @deftypefn {Function File} {@var{x} =} normc (@var{M})
 ## Normalize the columns of a matrix to a length of 1 and return the matrix.
 ##
 ## @example
@@ -32,9 +32,9 @@
 
 function X = normc(M)
   if (1 != nargin)
-    usage("normc(M)");
+    print_usage;
   endif
-  
+
   X = normr(M.').';
 endfunction
 

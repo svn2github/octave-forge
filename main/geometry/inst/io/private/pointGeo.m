0ab86a6f14c560b6a61deea5de8f1e6e13979fb3
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-10 04:18:08 +0000

    geometry: write points in mesh file with higher resolution
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9804 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/io/private/pointGeo.m
+++ main/geometry/inst/io/private/pointGeo.m
@@ -1,5 +1,5 @@
 %% Copyright (c) 2010 Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
-%% 
+%%
 %%    This program is free software: you can redistribute it and/or modify
 %%    it under the terms of the GNU General Public License as published by
 %%    the Free Software Foundation, either version 3 of the License, or
@@ -17,9 +17,9 @@
 %% @deftypefn {Function File} @var{str} =  poointGeo (@var{n}, @var{xyz}, @var{l})
 %% Generates a string for Gmsh Point format.
 %%
-%% Gmsh's simplest `elementary entity', a `Point'. A Point is defined by a list 
-%% of five numbers: @var{n} the identificator, @var{xyz} three coordinates (X, Y 
-%% and Z), and a characteristic length @var{l} that sets the target element size 
+%% Gmsh's simplest `elementary entity', a `Point'. A Point is defined by a list
+%% of five numbers: @var{n} the identificator, @var{xyz} three coordinates (X, Y
+%% and Z), and a characteristic length @var{l} that sets the target element size
 %% at the point:
 %% The distribution of the mesh element sizes is then obtained by
 %% interpolation of these characteristic lengths throughout the
@@ -28,5 +28,5 @@
 %% @end deftypefn
 
 function str = pointGeo(n,xyz,l)
-    str = sprintf('Point(%d) = {%f,%f,%f,%f};\n',n,xyz,l);
+    str = sprintf('Point(%d) = {%.16g,%.16g,%.16g,%.16g};\n',n,xyz,l);
 end

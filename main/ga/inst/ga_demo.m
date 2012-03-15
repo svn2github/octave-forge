000ddb8f8d0e561b75afeee1a46bf6667850dbd3
slackydeb <slackydeb@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-15 02:22:50 +0000

    ga: add dummy demo file
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9893 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/ga/inst/ga_demo.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2012 Luca Favatella <slackydeb@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn{Script File} {} ga_demo
+## Run a demo of the genetic algorithm package.  The current
+## implementation is only a placeholder.
+## @end deftypefn
+
+## Author: Luca Favatella <slackydeb@gmail.com>
+## Created: March 2012
+## Version: 0.0.1
+
+demo ga_demo
+
+
+%!demo
+%! % TODO

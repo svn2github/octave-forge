846dac674e458a9a2c3de71030b7c4653535e69b
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-21 00:13:56 +0000

    io: added post_install script. Workaround to remove PKG_ADD file from oct directory after package installation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@9983 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/io/post_install.m
@@ -0,0 +1,14 @@
+## this function removes the PKG_ADD script from the .oct directory of the io
+## package after installation. The reason is that PKG_ADD calls chk_spreadsheet_support
+## which belongs to the io package itself. While it is ok when called from the
+## .m directory (since chk_spreadsheet_support is there), when called from the
+## .oct directory it fails since the package hasn't been loaded yet
+
+function post_install (desc)
+  file = cstrcat (desc.archprefix, filesep, octave_config_info ("canonical_host_type"),
+                  "-", octave_config_info ("api_version"), filesep, "PKG_ADD");
+  [err, msg] = unlink (file);
+  if (err)
+    warning ("Unable to remove PKG_ADD: %S", msg);
+  endif
+endfunction

64960ceb3aa759e35cbbbb2992bcc83d08c4e9bd
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-22 17:33:22 +0000

    geometry: deprecating svg functions. Adding license to python scripts
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10004 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/io/@svg/parsePath.py
+++ main/geometry/inst/io/@svg/parsePath.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+## Copyright (c) 2012 Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import inkex, simplepath
 import sys
 #import getopt
@@ -8,7 +23,7 @@ def parsePaths (filen=None):
 
   svg = inkex.Effect ()
   svg.parse (filen)
-  
+
   paths = svg.document.xpath ('//svg:path', namespaces=inkex.NSS)
   for path in paths:
     D = simplepath.parsePath (path.attrib['d'])
@@ -17,14 +32,14 @@ def parsePaths (filen=None):
     for cmd,params in D:
       cmdlst.append(cmd)
       parlst.append(params)
-    
+
     print 'svgpath = struct("cmd","{0}","data",{{{1}}});' \
         .format(''.join(cmdlst),str(parlst).replace('[[','[').replace(']]',']'))
 
     print 'svgpathid = "{0}"; $'.format(path.attrib['id'])
 
-  
-  
+
+
 # ----------------------------
 
 if __name__=="__main__":
@@ -34,7 +49,7 @@ if __name__=="__main__":
     except getopt.GetoptError:
         usage()
         sys.exit(2)
-      
+
     doHelp = 0
     c = Context()
     c.doPrint = 1

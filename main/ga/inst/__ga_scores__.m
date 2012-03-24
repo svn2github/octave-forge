92a22ab9fc9fe8dd39d3f18d9e8bb59fd6e4a91b
slackydeb <slackydeb@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-24 16:32:50 +0000

    ga: restructure __ga_scores__ for better error handling
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10027 fc592f45-0881-4044-91ff-c96495a54512

--- main/ga/inst/__ga_scores__.m
+++ main/ga/inst/__ga_scores__.m
@@ -14,30 +14,35 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## Author: Luca Favatella <slackydeb@gmail.com>
-## Version: 5.5.2
+## Version: 5.6
 
 function Scores = __ga_scores__ (problem, Population)
-  if (strcmp (problem.options.Vectorized, "on")) ## using vectorized evaluation
-    if (strcmp (problem.options.UseParallel, "always"))
-      warning ("'Vectorized' option is 'on': ignoring 'UseParallel' option, even if it is 'always'");
-    endif
-    Scores = problem.fitnessfcn (Population);
-  else ## not using vectorized evaluation
-    if (! strcmp (problem.options.Vectorized, "off"))
+  switch problem.options.Vectorized
+    case "on" ## using vectorized evaluation
+      switch problem.options.UseParallel
+        case "always"
+          warning ("'Vectorized' option is 'on': ignoring 'UseParallel' option, even if it is 'always'");
+        case "never"
+          ## Nothing.
+        otherwise
+          warning ("'Vectorized' option is 'on': ignoring invalid 'UseParallel' option value (it should be 'always' or 'never')");
+      endswitch
+      Scores = problem.fitnessfcn (Population);
+    case "off" ## not using vectorized evaluation
+      switch problem.options.UseParallel
+        case "always" ## using parallel evaluation
+          error ("TODO: implement parallel evaluation of objective function");
+        case "never" ## using serial evaluation (i.e. loop)
+          [nrP ncP] = size (Population);
+          tmp = zeros (nrP, 1);
+          for index = 1:nrP
+            tmp(index, 1) = problem.fitnessfcn (Population(index, 1:ncP));
+          endfor
+          Scores = tmp(1:nrP, 1);
+        otherwise
+          error ("'UseParallel' option must be 'always' or 'never'");
+      endswitch
+    otherwise
       error ("'Vectorized' option must be 'on' or 'off'");
-    endif
-    if (strcmp (problem.options.UseParallel, "always")) ## using parallel evaluation
-      error ("TODO: implement parallel evaluation of objective function");
-    else ## using serial evaluation (i.e. loop)
-      if (! strcmp (problem.options.UseParallel, "never"))
-        error ("'UseParallel' option must be 'always' or 'never'");
-      endif
-      [nrP ncP] = size (Population);
-      tmp = zeros (nrP, 1);
-      for index = 1:nrP
-        tmp(index, 1) = problem.fitnessfcn (Population(index, 1:ncP));
-      endfor
-      Scores = tmp(1:nrP, 1);
-    endif
-  endif
+  endswitch
 endfunction

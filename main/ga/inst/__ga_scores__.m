8910fa9dbd284c9ad3d48cab85e092afef0b71f9
slackydeb <slackydeb@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-24 16:34:10 +0000

    ga: fix failing unit test
    A non-vectorized objective function must not work when
    vectorization is required.
    
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10033 fc592f45-0881-4044-91ff-c96495a54512

--- main/ga/inst/__ga_scores__.m
+++ main/ga/inst/__ga_scores__.m
@@ -14,9 +14,10 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## Author: Luca Favatella <slackydeb@gmail.com>
-## Version: 5.6
+## Version: 5.7
 
 function Scores = __ga_scores__ (problem, Population)
+  [nrP ncP] = size (Population);
   switch problem.options.Vectorized
     case "on" ## using vectorized evaluation
       switch problem.options.UseParallel
@@ -27,13 +28,12 @@ function Scores = __ga_scores__ (problem, Population)
         otherwise
           warning ("'Vectorized' option is 'on': ignoring invalid 'UseParallel' option value (it should be 'always' or 'never')");
       endswitch
-      Scores = problem.fitnessfcn (Population);
+      Scores = (problem.fitnessfcn (Population))(1:nrP, 1);
     case "off" ## not using vectorized evaluation
       switch problem.options.UseParallel
         case "always" ## using parallel evaluation
           error ("TODO: implement parallel evaluation of objective function");
         case "never" ## using serial evaluation (i.e. loop)
-          [nrP ncP] = size (Population);
           tmp = zeros (nrP, 1);
           for index = 1:nrP
             tmp(index, 1) = problem.fitnessfcn (Population(index, 1:ncP));

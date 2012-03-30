83cf24388ff519b38c15bdb2e26f647f6ac04e4b
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-03-30 08:26:56 +0000

    quaternion: add missing ## -*- texinfo -*-
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10107 fc592f45-0881-4044-91ff-c96495a54512

--- main/quaternion/inst/qi.m
+++ main/quaternion/inst/qi.m
@@ -13,6 +13,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+## -*- texinfo -*-
 ## @deftypefn {Function File} {} qi
 ## Create x-component of a quaternion's vector part.
 ##
--- main/quaternion/inst/qk.m
+++ main/quaternion/inst/qk.m
@@ -13,6 +13,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+## -*- texinfo -*-
 ## @deftypefn {Function File} {} qk
 ## Create z-component of a quaternion's vector part.
 ##

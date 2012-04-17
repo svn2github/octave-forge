d81814b2e06bfcc5a548bd9a8dcf9f1b4709a5f2
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2012-04-17 11:11:22 +0000

    maint-time: update license to GPLv3+
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10260 fc592f45-0881-4044-91ff-c96495a54512

--- main/time/inst/datesplit.m
+++ main/time/inst/datesplit.m
@@ -1,17 +1,17 @@
-## Copyright (C) 2001 Bill Denney <bill@givebillmoney.com>
+## Copyright (C) 2001 Bill Denney <bill@denney.ws>
 ##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
 ##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this program; If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {Y =} datesplit(date, P)
@@ -85,8 +85,6 @@
 ##    assumed year, it may become invalid.
 ##  * Internationalize.  Not all months use the English system.
 ##  * Vectorize.  That requires vectorization of regexp though...
-
-## Author: Bill Denney <bill@givebillmoney.com>
 
 function [y, m, d, h, mi, s] = datesplit(ds, P)
 
--- main/time/inst/year.m
+++ main/time/inst/year.m
@@ -1,18 +1,17 @@
-## Copyright (C) 2008 Bill Denney
+## Copyright (C) 2008 Bill Denney <bill@denney.ws>
 ##
-## This software is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or (at
-## your option) any later version.
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
 ##
-## This software is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this software; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {y =} year (Date)
@@ -21,9 +20,6 @@
 ##
 ## @seealso{date, datevec, now, day, month}
 ## @end deftypefn
-
-## Author: Bill Denney <bill@denney.ws>
-## Created: 21 Jan 2008
 
 function t = year (dates)
 

69fb46a4dd6832774eb019efde324371ce53f23d
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-05-01 20:31:07 +0000

    control-devel: compute pseudoinverse by SVD instead of \ operator
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10351 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/inst/arx.m
+++ extra/control-devel/inst/arx.m
@@ -28,7 +28,21 @@ function sys = arx (dat, na, nb)
   PhiU = toeplitz (U(1:end-1, :), [U(1, :); zeros(nb-1, 1)]);
   Phi = [-PhiY, PhiU];
   
-  Theta = Phi \ Y(2:end, :);
+  Theta = Phi \ Y(2:end, :)                 # naive formula
+  
+  ## solve linear least squares problem by pseudoinverse
+  ## the pseudoinverse is computed by singular value decomposition
+  ## M = U S V*  --->  M+ = V S+ U*
+  ## Th = Ph \ Y = Ph+ Y
+  ## Th = V S+ U' Y,   S+ = 1 ./ diag (S)
+  
+  [U, S, V] = svd (Phi, 0);                 # 0 for "economy size" decomposition, U overwrites input U
+  S = diag (S);                             # extract main diagonal
+  r = sum (S > eps*S(1));
+  V = V(:, 1:r);
+  S = S(1:r);
+  U = U(:, 1:r);
+  Theta = V * (S .\ (U' * Y(2:end, :)))     # U' is the conjugate transpose
   
   A = [1; Theta(1:na)];     # a0 = 1, a1 = Theta(1), an = Theta(n)
   B = Theta(na+1:end);      # b0 = 0 (leading zeros are removed by tf, no need to add one)

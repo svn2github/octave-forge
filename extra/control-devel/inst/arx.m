d6001fbf5d0494c361306a0e2048718e7a69f657
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-05-07 13:22:01 +0000

    control-devel: support miso arx identification
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10374 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/inst/arx.m
+++ extra/control-devel/inst/arx.m
@@ -15,20 +15,24 @@ function sys = arx (dat, na, nb)
   if (! is_real_scalar (na, nb))
     error ("arx: ");
     ## Test for integers
+    ## numel (nb) == size (dat, 3)
   endif
 
   ## TODO: handle MIMO and multi-experiment data
+  [~, p, m, ex] = size (dat);
 
   Y = dat.y{1};
   U = dat.u{1};
   Ts = dat.tsam{1};
   
-  n = max (na, nb);
+  max_nb = max (nb);
+  n = max (na, max_nb);
 
   ## avoid warning: toeplitz: column wins anti-diagonal conflict
   PhiY = toeplitz (Y(1:end-1, :), [Y(1, :); zeros(na-1, 1)]);
-  PhiU = toeplitz (U(1:end-1, :), [U(1, :); zeros(nb-1, 1)]);
-  Phi = [-PhiY, PhiU];
+  ## PhiU = toeplitz (U(1:end-1, :), [U(1, :); zeros(nb-1, 1)]);
+  PhiU = arrayfun (@(x) toeplitz (U(1:end-1, x), [U(1, x); zeros(nb(x)-1, 1)]), 1:m, "uniformoutput", false);
+  Phi = horzcat (-PhiY, PhiU{:});
   Phi = Phi(n:end, :)
   
   ## Theta = Phi \ Y(n+1:end, :);             # naive formula
@@ -48,8 +52,15 @@ function sys = arx (dat, na, nb)
   Theta = V * (S .\ (U' * Y(n+1:end, :)));  # U' is the conjugate transpose
   
   A = [1; Theta(1:na)];                     # a0 = 1, a1 = Theta(1), an = Theta(n)
-  B = [0; Theta(na+1:end)];                 # b0 = 0 (leading zero required by filt)
+  ## B = [0; Theta(na+1:end)];                 # b0 = 0 (leading zero required by filt)
+  ThetaB = Theta(na+1:end);
+  B = mat2cell (ThetaB, nb);
+  B = reshape (B, 1, []);
+  B = cellfun (@(x) [0; x], B, "uniformoutput", false);
   
-  sys = filt ({B, 1}, {A, A}, Ts);
+  ## sys = filt ({B, 1}, {A, A}, Ts);
+  num = [B, {1}];
+  den = repmat ({A}, 1, m+1);
+  sys = filt (num, den, Ts);
 
 endfunction

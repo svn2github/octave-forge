5aaa4b18fb559b4c003bb934979ef40fe7d14984
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-05-15 20:01:20 +0000

    control-devel: minor changes
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10446 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/inst/arx.m
+++ extra/control-devel/inst/arx.m
@@ -26,6 +26,8 @@
 
 function sys = arx (dat, na, nb)
 
+  ## TODO: delays
+
   if (nargin != 3)
     print_usage ();
   endif
@@ -97,9 +99,10 @@ endfunction
 function theta = __theta__ (phi, y, i, n)
     
   if (numel (phi) == 1)                             # single-experiment dataset
+    ## use "square-root algorithm"
     A = horzcat (phi{1}, y{1}(n(i)+1:end, i));      # [Phi, Y]
     R0 = triu (qr (A, 0));                          # 0 for economy-size R (without zero rows)
-    R1 = R0(1:end-1, 1:end-1);                      # R1 is triangular - can we exploit this in R1\R2
+    R1 = R0(1:end-1, 1:end-1);                      # R1 is triangular - can we exploit this in R1\R2?
     R2 = R0(1:end-1, end);
     theta = __ls_svd__ (R1, R2);                    # R1 \ R2
     

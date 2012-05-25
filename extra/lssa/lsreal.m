a06439a0cbc3062384c62ab26c310261ebdf2a3f
benjf5 <benjf5@fc592f45-0881-4044-91ff-c96495a54512>
2012-05-25 14:49:45 +0000

    Fixed lssa filenames, blatant error in lsreal.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10521 fc592f45-0881-4044-91ff-c96495a54512

--- extra/lssa/lsreal.m
+++ extra/lssa/lsreal.m
@@ -14,7 +14,7 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {transform =} nureal ( mag, time, maxfreq, numcoeff, numoctaves)
+## @deftypefn {Function File} {transform =} nureal ( time, mag, maxfreq, numcoeff, numoctaves)
 ##
 ## Return the real least-squares transform of the time series
 ## defined, based on the maximal frequency @var{maxfreq}, the
@@ -23,7 +23,7 @@
 ##
 ## @end deftypefn
 
-function transform = nureal( x, t, omegamax, ncoeff, noctave)
+function transform = nureal( t, x, omegamax, ncoeff, noctave)
   ## the R function runs the following command:
   ## nureal( double X, double Y, int min(X,Y), int ncoeff, int noctave, double omegamax, complex rp)
   ## this means that in the C, *tptr is X and *xptr is Y. Yes, I know. I think I'll rename them.
@@ -32,9 +32,9 @@ function transform = nureal( x, t, omegamax, ncoeff, noctave)
   ## where rp = complex(noctave*ncoeff) so ... I can just store that as noctave*ncoeff and have no
   ## problems, I guess.
   ## Possibly throw an error if ncoeff <= 0.
-  k = n = min ( min ( x , t ) ); ## THIS IS VECTOR-ONLY. I'd need to add another bit of code to
+  k = n = min(length(x),length(t)); ## THIS IS VECTOR-ONLY. I'd need to add another bit of code to
   ## make it array-safe, and that's not knowing right now what else will be necessary.
-  rp = zeros(1,(noctave * ncoeff)); ## In the C code, this is rendered as a Complex, but Octave doesn't care.
+  transform = zeros(1,(noctave * ncoeff)); ## In the C code, this is rendered as a Complex, but Octave doesn't care.
   od = 2 ^ ( - 1 / ncoeff ); ## this will cause a crash if ncoeff=0; prefer error & quit?
   o = omegamax;
   ## ot is just defined as a Real here, I'm leaving it until it needs to be called.
@@ -42,31 +42,19 @@ function transform = nureal( x, t, omegamax, ncoeff, noctave)
   ## zeta and iota are defined as Complex here, leaving them until they need to be defined.
   ## I'm not convinced I won't want ncoeff again, so ...
   ncoeffp = ncoeff;
-  for ( ncoeffp *= noctave , iter = 1 ; sign(ncoeffp--) ; o *= od )
-    #{
-    zeta = iota = 0;
-    for ( SRCFIRST ; SRCAVAIL ; SRCNEXT ) ##This is going to be vectorised shortly.
-      ## This code can't run yet ... I'm going to work out what SRCFIRST, SRCAVAIL, SRCNEXT are and
-      ## replace them with what they should be in this context. I've kept them as reminders.
-      ot = o * SRCT; ## Same with SRCT ... I think it means what was originally set as Y. Macros?
-      zeta += cos(ot) * SRCX;
-      zeta -= sin(ot) * SRCX * i; ## More sure now. I don't think I can vectorise this ... who am I kidding?
-      ot *= 2;
-      iota += cos(ot);
-      iota -= sin(ot) * i;
-    endfor
-    }#
-    ## Commented out the converted-from-C code because it's replaced by the four lines below.
+  ncoeffp *= noctave;
+  for iter = 1:ncoeffp
     ## This method is an application of Eq. 8 on page 6 of the text, as well as Eq. 7
-    ot = o .* t; 
+    ot = o .* t;
     zeta = sum( ( cos(ot) .* x ) - ( sin(ot) .* x .* i ) );
     ot = ot .* 2;
     iota = sum( cos(ot) - ( sin(ot) .* i ) );
-    zeta *= n1;
-    iota *= n1;
-    rp(iter++) = 2 / ( 1 - ( real(iota) ^ 2 ) - ( imag(iota) ^ 2 ) ) * ( conj(zeta) - (conj(iota) * zeta ));
+    zeta = zeta .* n1;
+    iota = iota .* n1;
+    transform(iter) = 2 / ( 1 - ( real(iota) ^ 2 ) - ( imag(iota) ^ 2 ) ) * ( conj(zeta) - (conj(iota) * zeta ));
+    o = o .* od;
   endfor
   
-  transform = rp;
+  ##  transform = rp;
 
 endfunction

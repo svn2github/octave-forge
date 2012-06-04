8211a7b8b7c12ae42ac95db02fd8e7c04468a81a
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2012-06-04 11:41:32 +0000

    ncread
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10561 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/octcdf/inst/ncread.m
@@ -0,0 +1,68 @@
+% x = ncread(filename,varname)
+% x = ncread(filename,varname,start,count,stride)
+% read the variable varname from file filename.
+
+function x = ncread(filename,varname,start,count,stride)
+
+nc = netcdf(filename,'r');
+nv = nc{varname};
+sz = size(nv); sz = sz(end:-1:1);
+nd = ndims(nv);
+
+if nargin == 2
+  start = ones(1,nd);
+  count = inf*ones(1,nd);
+end
+
+if nargin < 5
+  stride = ones(1,nd);
+end
+
+% end index
+
+endi = start + count.*stride;
+
+% replace inf in count
+
+i = endi == inf;
+endi(i) = sz(i);
+
+
+% load data
+
+% subsref structure
+subsr.type = '()';
+subsr.subs = cell(1,nd);
+for i=1:nd
+  subsr.subs{nd-i+1} = start(i):stride(i):endi(i);
+end
+start,endi
+
+x = subsref(nv,subsr);
+
+% apply attributes
+
+factor = nv.scale_factor(:);
+offset = nv.add_offset(:);
+fv = nv.FillValue_;
+
+if ~isempty(fv)
+  x(x == fv) = NaN;
+else
+  fv = nv.missing_value;
+  
+  if ~isempty(fv)
+    x(x == fv) = NaN;
+  end  
+end
+
+if ~isempty(factor)
+  x = x * factor;
+end
+
+if ~isempty(offset)
+  x = x + offset;
+end
+
+x = permute(x,[ndims(x):-1:1]);
+

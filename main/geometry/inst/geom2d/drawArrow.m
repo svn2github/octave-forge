3b369aafe5cd9b3922124dd43ca8cc11ccf8d4a2
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-08-03 08:31:19 +0000

    geometry: when porting all x1 and y1 where converted to texinfo vars. Now is corrected.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10800 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/geom2d/drawArrow.m
+++ main/geometry/inst/geom2d/drawArrow.m
@@ -10,8 +10,8 @@
 %%
 %% 1. Redistributions of source code must retain the above copyright notice, this
 %%    list of conditions and the following disclaimer.
-%%     
-%% 2. Redistributions in binary form must reproduce the above copyright notice, 
+%%
+%% 2. Redistributions in binary form must reproduce the above copyright notice,
 %%    this list of conditions and the following disclaimer in the documentation
 %%    and/or other materials provided with the distribution.
 %%
@@ -19,9 +19,9 @@
 %% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 %% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 %% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 %% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 %% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 %% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 %% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
@@ -37,7 +37,7 @@
 %% @deftypefnx {Function File} {@var{h} = } drawArrow (@dots{}, @var{L}, @var{W})
 %% @deftypefnx {Function File} {@var{h} = } drawArrow (@dots{}, @var{L}, @var{W},@var{TYPE})
 %% Draw an arrow on the current axis.
-%%   
+%%
 %%   draw an arrow between the points (@var{x1} @var{y1}) and (@var{x2} @var{y2}).
 %% The points can be given as a single array. @var{L}, @var{W} specify length
 %% and width of the arrow.
@@ -46,7 +46,7 @@
 %%   0: draw only two strokes
 %%   1: fill a triangle
 %%   .5: draw a half arrow (try it to see ...)
-%%   
+%%
 %%   Arguments can be single values or array of size [N*1]. In this case,
 %%   the function draws multiple arrows.
 %%
@@ -61,14 +61,14 @@ function varargout = drawArrow(varargin)
   % parse arrow coordinate
   var = varargin{1};
   if size(var, 2)==4
-       @var{x1} = var(:,1);
-       @var{y1} = var(:,2);
+      x1 = var(:,1);
+      y1 = var(:,2);
       x2 = var(:,3);
       y2 = var(:,4);
       varargin = varargin(2:end);
   elseif length(varargin)>3
-       @var{x1} = varargin{1};
-       @var{y1} = varargin{2};
+      x1 = varargin{1};
+      y1 = varargin{2};
       x2 = varargin{3};
       y2 = varargin{4};
       varargin = varargin(5:end);
@@ -76,31 +76,31 @@ function varargout = drawArrow(varargin)
       error('wrong number of arguments, please read the doc');
   end
 
-  l = 10*size(size( @var{x1}));
-  w = 5*ones(size( @var{x1}));
-  h = zeros(size( @var{x1}));
+  l = 10*size(size(x1));
+  w = 5*ones(size(x1));
+  h = zeros(size(x1));
 
   % exctract length of arrow
   if ~isempty(varargin)
       l = varargin{1};
-      if length( @var{x1})>length(l)
-          l = l(1)*ones(size( @var{x1}));
+      if length(x1)>length(l)
+          l = l(1)*ones(size(x1));
       end
   end
 
   % extract width of arrow
   if length(varargin)>1
       w = varargin{2};
-      if length( @var{x1})>length(w)
-          w = w(1)*ones(size( @var{x1}));
+      if length(x1)>length(w)
+          w = w(1)*ones(size(x1));
       end
   end
 
   % extract 'ratio' of arrow
   if length(varargin)>2
       h = varargin{3};
-      if length( @var{x1})>length(h)
-          h = h(1)*ones(size( @var{x1}));
+      if length(x1)>length(h)
+          h = h(1)*ones(size(x1));
       end
   end
 
@@ -108,7 +108,7 @@ function varargout = drawArrow(varargin)
   axis equal;
 
   % angle of the edge
-  theta = atan2(y2- @var{y1}, x2- @var{x1});
+  theta = atan2(y2-y1, x2-x1);
 
   % point on the 'left'
   xa1 = x2 - l.*cos(theta) - w.*sin(theta)/2;
@@ -121,7 +121,7 @@ function varargout = drawArrow(varargin)
   ya3 = y2 - l.*sin(theta).*h;
 
   % draw main edge
-  line([ @var{x1}'; x2'], [ @var{y1}'; y2'], 'color', [0 0 1]);
+  line([x1'; x2'], [y1'; y2'], 'color', [0 0 1]);
 
   % draw only 2 wings
   ind = find(h==0);
@@ -132,7 +132,7 @@ function varargout = drawArrow(varargin)
   ind = find(h~=0);
   patch([x2(ind) xa1(ind) xa3(ind) xa2(ind) x2(ind)]', ...
       [y2(ind) ya1(ind) ya3(ind) ya2(ind) y2(ind)]', [0 0 1]);
-     
+
 
   if nargout>0
       varargout{1}=h;

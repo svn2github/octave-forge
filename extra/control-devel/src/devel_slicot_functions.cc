67106a9f5f0fdcd8c5cb8fd042dad90b9d3eea39
paramaniac <paramaniac@fc592f45-0881-4044-91ff-c96495a54512>
2012-08-14 20:23:42 +0000

    control-devel: remove system identification stuff
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@10869 fc592f45-0881-4044-91ff-c96495a54512

--- extra/control-devel/src/devel_slicot_functions.cc
+++ extra/control-devel/src/devel_slicot_functions.cc
@@ -1,7 +1,3 @@
-#include "slident.cc"       // system identification
-#include "slib01cd.cc"      // compute initial state vector
-#include "slib01ad.cc"      // compute singular values
-
 #include "slident_a.cc"
 #include "slident_b.cc"
 #include "slident_c.cc"

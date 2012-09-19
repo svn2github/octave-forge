5c1f37118089d10621d9971062befeb893fb6bb8
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-09-19 22:10:39 +0000

    geometry: Ported all content files
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11055 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/geometry/inst/geom3d/polygons3d.m
@@ -0,0 +1,51 @@
+## Copyright (c) 2011, INRA
+## 2004-2011, David Legland <david.legland@grignon.inra.fr>
+## 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
+##
+## All rights reserved.
+## (simplified BSD License)
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+## 1. Redistributions of source code must retain the above copyright notice, this
+##    list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
+##
+## The views and conclusions contained in the software and documentation are
+## those of the authors and should not be interpreted as representing official
+## policies, either expressed or implied, of copyright holder.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} polygons3d ()
+## POLYGONS3D Description of functions operating on 3D polygons
+##
+##   A 3D polygon is simply a set of 3D points (called vertices) which are
+##   assumed to be located in the same plane.
+##   Several functions are provided for computing basic geometrical
+##   parameters (centroid, angles), or intersections with lines or planes.
+##
+## @seealso{polygon3dNormalAngle, polygonCentroid3d, clipConvexPolygon3dHP
+##   intersectLinePolygon3d, intersectLineTriangle3d, intersectRayPolygon3d
+##   drawPolygon3d, drawPolyline3d, fillPolygon3d}
+## @end deftypefn
+function polygons3d()
+
+  help polygons3d
+
+endfunction

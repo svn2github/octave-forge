d4e0b6c1ca2681044863c66918a455ce5531f61d
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-09-22 10:03:17 +0000

    mechanics: All copyrights fixed. hopefully :D
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11071 fc592f45-0881-4044-91ff-c96495a54512

--- main/mechanics/inst/core/EAmatrix.m
+++ main/mechanics/inst/core/EAmatrix.m
@@ -1,17 +1,17 @@
-%%copyright (c) 2011 Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
+%% Copyright (c) 2012 Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 %%
-%%    This program is free software: youcan redistribute itand/or modify
-%%    it under the terms of the GNU General Public Licenseas publishedby
-%%    the Free Software Foundation, either version 3 of the License, or
-%%   any later version.
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 3 of the License, or
+%% (at your option) any later version.
 %%
-%%    This program is distributed in the hope that it willbe useful,
-%%   but WITHOUTaNY WARRANTY; without even the implied warranty of
-%%    MERCHANTABILITY or FITNESS FORa PARTICULAR PURPOSE.  See the
-%%    GNU General Public License for more details.
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%% GNU General Public License for more details.
 %%
-%%    You should have receivedacopy of the GNU General Public License
-%%   along with this program. If not, see <http://www.gnu.org/licenses/>.
+%% You should have received a copy of the GNU General Public License
+%% along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 %% -*- texinfo -*-
 %% @deftypefn {Function File} {@var{output} = } EAmatrix (@var{angles}, @var{convention})

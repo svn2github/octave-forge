71c97a2afd6ff3fef70aaaf52cfad741978e47e8
mmarzolla <mmarzolla@fc592f45-0881-4044-91ff-c96495a54512>
2012-10-03 14:12:59 +0000

    updated documentation
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11199 fc592f45-0881-4044-91ff-c96495a54512

--- main/queueing/inst/qnclosedmultimva.m
+++ main/queueing/inst/qnclosedmultimva.m
@@ -767,3 +767,12 @@ endfunction
 %! legend("location","south");
 %! xlabel("Population mix \\beta for Class 1");
 %! ylabel("Resp. Time");
+
+%!demo
+%! S = [1 15 0.025; 1 15 0.500];
+%! P = zeros(2,3,2,3);
+%! P(1,1,1,3) = P(1,3,1,1) = 1;
+%! P(2,2,2,3) = P(2,3,2,2) = 1;
+%! N = [15 5];
+%! m = [-1 -1 1];
+%! [U R Q X] = qnclosedmultimva(N,S,P,m)

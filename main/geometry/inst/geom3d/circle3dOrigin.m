37bd1cdf0ec4574ef82459d82ee6634e7089a317
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-10-12 00:14:48 +0000

    geometry: porting
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11237 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/geom3d/circle3dOrigin.m
+++ main/geometry/inst/geom3d/circle3dOrigin.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,55 +23,44 @@
 ## OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-function theta = circle3dPosition(point, circle)
-#CIRCLE3DPOSITION Return the angular position of a point on a 3D circle
-#
-#   POS = circle3dPosition(POINT, CIRCLE)
-#   Returns angular position of point on the circle, in degrees, between 0
-#   and 360.
-#   with POINT: [xp yp zp]
-#   and CIRCLE: [X0 Y0 Z0 R THETA PHI] or [X0 Y0 Z0 R THETA PHI PSI]
-#   (THETA being the colatitude, and PHI the azimut)
-#
-#   See also:
-#   circles3d, circle3dOrigin, circle3dPoint
-#
-#   ---------
-#   author : David Legland 
-#   INRA - TPV URPOI - BIA IMASTE
-#   created the 21/02/2005
-#
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{p} =} circle3dOrigin([@var{xc} @var{yc} @var{zc} @var{r} @var{theta} @var{phi}])
+## @deftypefnx {Function File} {@var{p} =} circle3dOrigin([@var{xc} @var{yc} @var{zc} @var{r} @var{theta} @var{phi} @var{psi}])
+## Return the first point of a 3D circle
+##
+##   Returns the origin point of the circle, i.e. the first point used for
+##   drawing circle.
+##
+## @seealso{circles3d, points3d, circle3dPosition}
+## @end deftypefn
 
-#   HISTORY
-#   27/06/2007: change 3D angle convention
-#   2011-06-21 use degrees for angles
+function ori = circle3dOrigin(varargin)
 
+  # get center and radius
+  circle = varargin{1};
+  xc = circle(:,1);
+  yc = circle(:,2);
+  zc = circle(:,3);
+  r  = circle(:,4);
 
-# get center and radius
-xc = circle(:,1);
-yc = circle(:,2);
-zc = circle(:,3);
+  # get angle of normal
+  theta   = circle(:,5);
+  phi     = circle(:,6);
 
-# get angle of normal
-theta   = circle(:,5);
-phi     = circle(:,6);
+  # get roll
+  if size(circle, 2)==7
+      psi = circle(:,7);
+  else
+      psi = zeros(size(circle, 1), 1);
+  end
 
-# find origin of the circle
-ori     = circle3dOrigin(circle);
+  # create origin point
+  pt0 = [r 0 0];
 
-# normal vector of the supporting plane (cartesian coords)
-vn      = sph2cart2d([theta phi]);
+  # compute transformation from local basis to world basis
+  trans   = localToGlobal3d(xc, yc, zc, theta, phi, psi);
 
-# create plane containing the circle
-plane   = createPlane([xc yc zc], vn);
+  # transform the point
+  ori = transformPoint3d(pt0, trans);
 
-# find position of point on the circle plane
-pp0     = planePosition(ori,    plane);
-pp      = planePosition(point,  plane);
-
-# compute angles in the planes
-theta0  = mod(atan2(pp0(:,2), pp0(:,1)) + 2*pi, 2*pi);
-theta   = mod(atan2(pp(:,2), pp(:,1)) + 2*pi - theta0, 2*pi);
-
-# convert to degrees
-theta = theta * 180 / pi;
+endfunction
--- main/geometry/inst/geom3d/circle3dPosition.m
+++ main/geometry/inst/geom3d/circle3dPosition.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,34 +23,48 @@
 ## OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-function varargout = drawSphericalPolygon(sphere, poly, varargin)
-#DRAWSPHERICALPOLYGON  Draw a spherical polygon
-#
-#   output = drawSphericalPolygon(input)
-#
-#   Example
-#   drawSphericalPolygon
-#
-#   See also
-#
-#
-# ------
-# Author: David Legland
-# e-mail: david.legland@grignon.inra.fr
-# Created: 2012-02-09,    using Matlab 7.9.0.529 (R2009b)
-# Copyright 2012 INRA - Cepia Software Platform.
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{pos} =} circle3dPosition(@var{point}, @var{circle})
+## Return the angular position of a point on a 3D circle
+##
+##   Returns angular position of point on the circle, in degrees, between 0
+##   and 360.
+##   with @var{point}: [xp yp zp]
+##   and @var{circle}: [X0 Y0 Z0 R THETA PHI] or [X0 Y0 Z0 R THETA PHI PSI]
+##   (THETA being the colatitude, and PHI the azimut)
+##
+## @seealso{circles3d, circle3dOrigin, circle3dPoint}
+## @end deftypefn
 
-nv = size(poly, 1);
+function theta = circle3dPosition(point, circle)
 
-h = zeros(nv, 1);
-for i = 1:nv
-    v1 = poly(i, :);
-    v2 = poly(mod(i, nv) + 1, :);
+  # get center and radius
+  xc = circle(:,1);
+  yc = circle(:,2);
+  zc = circle(:,3);
 
-    h(i) = drawSphericalEdge(sphere, [v1 v2], varargin{:});
-end
+  # get angle of normal
+  theta   = circle(:,5);
+  phi     = circle(:,6);
 
+  # find origin of the circle
+  ori     = circle3dOrigin(circle);
 
-if nargout > 0
-    varargout = {h};
-end
+  # normal vector of the supporting plane (cartesian coords)
+  vn      = sph2cart2d([theta phi]);
+
+  # create plane containing the circle
+  plane   = createPlane([xc yc zc], vn);
+
+  # find position of point on the circle plane
+  pp0     = planePosition(ori,    plane);
+  pp      = planePosition(point,  plane);
+
+  # compute angles in the planes
+  theta0  = mod(atan2(pp0(:,2), pp0(:,1)) + 2*pi, 2*pi);
+  theta   = mod(atan2(pp(:,2), pp(:,1)) + 2*pi - theta0, 2*pi);
+
+  # convert to degrees
+  theta = theta * 180 / pi;
+
+endfunction

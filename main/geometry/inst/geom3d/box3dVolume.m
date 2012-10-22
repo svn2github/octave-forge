fc1a33e6511ecfe05ff81bc479f6701b53af05e7
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-10-22 16:20:26 +0000

    geometry: porting geom3d
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11329 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/geom3d/box3dVolume.m
+++ main/geometry/inst/geom3d/box3dVolume.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,56 +23,27 @@
 ## OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-function point = circle3dPoint(circle, pos)
-#CIRCLE3DPOINT Coordinates of a point on a 3D circle from its position
-#
-#   output = circle3dPoint(input)
-#
-#   Example
-#   # Draw some points on a 3D circle
-#     figure; hold on;
-#     # origin point
-#     pos1 = 0;
-#     drawPoint3d(circle3dPoint(circle, pos1), 'ro')
-#     # few points regularly spaced
-#     for i = 10:10:40
-#         drawPoint3d(circle3dPoint(circle, i))
-#     end
-#     # Draw point opposite to origin
-#     drawPoint3d(circle3dPoint(circle, 180), 'k*')
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{v} =} box3dVolume (@var{box})
+## Volume of a 3-dimensional box
+##
+##   A box is represented as a set of limits in each direction:
+##   BOX = [XMIN XMAX YMIN YMAX ZMIN ZMAX].
+##
+##   Example
+## @example
+##   [n e f] = createCubeOctahedron;
+##   box = boundingBox3d(n);
+##   vol = box3dVolume(box)
+##   vol =
+##       8
+## @end example
+##
+## @seealso{boxes3d, boundingBox3d}
+## @end deftypefn
 
-#
-#   See also
-#   circles3d, circle3dPosition
-#
-# ------
-# Author: David Legland
-# e-mail: david.legland@grignon.inra.fr
-# Created: 2011-06-21,    using Matlab 7.9.0.529 (R2009b)
-# Copyright 2011 INRA - Cepia Software Platform.
+function vol = box3dVolume(box)
 
-# extract circle coordinates
-xc  = circle(1);
-yc  = circle(2);
-zc  = circle(3);
-r   = circle(4);
+vol = prod(box(:, 2:2:end) - box(:, 1:2:end), 2);
 
-theta   = circle(5);
-phi     = circle(6);
-psi     = circle(7);
-
-# convert position to angle
-t = pos * pi / 180;
-
-# compute position on base circle
-x   = r * cos(t);
-y   = r * sin(t);
-z   = 0;
-pt0 = [x y z];
-
-# compute transformation from local basis to world basis
-trans   = localToGlobal3d(xc, yc, zc, theta, phi, psi);
-
-# compute points of transformed circle
-point   = transformPoint3d(pt0, trans);
-
+endfunction

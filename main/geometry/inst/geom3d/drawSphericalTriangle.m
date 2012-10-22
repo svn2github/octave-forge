10c295f508bce404908507eaaf109e81aed0ff3d
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2012-10-22 17:32:27 +0000

    geometry: porting geom3d
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11332 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/geom3d/drawSphericalTriangle.m
+++ main/geometry/inst/geom3d/drawSphericalTriangle.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,36 +23,36 @@
 ## OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{h} =} function_name (@var{sphere}, @var{p1},@var{p2},@var{p3})
+## Draw a triangle on a sphere
+## @seealso{drawSphere, fillSphericalTriangle, drawSphericalPolygon}
+## @end deftypefn
+
 function varargout = drawSphericalTriangle(sphere, p1, p2, p3, varargin)
-#DRAWSPHERICALTRIANGLE Draw a triangle on a sphere
-#
-#   drawSphericalTriangle(SPHERE, PT1, PT2, PT3);
-#
-#   See also
-#   drawSphere, fillSphericalTriangle, drawSphericalPolygon
-#
-#   ---------
-#   author : David Legland 
-#   INRA - TPV URPOI - BIA IMASTE
-#   created the 22/02/2005
-#
 
-#   HISTORY
-#   27/06/2007 manage spheres other than origin
-#   30/10/2008 replace intersectPlaneLine by intersectLinePlane
+  # extract data of the sphere
+  ori = sphere(:, 1:3);
 
-# extract data of the sphere
-ori = sphere(:, 1:3);
+  # extract direction vectors for each point
+  v1  = normalizeVector (p1 - ori);
+  v2  = normalizeVector (p2 - ori);
+  v3  = normalizeVector (p3 - ori);
 
-# extract direction vectors for each point
-v1  = normalizeVector(p1 - ori);
-v2  = normalizeVector(p2 - ori);
-v3  = normalizeVector(p3 - ori);
+  hold on
+  h1 = drawSphericalEdge (sphere, [v1 v2], varargin{:});
+  h2 = drawSphericalEdge (sphere, [v2 v3], varargin{:});
+  h3 = drawSphericalEdge (sphere, [v3 v1], varargin{:});
+  hold off
 
-h1 = drawSphericalEdge(sphere, [v1 v2], varargin{:});
-h2 = drawSphericalEdge(sphere, [v2 v3], varargin{:});
-h3 = drawSphericalEdge(sphere, [v3 v1], varargin{:});
+  if nargout > 0
+      varargout = {h1, h2, h3};
+  end
 
-if nargout > 0
-    varargout = {h1, h2, h3};
-end
+endfunction
+
+%!demo
+%! p = full(eye(3));
+%! s = [0 0 0 2];
+%! drawSphericalTriangle (s,p(1,:),p(2,:),p(3,:),"r");
+%! view(40,35)

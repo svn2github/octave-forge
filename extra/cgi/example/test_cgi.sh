49d914c0eceed0309f5a2a841e196fa5b12d49f4
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2012-11-19 21:05:28 +0000

    clean-up examples
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11432 fc592f45-0881-4044-91ff-c96495a54512

--- extra/cgi/example/test_cgi.sh
+++ extra/cgi/example/test_cgi.sh
@@ -1,8 +1,8 @@
-DOCUMENT_ROOT=/var/www QUERY_STRING="x=50%2C1,2&y=1,2,3&len=10&name=test&field=random_field" octave <<EOF
-addpath('/home/abarth/Octave/octave-forge/extra/cgi/inst/');
+export DOCUMENT_ROOT=/var/www 
+export REQUEST_METHOD=GET
+export QUERY_STRING="x=50%2C1,2&y=1,2,3&len=10&name=test&field=random_field" 
 
-CGI = cgi()
-cgi.form.y
-
-
+octave -q <<EOF
+CGI = cgi();
+disp(cgi.form.y)
 EOF

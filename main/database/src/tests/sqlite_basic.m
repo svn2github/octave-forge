257bff64d662db5204ef7510ed0f189784c1e0f2
i7tiol <i7tiol@fc592f45-0881-4044-91ff-c96495a54512>
2013-01-16 05:52:13 +0000

    Deleted old database package.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11579 fc592f45-0881-4044-91ff-c96495a54512

--- main/database/src/tests/sqlite_basic.m
+++
@@ -1,8 +0,0 @@
-sqlite3
-
-system("rm -f data.db");
-[res,db]=sqlite3_open("data.db");
-[res,stmt]=sqlite3_prepare(db,"create table some_table (id int4,str varchar);");
-res=sqlite3_step(stmt);
-sqlite3_finalize(stmt);
-sqlite3_close(db);

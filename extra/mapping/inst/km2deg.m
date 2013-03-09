54dbd7f020b7f71bd8dc0e1dc62aae1a41beeffd
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2013-03-09 17:51:15 +0000

    km2deg: implement radius and sphere options
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11713 fc592f45-0881-4044-91ff-c96495a54512

--- extra/mapping/inst/km2deg.m
+++ extra/mapping/inst/km2deg.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2008 Alexander Barth <abarth93@users.sourceforge.net>
+## Copyright (C) 2013 Carnë Draug <carandraug@octave.org>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -14,14 +15,29 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{a} =} km2deg(@var{x})
-## Convert a distance along a great circle of the Earth from km to degrees. A radius
-## of 6371 km is assumed.
+## @deftypefn  {Function File} {@var{deg} =} km2deg (@var{km})
+## @deftypefnx {Function File} {@var{deg} =} km2deg (@var{km}, @var{radius})
+## @deftypefnx {Function File} {@var{deg} =} km2deg (@var{km}, @var{sphere})
+## Convert distance to angle.
+##
+## Calculates the angle @var{deg} for a distance @var{km} in a sphere with
+## @var{radius} (also in kilometers). If unspecified, radius defaults to 6371,
+## the mean radius of Earth.
+##
+## Alternatively, @var{sphere} can be one of "sun", "mercury", "venus", "earth",
+## "moon", "mars", "jupiter", "saturn", "uranus", "neptune", or "pluto", in
+## which case radius will be set to that object mean radius.
 ## @end deftypefn
 
 ## Author: Alexander Barth <barth.alexander@gmail.com>
 
-function a = km2deg (x)
-  Re = 6371;
-  a = 180 * x/(pi*Re);
+function a = km2deg (x, radius = "earth")
+  if (nargin < 1 || nargin > 2)
+    print_usage();
+  elseif (isstr (radius))
+    radius = spheres_radius (obj);
+  elseif (! isnumeric (radius) || ! isscalar (radius))
+    error ("km2deg: RADIUS must be a numeric scalar");
+  endif
+  a = 180 * x/(pi*radius);
 endfunction

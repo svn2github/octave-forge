6ec5b6f1b5a60f712ef1c04d298da90dbbb14a8f
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2013-04-07 13:11:47 +0000

    geometry: removing \r character from text
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11792 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/polynomialCurves2d/polynomialCurveSetFit.m
+++ main/geometry/inst/polynomialCurves2d/polynomialCurveSetFit.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -24,13 +24,13 @@
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{coefs} @var{bnds}]=} polynomialCurveSetFit (@var{img})
+## @deftypefn {Function File} {@var{coefs}=} polynomialCurveSetFit (@var{img})
 ## @deftypefnx {Function File} {@dots{} =} polynomalCurveSetFit (@var{img}, @var{deg})
 ## @deftypefnx {Function File} {[@dots{} @var{lbl}] =} polynomalCurveSetFit (@dots{})
 ## Fit a set of polynomial curves to a segmented image
 ##
 ##   Result is a cell array of matrices. Each matrix is @var{deg}+1-by-2, and
-##   contains coefficients of polynomial curve for each coordinate. 
+##   contains coefficients of polynomial curve for each coordinate.
 ##   @var{bnds} contains the boundary of the parametrizations.
 ##   @var{img} is first binarised, then skeletonized.
 ##
@@ -44,13 +44,12 @@
 ## @seealso{polynomialCurves2d, polynomialCurveFit}
 ## @end deftypefn
 
-function [coefs bnds lblBranches] = polynomialCurveSetFit(seg, varargin)
+function [coefs lblBranches] = polynomialCurveSetFit(seg, varargin)
   # default degree for curves
   deg = 2;
   if ~isempty(varargin)
       deg = varargin{1};
   end
-
 
   # ajoute un contour
   seg([1 end], :) = 1;
@@ -63,18 +62,28 @@ function [coefs bnds lblBranches] = polynomialCurveSetFit(seg, varargin)
   imgNodes = imfilter(double(seg), ones([3 3])) .* seg > 3;
 
   # compute coordinate of nodes, as c entroids of the multiple points
-  lblNodes = bwlabel(imgNodes, 4);
+  lblNodes = bwlabel(imgNodes, 8);
   struct   = regionprops(lblNodes, 'Centroid');
   nodes = zeros(length(struct), 2);
   for i=1:length(struct)
-      nodes(i, 1:2) = struct(i).Centroid;
+      nodes(i, [2 1]) = struct(i).Centroid;
   end
+  figure(1)
+#  subplot(1,2,1)
+#  imshow(seg);
+#  hold on
+#  plot(nodes(:,1),nodes(:,2),'og')
+#  subplot(1,2,2)
+  imshow(imgNodes);
+  hold on
+  plot(nodes(:,1),nodes(:,2),'og')
+  keyboard
 
   # enleve les bords de l'image
   seg([1 end], :) = 0;
   seg(:, [1 end]) = 0;
 
-  # Isoles les branches
+  # Isolate the branches
   imgBranches = seg & ~imgNodes;
   lblBranches = bwlabel(imgBranches, 8);
 
@@ -95,23 +104,23 @@ function [coefs bnds lblBranches] = polynomialCurveSetFit(seg, varargin)
       # extract points corresponding to current curve
       imgBranch = lblBranches == i;
       points    = chainPixels (imgBranch);
-      
+
       # check number of points is sufficient
       if size(points, 1) < max(deg+1-2, 2)
           # find labels of nodes
           inds = unique(lblNodes(imdilate(imgBranch, true (3,3))));
           inds = inds(inds ~= 0);
-          
+
           if length(inds) < 2
               warning ("geometry:poylnomialCurveSetFit", ...
                    ['Could not find extremities of branch number ' num2str(i)]);
               continue;
           end
-          
+
           # consider extremity nodes
           node0 = nodes(inds(1), :);
           node1 = nodes(inds(2), :);
-          
+
           # use only a linear approximation
           xc = zeros(1, deg+1);
           yc = zeros(1, deg+1);
@@ -119,10 +128,9 @@ function [coefs bnds lblBranches] = polynomialCurveSetFit(seg, varargin)
           yc(1) = node0(2);
           xc(2) = node1(1)-node0(1);
           yc(2) = node1(2)-node0(2);
-          
+
           # assigne au tableau de courbes
           coefs{i} = [xc;yc];
-          bnds{i}  = [0 1];
           # next branch
           continue;
       end
@@ -130,23 +138,37 @@ function [coefs bnds lblBranches] = polynomialCurveSetFit(seg, varargin)
       # find nodes closest to first and last points of the current curve
       [dist, ind0] = minDistancePoints(points(1, :), nodes); ##ok<*ASGLU>
       [dist, ind1] = minDistancePoints(points(end, :), nodes);
-      
+
       # add nodes to the curve.
       points = [nodes(ind0,:); points; nodes(ind1,:)]; ##ok<AGROW>
-      
+
       # parametrization of the polyline
       t = parametrize(points);
       t = t / max(t);
-      
+
       # fit a polynomial curve to the set of points
       [xc yc] = polynomialCurveFit(...
           t, points, deg, ...
           0, {points(1,1), points(1,2)},...
           1, {points(end,1), points(end,2)});
-      
+
+
+      plot(points(:,1),points(:,2),'or')
+      hold on
+      drawPolynomialCurve ([0 1], xc,yc);
+      axis tight
+      v = axis();
+      hold off
+      imshow (~imgBranch)
+      hold on
+      plot(points(:,1),points(:,2),'or')
+      drawPolynomialCurve ([0 1], xc,yc);
+      axis xy
+      axis (v);
+      pause
+
       # stores result
       coefs{i} = [xc;yc];
-      bnds{i}  = t([1 end]);
   end
 
 endfunction
@@ -191,7 +213,7 @@ function points = chainPixels(img, varargin)
       for i = 1:size(points, 1)
           # avoid multiple neighbors (can happen in loops)
           ind = ind(1);
-          
+
           # add current point to chained curve
           points(i,:) = [x(ind) y(ind)];
 
@@ -202,12 +224,12 @@ function points = chainPixels(img, varargin)
           # find next candidate
           ind = find(abs(x-points(i,1))<=1 & abs(y-points(i,2))<=1);
       end
-      
+
   else
       for i = 1:size(points, 1)
           # avoid multiple neighbors (can happen in loops)
           ind = ind(1);
-          
+
           # add current point to chained curve
           points(i,:) = [x(ind) y(ind)];
 
@@ -235,7 +257,7 @@ endfunction
 %! hold on
 %! for i=1:numel(c)
 %!   if !isempty (c{i})
-%!     drawPolynomialCurve (t{i}, c{i}(1,:),c{i}(2,:));
+%!     drawPolynomialCurve ([0 1], c{i});
 %!   endif
 %! endfor
 %!

487af8f3546331ed0d634e0d4c0aafc0d0a16058
jpicarbajal <jpicarbajal@fc592f45-0881-4044-91ff-c96495a54512>
2013-04-14 18:31:52 +0000

    gsl: adding NEWS file and removing autoload
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11813 fc592f45-0881-4044-91ff-c96495a54512

--- main/geometry/inst/geom2d/geom2d_Contents.m
+++ main/geometry/inst/geom2d/geom2d_Contents.m
@@ -2,16 +2,16 @@
 ## Copyright (C) 2004-2011 INRA - CEPIA Nantes - MIAJ (Jouy-en-Josas)
 ## Copyright (C) 2012 Adapted to Octave by Juan Pablo Carbajal <carbajal@ifi.uzh.ch>
 ## All rights reserved.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ##     1 Redistributions of source code must retain the above copyright notice,
 ##       this list of conditions and the following disclaimer.
 ##     2 Redistributions in binary form must reproduce the above copyright
 ##       notice, this list of conditions and the following disclaimer in the
 ##       documentation and/or other materials provided with the distribution.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
 ## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE

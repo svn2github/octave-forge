0e9a63273c6b5029dd454286318685aab7e31136
cdf <cdf@fc592f45-0881-4044-91ff-c96495a54512>
2013-04-17 17:21:21 +0000

    apply changes requested by David
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11820 fc592f45-0881-4044-91ff-c96495a54512

--- extra/nurbs/inst/nrbcoons.m
+++ extra/nurbs/inst/nrbcoons.m
@@ -74,7 +74,7 @@ function srf = nrbcoons(u1, u2, v1, v2)
 %
 %    This program is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation, either version 2 of the License, or
+%    the Free Software Foundation, either version 3 of the License, or
 %    (at your option) any later version.
 
 %    This program is distributed in the hope that it will be useful,
--- extra/nurbs/inst/nrbcylind.m
+++ extra/nurbs/inst/nrbcylind.m
@@ -34,7 +34,7 @@ function surf = nrbcylind(height,radius,center,sang,eang)
 %
 %    This program is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation, either version 2 of the License, or
+%    the Free Software Foundation, either version 3 of the License, or
 %    (at your option) any later version.
 
 %    This program is distributed in the hope that it will be useful,

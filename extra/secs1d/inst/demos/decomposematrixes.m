759b0370cd535365017fc4786d240e199ea10db3
dvd7587 <dvd7587@fc592f45-0881-4044-91ff-c96495a54512>
2013-06-11 16:17:02 +0000

    added function for computing coefficinets for resistor example
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@11964 fc592f45-0881-4044-91ff-c96495a54512

---
+++ extra/secs1d/inst/demos/decomposematrixes.m
@@ -0,0 +1,69 @@
+## Copyright (C) 2013 davide
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## [ g, j, r ] = decomposematrixes (A, B, C, dt, x)
+## 
+## Compute coefficients of circuit-device coupling of the form:
+##
+## g * F + j(x) + r * I = 0;
+## 
+## 
+## INPUT
+## A B C : descriptor system 
+## dt : the time step in the backward Euler discretization;
+## x : state variables.
+## 
+## OUTPUT
+## g : coefficient of the patch voltage
+## j : constant bias term
+## r : coefficient of the current in the node
+## 
+## Author: davide <davide@davide-K53SV>
+## Created: 2013-06-10
+
+function [ g, j, r ] = decomposematrixes (A, B, C, dt, x)
+
+freq = 1/dt;
+
+%a{1,1} = A(1,1); % 0 by design
+a{1,2} = A(1,2:end);
+%a{2,1} = A(2:end,1); % 0 by design
+a{2,2} = A(2:end,2:end);
+
+b{1,1} = B(1,1);
+b{1,2} = B(1,2:end);
+b{2,1} = B(2:end,1);
+b{2,2} = B(2:end,2:end);
+
+e{1,1} = b{1,1};
+e{1,2} = a{1,2}*freq+b{1,2};
+e{2,1} = b{2,1};
+e{2,2} = a{2,2}*freq+b{2,2};
+f{1} = C(1);
+f{2} = C(2:end);
+
+if (det(e{2,2}) == 0)
+  g = 1;
+  j = -1;
+  r = 0;
+else
+  e22inv = inv(e{2,2});
+  g = e{1,1} - e{1,2}*e22inv*e{2,1};
+  j = f{1} - e{1,2}*e22inv*f{2} + freq*(-a{1,2} + e{1,2}*e22inv*a{2,2})*x;
+  r = 1;
+endif
+return
+endfunction

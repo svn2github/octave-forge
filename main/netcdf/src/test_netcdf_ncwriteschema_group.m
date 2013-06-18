2d5df29ef1a4653ded9e31b7d6a3e7f16e9dd91c
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2013-06-18 08:19:00 +0000

    add group test
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12016 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/netcdf/src/test_netcdf_ncwriteschema_group.m
@@ -0,0 +1,39 @@
+
+fname = [tempname '-octave-netcdf-scheme-group.nc'];
+
+clear s
+s.Name   = '/';
+s.Format = 'netcdf4';
+s.Dimensions(1).Name   = 'lon';
+s.Dimensions(1).Length = 20;
+s.Dimensions(2).Name   = 'lat';
+s.Dimensions(2).Length = 10;
+
+s.Attributes(1).Name = 'institution';
+s.Attributes(1).Value = 'GHER, ULg';
+
+s.Groups(1).Name = 'forecast';
+s.Groups(1).Variables(1).Name = 'temp';
+s.Groups(1).Variables(1).Dimensions = s.Dimensions;
+s.Groups(1).Variables(1).Datatype = 'double';
+s.Groups(1).Variables(1).Attributes(1).Name = 'long_name';
+s.Groups(1).Variables(1).Attributes(1).Value = 'temperature';
+
+s.Groups(2) = s.Groups(1);
+s.Groups(2).Name = 'analysis';
+
+ncwriteschema(fname,s);
+%system(['ncdump -h ' fname])
+
+info = ncinfo(fname);
+assert(strcmp(info.Attributes(1).Name,s.Attributes(1).Name))
+assert(strcmp(info.Attributes(1).Value,s.Attributes(1).Value))
+
+assert(strcmp(s.Groups(1).Name,info.Groups(1).Name))
+
+z = randn(20,10);
+ncwrite(fname,'/forecast/temp',z);
+z2 = ncread(fname,'/forecast/temp');
+assert(isequal(z,z2))
+
+delete(fname);

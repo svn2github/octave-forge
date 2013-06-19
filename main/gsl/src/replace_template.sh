087c4ea050f0db1eeed9346c837354995b78d4f5
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2013-06-19 06:33:28 +0000

    gsl: moved to a separate mercurial repo
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12025 fc592f45-0881-4044-91ff-c96495a54512

--- main/gsl/src/replace_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sed -e 's/\\/\\\\/g;s/$/\\n\\/g' < docstring.txt > docstring2.txt
-sed -e "s/GSL_OCTAVE_NAME/$octave_name/g;s/GSL_FUNC_NAME/$funcname/g;/GSL_FUNC_DOCSTRING/r docstring2.txt" -e "/GSL_FUNC_DOCSTRING/d" $1
-rm -f docstring.txt docstring2.txt

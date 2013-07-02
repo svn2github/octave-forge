75b230164f17a629cb2b286365141d048e85d589
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2013-07-02 19:54:08 +0000

    source code restructuration (3)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12095 fc592f45-0881-4044-91ff-c96495a54512

--- main/netcdf/inst/test_netcdf.m
+++ main/netcdf/inst/test_netcdf.m
@@ -132,19 +132,19 @@ assert(all(vari2 == vari))
 netcdf.close(ncid);
 delete(fname);
 
-test_netcdf_package_type('byte','int8');
-test_netcdf_package_type('ubyte','uint8');
-test_netcdf_package_type('short','int16');
-test_netcdf_package_type('ushort','uint16');
-test_netcdf_package_type('int','int32');
-test_netcdf_package_type('uint','uint32');
-test_netcdf_package_type('int64','int64');
-test_netcdf_package_type('uint64','uint64');
+test_netcdf_type('byte','int8');
+test_netcdf_type('ubyte','uint8');
+test_netcdf_type('short','int16');
+test_netcdf_type('ushort','uint16');
+test_netcdf_type('int','int32');
+test_netcdf_type('uint','uint32');
+test_netcdf_type('int64','int64');
+test_netcdf_type('uint64','uint64');
 
-test_netcdf_package_type('double','double');
-test_netcdf_package_type('float','single');
+test_netcdf_type('double','double');
+test_netcdf_type('float','single');
 
-test_netcdf_package_type('char','char');
+test_netcdf_type('char','char');
 
 
 test_scalar_variable

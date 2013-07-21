602fbfd247993591e5a4a35bdb2a2804a67f2cce
cdf <cdf@fc592f45-0881-4044-91ff-c96495a54512>
2013-07-21 18:45:00 +0000

    fix computation of current densities
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12134 fc592f45-0881-4044-91ff-c96495a54512

--- extra/secs1d/inst/secs3d_coupled_circuit_newton.m
+++ extra/secs1d/inst/secs3d_coupled_circuit_newton.m
@@ -440,27 +440,25 @@ function [Jn, Jp] = compute_currents ...
                       (device, material, constants, algorithm, 
                        mobilityn, mobilityp, V, n, p, Fn, Fp)
 
-  [Ex,Ey,Ez] = bim3c_pde_gradient(device.msh, -V / constants.Vth);
-  [dndx,dndy,dndz] = bim3c_pde_gradient(device.msh, n);
-  [dpdx,dpdy,dpdz] = bim3c_pde_gradient(device.msh, p);
+  [Fnx,Fny,Fnz] = bim3c_pde_gradient (device.msh, Fn);
+  [Fpx,Fpy,Fpz] = bim3c_pde_gradient (device.msh, Fp);
+
   nelemental = sum (n(device.msh.t(1:4, :)), 1)(:) / 4;
   pelemental = sum (p(device.msh.t(1:4, :)), 1)(:) / 4;
 
-  Jn.x = constants.q * constants.Vth * mobilityn .* ...
-        (dndx(:) + Ex(:) .* nelemental) ;
-  Jn.y = constants.q * constants.Vth * mobilityn .* ...
-        (dndy(:) + Ey(:) .* nelemental) ; 
-  Jn.z = constants.q * constants.Vth * mobilityn .* ...
-        (dndz(:) + Ez(:) .* nelemental) ; 
-  Jn.mag = sqrt(Jn.x .^2 + Jn.y .^2 + Jn.z .^2);
+  tmp  = -constants.q * mobilityn .* nelemental;
+  Jn.x = tmp * Fnx;
+  Jn.y = tmp * Fny; 
+  Jn.z = tmp * Fnz;
+ 
+  Jn.mag = sqrt (Jn.x .^2 + Jn.y .^2 + Jn.z .^2);
 
-  Jp.x = constants.q * constants.Vth * mobilityp .* ...
-        (- dpdx(:) + Ex(:) .* pelemental) ; 
-  Jp.y = constants.q * constants.Vth * mobilityp .* ...
-        (- dpdx(:) + Ey(:) .* pelemental) ; 
-  Jp.z = constants.q * constants.Vth * mobilityp .* ...
-        (- dpdz(:) + Ez(:) .* pelemental) ; 
-  Jp.mag = sqrt(Jp.x .^2 + Jp.y .^2 + Jp.z .^2);
+  tmp  = -constants.q * mobilityp .* pelemental;
+  Jp.x = tmp * Fpx;
+  Jp.y = tmp * Fpy;
+  Jp.z = tmp * Fpz;
+
+  Jp.mag = sqrt (Jp.x .^2 + Jp.y .^2 + Jp.z .^2);
   
 endfunction
 

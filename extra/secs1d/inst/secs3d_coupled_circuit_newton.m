05ef67db71a3d1cc6b2c3352aa5774382e714138
cdf <cdf@fc592f45-0881-4044-91ff-c96495a54512>
2013-07-21 20:34:27 +0000

    fix computation of current densities
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12135 fc592f45-0881-4044-91ff-c96495a54512

--- extra/secs1d/inst/secs3d_coupled_circuit_newton.m
+++ extra/secs1d/inst/secs3d_coupled_circuit_newton.m
@@ -447,16 +447,16 @@ function [Jn, Jp] = compute_currents ...
   pelemental = sum (p(device.msh.t(1:4, :)), 1)(:) / 4;
 
   tmp  = -constants.q * mobilityn .* nelemental;
-  Jn.x = tmp * Fnx;
-  Jn.y = tmp * Fny; 
-  Jn.z = tmp * Fnz;
+  Jn.x = tmp(:) .* Fnx(:);
+  Jn.y = tmp(:) .* Fny(:); 
+  Jn.z = tmp(:) .* Fnz(:);
  
   Jn.mag = sqrt (Jn.x .^2 + Jn.y .^2 + Jn.z .^2);
 
   tmp  = -constants.q * mobilityp .* pelemental;
-  Jp.x = tmp * Fpx;
-  Jp.y = tmp * Fpy;
-  Jp.z = tmp * Fpz;
+  Jp.x = tmp(:) .* Fpx(:);
+  Jp.y = tmp(:) .* Fpy(:);
+  Jp.z = tmp(:) .* Fpz(:);
 
   Jp.mag = sqrt (Jp.x .^2 + Jp.y .^2 + Jp.z .^2);
   

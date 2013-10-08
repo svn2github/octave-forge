d9d6895adb888be4de29fcc5e2ce37885785f687
i7tiol <i7tiol@fc592f45-0881-4044-91ff-c96495a54512>
2013-10-08 13:54:46 +0000

    support viewing large objects with viewers needing a temporary file
    * inst/pq_lo_view.m: New function file.
    * src/__pq_internal_exit__.cc: New file.
    * src/Makefile.in: Include __pq_internal_exit__.cc.
    * NEWS, INDEX, DESCRIPTION: Respective changes.
    
    
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12283 fc592f45-0881-4044-91ff-c96495a54512

---
+++ main/database/src/__pq_internal_exit__.cc
@@ -0,0 +1,38 @@
+// Copyright (C) 2008, 2013 Olaf Till <olaf.till@uni-jena.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+#include <octave/oct.h>
+
+#include <unistd.h>
+#include <signal.h>
+
+// PKG_ADD: autoload ("__pq_internal_exit__", "pq_interface.oct");
+
+// This function duplicates __exit__.cc to avoid the dependency on the
+// package main/general.
+
+DEFUN_DLD (__pq_internal_exit__, args, , 
+  "-*- texinfo -*-\n\
+@deftypefn {Loadable Function} __pq_internal_exit__ (status)\n\
+This is a wrapper over the POSIX _exit() system call. Calling this function\n\
+will terminate the running process immediatelly, bypassing normal Octave\n\
+terminating sequence. It is suitable to terminate a forked process. It\n\
+should be considered expert-only and not to be used in normal code.\n\
+@end deftypefn") 
+{
+  _exit (args.length () > 0 ? args(0).int_value () : 0);
+}

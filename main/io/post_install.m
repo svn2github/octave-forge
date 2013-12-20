f0da130c030bc4f84e4aae001b2dcb947330f112
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2013-12-20 22:23:23 +0000

    Commented out unlink command; it appears this file isn't required anymore
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12424 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/post_install.m
+++ main/io/post_install.m
@@ -4,10 +4,13 @@
 ## .m directory (since chk_spreadsheet_support is there), when called from the
 ## .oct directory it fails since the package hasn't been loaded yet
 
+## Update 2013-12-20: Doesn't seem to be needed; io package loads OK and properly
+##                    adds java class libs to the javaclasspath => unlink commented out
+
 function post_install (desc)
   file = cstrcat (desc.archprefix, filesep, octave_config_info ("canonical_host_type"),
                   "-", octave_config_info ("api_version"), filesep, "PKG_ADD");
-  [err, msg] = unlink (file);
+  ## [err, msg] = unlink (file);  ## Currently commented out
   if (err)
     warning ("Unable to remove PKG_ADD: %s", msg);
     printf ("For spreadsheet I/O you may need to manually add the required\n", ... 

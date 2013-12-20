4aa39775567b6dea95c62d05f34ce308e5eb2822
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2013-12-20 12:45:45 +0000

    Add option to test with different interfaces for writing/reading
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12389 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/io_ods_testscript.m
+++ main/io/inst/io_ods_testscript.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2012 Philip Nienhuis <pr.nienhuis at users.sf.net>
+## Copyright (C) 2012,2013 Philip Nienhuis <pr.nienhuis at users.sf.net>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -14,113 +14,138 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## (Internal function) Check proper operation of ODS spreadsheet scripts.
-## Before running, a character variable 'intf' should be initialized with
-## a value of 'otk', 'jod', or 'uno'.
 
 ## Author: Philip Nienhuis
 ## Created: 2012-02-25
 ## Updates:
 ## 2012-09-04 Add small delay between LibreOffice calls to avoid lock-ups with UNO
+## 2013-04-21 Made it into a function
+## 2013-12-18 Add option to write and read with different interfaces (needed for OCT)
+##     ''     Catch more erroneous read-back results
 
-printf ("\nTesting .ods interface %s ...\n", intf);
+function io_ods_testscript (intf, fname, intf2='')
 
-isuno = false; dly = 0.25;
-if (strcmp (lower (intf), 'uno'));
-  isuno = true;
-endif
-
-## 1. Initialize test arrays
-printf ("\n 1. Initialize arrays.\n");
-arr1 = [ 1 2; 3 4.5];
-arr2 = {'r1c1', '=c2+d2'; '', 'r2c2'; true, -83.4};
-opts = struct ("formulas_as_text", 0);
-
-## 2. Insert empty sheet
-printf ("\n 2. Insert first empty sheet.\n");
-odswrite ('io-test.ods', {''}, 'EmptySheet', 'b4', intf); if (isuno); sleep (dly); endif
-
-## 3. Add data to test sheet
-printf ("\n 3. Add data to test sheet.\n");
-odswrite ('io-test.ods', arr1, 'Testsheet', 'c2:d3', intf); if (isuno); sleep (dly); endif
-odswrite ('io-test.ods', arr2, 'Testsheet', 'd4:z20', intf); if (isuno); sleep (dly); endif
-
-## 4. Insert another sheet
-printf ("\n 4. Add another sheet with just one number in A1.\n");
-odswrite ('io-test.ods', [1], 'JustOne', 'A1', intf); if (isuno); sleep (dly); endif
-
-## 5. Get sheet info & find sheet with data and data range
-printf ("\n 5. Explore sheet info.\n");
-[~, shts] = odsfinfo ('io-test.ods', intf); if (isuno); sleep (dly); endif
-shnr = strmatch ('Testsheet', shts(:, 1));                      # Note case!
-crange = shts{shnr, 2};
-
-## 6. Read data back
-printf ("\n 6. Read data back.\n");
-[num, txt, raw, lims] = odsread ('io-test.ods', shnr, crange, intf); if (isuno); sleep (dly); endif
-
-## 7. Here come the tests, part 1
-printf ("\n 7. Tests part 1 (basic I/O):\n");
-try
-  printf ("    ...Numeric array... ");
-  assert (num(1:2, 1:3), [1, 2, NaN; 3, 4.5, NaN], 1e-10);
-  assert (num(4:5, 1:3), [NaN, NaN, NaN; NaN, 1, -83.4], 1e-10);
-  assert (num(3, 1:2), [NaN, NaN], 1e-10);
-  # Just check if it's numeric, the value depends too much on cached results
-  assert (isnumeric (num(3,3)), true);
-  printf ("matches.\n");
-catch
-  printf ("Hmmm.... error, see 'num'\n");
-  keyboard
-end_try_catch
-try
-  printf ("    ...Cellstr array... ");
-  assert (txt{1, 1}, 'r1c1');
-  assert (txt{2, 2}, 'r2c2');
-  printf ("matches.\n");
-catch
-  printf ("Hmmm.... error, see 'txt'\n"); 
-  keyboard
-end_try_catch
-try
-  printf ("    ...Boolean... "); 
-  assert (islogical (raw{5, 2}), true);                         # Fails on JOD
-  printf ("recovered.\n");
-catch
-  if (isnumeric (raw{5, 2}))
-    printf ("recovered as numeric '1' rather than logical TRUE\n");
-  else
-    printf ("Hmmm.... error, see 'raw'\n");
-    keyboard
+  printf ("\nTesting .ods interface %s using file %s...\n", upper (intf), fname);
+  
+  isuno = false; dly = 0.25;
+  if (strcmpi (intf, 'oct'))
+    if (isempty (intf2))
+      intf2 = 'otk';
+    endif
   endif
-end_try_catch
-
-## Check if formulas_as_text works:
-printf ("\n 8. Repeat reading, now return formulas as text\n");
-opts.formulas_as_text = 1;
-ods = odsopen ('io-test.ods', 0, intf); if (isuno); sleep (dly); endif
-raw = ods2oct (ods, shnr, crange, opts); if (isuno); sleep (dly); endif
-ods = odsclose (ods); if (isuno); sleep (dly); endif
-clear ods;
-
-## 9. Here come the tests, part 2. Fails on COM
-printf ("\n 9. Tests part 2 (read back formula):\n");
-
-try
-  # Just check if it contains any string
-  assert ( (ischar (raw{3, 3}) && ~isempty (raw(3, 3))), true); 
-  printf ("    ...OK, formula recovered ('%s').\n", raw{3, 3});
-catch
-  printf ("Hmmm.... error, see 'raw(3, 3)'");
-  if (isnumeric (raw{3, 3}))
-    printf (" (equals %f, should be a string like '=c2+d2')\n", raw{3, 3}); 
+  if (isempty (intf2))
+    intf2 = intf;
   else
-    printf ("\n"); 
-    keyboard
+    printf ("(Writing files is done with interface %s)\n", upper (intf2));
   endif
-end_try_catch
+  if (strcmpi (intf, "uno") || strcmpi (intf2, "uno"));
+    isuno = true;
+  endif
+  
+  ## 1. Initialize test arrays
+  printf ("\n 1. Initialize arrays.\n");
+  arr1 = [ 1 2; 3 4.5];
+  arr2 = {'r1c1', '=c2+d2'; '', 'r2c2'; true, -83.4};
+  opts = struct ("formulas_as_text", 0);
+  
+  ## 2. Insert empty sheet
+  printf ("\n 2. Insert first empty sheet.\n");
+  odswrite (fname, {''}, 'EmptySheet', 'b4', intf2); if (isuno); sleep (dly); endif
+  
+  ## 3. Add data to test sheet
+  printf ("\n 3. Add data to test sheet.\n");
+  odswrite (fname, arr1, 'Testsheet', 'c2:d3', intf2); if (isuno); sleep (dly); endif
+  odswrite (fname, arr2, 'Testsheet', 'd4:z20', intf2); if (isuno); sleep (dly); endif
+  
+  ## 4. Insert another sheet
+  printf ("\n 4. Add another sheet with just one number in A1.\n");
+  odswrite (fname, [1], 'JustOne', 'A1', intf2); if (isuno); sleep (dly); endif
+  
+  ## 5. Get sheet info & find sheet with data and data range
+  printf ("\n 5. Explore sheet info.\n");
+  [~, shts] = odsfinfo (fname, intf); if (isuno); sleep (dly); endif
+  shnr = strmatch ('Testsheet', shts(:, 1));                      # Note case!
+  crange = shts{shnr, 2};
 
-## 10. Clean up
-printf ("\n10. Cleaning up.....");
-delete 'io-test.ods';
-clear arr1 arr2 ods num txt raw lims opts shnr shts crange;
-printf (" OK\n");
+  ## 6. Read data back
+  printf ("\n 6. Read data back.\n");
+  [num, txt, raw, lims] = odsread (fname, shnr, crange, intf); if (isuno); sleep (dly); endif
+  
+  ## First check: has anything been read at all?x
+  if (isempty (raw))
+    printf ("No data at all have been read... test failed.\n");
+    return
+  elseif (isempty (num))
+    printf ("No numeric data have been read... test failed.\n");
+    return
+  elseif (isempty (txt))
+    printf ("No text data have been read... test failed.\n");
+    return
+  endif
+
+  ## 7. Here come the tests, part 1
+  printf ("\n 7. Tests part 1 (basic I/O):\n");
+  try
+    printf ("    ...Numeric array... ");
+    assert (num(1:2, 1:3), [1, 2, NaN; 3, 4.5, NaN], 1e-10);
+    assert (num(4:5, 1:3), [NaN, NaN, NaN; NaN, 1, -83.4], 1e-10);
+    assert (num(3, 1:2), [NaN, NaN], 1e-10);
+    # Just check if it's numeric, the value depends too much on cached results
+    assert (isnumeric (num(3,3)), true);
+    printf ("matches.\n");
+  catch
+    printf ("Hmmm.... error, see 'num'\n");
+  end_try_catch
+  try
+    printf ("    ...Cellstr array... ");
+    assert (txt{1, 1}, 'r1c1');
+    assert (txt{2, 2}, 'r2c2');
+    printf ("matches.\n");
+  catch
+    printf ("Hmmm.... error, see 'txt'\n"); 
+  end_try_catch
+  try
+    printf ("    ...Boolean... "); 
+    assert (islogical (raw{5, 2}), true);                ## Fails on JOD
+    printf ("recovered.\n");
+  catch
+    if (isnumeric (raw{5, 2}))
+      printf ("recovered as numeric '1' rather than logical TRUE\n");
+    else
+      printf ("Hmmm.... error, see 'raw'\n");
+    endif
+  end_try_catch
+
+  ## Check if formulas_as_text works:
+  printf ("\n 8. Repeat reading, now return formulas as text\n");
+  opts.formulas_as_text = 1;
+  ods = odsopen (fname, 0, intf); if (isuno); sleep (dly); endif
+  raw = ods2oct (ods, shnr, crange, opts); if (isuno); sleep (dly); endif
+  ods = odsclose (ods); if (isuno); sleep (dly); endif
+  clear ods;
+  
+  ## 9. Here come the tests, part 2. Fails on COM
+  printf ("\n 9. Tests part 2 (read back formula):\n");
+  
+  try
+    # Just check if it contains any string
+    assert ( (ischar (raw{3, 3}) && ~isempty (raw(3, 3))), true); 
+    printf ("    ...OK, formula recovered ('%s').\n", raw{3, 3});
+  catch
+    printf ("Hmmm.... error, see 'raw(3, 3)'");
+    if (isempty (raw{3, 3}))
+      printf (" (<empty>, should be a string like '=c2+d2')\n");
+    elseif (isnumeric (raw{3, 3}))
+      printf (" (equals %f, should be a string like '=c2+d2')\n", raw{3, 3}); 
+    else
+      printf ("\n"); 
+    endif
+  end_try_catch
+  
+  ## 10. Clean up
+  printf ("\n10. Cleaning up.....");
+  delete (fname);
+  clear arr1 arr2 ods num txt raw lims opts shnr shts crange;
+  printf (" OK\n");
+  
+  endfunction

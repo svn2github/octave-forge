8aacdae008e9c340643d721edd4a238a2176a853
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2013-12-20 12:54:43 +0000

    Work around jOpenDocument bug adding "A" to A1, "B" to B1 in new files; style fixes
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12397 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/inst/private/__JOD_oct2spsh__.m
+++ main/io/inst/private/__JOD_oct2spsh__.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2009,2010,2011,2012 Philip Nienhuis <pr.nienhuis at users.sf.net>
+## Copyright (C) 2009,2010,2011,2012,2013 Philip Nienhuis
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -16,7 +16,7 @@
 ## __JOD_oct2spsh__ - write data from octave to an ODS spreadsheet using the
 ## jOpenDocument interface.
 ##
-## Author: Philip Nienhuis
+## Author: Philip Nienhuis <pr.nienhuis at users.sf.net>
 ## Created: 2009-12-13
 ## First usable version: 2010-01-14
 ## Updates:
@@ -40,13 +40,20 @@
 ## 2012-02-26 Write logicals as doubles (bug in jOpenDocument, would write as text)
 ## 2012-10-12 Renamed & moved into ./private
 ## 2012-10-24 Style fixes
+## 2013-12-01 Style fixes, copyright string updates
+## 2013-12-19 More style fixes
+##     ''     Work around jOpenDocument bug adding "A" to A1, "B" to B1 in new files
 
 function [ ods, rstatus ] = __JOD_oct2spsh__ (c_arr, ods, wsh, crange)
 
-  rstatus = 0; sh = []; changed = 0;
+  rstatus = 0; 
+  sh = []; 
+  changed = 0;
 
   ## Get worksheet. Use first one if none given
-  if (isempty (wsh)) wsh = 1; endif
+  if (isempty (wsh))
+    wsh = 1;
+  endif
   sh_cnt = ods.workbook.getSheetCount ();
   if (isnumeric (wsh))
     if (wsh > 1024)
@@ -83,9 +90,17 @@ function [ ods, rstatus ] = __JOD_oct2spsh__ (c_arr, ods, wsh, crange)
           sh = ods.workbook.addSheet (sh_cnt, wsh);
           changed = 1;
         endif
+        ## jOpenDocument bug: JOD seems to add "A" to A1 and "B" to B1
+        try
+          if (! isempty (sh.getCellAt (0, 0).getValue))
+            sh.getCellAt (0, 0).clearValue();
+            sh.getCellAt (1, 0).clearValue();
+          endif
+        catch
+        end_try_catch
       else
         error (["jOpenDocument v. 1.2b2 does not support adding sheets" ...
-                " - upgrade to v. 1.2b3\n"]);
+                " - upgrade to v. 1.3\n"]);
       endif
     endif
   endif
@@ -97,33 +112,39 @@ function [ ods, rstatus ] = __JOD_oct2spsh__ (c_arr, ods, wsh, crange)
     nrows = nr;
     ncols = nc;
   elseif (isempty (strfind (deblank (crange), ":"))) 
-    [dummy1, dummy2, dummy3, trow, lcol] = parse_sp_range (crange);
+    [~, ~, ~, trow, lcol] = parse_sp_range (crange);
     nrows = nr;
     ncols = nc;
     ## Row/col = 0 based in jOpenDocument
-    trow = trow - 1; lcol = lcol - 1;
+    trow = trow - 1; 
+    lcol = lcol - 1;
   else
-    [dummy, nrows, ncols, trow, lcol] = parse_sp_range (crange);
+    [~, nrows, ncols, trow, lcol] = parse_sp_range (crange);
     ## Row/col = 0 based in jOpenDocument
-    trow = trow - 1; lcol = lcol - 1;
+    trow = trow - 1; 
+    lcol = lcol - 1;
   endif
 
   if (trow > 65535 || lcol > 1023)
     error ("Topleft cell beyond spreadsheet limits (AMJ65536).");
   endif
   ## Check spreadsheet capacity beyond requested topleft cell
-  nrows = min (nrows, 65536 - trow);    ## Remember, lcol & trow are zero-based
+  nrows = min (nrows, 65536 - trow);      ## Remember, lcol & trow are zero-based
   ncols = min (ncols, 1024 - lcol);
   ## Check array size and requested range
   nrows = min (nrows, nr);
   ncols = min (ncols, nc);
-  if (nrows < nr || ncols < nc) warning ("Array truncated to fit in range"); endif
+  if (nrows < nr || ncols < nc)
+    warning ("Array truncated to fit in range");
+  endif
 
-  if (isnumeric (c_arr)) c_arr = num2cell (c_arr); endif
+  if (isnumeric (c_arr))
+    c_arr = num2cell (c_arr);
+  endif
 
   ## Ensure sheet capacity is large enough to contain new data
   try    ## try-catch needed to work around bug in jOpenDocument v 1.2b3 and earlier
-    sh.ensureColumnCount (lcol + ncols);  # Remember, lcol & trow are zero-based
+    sh.ensureColumnCount (lcol + ncols);  ## Remember, lcol & trow are zero-based
   catch  ## catch is needed for new empty sheets (first ensureColCnt() hits null ptr)
     sh.ensureColumnCount (lcol + ncols);
     ## Kludge needed because upper row is defective (NPE jOpenDocument bug). ?Fixed in 1.2b4?
@@ -139,9 +160,11 @@ function [ ods, rstatus ] = __JOD_oct2spsh__ (c_arr, ods, wsh, crange)
   for ii = 1 : nrows
     for jj = 1 : ncols
       val = c_arr {ii, jj};
-      if ((isnumeric (val) && ~isnan (val)) || ischar (val) || islogical (val))
+      if ((isnumeric (val) && ! isnan (val)) || ischar (val) || islogical (val))
         ## FIXME: jOpenDocument doesn't really support writing booleans (doesn't set OffValAttr)
-        if (islogical (val)); val = double (val); endif
+        if (islogical (val))
+          val = double (val);
+        endif
         try
           sh.getCellAt (jj + lcol - 1, ii + trow - 1).clearValue();
           jcell = sh.getCellAt (jj + lcol - 1, ii + trow - 1).setValue (val);

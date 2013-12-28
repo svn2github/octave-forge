a43430ded5d93c21a13d94bc613acac666d8e1d7
prnienhuis <prnienhuis@fc592f45-0881-4044-91ff-c96495a54512>
2013-12-28 16:07:09 +0000

    Provisionally commented out all effective code (file may not be needed anymore)
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12426 fc592f45-0881-4044-91ff-c96495a54512

--- main/io/post_install.m
+++ main/io/post_install.m
@@ -4,16 +4,18 @@
 ## .m directory (since chk_spreadsheet_support is there), when called from the
 ## .oct directory it fails since the package hasn't been loaded yet
 
-## Update 2013-12-20: Doesn't seem to be needed; io package loads OK and properly
-##                    adds java class libs to the javaclasspath => unlink commented out
+## Updates:
+## 2013-12-20 Doesn't seem to be needed; io package loads OK and properly
+##            adds java class libs to the javaclasspath => unlink commented out
+## 2013-12-28 Provisionally commented out all effective code
 
 function post_install (desc)
-  file = cstrcat (desc.archprefix, filesep, octave_config_info ("canonical_host_type"),
-                  "-", octave_config_info ("api_version"), filesep, "PKG_ADD");
-  ## [err, msg] = unlink (file);  ## Currently commented out
-  if (err)
-    warning ("Unable to remove PKG_ADD: %s", msg);
-    printf ("For spreadsheet I/O you may need to manually add the required\n", ... 
-            "Java class libs to the javaclasspath");
-  endif
+#  file = cstrcat (desc.archprefix, filesep, octave_config_info ("canonical_host_type"),
+#                  "-", octave_config_info ("api_version"), filesep, "PKG_ADD");
+#  ## [err, msg] = unlink (file);  ## Currently commented out
+#  if (err)
+#    warning ("Unable to remove PKG_ADD: %s", msg);
+#    printf ("For spreadsheet I/O you may need to manually add the required\n", ... 
+#            "Java class libs to the javaclasspath");
+#  endif
 endfunction

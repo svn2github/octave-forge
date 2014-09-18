939ef464b10c4ef4ed7ad254fb7b37327a0e77d6
abarth93 <abarth93@fc592f45-0881-4044-91ff-c96495a54512>
2014-09-18 14:59:30 +0000

    close netcdf file in test test_netcdf_attributes.m
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12709 fc592f45-0881-4044-91ff-c96495a54512

--- main/netcdf/inst/private/test_netcdf_attributes.m
+++ main/netcdf/inst/private/test_netcdf_attributes.m
@@ -1,4 +1,4 @@
-%% Copyright (C) 2013 Alexander Barth
+%% Copyright (C) 2013-2014 Alexander Barth
 %%
 %% This program is free software; you can redistribute it and/or modify
 %% it under the terms of the GNU General Public License as published by
@@ -287,6 +287,7 @@ assert(strcmp(name,'x'));
 netcdf.renameDim(ncid,dimid,'y');
 [name,len] = netcdf.inqDim(ncid,dimid);
 assert(strcmp(name,'y'));
+netcdf.close(ncid);
 delete(fname);
 
 

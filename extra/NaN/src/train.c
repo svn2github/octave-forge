f55a1d625a4cc6ebcb32bcf1b0a8f0bdf7e678c2
schloegl <schloegl@fc592f45-0881-4044-91ff-c96495a54512>
2015-04-12 19:00:36 +0000

    [nan] fix liblinear - use train/predict from liblinear/matlab
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12778 fc592f45-0881-4044-91ff-c96495a54512

--- extra/NaN/src/train.c
+++ extra/NaN/src/train.c
@@ -28,18 +28,29 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <errno.h>
 #include "linear.h"
+
+#include "mex.h"
+#include "linear_model_matlab.h"
+
+#ifdef tmwtypes_h
+  #if (MX_API_VER<=0x07020000)
+    typedef int mwIndex;
+  #endif 
+#endif 
+
+#define CMD_LEN 2048
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 #define INF HUGE_VAL
 
 void print_null(const char *s) {}
+void print_string_matlab(const char *s) {mexPrintf(s);}
 
 void exit_with_help()
 {
-	printf(
-	"Usage: train [options] training_set_file [model_file]\n"
-	"options:\n"
+	mexPrintf(
+	"Usage: model = train(training_label_vector, training_instance_matrix, 'liblinear_options', 'col');\n"
+	"liblinear_options:\n"
 	"-s type : set type of solver (default 1)\n"
 	"	0 -- L2-regularized logistic regression (primal)\n"
 	"	1 -- L2-regularized L2-loss support vector classification (dual)\n"	
@@ -64,108 +75,47 @@ void exit_with_help()
 	"-wi weight: weights adjust the parameter C of different classes (see README for details)\n"
 	"-v n: n-fold cross validation mode\n"
 	"-q : quiet mode (no outputs)\n"
+	"col:\n"
+	"	if 'col' is setted, training_instance_matrix is parsed in column format, otherwise is in row format\n"
 	);
-	exit(1);
 }
 
-void exit_input_error(int line_num)
-{
-	fprintf(stderr,"Wrong input format at line %d\n", line_num);
-	exit(1);
-}
-
-static char *line = NULL;
-static int max_line_len;
-
-static char* readline(FILE *input)
-{
-	int len;
-	
-	if(fgets(line,max_line_len,input) == NULL)
-		return NULL;
-
-	while(strrchr(line,'\n') == NULL)
-	{
-		max_line_len *= 2;
-		line = (char *) realloc(line,max_line_len);
-		len = (int) strlen(line);
-		if(fgets(line+len,max_line_len-len,input) == NULL)
-			break;
-	}
-	return line;
-}
-
-void parse_command_line(int argc, char **argv, char *input_file_name, char *model_file_name);
-void read_problem(const char *filename);
-void do_cross_validation();
-
+// liblinear arguments
+struct parameter param;		// set by parse_command_line
+struct problem prob;		// set by read_problem
+struct model *model_;
 struct feature_node *x_space;
-struct parameter param;
-struct problem prob;
-struct model* model_;
-int flag_cross_validation;
+int cross_validation_flag;
+int col_format_flag;
 int nr_fold;
 double bias;
 
-int main(int argc, char **argv)
-{
-	char input_file_name[1024];
-	char model_file_name[1024];
-	const char *error_msg;
-
-	parse_command_line(argc, argv, input_file_name, model_file_name);
-	read_problem(input_file_name);
-	error_msg = check_parameter(&prob,&param);
-
-	if(error_msg)
-	{
-		fprintf(stderr,"Error: %s\n",error_msg);
-		exit(1);
-	}
-
-	if(flag_cross_validation)
-	{
-		do_cross_validation();
-	}
-	else
-	{
-		model_=train(&prob, &param);
-		if(save_model(model_file_name, model_))
-		{
-			fprintf(stderr,"can't save model to file %s\n",model_file_name);
-			exit(1);
-		}
-		free_and_destroy_model(&model_);
-	}
-	destroy_param(&param);
-	free(prob.y);
-	free(prob.x);
-	free(x_space);
-	free(line);
-
-	return 0;
-}
-
-void do_cross_validation()
+double do_cross_validation()
 {
 	int i;
 	int total_correct = 0;
-	int *target = Malloc(int, prob.l);
+	int *target = Malloc(int,prob.l);
+	double retval = 0.0;
 
 	cross_validation(&prob,&param,nr_fold,target);
 
 	for(i=0;i<prob.l;i++)
 		if(target[i] == prob.y[i])
 			++total_correct;
-	printf("Cross Validation Accuracy = %g%%\n",100.0*total_correct/prob.l);
+	mexPrintf("Cross Validation Accuracy = %g%%\n",100.0*total_correct/prob.l);
+	retval = 100.0*total_correct/prob.l;
 
 	free(target);
+	return retval;
 }
 
-void parse_command_line(int argc, char **argv, char *input_file_name, char *model_file_name)
+// nrhs should be 3
+int parse_command_line(int nrhs, const mxArray *prhs[], char *model_file_name)
 {
-	int i;
-	void (*print_func)(const char*) = NULL;	// default printing to stdout
+	int i, argc = 1;
+	char cmd[CMD_LEN];
+	char *argv[CMD_LEN/2];
+	void (*print_func)(const char *) = print_string_matlab;	// default printing to matlab display
 
 	// default values
 	param.solver_type = L2R_L2LOSS_SVC_DUAL;
@@ -174,33 +124,60 @@ void parse_command_line(int argc, char **argv, char *input_file_name, char *mode
 	param.nr_weight = 0;
 	param.weight_label = NULL;
 	param.weight = NULL;
-	flag_cross_validation = 0;
+	cross_validation_flag = 0;
+	col_format_flag = 0;
 	bias = -1;
+
+
+	if(nrhs <= 1)
+		return 1;
+
+	if(nrhs == 4)
+	{
+		mxGetString(prhs[3], cmd, mxGetN(prhs[3])+1);
+		if(strcmp(cmd, "col") == 0)
+			col_format_flag = 1;
+	}
+
+	// put options in argv[]
+	if(nrhs > 2)
+	{
+		mxGetString(prhs[2], cmd,  mxGetN(prhs[2]) + 1);
+		if((argv[argc] = strtok(cmd, " ")) != NULL)
+			while((argv[++argc] = strtok(NULL, " ")) != NULL)
+				;
+	}
 
 	// parse options
 	for(i=1;i<argc;i++)
 	{
 		if(argv[i][0] != '-') break;
-		if(++i>=argc)
-			exit_with_help();
+		++i;
+		if(i>=argc && argv[i-1][1] != 'q') // since option -q has no parameter
+			return 1;
 		switch(argv[i-1][1])
 		{
 			case 's':
 				param.solver_type = atoi(argv[i]);
 				break;
-
 			case 'c':
 				param.C = atof(argv[i]);
 				break;
-
 			case 'e':
 				param.eps = atof(argv[i]);
 				break;
-
 			case 'B':
 				bias = atof(argv[i]);
 				break;
-
+			case 'v':
+				cross_validation_flag = 1;
+				nr_fold = atoi(argv[i]);
+				if(nr_fold < 2)
+				{
+					mexPrintf("n-fold cross validation: n must >= 2\n");
+					return 1;
+				}
+				break;
 			case 'w':
 				++param.nr_weight;
 				param.weight_label = (int *) realloc(param.weight_label,sizeof(int)*param.nr_weight);
@@ -208,48 +185,17 @@ void parse_command_line(int argc, char **argv, char *input_file_name, char *mode
 				param.weight_label[param.nr_weight-1] = atoi(&argv[i-1][2]);
 				param.weight[param.nr_weight-1] = atof(argv[i]);
 				break;
-
-			case 'v':
-				flag_cross_validation = 1;
-				nr_fold = atoi(argv[i]);
-				if(nr_fold < 2)
-				{
-					fprintf(stderr,"n-fold cross validation: n must >= 2\n");
-					exit_with_help();
-				}
-				break;
-
 			case 'q':
 				print_func = &print_null;
 				i--;
 				break;
-
 			default:
-				fprintf(stderr,"unknown option: -%c\n", argv[i-1][1]);
-				exit_with_help();
-				break;
+				mexPrintf("unknown option\n");
+				return 1;
 		}
 	}
 
 	set_print_string_function(print_func);
-
-	// determine filenames
-	if(i>=argc)
-		exit_with_help();
-
-	strcpy(input_file_name, argv[i]);
-
-	if(i<argc-1)
-		strcpy(model_file_name,argv[i+1]);
-	else
-	{
-		char *p = strrchr(argv[i],'/');
-		if(p==NULL)
-			p = argv[i];
-		else
-			++p;
-		sprintf(model_file_name,"%s.model",p);
-	}
 
 	if(param.eps == INF)
 	{
@@ -260,106 +206,178 @@ void parse_command_line(int argc, char **argv, char *input_file_name, char *mode
 		else if(param.solver_type == L1R_L2LOSS_SVC || param.solver_type == L1R_LR)
 			param.eps = 0.01;
 	}
+	return 0;
 }
 
-// read in a problem (in libsvm format)
-void read_problem(const char *filename)
+static void fake_answer(mxArray *plhs[])
 {
-	int max_index, inst_max_index, i;
-	long int elements, j;
-	FILE *fp = fopen(filename,"r");
-	char *endptr;
-	char *idx, *val, *label;
+	plhs[0] = mxCreateDoubleMatrix(0, 0, mxREAL);
+}
 
-	if(fp == NULL)
+int read_problem_sparse(const mxArray *label_vec, const mxArray *instance_mat)
+{
+	int i, j, k, low, high;
+	mwIndex *ir, *jc;
+	int elements, max_index, num_samples, label_vector_row_num;
+	double *samples, *labels;
+	mxArray *instance_mat_col; // instance sparse matrix in column format
+
+	prob.x = NULL;
+	prob.y = NULL;
+	x_space = NULL;
+
+	if(col_format_flag)
+		instance_mat_col = (mxArray *)instance_mat;
+	else
 	{
-		fprintf(stderr,"can't open input file %s\n",filename);
-		exit(1);
-	}
-
-	prob.l = 0;
-	elements = 0;
-	max_line_len = 1024;
-	line = Malloc(char,max_line_len);
-	while(readline(fp)!=NULL)
-	{
-		char *p = strtok(line," \t"); // label
-
-		// features
-		while(1)
+		// transpose instance matrix
+		mxArray *prhs[1], *plhs[1];
+		prhs[0] = mxDuplicateArray(instance_mat);
+		if(mexCallMATLAB(1, plhs, 1, prhs, "transpose"))
 		{
-			p = strtok(NULL," \t");
-			if(p == NULL || *p == '\n') // check '\n' as ' ' may be after the last feature
-				break;
-			elements++;
+			mexPrintf("Error: cannot transpose training instance matrix\n");
+			return -1;
 		}
-		elements++; // for bias term
-		prob.l++;
+		instance_mat_col = plhs[0];
+		mxDestroyArray(prhs[0]);
 	}
-	rewind(fp);
+
+	// the number of instance
+	prob.l = (int) mxGetN(instance_mat_col);
+	label_vector_row_num = (int) mxGetM(label_vec);
+
+	if(label_vector_row_num!=prob.l)
+	{
+		mexPrintf("Length of label vector does not match # of instances.\n");
+		return -1;
+	}
+	
+	// each column is one instance
+	labels = mxGetPr(label_vec);
+	samples = mxGetPr(instance_mat_col);
+	ir = mxGetIr(instance_mat_col);
+	jc = mxGetJc(instance_mat_col);
+
+	num_samples = (int) mxGetNzmax(instance_mat_col);
+
+	elements = num_samples + prob.l*2;
+	max_index = (int) mxGetM(instance_mat_col);
+
+	prob.y = Malloc(int, prob.l);
+	prob.x = Malloc(struct feature_node*, prob.l);
+	x_space = Malloc(struct feature_node, elements);
 
 	prob.bias=bias;
 
-	prob.y = Malloc(int,prob.l);
-	prob.x = Malloc(struct feature_node *,prob.l);
-	x_space = Malloc(struct feature_node,elements+prob.l);
-
-	max_index = 0;
-	j=0;
+	j = 0;
 	for(i=0;i<prob.l;i++)
 	{
-		inst_max_index = 0; // strtol gives 0 if wrong format
-		readline(fp);
 		prob.x[i] = &x_space[j];
-		label = strtok(line," \t\n");
-		if(label == NULL) // empty line
-			exit_input_error(i+1);
-
-		prob.y[i] = (int) strtol(label,&endptr,10);
-		if(endptr == label || *endptr != '\0')
-			exit_input_error(i+1);
-
-		while(1)
+		prob.y[i] = (int) labels[i];
+		low = (int) jc[i], high = (int) jc[i+1];
+		for(k=low;k<high;k++)
 		{
-			idx = strtok(NULL,":");
-			val = strtok(NULL," \t");
-
-			if(val == NULL)
-				break;
-
-			errno = 0;
-			x_space[j].index = (int) strtol(idx,&endptr,10);
-			if(endptr == idx || errno != 0 || *endptr != '\0' || x_space[j].index <= inst_max_index)
-				exit_input_error(i+1);
-			else
-				inst_max_index = x_space[j].index;
-
-			errno = 0;
-			x_space[j].value = strtod(val,&endptr);
-			if(endptr == val || errno != 0 || (*endptr != '\0' && !isspace(*endptr)))
-				exit_input_error(i+1);
-
-			++j;
+			x_space[j].index = (int) ir[k]+1;
+			x_space[j].value = samples[k];
+			j++;
+	 	}
+		if(prob.bias>=0)
+		{
+			x_space[j].index = max_index+1;
+			x_space[j].value = prob.bias;
+			j++;
 		}
-
-		if(inst_max_index > max_index)
-			max_index = inst_max_index;
-
-		if(prob.bias >= 0)
-			x_space[j++].value = prob.bias;
-
 		x_space[j++].index = -1;
 	}
 
-	if(prob.bias >= 0)
+	if(prob.bias>=0)
+		prob.n = max_index+1;
+	else
+		prob.n = max_index;
+
+	return 0;
+}
+
+// Interface function of matlab
+// now assume prhs[0]: label prhs[1]: features
+void mexFunction( int nlhs, mxArray *plhs[],
+		int nrhs, const mxArray *prhs[] )
+{
+	const char *error_msg;
+	// fix random seed to have same results for each run
+	// (for cross validation)
+	srand(1);
+
+	// Transform the input Matrix to libsvm format
+	if(nrhs > 1 && nrhs < 5)
 	{
-		prob.n=max_index+1;
-		for(i=1;i<prob.l;i++)
-			(prob.x[i]-2)->index = prob.n; 
-		x_space[j-2].index = prob.n;
+		int err=0;
+
+		if(!mxIsDouble(prhs[0]) || !mxIsDouble(prhs[1])) {
+			mexPrintf("Error: label vector and instance matrix must be double\n");
+			fake_answer(plhs);
+			return;
+		}
+
+		if(parse_command_line(nrhs, prhs, NULL))
+		{
+			exit_with_help();
+			destroy_param(&param);
+			fake_answer(plhs);
+			return;
+		}
+
+		if(mxIsSparse(prhs[1]))
+			err = read_problem_sparse(prhs[0], prhs[1]);
+		else
+		{
+			mexPrintf("Training_instance_matrix must be sparse\n");
+			destroy_param(&param);
+			fake_answer(plhs);
+			return;
+		}
+
+		// train's original code
+		error_msg = check_parameter(&prob, &param);
+
+		if(err || error_msg)
+		{
+			if (error_msg != NULL)
+				mexPrintf("Error: %s\n", error_msg);
+			destroy_param(&param);
+			free(prob.y);
+			free(prob.x);
+			free(x_space);
+			fake_answer(plhs);
+			return;
+		}
+
+		if(cross_validation_flag)
+		{
+			double *ptr;
+			plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
+			ptr = mxGetPr(plhs[0]);
+			ptr[0] = do_cross_validation();
+		}
+		else
+		{
+			const char *error_msg;
+
+			model_ = train(&prob, &param);
+			error_msg = model_to_matlab_structure(plhs, model_);
+			if(error_msg)
+				mexPrintf("Error: can't convert libsvm model to matrix structure: %s\n", error_msg);
+			free_and_destroy_model(&model_);
+		}
+		destroy_param(&param);
+		free(prob.y);
+		free(prob.x);
+		free(x_space);
 	}
 	else
-		prob.n=max_index;
-
-	fclose(fp);
+	{
+		exit_with_help();
+		fake_answer(plhs);
+		return;
+	}
 }

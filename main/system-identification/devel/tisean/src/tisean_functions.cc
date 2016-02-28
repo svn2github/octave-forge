8181d7ac90ca6970a107fcf2bf9d0185deb95c25
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2016-02-28 23:36:10 +0000

    maint: remove system-identification which has been moved to its own mercurial repository.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12911 fc592f45-0881-4044-91ff-c96495a54512

--- main/system-identification/devel/tisean/src/tisean_functions.cc
+++
@@ -1,1 +0,0 @@
-#include "delay.c"  // produces delay vectors either from a scalar or from a multivariate time series.
--- main/system-identification/post_install.m
+++
@@ -1,7 +0,0 @@
-function post_install (desc)
-%% Prepares for installation a package that is organized in subfolders
-%% Since src is compiled only in the package main dir
-%% I need to remove the PKG_ADD and PKG_DEL from the architecture dependent folder
-
-
-end

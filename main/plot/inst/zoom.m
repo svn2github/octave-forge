b3b1f35decd4324e4a9b48f7bcfa12f654a4925a
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2016-02-28 23:18:27 +0000

    maint: remove plot which has been moved to its own mercurial repository.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12909 fc592f45-0881-4044-91ff-c96495a54512

--- main/plot/inst/zoom.m
+++
@@ -1,9 +0,0 @@
-## Author: Paul Kienzle <pkienzle@users.sf.net>
-## This program is granted to the public domain.
-
-## -*- texinfo -*-
-## @deftypefn {Function File} {} zoom
-## Compatibility function; does nothing.
-## @end deftypefn
-
-function zoom endfunction

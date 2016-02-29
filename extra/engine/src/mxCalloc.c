985a9dbd117488d3557b0c61ac7667022b1081c8
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2016-02-29 14:10:17 +0000

    maint: remove engine which has been moved to its own mercurial repository.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12920 fc592f45-0881-4044-91ff-c96495a54512

--- extra/engine/src/mxCalloc.c
+++
@@ -1,7 +0,0 @@
-#include "engine.h"
-#include <stdlib.h>
-
-void *mxCalloc( unsigned int n, unsigned int size )
-{
-  return calloc( (size_t) n, (size_t) size );
-}
--- extra/engine/src/mxFree.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include "engine.h"
-
-void mxFree( void *ptr )
-{
-  free( ptr );
-}

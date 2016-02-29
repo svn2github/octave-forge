a10949ba9063ab0528d019b2f8977a283c6701cb
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2016-02-29 14:33:22 +0000

    maint: remove soctcl which has been moved to its own mercurial repository.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12924 fc592f45-0881-4044-91ff-c96495a54512

--- extra/soctcl/inst/tclsend.m
+++
@@ -1,9 +0,0 @@
-## Author: Paul Kienzle
-## This program is public domain.
-function tclsend(name, m)
-  if ischar(m)
-    send(name,m);
-  else
-    send(name,sprintf("%.15g ", m));
-  endif
-endfunction

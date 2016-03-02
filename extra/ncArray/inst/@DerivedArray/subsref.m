ace058a438abdfec5b61a0d9ae804f540ad7e15b
carandraug <carandraug@fc592f45-0881-4044-91ff-c96495a54512>
2016-03-02 20:16:38 +0000

    maint: remove ncArray which has been moved to its own mercurial repository.
    git-svn-id: svn://svn.code.sf.net/p/octave/code/trunk/octave-forge@12942 fc592f45-0881-4044-91ff-c96495a54512

--- extra/ncArray/inst/@DerivedArray/subsref.m
+++
@@ -1,9 +0,0 @@
-function varargout = subsref(self,idx)
-
-  p = {};
-  for i = 1:length(self.params)
-    %i,self.params{i},idx
-    p{i} = subsref(self.params{i},idx);
-  end
-
-  varargout{1} = self.fun(p{:});
--- extra/ncArray/inst/derivedArray.m
+++
@@ -1,3 +0,0 @@
-function da = derived(fun,param);
-
-da = ncArray(DerivedArray(fun,param),dims(param{1}),coord(param{1}));
